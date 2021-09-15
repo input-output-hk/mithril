fef83b25c4d15f09944a339fc4859406526c8942
Brent Carmer <bcarmer@galois.com>
2021-09-15 08:46:57 -0700

    Update integration test with new understanding of stake and index

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,11 +1,12 @@
 use mithril::merkle_tree::new_constants;
 use mithril::key_reg::KeyReg;
 use mithril::party::Party;
-use mithril::{Stake, Index};
+use mithril::Index;
+use rand;
 
 #[test]
-fn sign() {
-    let nparties = 1000;
+fn test_full_protocol() {
+    let nparties = 100;
     let msg = rand::random::<[u8;16]>();
     let constants = new_constants();
 
@@ -18,7 +19,7 @@ fn sign() {
     let mut ps = Vec::with_capacity(nparties);
 
     for pid in 0..nparties {
-        let stake = Stake(0.1); // XXX: How to set stake?
+        let stake = 1 + (rand::random::<u64>() % 9999);
         let mut p = Party::setup(pid, stake, &constants);
         p.register(&mut key_reg);
         ps.push(p);
@@ -32,23 +33,24 @@ fn sign() {
     // operation phase //
     /////////////////////
 
-    let index = Index(0); // XXX: not clear right now what index is for
-
     let mut sigs = Vec::new();
+    let mut ixs  = Vec::new();
     for p in &ps {
-        if let Some(sig) = p.create_sig(&msg, index) {
+        let ix = Index::random();
+        if let Some(sig) = p.create_sig(&msg, ix) {
             sigs.push(sig);
+            ixs.push(ix);
         }
     }
 
     // Check all parties can verify every sig
-    for s in &sigs {
+    for (s,ix) in sigs.iter().zip(&ixs) {
         for p in &ps {
-            assert!(p.verify(s.clone(), index, &msg), "Verification failed");
+            assert!(p.verify(s.clone(), *ix, &msg), "Verification failed");
         }
     }
 
     // Aggregate sig
-    let msig = ps[0].aggregate(&sigs, &[index], &msg).expect("Aggregation failed");
+    let msig = ps[0].aggregate(&sigs, &ixs, &msg).expect("Aggregation failed");
     assert!(ps[0].verify_aggregate(&msig, &msg), "Aggregate verification failed");
 }
