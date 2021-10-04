2308e43efccd8a9e7fb7ba7a97f7f864bf0419c3
Arnaud Bailly <arnaud.bailly@iohk.io>
2021-10-04 20:31:28 +0000

    Can build a merkle-patricia tree out of arbitrary UTXO

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,5 @@
-((haskell-mode . ((lsp-haskell-formatting-provider "ormolu")
-                  (lsp-haskell-fourmolu-on nil))))
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((haskell-mode . ((lsp-haskell-formatting-provider . "ormolu")
+                  (lsp-haskell-fourmolu-on . (nil)))))
--- test/MithrilSpec.hs
+++ test/MithrilSpec.hs
@@ -1,29 +1,88 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE NamedFieldPuns #-}
+
 module MithrilSpec where
 
-import Test.Hspec
-import Data.ByteString(ByteString)
+import Codec.Serialise (Serialise (..), serialise)
+import Data.ByteString (ByteString, pack)
+import Data.ByteString.Lazy (toStrict)
 import qualified Data.Map as Map
-import Test.Hspec.QuickCheck (prop)
 import Data.Maybe (isJust, isNothing)
-import Data.Tree.MerklePatricia (mkProof, MerklePatriciaTree(..), Blake2b_224)
-import Test.QuickCheck (Arbitrary(..))
+import Data.Tree.MerklePatricia (Blake2b_224, MerklePatriciaTree (..), fromList, mkProof)
+import GHC.Generics (Generic)
+import GHC.Natural (Natural)
+import Test.Hspec
+import Generic.Random(genericArbitrary, uniform)
+import Test.Hspec.QuickCheck (prop)
+import Test.QuickCheck (Arbitrary (..), NonEmptyList (getNonEmpty), vectorOf, choose, Gen)
+import Generic.Random (genericArbitrary)
+
+genHash :: Gen ByteString
+genHash = pack <$> vectorOf 28 arbitrary
 
 newtype Address = Address ByteString
-  deriving (Eq, Show)
+  deriving newtype (Eq, Show, Serialise)
+
+instance Arbitrary Address where
+  arbitrary = Address <$> genHash
 
 newtype DatumHash = DatumHash ByteString
-  deriving (Eq, Show)
+  deriving newtype (Eq, Show, Serialise)
 
-data Value = Value { coins :: Integer,  assets :: Map.Map ByteString [(ByteString, Integer)]}
-  deriving (Eq, Show)
+instance Arbitrary DatumHash where
+  arbitrary = DatumHash <$> genHash
 
+newtype MonetaryPolicy = MonetaryPolicy ByteString
+  deriving newtype (Eq, Show, Ord, Serialise)
+
+instance Arbitrary MonetaryPolicy where
+  arbitrary = MonetaryPolicy <$> genHash
+
+newtype AssetName = AssetName ByteString
+  deriving newtype (Eq, Show, Serialise)
+
+instance Arbitrary AssetName where
+  arbitrary  = do
+    len <- choose (0,128)
+    AssetName . pack <$> vectorOf len arbitrary
+    
+data Value = Value
+  { coins :: Integer,
+    assets :: Map.Map MonetaryPolicy [(AssetName, Integer)]
+  }
+  deriving (Eq, Show, Generic, Serialise)
+
+instance Arbitrary Value where
+  arbitrary = genericArbitrary uniform
+  
+newtype TxId = TxId {txId :: ByteString}
+  deriving newtype (Eq, Show, Serialise)
+
+instance Arbitrary TxId where
+  arbitrary = TxId <$> genHash
+  
+newtype TxIx = TxIx {txIx :: Natural}
+  deriving newtype (Eq, Show, Serialise)
+
+instance Arbitrary TxIx where
+  arbitrary = TxIx . fromInteger <$> choose (1,1024)
+  
+newtype TxRef = TxRef (TxId, TxIx)
+  deriving newtype (Eq, Show, Serialise)
+
+instance Arbitrary TxRef where
+  arbitrary = TxRef <$> arbitrary
+  
 data Utxo = Utxo
-  { address :: Address,
+  { txRef :: TxRef,
+    address :: Address,
     datum :: DatumHash,
     value :: Value
   }
-  deriving (Eq, Show)
+  deriving (Eq, Show, Generic, Serialise)
 
 data Utxos = Utxos
   { utxoSet :: [Utxo],
@@ -32,15 +91,27 @@ data Utxos = Utxos
   }
   deriving (Eq, Show)
 
+instance Arbitrary Utxo where
+  arbitrary = genericArbitrary uniform
+
 instance Arbitrary Utxos where
-  arbitrary = undefined
-  
+  arbitrary = do
+    utxoSet <- getNonEmpty <$> arbitrary
+    otherUtxo <- arbitrary
+    pure $
+      Utxos
+        { utxoSet,
+          inUtxo = toStrict $ serialise $ txRef $ head utxoSet,
+          notInUtxo = toStrict $ serialise $ txRef otherUtxo
+        }
+
+makeTree :: [Utxo] -> MerklePatriciaTree Blake2b_224 Utxo
+makeTree utxos =
+  fromList $ zip (toStrict . serialise . txRef <$> utxos) utxos
+
 spec :: Spec
 spec = parallel $ do
   prop "builds merkle-tree for UTXO and provide proofs" $ \Utxos {utxoSet, inUtxo, notInUtxo} ->
     let mpt = makeTree utxoSet
      in isJust (mkProof inUtxo mpt)
           && isNothing (mkProof notInUtxo mpt)
-
-makeTree :: [Utxo] -> MerklePatriciaTree Blake2b_224 Utxo
-makeTree = error "not implemented"
