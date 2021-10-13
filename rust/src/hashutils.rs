0abc58edb3aa51930d1a6366bb8f292add09888c
Alexander Bakst <abakst@galois.com>
2021-10-13 16:30:50 -0700

    Use `write_all` instead of `write`.

--- rust/src/hashutils.rs
+++ rust/src/hashutils.rs
@@ -5,7 +5,7 @@ use std::vec::Vec;
 
 pub fn hash_message(msg: &[u8], out_len: usize) -> Vec<u8> {
     let mut hasher = Shake256::default();
-    hasher.write(msg).unwrap();
+    hasher.write_all(msg).unwrap();
     let out = hasher.finalize_boxed(out_len);
     out.to_vec()
 }
