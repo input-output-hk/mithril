c9316638703f605a48359df86b4697462059e82c
Alexander Bakst <abakst@galois.com>
2021-10-19 09:31:15 -0700

    Update `hash_to_curve` implementation according to (Boneh et al, 2001).

--- rust/src/mithril_curves.rs
+++ rust/src/mithril_curves.rs
@@ -1,19 +1,38 @@
 //! Abstractions for working with ark curves
-use crate::hashutils::hash_message;
-
 use ark_ec::AffineCurve;
-use ark_ff::{Field, FpParameters, PrimeField};
+use ark_ff::to_bytes;
+use blake2::{Blake2b, Digest};
+use num_bigint::BigUint;
 
-pub fn hash_to_curve<C: AffineCurve>(bytes: &[u8]) -> C {
-    let needed =
-        <<<C::BaseField as Field>::BasePrimeField as PrimeField>::Params as FpParameters>::MODULUS_BITS / 8;
-    let x: &[u8] = &hash_message(bytes, needed as usize);
-    let mut q = num_bigint::BigUint::from_bytes_le(x);
+/// Implements
+/// https://datatracker.ietf.org/doc/draft-irtf-cfrg-hash-to-curve/03/
+///
+/// Note that while this implementation has side channel vulnerabilities,
+/// the hashed value is not secret in our case, so this is not an issue.
+pub(crate) fn hash_to_curve<C: AffineCurve>(bytes: &[u8]) -> C {
+    // ctr = 0
+    let mut ctr = 0_u32;
     loop {
-        if let Some(elt) = C::from_random_bytes(&q.to_bytes_le()) {
-            return elt;
-        } else {
-            q += 1u8;
+        //  CTR = I20SP(ctr, 4)
+        let ctr_string = ctr.to_be_bytes();
+        //  ctr = ctr + 1
+        ctr += 1;
+        //  attempted_hash = Hash(m || CTR)
+        let attempted_hash = Blake2b::digest(&[bytes, &ctr_string].concat());
+
+        // h = RS2ECP(attempted_hash)
+        if let Some(mut h) = C::from_random_bytes(&attempted_hash[..]) {
+            // if H is not "INVALID" ...
+            let cofactor = BigUint::from_bytes_be(&to_bytes!(C::COFACTOR).unwrap());
+            if cofactor > BigUint::from(1u8) {
+                // ... and cofactor > 1
+                // set h = h * cofactor
+                h = h.mul_by_cofactor();
+            }
+            if !h.is_zero() {
+                // ... if not point at infinity, we're done
+                return h;
+            }
         }
     }
 }
