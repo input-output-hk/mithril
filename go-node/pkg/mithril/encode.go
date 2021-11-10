db004fcbd817137930bb07865424b1c8553f38b8
Alex Gurin <agurin@i.ua>
2021-11-10 15:59:50 +0200

    Rust LIB integration with tests

---
+++ go-node/pkg/mithril/encode.go
@@ -0,0 +1,52 @@
+package mithril
+
+import "C"
+import "unsafe"
+
+/*
+#include "mithril.h"
+*/
+import "C"
+
+func encodeSecretKey(key C.MspSkPtr) []byte {
+	return encodeToBytes(func(size *C.ulong, buf **C.uchar) {
+		C.msp_serialize_secret_key(key, size, buf)
+	})
+}
+
+func encodePublicKey(key C.MspPkPtr) []byte {
+	return encodeToBytes(func(size *C.ulong, buf **C.uchar) {
+		C.msp_serialize_verification_key(key, size, buf)
+	})
+}
+
+func encodeInitializer(si Initializer) []byte {
+	return encodeToBytes(func(size *C.ulong, buf **C.uchar) {
+		C.stm_serialize_initializer(si.ptr, size, buf)
+	})
+}
+
+func encodeSignature(sig C.SigPtr) []byte {
+	return encodeToBytes(func(size *C.ulong, buf **C.uchar) {
+		C.stm_serialize_sig(sig, size, buf)
+	})
+}
+
+func encodeMultiSign(ms C.MultiSigPtr) []byte {
+	return encodeToBytes(func(size *C.ulong, buf **C.uchar) {
+		C.stm_serialize_multi_sig(ms, size, buf)
+	})
+}
+
+func encodeToBytes(serializeCB func(size *C.ulong, buf **C.uchar)) []byte {
+	var cSize C.ulong
+	var cBytes *C.uchar
+
+	serializeCB(&cSize, &cBytes)
+	defer C.free(unsafe.Pointer(cBytes))
+
+	bytes := make([]byte, cSize)
+	copy(bytes[:], []byte(C.GoBytes(unsafe.Pointer(cBytes), C.int(cSize))))
+
+	return bytes
+}
--- go-node/pkg/mithril/signer.go
+++ go-node/pkg/mithril/signer.go
@@ -4,45 +4,92 @@ package mithril
 #include "mithril.h"
 */
 import "C"
+import "encoding/hex"
 
-//type (
-//	MultiSign      = C.MultiSigPtr
-//	MultiSignConst = C.MultiSigConstPtr
-//)
-//
-//// what is this?
-//type StmSigner struct {
-//	ptr C.StmSignerPtr
-//}
-//
-//func (s StmSigner) EligibilityCheck(index int64, msg string) bool {
-//	rv := C.stm_signer_eligibility_check(s.ptr, C.CString(msg), C.ulonglong(index))
-//	return bool(rv)
-//}
-//
-//func (s StmSigner) Sign(index int64, msg string) (*Signature, error) {
-//	var sig Signature
-//
-//	if C.stm_signer_sign(s.ptr, C.CString(msg), C.ulonglong(index), &sig.ptr); sig.ptr == nil {
-//		return nil, ErrSignFailed
-//	}
-//
-//	return &sig, nil
-//}
-//
-//func (s StmSigner) GetClerk() StmClerk {
-//	return StmClerk{ptr: C.stm_clerk_from_signer(s.ptr)}
-//}
-//
-//type Signature struct {
-//	ptr     C.SigPtr
-//	isFreed bool
-//}
-//
-//func (s *Signature) Free() {
-//	if s.isFreed {
-//		return
-//	}
-//	s.isFreed = true
-//	C.free_sig(s.ptr)
-//}
+func NewSigner(initializer Initializer, participants []Participant) *Signer {
+
+	partyIds := make([]C.PartyId, len(participants))
+	partyStakes := make([]C.Stake, len(participants))
+	partyKeys := make([]C.MspPkPtr, len(participants))
+
+	for i, p := range participants {
+		partyIds[i] = C.PartyId(p.PartyId)
+		partyStakes[i] = C.Stake(p.Stake)
+		partyKeys[i] = p.pk
+	}
+
+	return &Signer{
+		ptr: C.stm_initializer_new_signer(initializer.ptr,
+			C.ulong(len(participants)),
+			&partyIds[0],
+			&partyStakes[0],
+			&partyKeys[0]),
+	}
+}
+
+type Signer struct {
+	ptr C.StmSignerPtr
+}
+
+func (s Signer) EligibilityCheck(index int64, msg string) bool {
+	rv := C.stm_signer_eligibility_check(s.ptr, C.CString(msg), C.ulonglong(index))
+	return bool(rv)
+}
+
+func (s Signer) Sign(index int64, msg string) (*Signature, error) {
+	var sig Signature
+
+	if C.stm_signer_sign(s.ptr, C.CString(msg), C.ulonglong(index), &sig.ptr); sig.ptr == nil {
+		return nil, ErrSignFailed
+	}
+
+	sig.index = index
+	return &sig, nil
+}
+
+func (s Signer) Clerk() Clerk {
+	return Clerk{ptr: C.stm_clerk_from_signer(s.ptr)}
+}
+
+type Signature struct {
+	ptr     C.SigPtr
+	index   int64
+	isFreed bool
+}
+
+func (s Signature) Encode() []byte {
+	return encodeSignature(s.ptr)
+}
+
+func (s Signature) HexString() string {
+	return hex.EncodeToString(s.Encode())
+}
+
+func (s Signature) Index() int64 {
+	return s.index
+}
+
+func (s *Signature) Free() {
+	if s.isFreed {
+		return
+	}
+	s.isFreed = true
+	C.free_sig(s.ptr)
+}
+
+type MultiSign struct {
+	ptr     C.MultiSigPtr
+	isFreed bool
+}
+
+func (ms MultiSign) Encode() []byte {
+	return encodeMultiSign(ms.ptr)
+}
+
+func (ms *MultiSign) Free() {
+	if ms.isFreed {
+		return
+	}
+	ms.isFreed = true
+	C.free_multi_sig(ms.ptr)
+}
