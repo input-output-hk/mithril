a06efbbc1282f2e7848f9977952ad1a51c9c1f3c
demonsh <dmytro.sukhiy@gmail.com>
2021-11-13 18:11:51 +0200

    Fetch unspent UTXO from block for address

--- go-node/pkg/cardano/db/utxo.go
+++ go-node/pkg/cardano/db/utxo.go
@@ -11,38 +11,66 @@ type Querier interface {
 	Query(ctx context.Context, sql string, args ...interface{}) (pgx.Rows, error)
 }
 
+const getTxOutputsSQL = `-- select all spent out till block
+with
+     -- select all outputs till block_no
+     tx_o as (select tx_out.id, tx_id, index,address,address_raw, payment_cred, stake_address_id, value
+                from tx_out
+                         inner join tx t on t.id = tx_out.tx_id
+                         inner join block on t.block_id=block.id
+                where block_no<$1),
+
+     -- In this select i'm not sure which id sould be used
+     -- tx_in_id or tx_out_id?????
+
+     -- select all inputs till block_no
+     tx_i as (select *
+               from tx_in
+                        inner join tx t on t.id = tx_in.tx_in_id
+                        inner join block on t.block_id=block.id
+               where block_no<$1
+     )
+select tx_outer.address,tx_outer.index, tx_outer.value, tx_outer.tx_id
+from tx_o as tx_outer
+where
+    not exists
+        ( select *
+          from
+              tx_o
+                  inner join
+              tx_i
+              on tx_o.tx_id = tx_i.tx_out_id and
+                 tx_o.index = tx_i.tx_out_index
+          where tx_outer.id = tx_o.id
+        )
+ORDER BY tx_outer.address;`
+
 // UTXORepository type for utxo repository
 type UTXORepository struct {
 }
 
 // GetTxOutputs get sets from Conn
-func (r *UTXORepository) GetTxOutputs(tx Querier, blockNumber int) (txOuts map[types.Address][]*types.TxOut, err error) {
+func (r *UTXORepository) GetTxOutputs(tx Querier, blockNumber int64) (txOuts map[types.Address][]*types.UTXO, err error) {
 
-	rows, err := tx.Query(context.Background(), `select tx_outer.address,tx_outer.index,tx_outer.value,tx_outer.tx_id   from tx_out as tx_outer where
-    												 not exists
-							( select tx_out.id from tx_out inner join tx_in
-																	  on tx_out.tx_id = tx_in.tx_out_id and tx_out.index = tx_in.tx_out_index
-							  where tx_outer.id = tx_out.id
-							)
-ORDER BY tx_outer.address LIMIT 100`)
+	rows, err := tx.Query(context.Background(), getTxOutputsSQL, blockNumber)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer rows.Close()
 
-	txOuts = make(map[types.Address][]*types.TxOut)
+	txOuts = make(map[types.Address][]*types.UTXO)
 
 	for rows.Next() {
 		/*
 		   1. group tx output
 		*/
-		txOut := &types.TxOut{}
+		txOut := &types.UTXO{}
 		err := rows.Scan(&txOut.Address, &txOut.Index, &txOut.Value, &txOut.TxID)
 		if err != nil {
 			log.Fatal(err)
 		}
 		if txOuts[txOut.Address] == nil {
-			txOuts[txOut.Address] = []*types.TxOut{}
+			txOuts[txOut.Address] = []*types.UTXO{}
 		}
 		txOuts[txOut.Address] = append(txOuts[txOut.Address], txOut)
 
