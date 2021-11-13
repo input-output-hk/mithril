7d7fed6e800493c04659cdb8caf2b4390ce45d39
demonsh <dmytro.sukhiy@gmail.com>
2021-11-13 18:12:42 +0200

    Process UTXO MT interface

---
+++ go-node/pkg/cardano/cardano.go
@@ -0,0 +1,38 @@
+package cardano
+
+import (
+	"context"
+	"github.com/input-output-hk/mithril/go-node/pkg/cardano/db"
+	"github.com/input-output-hk/mithril/go-node/pkg/cardano/mt"
+	"github.com/input-output-hk/mithril/go-node/pkg/cardano/types"
+	"github.com/jackc/pgx/v4"
+)
+
+func ProcessUTXO(storage db.Storage, tree mt.MerkleTree, blockNumber int64) (root []byte, err error) {
+	// Get UTXO from DB
+	var txOuts map[types.Address][]*types.UTXO
+
+	err = storage.Transact(context.TODO(), func(tx pgx.Tx) error {
+		txOuts, err = storage.UTXORepository.GetTxOutputs(tx, blockNumber)
+
+		// Calculate hashes
+		//for address, utxos := range txOuts {
+		for _, utxos := range txOuts {
+			for _, utxo := range utxos {
+				var err error
+				hash, err := mt.CalculateHash(utxo)
+				if err != nil {
+					return err
+				}
+
+				tree.Add(hash)
+			}
+		}
+
+		root, err = tree.GetRoot()
+
+		return err
+	})
+
+	return root, err
+}
