239868d96f98faa70ea5f1f615143e746b253ea5
demonsh <dmytro.sukhiy@gmail.com>
2021-11-13 18:10:27 +0200

    Rename types

--- go-node/pkg/cardano/types/utxo.go
+++ go-node/pkg/cardano/types/utxo.go
@@ -1,30 +1,30 @@
 package types
 
-type (
-	Output struct {
-		Index   int64
-		Address string
-		Value   int
-	}
-
-	UTxO struct {
-		Hash    string
-		TxId    string
-		Outputs []Output
-	}
-
-	UTXOSet struct {
-		BlockNumber uint64
-		BlockHash   string
-		UTxOs       []UTxO
-	}
-)
+//type (
+//	Output struct {
+//		Index   int64
+//		Address string
+//		Value   int
+//	}
+//
+//	UTxO struct {
+//		Hash    string
+//		TxId    string
+//		Outputs []Output
+//	}
+//
+//	UTXOSet struct {
+//		BlockNumber uint64
+//		BlockHash   string
+//		UTxOs       []UTxO
+//	}
+//)
 
 // Address is an alias for string that represents address field
 type Address string
 
-// TxOut is basic type that represents tx output
-type TxOut struct {
+// UTXO is basic type that represents unspent tx output
+type UTXO struct {
 	Address Address
 	TxID    int64
 	Index   int64
