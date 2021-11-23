420ade6be6c1d751cd57f15541a8372d35da0a76
iquerejeta <querejeta.azurmendi@iohk.io>
2021-11-23 15:12:20 +0000

    explicit separation of clerk from pub data test

--- rust/c-tests/test.c
+++ rust/c-tests/test.c
@@ -7,6 +7,87 @@ extern "C" {
 #define NEEDED_SIGS 5
 #endif
 
+TEST(stm, clerkFromPublicData) {
+    // The following data is public, and known to all participants
+    const char *msg = "some message";
+
+    StmParameters params;
+    params.k = NEEDED_SIGS;
+    params.m = 101;
+    params.phi_f = 0.2;
+
+    Index indices[NEEDED_SIGS];
+
+    // Test with 2 parties, one with all the stake, one with none.
+    PartyId party_ids[2] = {1, 2};
+    Stake   party_stake[2] = {1, 0};
+    MspPkPtr keys[2];
+    StmSignerPtr signer;
+
+    // Scope of the signers, which is not required knowledge for the clerk. Signers initialise.
+    {
+        StmInitializerPtr initializer[2];
+
+        for (int i = 0; i < 2; i++) {
+            initializer[i] = stm_intializer_setup(params, party_ids[i], party_stake[i]);
+            keys[i] = stm_initializer_verification_key(initializer[i]);
+        }
+        signer = stm_initializer_new_signer(initializer[0], 2, party_ids, party_stake, keys);
+    }
+
+    // Now the public keys are broadcast, and a third party (i.e. a Clerk), can register all participating parties.
+
+    // The Clerk first needs to run the registration with all participating parties. We explicitly list all steps.
+    // First it defines which are the parties that can participate (this can happen before the clerk knows their
+    // public keys).
+    KeyRegPtr clerk_kr = key_registration(2, party_ids, party_stake);
+    // Next it registers their public keys
+    ASSERT_EQ(register_party(clerk_kr, party_ids[0], keys[0]), 0);
+    ASSERT_EQ(register_party(clerk_kr, party_ids[1], keys[1]), 0);
+    // Now the registration phases closes, and no other parties can be included
+    close_registration(clerk_kr);
+    // After closing the registration, the clerk can generate the global key
+    MerkleTreePtr avk;
+    ASSERT_EQ(generate_avk(clerk_kr, &avk), 0);
+    StmClerkPtr clerk = stm_clerk_new(params, avk, 1);
+
+    // The following can happen several times for distinct messaged, without requiring to run the registration phase
+    // over again. Again, the following is out of scope of the Clerk. The latter only needs access to the resulting
+    // signatures.
+    SigPtr sig[NEEDED_SIGS];
+    {
+        int success = 0;
+        for (Index i = 0; i < 100 && success < NEEDED_SIGS; i++) {
+            if (stm_signer_eligibility_check(signer, msg, i)) {
+                indices[success++] = i;
+            }
+        }
+
+        for (int i = 0; i < NEEDED_SIGS; i++) {
+            stm_signer_sign(signer, msg, indices[i], &sig[i]);
+            ASSERT_NE(sig[i], nullptr);
+        }
+    }
+
+    // Now, the Clerk can verify each signature individually.
+    for (int i = 0; i < NEEDED_SIGS; i++) {
+        ASSERT_EQ(stm_clerk_verify_sig(clerk, sig[i], indices[i], msg), 0);
+    }
+
+    // And finally, it aggregates them.
+    MultiSigPtr multi_sig;
+    int r = stm_clerk_aggregate(clerk, NEEDED_SIGS, sig, indices, msg, &multi_sig);
+    ASSERT_EQ(r, 0);
+    ASSERT_NE(multi_sig, nullptr);
+
+    int64_t msig_ok = stm_clerk_verify_msig(clerk, multi_sig, msg);
+    ASSERT_EQ(msig_ok, 0);
+    free_stm_clerk(clerk);
+    for (int i = 0; i < NEEDED_SIGS; i++)
+        free_sig(sig[i]);
+    free_multi_sig((MultiSigPtr)multi_sig);
+}
+
 TEST(stm, produceAndVerifyAggregateSignature) {
     const char *msg = "some message";
 
@@ -72,19 +153,7 @@ TEST(stm, produceAndVerifyAggregateSignature) {
         ASSERT_NE(sig[i], nullptr);
     }
 
-    // The Clerk first needs to run the registration with all participating parties. We explicitly list all steps.
-    // First it defines which are the parties than can participate (this can happen before the clerk knows their
-    // public keys).
-    KeyRegPtr clerk_kr = key_registration(2, party_ids, party_stake);
-    // Next it registers their public keys
-    ASSERT_EQ(register_party(clerk_kr, party_ids[0], keys[0]), 0);
-    ASSERT_EQ(register_party(clerk_kr, party_ids[1], keys[1]), 0);
-    // Now the registration phases closes, and no other parties can be included
-    close_registration(clerk_kr);
-    // After closing the registration, the clerk can generate the global key
-    MerkleTreePtr avk;
-    ASSERT_EQ(generate_avk(clerk_kr, &avk), 0);
-    StmClerkPtr clerk = stm_clerk_new(params, avk, 1);
+    StmClerkPtr clerk = stm_clerk_from_signer(signer);
     for (int i = 0; i < NEEDED_SIGS; i++) {
       ASSERT_EQ(stm_clerk_verify_sig(clerk, sig[i], indices[i], msg), 0);
     }
