6b4cccf86d39d35e1a7ad70c325d741664becafc
James LaMar <james.lamar@galois.com>
2022-03-13 22:15:43 -0700

    More networking/infrastructure

--- monitor/src/Mithril/Network.hs
+++ monitor/src/Mithril/Network.hs
@@ -5,13 +5,14 @@ import Control.Concurrent.Chan(Chan)
 import qualified Control.Concurrent.Chan as Chan
 import qualified Control.Concurrent as Conc
 import Control.Monad(forever, void)
+import Control.Exception(bracket)
 
 import qualified Network.HTTP.Simple as Client
 import qualified Web.Scotty as Scotty
 import Data.Aeson (ToJSON, FromJSON)
 import Data.Map (Map)
-import Network.Wai.Handler.Warp as Warp
-import Network.HTTP.Types.Status as HTTPStatus
+import qualified Network.Wai.Handler.Warp as Warp
+import qualified Network.HTTP.Types.Status as HTTPStatus
 
 
 -------------------------------------------------------------------------------
@@ -66,21 +67,36 @@ routedMessageEndpoint o =
 
       Scotty.defaultHandler $ \_ -> Scotty.status HTTPStatus.status400
 
-restNode :: (Scotty.Parsable pid, FromJSON msg, ToJSON msg) => Warp.Settings -> (pid -> pid -> Maybe String) -> IO (Node (RoutedMessage pid msg))
+restNode :: (Scotty.Parsable pid, FromJSON msg, ToJSON msg) => Warp.Settings -> (pid -> pid -> Maybe String) -> IO (Node (RoutedMessage pid msg), IO ())
 restNode settings route =
   do  inChan <- Chan.newChan
       outChan <- Chan.newChan
 
       let ep = routedMessageEndpoint (Chan.writeChan inChan)
       -- TODO: don't ignore errors?
-      Conc.forkIO $ forever (Chan.readChan outChan >>= sendRoutedMessage route)
+      clientThread <- Conc.forkIO $ forever (Chan.readChan outChan >>= sendRoutedMessage route)
 
       let sopts = Scotty.Options  { Scotty.verbose = 0, Scotty.settings = settings }
-      Conc.forkIO $ Scotty.scottyOpts sopts ep
+      serverThread <- Conc.forkIO $ Scotty.scottyOpts sopts ep
 
-      pure $ mkChanNode inChan outChan
+      let shutdown =
+            do  Conc.killThread serverThread
+                Conc.killThread clientThread
+
+      pure (mkChanNode inChan outChan, shutdown)
+
+restNode' :: (Scotty.Parsable pid, FromJSON msg, ToJSON msg) => Int -> (pid -> pid -> Maybe String) -> IO (Node (RoutedMessage pid msg), IO ())
+restNode' port = restNode $ Warp.setPort port Warp.defaultSettings
 
 
-
+withRestNode :: (Scotty.Parsable pid, FromJSON msg, ToJSON msg) => Int -> (pid -> pid -> Maybe String) -> (Node (RoutedMessage pid msg) -> IO a) -> IO a
+withRestNode port route action =
+  bracket acquire
+          shutdown
+          act
+  where
+    acquire = restNode' port route
+    shutdown (_, s) = s
+    act (n, _) = action n
 
 
---
+++ monitor/src/Mithril/Simple.hs
@@ -0,0 +1,195 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE LambdaCase #-}
+
+module Mithril.Simple where
+
+import qualified Data.List as List
+import Data.Text(Text)
+import qualified Data.Text.IO as TIO
+import Data.Map(Map)
+import Data.Maybe(maybeToList, isNothing)
+import Data.Foldable(foldl')
+import qualified System.Process as Process
+import qualified Data.Aeson as JSON
+import Data.Aeson((.=))
+import qualified System.Directory as Dir
+import qualified System.IO.Temp as Temp
+import Control.Monad(forM_)
+import qualified Data.IORef as IORef
+
+import qualified Mithril.Network as MNet
+import qualified Mithril.Monitor as Monitor
+import qualified Mithril.Messages as Messages
+import Mithril.Messages (PartyId, Stake)
+
+type RMsg = MNet.RoutedMessage Messages.PartyId Messages.Message
+
+data MOutput =
+    Send RMsg
+  | Err Text
+
+type Monitor = Monitor.Monitor RMsg MOutput
+
+send :: RMsg -> Monitor ()
+send = Monitor.log . Send
+
+sendFromTo :: PartyId -> PartyId -> Messages.Message -> Monitor ()
+sendFromTo from to msg = send $ MNet.RoutedMessage from to msg
+
+err :: Text -> Monitor ()
+err = Monitor.log . Err
+
+-- configure a lab and run a monitor after setting up
+testMonitor :: LabConfig -> Monitor () -> IO ()
+testMonitor lc mon =
+  do  withLab lc (`go` mon)
+
+  where
+    go labNode mon =
+      do  -- read any monitor output
+          let (mon', out) = Monitor.output mon
+
+          -- dispatch all output produced by the monitor
+          out `forM_` \case
+            Send msg -> MNet.sendTo labNode msg
+            Err e -> TIO.putStrLn e
+
+          -- if monitor is complete
+          case Monitor.getResult mon' of
+            Nothing ->
+              do  msg <- MNet.recvFrom labNode
+                  go labNode (mon' `Monitor.observe` msg)
+            Just () -> pure ()
+
+
+
+
+
+
+
+
+defaultParams :: Messages.Parameters
+defaultParams =
+  Messages.Parameters { Messages.parametersK = 5
+                     , Messages.parametersM = 100
+                     , Messages.parametersPhiF = 0.2
+                     }
+
+
+
+-------------------------------------------------------------------------------
+-- Config/Setup
+
+data NodeConfig = NodeConfig
+  { nConfigPartyId :: PartyId
+  , nConfigLocalAddress :: Maybe String
+  , nConfigExtAddress :: String
+  , nConfigStake :: Stake
+  }
+  deriving(Eq)
+
+data NetworkConfig = NetworkConfig
+  { netNodes :: [NodeConfig]
+  , netParams :: Messages.Parameters
+  }
+  deriving(Eq)
+
+data LabConfig = LabConfig
+  { labNet :: NetworkConfig
+  , labRestPort :: Int
+  , labRestBase :: String
+  , labRustDir :: String
+  }
+
+instance JSON.ToJSON NodeConfig where
+  toJSON nc =
+    JSON.object [ "id" .= nConfigPartyId nc
+                , "address_local" .= nConfigLocalAddress nc
+                , "address_endpoint" .= nConfigExtAddress nc
+                , "stake" .= nConfigStake nc
+                ]
+
+instance JSON.ToJSON NetworkConfig where
+  toJSON nc =
+    JSON.object [ "nodes" .= netNodes nc
+                , "parameters" .= netParams nc
+                ]
+
+localConfig :: PartyId -> Stake -> Int -> NodeConfig
+localConfig pid stake port =
+  NodeConfig { nConfigPartyId = pid
+             , nConfigLocalAddress = Just ("localhost:" ++ show port)
+             , nConfigExtAddress = "http://localhost:" ++ show port ++ "/"
+             , nConfigStake = stake
+             }
+
+virtualConfig :: PartyId -> Stake -> NodeConfig
+virtualConfig pid stake =
+  NodeConfig { nConfigPartyId = pid
+             , nConfigLocalAddress = Nothing
+             , nConfigExtAddress = ""
+             , nConfigStake = stake
+             }
+
+isVirtualNode :: NodeConfig -> Bool
+isVirtualNode c = isNothing (nConfigLocalAddress c)
+
+proxyNodeConfig :: String -> NodeConfig -> NodeConfig
+proxyNodeConfig baseUri config =
+  config { nConfigExtAddress = baseUri ++ "/" ++ show (nConfigPartyId config) ++ "/"
+         }
+
+proxyNetConfigFor :: (NodeConfig -> NodeConfig) -> NodeConfig -> NetworkConfig -> NetworkConfig
+proxyNetConfigFor pxy node net = net { netNodes = proxy <$> netNodes net }
+  where
+    proxy n | n == node = node
+            | otherwise = pxy n
+
+withLaunchNode :: LabConfig -> NodeConfig -> IO a -> IO a
+withLaunchNode labCfg nc a =
+  case nConfigLocalAddress nc of
+    Nothing -> a
+    Just la ->
+      withNetCfgFile proxiedNetConfig $ \netCfgFile ->
+        let pid = nConfigPartyId nc
+            nproc = Process.proc "cargo" ["run", "--", "--node-id", show pid, "--config-file", netCfgFile]
+            nproc' = nproc { Process.cwd = Just (labRustDir labCfg) }
+        in Process.withCreateProcess nproc' (\_ _ _ _ -> a)
+  where
+    proxiedNetConfig = proxyNetConfigFor (proxyNodeConfig (labRestBase labCfg)) nc (labNet labCfg)
+
+route :: NetworkConfig -> PartyId -> PartyId -> Maybe String
+route nc _ to = nConfigExtAddress <$> nodeConfig
+  where
+    nodeConfig = List.find (\c -> nConfigPartyId c == to && not (isVirtualNode c)) (netNodes nc)
+
+withNetCfgFile :: NetworkConfig -> (FilePath -> IO a) -> IO a
+withNetCfgFile nc f = Temp.withSystemTempFile "testlab-netconfig" (\path _ -> JSON.encodeFile path nc >> f path)
+
+withLab :: LabConfig -> (MNet.Node RMsg -> IO a) -> IO a
+withLab labCfg a = withLaunchNodes labCfg (MNet.withRestNode (labRestPort labCfg) (route netCfg) a)
+  where
+    netCfg = labNet labCfg
+    withLaunchNodes labCfg a = foldr (withLaunchNode labCfg) a (netNodes netCfg)
+
+-------------------------------------------------------------------------------
+
+labConfig1 :: LabConfig
+labConfig1 =
+  LabConfig { labNet = labNetCfg
+            , labRustDir = "../rust-node"
+            , labRestBase = "http://localhost:8000/"
+            , labRestPort = 8000
+            }
+  where
+    labNetCfg =
+      NetworkConfig { netParams = defaultParams
+                    , netNodes = [ localConfig 1 1 8001
+                                 , localConfig 2 1 8002
+                                 ]
+                    }
+
+
+
+
