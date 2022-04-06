bfaae44609761aa7b68fe8ff7e6b598f057fa1be
DJO <790521+Alenar@users.noreply.github.com>
2022-04-06 17:51:37 +0200

    Base docusaurus install

---
+++ docs/docs/intro.md
@@ -0,0 +1,12 @@
+# Documentation
+
+## Reports
+
+* [PoC 2022-01 Milestone Report](./2022-01-report.md)
+
+## Technical Documentation
+
+* [Mithril Protocol](./mithril-protocol.md)
+* [Mithril Architecture](./mithril-architecture.md)
+* [Mithril Library API](https://input-output-hk.github.io/mithril/mithril-core/doc/mithril/index.html)
+* [Mithril Test Lab](./mithril-test-lab.md)
---
+++ docs/docusaurus.config.js
@@ -0,0 +1,119 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Mithril',
+  tagline: 'User Manual, Core Concepts and API Reference',
+  url: 'https://input-output-hk.github.io',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'Input Output',
+  projectName: 'Mithril',
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl: 'https://github.com/input-output-hk/mithril/tree/main/docs/docs/',
+        },
+        blog: {
+          path: 'milestone',
+          routeBasePath: 'milestones/',
+          blogTitle: "milestones",
+          blogSidebarTitle: "Milestones",
+          sortPosts: 'ascending',
+          showReadingTime: true,
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'My Site',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Tutorial',
+          },
+          {to: '/milestones', label: 'Milestones', position: 'right'},
+          {
+            href: 'https://github.com/facebook/docusaurus',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/docusaurus',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/docusaurus',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} <strong>Input Output</strong> <br/> <small>Built with Docusaurus</small>`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
