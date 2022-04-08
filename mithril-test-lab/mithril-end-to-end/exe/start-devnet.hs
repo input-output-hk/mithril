b8ff0ac5363b10ee383a7023193bd0ba843b193c
Arnaud Bailly <arnaud.bailly@iohk.io>
2022-04-08 14:15:02 +0000

    Basic devnet with 3 cardano nodes

--- mithril-test-lab/mithril-end-to-end/exe/start-devnet.hs
+++ mithril-test-lab/mithril-end-to-end/exe/start-devnet.hs
@@ -1,4 +1,21 @@
+import CardanoCluster (ClusterConfig (..), defaultNetworkId, withCluster)
+import Control.Tracer (Tracer (Tracer))
+import Data.Aeson (encode)
+import qualified Data.ByteString.Lazy as LBS
 import Hydra.Prelude
+import System.Directory (createDirectoryIfMissing, getCurrentDirectory)
+import System.FilePath ((</>))
 
 main :: IO ()
-main = putStrLn "Hello World!"
+main = do
+  workDir <- (</> "devnet") <$> getCurrentDirectory
+  createDirectoryIfMissing True workDir
+  let config =
+        ClusterConfig
+          { parentStateDirectory = workDir,
+            networkId = defaultNetworkId
+          }
+      tr = Tracer $ \a -> LBS.hPut stdout (encode a <> "\n")
+  withCluster tr config $ \_cluster -> do
+    putTextLn "Press enter to quit"
+    void getLine
