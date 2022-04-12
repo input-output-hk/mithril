7ded2c3a4cc6af12bc9a953ea72fb8702d7751fb
Arnaud Bailly <arnaud.bailly@iohk.io>
2022-04-12 14:59:48 +0000

    Flesh out aggregator in ETE test

--- mithril-test-lab/mithril-end-to-end/src/Mithril/Aggregator.hs
+++ mithril-test-lab/mithril-end-to-end/src/Mithril/Aggregator.hs
@@ -5,16 +5,35 @@ module Mithril.Aggregator where
 
 import Control.Tracer (Tracer, traceWith)
 import Hydra.Prelude
+import System.FilePath ((</>))
+import System.Process (CreateProcess (..), StdStream (UseHandle), proc, withCreateProcess)
+import Test.Hydra.Prelude (checkProcessHasNotDied)
+import Test.Network.Ports (randomUnusedTCPPort)
 
 data Aggregator = Aggregator {aggregatorPort :: Int}
 
-data AggregatorLog = AggregatorStarted Int
+data AggregatorLog
+  = AggregatorStarted Int
+  | StartingAggregator {workDirectory :: FilePath}
   deriving stock (Eq, Show, Generic)
   deriving anyclass (ToJSON, FromJSON)
 
 -- TODO: start an aggregator server on some default configuration that allocates random
 -- port and wait for 'action' to terminate before closing the server.
-withAggregator :: Tracer IO AggregatorLog -> (Aggregator -> IO a) -> IO a
-withAggregator tracer action = do
-  traceWith tracer (AggregatorStarted 0)
-  action (Aggregator 0)
+withAggregator :: FilePath -> Tracer IO AggregatorLog -> (Aggregator -> IO a) -> IO a
+withAggregator workDir tracer action = do
+  port <- randomUnusedTCPPort
+  let process = aggregatorProcess (Just workDir) port
+      logFile = workDir </> "aggregator.log"
+  traceWith tracer (StartingAggregator workDir)
+  withFile logFile WriteMode $ \out ->
+    withCreateProcess process {std_out = UseHandle out, std_err = UseHandle out} $ \_stdin _stdout _stderr processHandle ->
+      race
+        (checkProcessHasNotDied "mithril-aggregator" processHandle)
+        (traceWith tracer (AggregatorStarted port) >> action (Aggregator port))
+        >>= \case
+          Left _ -> error "should never happen"
+          Right a -> pure a
+
+aggregatorProcess :: Maybe FilePath -> Int -> CreateProcess
+aggregatorProcess cwd port = (proc "mithril-aggregator" ["--server-port", show port]) {cwd}
