c559edb6eaa07f14e89272a62ac867e3bccc24c0
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2022-04-13 19:06:03 +0200

    Enhanced implementation Mithril Client #102 * Better handling of the configuration * Better separation between snapshot data and display * Separated the AggregatorHandler fake implementation to new file * Added 'show' snapshot subcommand

---
+++ mithril-network/mithril-client/src/aggregator_fake.rs
@@ -0,0 +1,41 @@
+// TODO: To delete once it is not needed anymore
+use async_trait::async_trait;
+use log::debug;
+use std::sync::Arc;
+
+use crate::aggregator::AggregatorHandler;
+use crate::entities::*;
+use mithril_aggregator::fake_data;
+
+/// AggregatorHandlerFake is a fake AggregatorHandler
+pub struct AggregatorHandlerFake {}
+
+impl AggregatorHandlerFake {
+    /// AggregatorHandlerFake factory
+    #[allow(dead_code)]
+    pub fn new(_config: Arc<Config>) -> Self {
+        debug!("New AggregatorHandlerFake created");
+        Self {}
+    }
+}
+
+#[async_trait]
+impl AggregatorHandler for AggregatorHandlerFake {
+    /// List snapshots
+    async fn list_snapshots(&self) -> Result<Vec<Snapshot>, String> {
+        let snapshots = fake_data::snapshots(5);
+        Ok(snapshots)
+    }
+
+    /// Get snapshot details
+    async fn get_snapshot_details(&self, _digest: String) -> Result<Snapshot, String> {
+        let snapshot = fake_data::snapshots(1).first().unwrap().to_owned();
+        Ok(snapshot)
+    }
+
+    /// Download Snapshot
+    async fn download_snapshot(&self, digest: String) -> Result<(), String> {
+        debug!("Download snapshot {}", digest);
+        Ok(())
+    }
+}
