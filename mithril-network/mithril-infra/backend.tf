7a102110320cb769c0fa0e037cc8fd3a4a494e44
Arnaud Bailly <arnaud.bailly@iohk.io>
2022-04-16 17:47:24 +0200

    Provide values for provider and backend config

--- mithril-network/mithril-infra/backend.tf
+++ mithril-network/mithril-infra/backend.tf
@@ -2,5 +2,7 @@ terraform {
   # we use gcloud as backend, configuration of state bucker and zone
   # should be provided on the command-line when init-ing the state
   backend "gcs" {
+    bucket = "hydra-terraform-admin"
+    backend-config = "prefix=terraform/mithril-aggregator"
   }
 }
