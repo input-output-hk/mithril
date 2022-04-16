6c9403b3e719a17e7ff6c75d464a77749894d9f5
Arnaud Bailly <arnaud.bailly@iohk.io>
2022-04-16 17:49:08 +0200

    Fix backend config

--- mithril-network/mithril-infra/backend.tf
+++ mithril-network/mithril-infra/backend.tf
@@ -3,6 +3,6 @@ terraform {
   # should be provided on the command-line when init-ing the state
   backend "gcs" {
     bucket = "hydra-terraform-admin"
-    backend-config = "prefix=terraform/mithril-aggregator"
+    prefix = "terraform/mithril-aggregator"
   }
 }
