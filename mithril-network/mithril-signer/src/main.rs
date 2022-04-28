682a398d19e122349a4473406152394d926e73b8
DJO <790521+Alenar@users.noreply.github.com>
2022-04-28 12:58:08 +0200

    Signer: use slog for structured json logging

--- mithril-network/mithril-signer/src/main.rs
+++ mithril-network/mithril-signer/src/main.rs
@@ -1,8 +1,9 @@
 use crate::entities::Config;
 use clap::Parser;
-use clap_verbosity_flag::{InfoLevel, Verbosity};
-use log::debug;
+use slog::{o, Drain, Level, Logger};
+use slog_scope::debug;
 use std::env;
+use std::sync::Arc;
 
 mod entities;
 
@@ -16,23 +17,39 @@ pub struct Args {
     run_mode: String,
 
     /// Verbosity level
-    #[clap(flatten)]
-    verbose: Verbosity<InfoLevel>,
+    #[clap(short, long, parse(from_occurrences))]
+    verbose: usize,
+}
+
+impl Args {
+    fn log_level(&self) -> Level {
+        match self.verbose {
+            0 => Level::Warning,
+            1 => Level::Info,
+            2 => Level::Debug,
+            _ => Level::Trace,
+        }
+    }
+}
+
+fn build_logger(min_level: Level) -> Logger {
+    let drain = slog_bunyan::new(std::io::stdout())
+        .set_pretty(false)
+        .build()
+        .fuse();
+    let drain = slog::LevelFilter::new(drain, min_level).fuse();
+    let drain = slog_async::Async::new(drain).build().fuse();
+
+    Logger::root(Arc::new(drain), o!())
 }
 
 fn main() {
     // Load args
     let args = Args::parse();
-
-    // Init logger
-    env_logger::Builder::new()
-        .target(env_logger::Target::Stdout)
-        .filter_level(args.verbose.log_level_filter())
-        .init();
+    let _guard = slog_scope::set_global_logger(build_logger(args.log_level()));
 
     // Load config
     let run_mode = env::var("RUN_MODE").unwrap_or(args.run_mode);
-    debug!("Run Mode: {}", run_mode);
     let config: Config = config::Config::builder()
         .add_source(config::File::with_name(&format!("./config/{}.json", run_mode)).required(false))
         .add_source(config::Environment::default())
@@ -40,7 +57,7 @@ fn main() {
         .unwrap()
         .try_deserialize()
         .unwrap();
-    debug!("{:?}", config);
+    debug!("Started"; "run_mode" => &run_mode, "config" => format!("{:?}", config));
 
     println!("Hello, world!");
 }
