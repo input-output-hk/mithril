e6ede1309ee65d6e1712bf062d013d0251995b46
DJO <790521+Alenar@users.noreply.github.com>
2022-05-03 10:33:07 +0200

    Signer keeps track of current pending cert #144, #145

--- mithril-signer/src/signer.rs
+++ mithril-signer/src/signer.rs
@@ -1,4 +1,4 @@
-use mithril_aggregator::entities::CertificatePending;
+use mithril_aggregator::entities::{Beacon, CertificatePending};
 
 #[cfg(test)]
 use mockall::automock;
@@ -12,6 +12,7 @@ pub trait CertificateHandler {
 
 struct Signer {
     certificate_handler: Box<dyn CertificateHandler>,
+    current_beacon: Option<Beacon>,
 }
 
 impl Signer {
@@ -22,16 +23,27 @@ impl Signer {
     ) -> Self {
         Self {
             certificate_handler,
+            current_beacon: None,
         }
     }
 
-    fn run(&self) {
-        if let Some(_certificate) = self
+    fn run(&mut self) {
+        if let Some(pending_certificate) = self
             .certificate_handler
             .retrieve_pending_certificate()
             .unwrap()
         {
-            self.certificate_handler.register_signatures("");
+            let must_register_signature = match &self.current_beacon {
+                None => {
+                    self.current_beacon = Some(pending_certificate.beacon);
+                    true
+                }
+                Some(beacon) => beacon != &pending_certificate.beacon,
+            };
+
+            if must_register_signature {
+                let _ = self.certificate_handler.register_signatures("");
+            }
         }
     }
 }
@@ -40,6 +52,20 @@ impl Signer {
 mod tests {
     use super::*;
     use mithril_aggregator::fake_data;
+
+    #[test]
+    fn signer_doesnt_sign_when_there_is_no_pending_certificate() {
+        let party_id = "";
+        let signing_key = "";
+        let mut mock_certificate_handler = MockCertificateHandler::new();
+        mock_certificate_handler
+            .expect_retrieve_pending_certificate()
+            .return_const(Ok(None))
+            .once();
+
+        let mut signer = Signer::new(party_id, signing_key, Box::new(mock_certificate_handler));
+        signer.run();
+    }
 
     #[test]
     fn signer_sign_when_triggered_by_pending_certificate() {
@@ -60,7 +86,27 @@ mod tests {
             .return_const(Ok(()))
             .once();
 
-        let signer = Signer::new(party_id, signing_key, Box::new(mock_certificate_handler));
+        let mut signer = Signer::new(party_id, signing_key, Box::new(mock_certificate_handler));
+        signer.run();
+        signer.run();
+    }
+
+    #[test]
+    fn signer_sign_only_once_if_pending_certificate_has_not_changed() {
+        let party_id = "";
+        let signing_key = "";
+        let mut mock_certificate_handler = MockCertificateHandler::new();
+        let pending_certificate = fake_data::certificate_pending();
+        mock_certificate_handler
+            .expect_retrieve_pending_certificate()
+            .return_const(Ok(Some(pending_certificate)))
+            .times(2);
+        mock_certificate_handler
+            .expect_register_signatures()
+            .return_const(Ok(()))
+            .once();
+
+        let mut signer = Signer::new(party_id, signing_key, Box::new(mock_certificate_handler));
         signer.run();
         signer.run();
     }
