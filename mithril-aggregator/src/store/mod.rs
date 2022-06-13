1dd44c75de70b9e8947b57ad907c4e86a55f70b1
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2022-06-13 16:11:13 +0200

    Wire common 'adapter' in Aggregator

--- mithril-aggregator/src/store/mod.rs
+++ mithril-aggregator/src/store/mod.rs
@@ -1,18 +1,18 @@
-pub mod adapter;
 mod certificate_store;
 mod error;
 mod pending_certificate_store;
 mod stake_store;
 mod verification_key_store;
 
-pub use adapter::{AdapterError, JsonFileStoreAdapter, MemoryAdapter, StoreAdapter};
 pub use certificate_store::CertificateStore;
 pub use error::StoreError;
+pub use mithril_common::store::adapter;
+pub use mithril_common::store::adapter::{
+    AdapterError, DumbStoreAdapter, FailStoreAdapter, JsonFileStoreAdapter, MemoryAdapter,
+    StoreAdapter,
+};
 pub use pending_certificate_store::CertificatePendingStore;
 pub use stake_store::{StakeStore, StakeStoreError, StakeStorer};
 pub use verification_key_store::{
     VerificationKeyStore, VerificationKeyStoreError, VerificationKeyStorer,
 };
-
-#[cfg(test)]
-pub use adapter::{DumbStoreAdapter, FailStoreAdapter};
