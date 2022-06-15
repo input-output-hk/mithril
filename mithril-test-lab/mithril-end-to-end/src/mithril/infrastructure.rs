d68f3121788835ac5b54c59c48fc95165575712e
Grégoire HUBERT <ghubert@palo-it.com>
2022-06-15 17:17:16 +0200

    create immutable file to test snapshot creation

--- mithril-test-lab/mithril-end-to-end/src/mithril/infrastructure.rs
+++ mithril-test-lab/mithril-end-to-end/src/mithril/infrastructure.rs
@@ -5,6 +5,7 @@ use std::path::{Path, PathBuf};
 pub struct MithrilInfrastructure {
     work_dir: PathBuf,
     bin_dir: PathBuf,
+    db_dir: PathBuf,
     aggregator: Aggregator,
     signer: Signer,
 }
@@ -25,6 +26,7 @@ impl MithrilInfrastructure {
         Ok(Self {
             work_dir: work_dir.to_path_buf(),
             bin_dir: bin_dir.to_path_buf(),
+            db_dir: db_dir.to_path_buf(),
             aggregator,
             signer,
         })
@@ -48,5 +50,23 @@ impl MithrilInfrastructure {
 
     pub fn build_client(&self) -> Result<Client, String> {
         Client::new(self.aggregator.endpoint(), &self.work_dir, &self.bin_dir)
+    }
+
+    pub fn add_immutable(&self) -> Result<(), Box<dyn std::error::Error>> {
+        let db_path = self.db_dir.join("immutable");
+        let glob_expr = format!("{}/*.chunk", db_path.to_string_lossy());
+
+        let mut filelist = glob::glob(&glob_expr)?
+            .map(|f| {
+                str::parse::<usize>(f.unwrap().file_stem().unwrap().to_str().unwrap()).unwrap()
+            })
+            .collect::<Vec<usize>>();
+        filelist.sort();
+        let new_number = filelist.pop().unwrap() + 1;
+
+        std::fs::File::create(db_path.join(format!("{:05}.chunk", new_number)))?;
+        std::fs::File::create(db_path.join(format!("{:05}.primary", new_number)))?;
+        std::fs::File::create(db_path.join(format!("{:05}.secondary", new_number)))?;
+        Ok(())
     }
 }
