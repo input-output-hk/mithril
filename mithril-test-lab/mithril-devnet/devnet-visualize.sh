82bdc67a82ded0691a0f9ef115d84facb71f77e4
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2022-06-16 11:34:06 +0200

    Add visualize devnet command

---
+++ mithril-test-lab/mithril-devnet/devnet-visualize.sh
@@ -0,0 +1,17 @@
+# Default values
+if [ -z "${ROOT}" ]; then 
+  ROOT="artifacts"
+fi
+
+# Change directory
+cd ${ROOT}
+
+# Stop devnet
+echo "====================================================================="
+echo " Visualize Mithril/Cardano devnet"
+echo "====================================================================="
+echo
+docker run --rm -it --name dcv -v ${PWD}:/input pmsipilot/docker-compose-viz render -m image docker-compose.yaml -r --output-file=network.png --force
+mimeopen -d network.png
+echo
+
