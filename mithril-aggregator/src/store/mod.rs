5aab64efe883f8774dc15fec274b067fb8c4de17
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2022-06-23 15:42:57 +0200

    Add single signature store implementation

--- mithril-aggregator/src/store/mod.rs
+++ mithril-aggregator/src/store/mod.rs
@@ -1,11 +1,15 @@
 mod certificate_store;
 mod error;
 mod pending_certificate_store;
+mod single_signature_store;
 mod verification_key_store;
 
 pub use certificate_store::CertificateStore;
 pub use error::StoreError;
 pub use pending_certificate_store::CertificatePendingStore;
+pub use single_signature_store::{
+    SingleSignatureStore, SingleSignatureStoreError, SingleSignatureStorer,
+};
 pub use verification_key_store::{
     VerificationKeyStore, VerificationKeyStoreError, VerificationKeyStorer,
 };
