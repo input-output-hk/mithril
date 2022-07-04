48c7e5ef193cc645db5a8f7c69bf36a1d960e817
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2022-07-04 12:06:37 +0200

    Fix indentation to 2 spaces

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -6,181 +6,181 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-    title: 'Mithril',
-    tagline: 'User Manual, Core Concepts and API Reference',
-    url: 'https://input-output-hk.github.io',
-    baseUrl: '/doc/',
-    onBrokenLinks: 'throw',
-    onBrokenMarkdownLinks: 'warn',
-    favicon: 'img/favicon.ico',
-    organizationName: 'Input Output',
-    projectName: 'Mithril',
+  title: 'Mithril',
+  tagline: 'User Manual, Core Concepts and API Reference',
+  url: 'https://input-output-hk.github.io',
+  baseUrl: '/doc/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'Input Output',
+  projectName: 'Mithril',
 
-    presets: [
-        [
-            'classic',
-            /** @type {import('@docusaurus/preset-classic').Options} */
-            ({
-                docs: {
-                    path: 'root',
-                    routeBasePath: '/',
-                    sidebarPath: require.resolve('./sidebars.js'),
-                    editUrl: 'https://github.com/input-output-hk/mithril/tree/main/docs/root/',
-                },
-                blog: {
-                    path: 'adr',
-                    routeBasePath: 'adr/',
-                    blogTitle: "ADR",
-                    blogSidebarTitle: "ADR",
-                    sortPosts: 'ascending',
-                    showReadingTime: true,
-                },
-                theme: {
-                    customCss: require.resolve('./src/css/custom.css'),
-                },
-            }),
-        ],
-        [
-            'redocusaurus',
-            {
-                specs: [
-                    {
-                        spec: '../openapi.yaml',
-                        route: '/aggregator-api/',
-                    },
-                ],
-                theme: {
-                    primaryColor: '#1890ff',
-                },
-            },
-        ],
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          path: 'root',
+          routeBasePath: '/',
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl: 'https://github.com/input-output-hk/mithril/tree/main/docs/root/',
+        },
+        blog: {
+          path: 'adr',
+          routeBasePath: 'adr/',
+          blogTitle: "ADR",
+          blogSidebarTitle: "ADR",
+          sortPosts: 'ascending',
+          showReadingTime: true,
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
     ],
+    [
+      'redocusaurus',
+      {
+        specs: [
+          {
+            spec: '../openapi.yaml',
+            route: '/aggregator-api/',
+          },
+        ],
+        theme: {
+          primaryColor: '#1890ff',
+        },
+      },
+    ],
+  ],
 
-    themeConfig:
-        /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-        ({
-            announcementBar: {
-                id: 'announcement',
-                content:
-                    'We are actively looking for SPO to test Mithril on the Cardano testnet. Contact us  <a rel="noopener noreferrer" href="mailto:spo-mithirl@iohk.io">here</a> if you are interested!',
-                backgroundColor: '#2e8555',
-                textColor: '#f1f1f1',
-                isCloseable: true,
-            },
-            navbar: {
-                title: 'Mithril',
-                logo: {
-                    alt: 'Mithril Logo',
-                    src: 'img/logo.png',
-                },
-                items: [
-                    {
-                        type: 'doc',
-                        docId: 'manual/getting-started/welcome',
-                        position: 'left',
-                        label: 'User Manual',
-                    },
-                    {
-                        type: 'doc',
-                        docId: 'mithril/intro',
-                        label: 'About Mithril',
-                        position: 'left',
-                    },
-                    {
-                        type: 'dropdown',
-                        label: 'Developer Docs',
-                        position: 'left',
-                        items: [
-                            {
-                                to: '/aggregator-api',
-                                label: 'Aggregator Node - API Reference'
-                            },
-                            {
-                                href: 'https://mithril.network/mithril-aggregator/doc/mithril_aggregator/index.html',
-                                label: 'Aggregator Node - Rust documentation'
-                            },
-                            {
-                                href: 'https://mithril.network/mithril-signer/doc/mithril_signer/index.html',
-                                label: 'Signer Node - Rust documentation'
-                            },
-                            {
-                                href: 'https://mithril.network/mithril-client/doc/mithril_client/index.html',
-                                label: 'Client Node - Rust documentation'
-                            },
-                            {
-                                href: 'https://mithril.network/mithril-core/doc/mithril/index.html',
-                                label: 'Cryptographic Core Library - Rust documentation'
-                            },
-                            {
-                                href: 'https://mithril.network/mithril-common/doc/mithril_common/index.html',
-                                label: 'Common Node Library - Rust documentation'
-                            },
-                        ]
-                    },
-                    {
-                        to: '/glossary',
-                        label: 'Glossary',
-                        position: 'right',
-                    },
-                    { to: '/adr', label: 'ADRs', position: 'right' },
-                    {
-                        className: 'header-github-link',
-                        html: ':before',
-                        href: 'https://github.com/input-output-hk/mithril/',
-                        position: 'right',
-                    },
-                ],
-            },
-            footer: {
-                style: 'dark',
-                links: [
-                    {
-                        title: 'Contributing',
-                        items: [
-                            {
-                                label: 'Project Charter',
-                                href: 'https://github.com/input-output-hk/mithril/wiki/Project-Charter',
-                            },
-                            {
-                                label: 'Architectural Decision Records',
-                                to: '/adr',
-                            }
-                        ],
-                    },
-                    {
-                        title: 'Community',
-                        items: [
-                            {
-                                label: 'Github Discussions',
-                                href: 'https://github.com/input-output-hk/mithril/discussions',
-                            },
-                            {
-                                label: 'Stack Exchange',
-                                href: 'https://cardano.stackexchange.com/questions/tagged/mithril',
-                            },
-                        ],
-                    },
-                    {
-                        title: 'More',
-                        items: [
-                            {
-                                label: 'Logbook',
-                                href: 'https://github.com/input-output-hk/mithril/wiki/Logbook'
-                            },
-                            {
-                                label: 'Input Output (Blog)',
-                                href: 'https://iohk.io/en/blog'
-                            },
-                        ],
-                    },
-                ],
-                copyright: `Copyright © ${new Date().getFullYear()} <strong>Input Output</strong> <br/> <small>Built with Docusaurus</small>`,
-            },
-            prism: {
-                theme: lightCodeTheme,
-                darkTheme: darkCodeTheme,
-            },
-        }),
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      announcementBar: {
+        id: 'announcement',
+        content:
+          'We are actively looking for SPO to test Mithril on the Cardano testnet. Contact us  <a rel="noopener noreferrer" href="mailto:spo-mithirl@iohk.io">here</a> if you are interested!',
+        backgroundColor: '#2e8555',
+        textColor: '#f1f1f1',
+        isCloseable: true,
+      },
+      navbar: {
+        title: 'Mithril',
+        logo: {
+          alt: 'Mithril Logo',
+          src: 'img/logo.png',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'manual/getting-started/welcome',
+            position: 'left',
+            label: 'User Manual',
+          },
+          {
+            type: 'doc',
+            docId: 'mithril/intro',
+            label: 'About Mithril',
+            position: 'left',
+          },
+          {
+            type: 'dropdown',
+            label: 'Developer Docs',
+            position: 'left',
+            items: [
+              {
+                to: '/aggregator-api',
+                label: 'Aggregator Node - API Reference'
+              },
+              {
+                href: 'https://mithril.network/mithril-aggregator/doc/mithril_aggregator/index.html',
+                label: 'Aggregator Node - Rust documentation'
+              },
+              {
+                href: 'https://mithril.network/mithril-signer/doc/mithril_signer/index.html',
+                label: 'Signer Node - Rust documentation'
+              },
+              {
+                href: 'https://mithril.network/mithril-client/doc/mithril_client/index.html',
+                label: 'Client Node - Rust documentation'
+              },
+              {
+                href: 'https://mithril.network/mithril-core/doc/mithril/index.html',
+                label: 'Cryptographic Core Library - Rust documentation'
+              },
+              {
+                href: 'https://mithril.network/mithril-common/doc/mithril_common/index.html',
+                label: 'Common Node Library - Rust documentation'
+              },
+            ]
+          },
+          {
+            to: '/glossary',
+            label: 'Glossary',
+            position: 'right',
+          },
+          { to: '/adr', label: 'ADRs', position: 'right' },
+          {
+            className: 'header-github-link',
+            html: ':before',
+            href: 'https://github.com/input-output-hk/mithril/',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Contributing',
+            items: [
+              {
+                label: 'Project Charter',
+                href: 'https://github.com/input-output-hk/mithril/wiki/Project-Charter',
+              },
+              {
+                label: 'Architectural Decision Records',
+                to: '/adr',
+              }
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Github Discussions',
+                href: 'https://github.com/input-output-hk/mithril/discussions',
+              },
+              {
+                label: 'Stack Exchange',
+                href: 'https://cardano.stackexchange.com/questions/tagged/mithril',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Logbook',
+                href: 'https://github.com/input-output-hk/mithril/wiki/Logbook'
+              },
+              {
+                label: 'Input Output (Blog)',
+                href: 'https://iohk.io/en/blog'
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} <strong>Input Output</strong> <br/> <small>Built with Docusaurus</small>`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
 };
 
 module.exports = config;
--- docs/src/components/HomepageFeatures/index.js
+++ docs/src/components/HomepageFeatures/index.js
@@ -3,64 +3,64 @@ import clsx from 'clsx';
 import styles from './styles.module.css';
 
 const FeatureList = [
-    {
-        title: 'Modular',
-        Svg: require('@site/static/img/card-operate-a-stake-pool.svg').default,
-        description: (
-            <>
-                Mithril is a modular set of tools to accelerate blockchain synchronization.
-                Find components description in Topologies.
-            </>
-        ),
-        link: '/doc/category/topologies',
-    },
-    {
-        title: 'How it works',
-        Svg: require('@site/static/img/card-native-tokens.svg').default,
-        description: (
-            <>
-                How does the heart of Mithril work?
-                Find how the Mithril multisignature works in Core Concepts.
-            </>
-        ),
-        link: '/doc/category/core-concepts',
-    },
-    {
-        title: 'Get started',
-        Svg: require('@site/static/img/card-get-started.svg').default,
-        description: (
-            <>
-                Quick instructions to build it.
-            </>
-        ),
-        link: '/doc/manual/getting-started/welcome',
-    },
+  {
+    title: 'Modular',
+    Svg: require('@site/static/img/card-operate-a-stake-pool.svg').default,
+    description: (
+      <>
+        Mithril is a modular set of tools to accelerate blockchain synchronization.
+        Find components description in Topologies.
+      </>
+    ),
+    link: '/doc/category/topologies',
+  },
+  {
+    title: 'How it works',
+    Svg: require('@site/static/img/card-native-tokens.svg').default,
+    description: (
+      <>
+        How does the heart of Mithril work?
+        Find how the Mithril multisignature works in Core Concepts.
+      </>
+    ),
+    link: '/doc/category/core-concepts',
+  },
+  {
+    title: 'Get started',
+    Svg: require('@site/static/img/card-get-started.svg').default,
+    description: (
+      <>
+        Quick instructions to build it.
+      </>
+    ),
+    link: '/doc/manual/getting-started/welcome',
+  },
 ];
 
 function Feature({ Svg, title, description, link }) {
-    return (
-        <div className={clsx('col col--4')}>
-            <div className="text--center">
-                <a href={link}><Svg className={styles.featureSvg} role="img" /></a>
-            </div>
-            <div className="text--center padding-horiz--md">
-                <h3>{title}</h3>
-                <p>{description}</p>
-            </div>
-        </div>
-    );
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <a href={link}><Svg className={styles.featureSvg} role="img" /></a>
+      </div>
+      <div className="text--center padding-horiz--md">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
 }
 
 export default function HomepageFeatures() {
-    return (
-        <section className={styles.features}>
-            <div className="container">
-                <div className="row">
-                    {FeatureList.map((props, idx) => (
-                        <Feature key={idx} {...props} />
-                    ))}
-                </div>
-            </div>
-        </section>
-    );
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
