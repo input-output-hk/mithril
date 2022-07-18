7868ae8f0c4e363bcc669df065ce747bfc102c8c
Grégoire HUBERT <ghubert@palo-it.com>
2022-07-18 14:25:52 +0200

    refactor test initialization

---
+++ mithril-aggregator/tests/simple.rs
@@ -0,0 +1,24 @@
+mod init;
+use std::sync::Arc;
+
+use init::initialize_dependencies;
+use mithril_aggregator::{AggregatorRunner, AggregatorRuntime};
+use tokio::time::Duration;
+
+#[tokio::test]
+async fn simple_scenario() {
+    let (_deps, config) = initialize_dependencies().await;
+    let mut runtime = AggregatorRuntime::new(
+        Duration::from_millis(config.interval),
+        None,
+        Arc::new(AggregatorRunner::new(config)),
+    )
+    .await
+    .unwrap();
+
+    if let Err(e) = runtime.cycle().await {
+        panic!("FIRST CYCLE FAILED: {:?}", e);
+    }
+
+    assert_eq!("signing", runtime.get_state());
+}
