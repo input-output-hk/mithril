79814cf86eab46edc080e5043b022e6edabf481a
DJO <790521+Alenar@users.noreply.github.com>
2022-07-27 17:33:30 +0200

    Showcase: Better 'responsivness' with 4 snapshots per rows

--- docs/src/components/Showcase/SnapshotsList/index.js
+++ docs/src/components/Showcase/SnapshotsList/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import styles from './styles.module.css';
 
 /*
  * Code from: https://stackoverflow.com/a/18650828
@@ -26,7 +27,20 @@ export default function SnapshotsList(props) {
     let fetchSnapshots = () => {
       fetch(`${props.aggregator}/snapshots`)
         .then(response => response.json())
-        .then(data => setSnapshots(data))
+        .then(data => {
+          let grouped_snapshots = [];
+          for (let i = 0; i < data.length; i++) {
+            // 4 snapshots per rows, if you modify this also update the "col--xx" class below
+            if (i % 4 === 0) {
+              grouped_snapshots.push([]);
+            }
+            let snapshot = data[i];
+            snapshot.is_latest = i === 0;
+            grouped_snapshots[grouped_snapshots.length - 1].push(snapshot);
+          }
+          
+          setSnapshots(grouped_snapshots);
+        })
         .catch(error => {
           setSnapshots([]);
           console.error("Fetch snapshots error:", error);
@@ -46,27 +60,33 @@ export default function SnapshotsList(props) {
       {Object.entries(snapshots).length === 0
         ? <p>No snapshot available</p>
         :
-        <div className="row">
-          {snapshots.map((snapshot, index) =>
-            <div key={snapshot.digest} className="card margin-right--md margin-bottom--md shadow--md">
-              <div className="card__header">
-                <h3>
-                  {snapshot.digest.slice(0, 24)}
-                </h3>
-              </div>
-              <div className="card__body">
-                <div>Epoch: {snapshot.beacon.epoch}</div>
-                <div>Immutable File Number: {snapshot.beacon.immutable_file_number}</div>
-                <div>Certificate hash: <br/> {snapshot.certificate_hash.slice(0, 30)}</div>
-                <div>Created at: <br/> {new Date(snapshot.created_at).toLocaleString()}</div>
-                <div>Size: {formatBytes(snapshot.size)}</div>
-              </div>
-              <div className="card__footer text--right">
-                { index === 0 &&
-                  <span className="badge badge--primary margin-left--sm">Latest</span>
-                }
-                <span className="badge badge--secondary margin-left--sm">{snapshot.beacon.network}</span>
-              </div>
+        <div className={"container " + styles.snapshots_container}>
+          {snapshots.map((snapshot_row, row_index) =>
+            <div className="row" key={row_index}>
+              {snapshot_row.map(snapshot =>
+                <div key={snapshot.digest} className="padding-right--sm padding-bottom--sm col col--3">
+                  <div className="card">
+                    <div className="card__header">
+                      <h3>
+                        {snapshot.digest}
+                      </h3>
+                    </div>
+                    <div className="card__body">
+                      <div>Epoch: {snapshot.beacon.epoch}</div>
+                      <div>Immutable File Number: {snapshot.beacon.immutable_file_number}</div>
+                      <div>Certificate hash: <br/> {snapshot.certificate_hash}</div>
+                      <div>Created at: <br/> {new Date(snapshot.created_at).toLocaleString()}</div>
+                      <div>Size: {formatBytes(snapshot.size)}</div>
+                    </div>
+                    <div className="card__footer text--right">
+                      {snapshot.is_latest &&
+                        <span className="badge badge--primary margin-left--sm">Latest</span>
+                      }
+                      <span className="badge badge--secondary margin-left--sm">{snapshot.beacon.network}</span>
+                    </div>
+                  </div>
+                </div>
+              )}
             </div>
           )}
         </div>
