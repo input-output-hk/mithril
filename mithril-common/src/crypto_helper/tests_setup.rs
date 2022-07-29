65d9c49a0a33bf9c8a67998ce367f4b2acbf4eb5
DJO <790521+Alenar@users.noreply.github.com>
2022-07-29 16:46:24 +0200

    Common: add doc for the crypto_helpers types

--- mithril-common/src/crypto_helper/tests_setup.rs
+++ mithril-common/src/crypto_helper/tests_setup.rs
@@ -4,6 +4,7 @@ use crate::entities::{ProtocolMessage, ProtocolMessagePartKey};
 use rand_chacha::ChaCha20Rng;
 use rand_core::{RngCore, SeedableRng};
 
+/// Instantiate a [ProtocolMessage] using fake data, use this for tests only.
 pub fn setup_message() -> ProtocolMessage {
     let mut protocol_message = ProtocolMessage::new();
     protocol_message.set_message_part(
@@ -17,6 +18,7 @@ pub fn setup_message() -> ProtocolMessage {
     protocol_message
 }
 
+/// Instantiate a [ProtocolParameters] using fake data, use this for tests only.
 pub fn setup_protocol_parameters() -> ProtocolParameters {
     ProtocolParameters {
         m: 10,
@@ -25,6 +27,7 @@ pub fn setup_protocol_parameters() -> ProtocolParameters {
     }
 }
 
+/// Instantiate a list of signers using fake data, use this for tests only.
 pub fn setup_signers(
     total: u64,
 ) -> Vec<(
