57c7c7d464bee4983978b0b026a8284b30963c7a
Grégoire HUBERT / PALO-IT <ghubert@palo-it.com>
2022-08-03 16:16:29 +0200

    [wip] updat stake distribution

--- mithril-signer/src/state_machine/runner.rs
+++ mithril-signer/src/state_machine/runner.rs
@@ -1,7 +1,12 @@
 use async_trait::async_trait;
-use mithril_common::entities::{Beacon, CertificatePending};
+#[cfg(test)]
+use mockall::automock;
 use slog_scope::info;
-use std::error::Error;
+use std::error::Error as StdError;
+use thiserror::Error;
+
+use mithril_common::entities::{Beacon, CertificatePending, Epoch, SignerWithStake};
+use mithril_common::store::stake_store::StakeStorer;
 
 use crate::Config;
 
@@ -11,13 +16,21 @@ use super::signer_services::SignerServices;
 pub trait Runner {
     async fn get_pending_certificate(
         &self,
-    ) -> Result<Option<CertificatePending>, Box<dyn Error + Sync + Send>>;
+    ) -> Result<Option<CertificatePending>, Box<dyn StdError + Sync + Send>>;
 
-    async fn get_current_beacon(&self) -> Result<Beacon, Box<dyn Error + Sync + Send>>;
+    async fn get_current_beacon(&self) -> Result<Beacon, Box<dyn StdError + Sync + Send>>;
 
-    async fn register_to_aggregator(&self) -> Result<(), Box<dyn Error + Sync + Send>>;
+    async fn register_signer_to_aggregator(&self) -> Result<(), Box<dyn StdError + Sync + Send>>;
 
-    async fn update_stake_distribution(&self) -> Result<(), Box<dyn Error + Sync + Send>>;
+    async fn update_stake_distribution(&self) -> Result<(), Box<dyn StdError + Sync + Send>>;
+}
+
+#[derive(Debug, Clone, PartialEq, Error)]
+pub enum RuntimeError {
+    #[error("No value returned by the subsystem.")]
+    NoValueError,
+    #[error("No stake associated with this signer.")]
+    NoStake,
 }
 
 pub struct SignerRunner {
@@ -31,11 +44,13 @@ impl SignerRunner {
     }
 }
 
+#[cfg_attr(test, automock)]
 #[async_trait]
 impl Runner for SignerRunner {
     async fn get_pending_certificate(
         &self,
-    ) -> Result<Option<CertificatePending>, Box<dyn Error + Sync + Send>> {
+    ) -> Result<Option<CertificatePending>, Box<dyn StdError + Sync + Send>> {
+        info!("runner: get_pending_certificate");
         self.services
             .certificate_handler
             .retrieve_pending_certificate()
@@ -43,48 +58,164 @@ impl Runner for SignerRunner {
             .map_err(|e| e.into())
     }
 
-    async fn get_current_beacon(&self) -> Result<Beacon, Box<dyn Error + Sync + Send>> {
+    async fn get_current_beacon(&self) -> Result<Beacon, Box<dyn StdError + Sync + Send>> {
+        info!("runner: get_current_epoch");
+        todo!()
+        /*
+        self.services
+            .chain_observer
+            .get_current_beacon()
+            .await?
+            .ok_or_else(|| RuntimeError::NoValueError.into())
+        */
+    }
+
+    async fn register_signer_to_aggregator(&self) -> Result<(), Box<dyn StdError + Sync + Send>> {
         todo!()
     }
 
-    async fn register_to_aggregator(&self) -> Result<(), Box<dyn Error + Sync + Send>> {
-        todo!()
-    }
-
-    async fn update_stake_distribution(&self) -> Result<(), Box<dyn Error + Sync + Send>> {
-        info!("Update stake distribution");
-        if let Some(_stake_distribution) = self
+    async fn update_stake_distribution(&self) -> Result<(), Box<dyn StdError + Sync + Send>> {
+        info!("runner: update_stake_distribution");
+        let current_epoch = self
+            .services
+            .chain_observer
+            .get_current_epoch()
+            .await?
+            .ok_or_else(|| RuntimeError::NoValueError)?;
+        let stake_distribution = self
             .services
             .chain_observer
             .get_current_stake_distribution()
             .await?
-        {}
+            .ok_or_else(|| RuntimeError::NoValueError)?;
+        let signer_stake_distribution = stake_distribution
+            .get(&self.config.party_id)
+            .ok_or_else(|| RuntimeError::NoStake)?;
+        let verification_key_pop = self
+            .services
+            .single_signer
+            .get_protocol_initializer()
+            .ok_or_else(|| RuntimeError::NoValueError)?
+            .verification_key();
+        let signer = SignerWithStake::new(
+            self.config.party_id.clone(),
+            verification_key_pop.vk.to_string(),
+            *signer_stake_distribution,
+        );
+        self.services
+            .stake_store
+            .save_stake(current_epoch, signer)
+            .await?;
 
-        /*
-        {
-            Some(stake_distribution) => {
-                match self.chain_observer.read().await.get_current_epoch().await? {
-                    Some(epoch) => {
-                        let stake_store = self.stake_store.as_ref().write().await;
-                        for (party_id, stake) in &stake_distribution {
-                            stake_store
-                                .save_stake(
-                                    (epoch as i64 + SIGNER_EPOCH_RECORDING_OFFSET) as u64,
-                                    SignerWithStake::new(
-                                        party_id.to_owned(),
-                                        "".to_string(),
-                                        *stake,
-                                    ),
-                                )
-                                .await?;
-                        }
-                        Ok(true)
-                    }
-                    None => Ok(false),
-                }
-            }
-            None => Ok(false),
-        } */
         todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{path::PathBuf, sync::Arc};
+
+    use mithril_common::chain_observer::FakeObserver;
+    use mithril_common::digesters::DumbDigester;
+    use mithril_common::store::adapter::DumbStoreAdapter;
+    use mithril_common::store::stake_store::StakeStore;
+
+    use crate::{CertificateHandlerHTTPClient, MithrilSingleSigner};
+
+    use super::*;
+
+    fn get_current_beacon() -> Beacon {
+        Beacon {
+            network: "whatever".to_string(),
+            epoch: 9,
+            immutable_file_number: 999,
+        }
+    }
+
+    fn init_services() -> SignerServices {
+        SignerServices {
+            stake_store: Arc::new(StakeStore::new(Box::new(DumbStoreAdapter::new()))),
+            certificate_handler: Arc::new(Box::new(CertificateHandlerHTTPClient::new(
+                "whatever".to_string(),
+            ))),
+            chain_observer: Arc::new(Box::new(FakeObserver::default())),
+            digester: Arc::new(Box::new(DumbDigester::new(
+                "whatever",
+                get_current_beacon().immutable_file_number,
+                true,
+            ))),
+            single_signer: Arc::new(Box::new(MithrilSingleSigner::new(
+                "PARTY_01".to_string(),
+                "whatever",
+            ))),
+        }
+    }
+
+    fn init_runner(
+        maybe_services: Option<SignerServices>,
+        maybe_config: Option<Config>,
+    ) -> SignerRunner {
+        let services = init_services();
+        let config = Config {
+            aggregator_endpoint: "http://0.0.0.0:3000".to_string(),
+            cardano_cli_path: PathBuf::new(),
+            cardano_node_socket_path: PathBuf::new(),
+            db_directory: PathBuf::new(),
+            network: "whatever".to_string(),
+            network_magic: None,
+            party_id: "PARTY_01".to_string(),
+            run_interval: 100,
+            stake_store_directory: PathBuf::new(),
+        };
+
+        SignerRunner {
+            config: maybe_config.unwrap_or(config),
+            services: maybe_services.unwrap_or(services),
+        }
+    }
+    #[tokio::test]
+    async fn test_get_current_beacon() {
+        let runner = init_runner(None, None);
+        let beacon = get_current_beacon();
+
+        assert_eq!(
+            beacon,
+            runner
+                .get_current_beacon()
+                .await
+                .expect("Get current beacon should not fail.")
+        );
+    }
+
+    #[tokio::test]
+    async fn test_update_distribution() {
+        let mut services = init_services();
+        let stake_store = services.stake_store.clone();
+        let _certificate_store = services.certificate_handler.clone();
+        let current_epoch = services
+            .chain_observer
+            .get_current_epoch()
+            .await
+            .expect("chain observer should not fail")
+            .expect("the observer should return an epoch");
+        let runner = init_runner(Some(services), None);
+        assert!(stake_store
+            .get_stakes(current_epoch)
+            .await
+            .expect("getting stakes from store should not fail")
+            .is_none());
+
+        runner
+            .update_stake_distribution()
+            .await
+            .expect("update_stake_distribution should not fail.");
+
+        let stake_distribution = stake_store
+            .get_stakes(current_epoch)
+            .await
+            .expect("getting stakes from store should not fail")
+            .expect("there should be stakes for this epoch");
+
+        assert_eq!(1, stake_distribution.len());
     }
 }
--- mithril-signer/src/state_machine/state_machine.rs
+++ mithril-signer/src/state_machine/state_machine.rs
@@ -1,20 +1,21 @@
-use slog_scope::{debug, error, info, trace};
+use slog_scope::{debug, error, info};
 use std::{error::Error, thread::sleep, time::Duration};
 
 use mithril_common::entities::Beacon;
 
-use crate::certificate_handler;
-
 use super::Runner;
 
+#[derive(Debug, PartialEq)]
 pub struct RegisteredState {
     beacon: Beacon,
 }
 
+#[derive(Debug, PartialEq)]
 pub struct SignedState {
     beacon: Beacon,
 }
 
+#[derive(Debug, PartialEq)]
 pub enum SignerState {
     Unregistered,
     Registered(RegisteredState),
@@ -40,13 +41,22 @@ impl StateMachine {
         }
     }
 
+    pub fn get_state(&self) -> &SignerState {
+        &self.state
+    }
+
     /// perform a cycle of the state machine
     pub async fn cycle(&mut self) -> Result<(), Box<dyn Error + Sync + Send>> {
+        info!("================================================================================");
+        info!("New state machine cycle.");
+
         match &self.state {
             SignerState::Unregistered => {
-                if self.runner.get_pending_certificate().await?.is_some() {
-                    let beacon = self.runner.get_current_beacon().await?;
+                if let Some(pending_certificate) = self.runner.get_pending_certificate().await? {
+                    debug!("Pending certificate found, launching registration.");
+                    let beacon = pending_certificate.beacon;
                     self.runner.update_stake_distribution().await?;
+                    self.runner.register_signer_to_aggregator().await?;
                     self.state = SignerState::Registered(RegisteredState { beacon });
                 }
 
@@ -69,6 +79,86 @@ impl StateMachine {
             }
             info!("Sleeping for {} ms", self.state_sleep.as_millis());
             sleep(self.state_sleep);
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use mithril_common::{
+        entities::{Epoch, ImmutableFileNumber},
+        fake_data, CardanoNetwork,
+    };
+
+    use super::*;
+    use crate::state_machine::runner::MockSignerRunner;
+
+    fn init_state_machine(init_state: SignerState, runner: MockSignerRunner) -> StateMachine {
+        StateMachine {
+            state: init_state,
+            runner: Box::new(runner),
+            state_sleep: Duration::from_millis(100),
+        }
+    }
+
+    fn create_beacon(epoch: Epoch, immutable_file_number: ImmutableFileNumber) -> Beacon {
+        Beacon {
+            network: "whatever".to_string(),
+            epoch,
+            immutable_file_number,
+        }
+    }
+
+    #[tokio::test]
+    async fn unregistered_no_transition() {
+        let mut runner = MockSignerRunner::new();
+        runner
+            .expect_get_pending_certificate()
+            .times(1)
+            .returning(|| Ok(None));
+        let mut state_machine = init_state_machine(SignerState::Unregistered, runner);
+        state_machine
+            .cycle()
+            .await
+            .expect("Cycling the state machine should not fail");
+
+        assert_eq!(&SignerState::Unregistered, state_machine.get_state());
+    }
+
+    #[tokio::test]
+    async fn unregistered_to_registered() {
+        let mut runner = MockSignerRunner::new();
+        runner
+            .expect_get_pending_certificate()
+            .times(1)
+            .returning(|| Ok(Some(fake_data::certificate_pending())));
+        runner
+            .expect_get_current_beacon()
+            .times(1)
+            .returning(move || Ok(create_beacon(10, 999)));
+        runner
+            .expect_update_stake_distribution()
+            .times(1)
+            .returning(|| Ok(()));
+        runner
+            .expect_register_signer_to_aggregator()
+            .times(1)
+            .returning(|| Ok(()));
+
+        let mut state_machine = init_state_machine(SignerState::Unregistered, runner);
+
+        state_machine
+            .cycle()
+            .await
+            .expect("Cycling the state machine should not fail");
+
+        if let SignerState::Registered(state) = state_machine.get_state() {
+            assert_eq!(create_beacon(10, 999), state.beacon);
+        } else {
+            panic!(
+                "state machine did not return a RegisteredState but {:?}",
+                state_machine.get_state()
+            );
         }
     }
 }
