fd44da3956e8aa549bb6d9c129842d021da438d4
DJO <790521+Alenar@users.noreply.github.com>
2022-10-12 12:25:47 +0200

    Common: add ClientError for http server needs
    Also rename `internal_server_error.rs` to `http_server_error.rs` to keep both http server error in the same file

--- mithril-common/src/entities/http_server_error.rs
+++ mithril-common/src/entities/http_server_error.rs
@@ -13,3 +13,20 @@ impl InternalServerError {
         InternalServerError { message }
     }
 }
+
+/// Representation of a Client Error raised by an http server
+#[derive(Clone, Debug, PartialEq, Eq, Default, Serialize, Deserialize)]
+pub struct ClientError {
+    /// error label
+    pub label: String,
+
+    /// error message
+    pub message: String,
+}
+
+impl ClientError {
+    /// ClientError factory
+    pub fn new(label: String, message: String) -> ClientError {
+        ClientError { label, message }
+    }
+}
