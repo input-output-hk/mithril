43ada6881b249af5443007273a24b9cf4480afa0
Grégoire HUBERT / PALO-IT <ghubert@palo-it.com>
2022-10-21 15:19:41 +0200

    remove json store adapter

--- mithril-common/src/store/adapter/mod.rs
+++ mithril-common/src/store/adapter/mod.rs
@@ -3,12 +3,10 @@
 //! and two more for testing ([a stub with one record][DumbStoreAdapter] and one which
 //! [always fails][FailStoreAdapter]).
 
-mod jsonfile_store_adapter;
 mod memory_adapter;
 mod sqlite_adapter;
 mod store_adapter;
 
-pub use jsonfile_store_adapter::JsonFileStoreAdapter;
 pub use memory_adapter::MemoryAdapter;
 pub use sqlite_adapter::{SQLiteAdapter, SQLiteResultIterator};
 pub use store_adapter::*;
