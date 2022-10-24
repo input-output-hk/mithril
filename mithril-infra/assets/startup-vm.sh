23e2776ce971019087b3e4a38fbbdd2ed6df2da0
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2022-10-24 09:28:53 +0200

    Add VM startup script

---
+++ mithril-infra/assets/startup-vm.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Remove /startup-ready.txt
+rm -f /startup-ready.txt
+
+# Update and install dependencies
+sudo apt update -y
+sudo apt install -y tree ca-certificates curl gnupg lsb-release 
+
+# Install sqlite3
+sudo apt install -y sqlite3
+
+# Install docker & docker-compose
+sudo mkdir -p /etc/apt/keyrings
+rm -f /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update -y
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-compose
+sudo usermod -aG docker curry
+sudo chown "curry":"curry" /home/curry/.docker -R
+sudo chmod g+rwx "/home/curry/.docker" -R
+sudo service docker start
+
+# Add /startup-ready.txt
+touch /startup-ready.txt
