aca39fb808d54c41a7be4b5b9ca13e00971bfe16
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2022-11-02 15:45:21 +0100

    Update explorer endpoints

--- mithril-explorer/pages/index.js
+++ mithril-explorer/pages/index.js
@@ -8,6 +8,8 @@ import styles from "../styles/Home.module.css";
 
 const available_aggregators = [
   "https://aggregator.release-preprod.api.mithril.network/aggregator",
+  "https://aggregator.pre-release-preview.api.mithril.network/aggregator",
+  "https://aggregator.testing-preview.api.mithril.network/aggregator",
   "https://aggregator.api.mithril.network/aggregator",
   "http://localhost:8080/aggregator"
 ];
