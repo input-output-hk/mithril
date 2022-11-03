686ada79ed275c3390a062bf69940e347bf8e7cc
DJO <790521+Alenar@users.noreply.github.com>
2022-11-03 19:20:05 +0100

    We can add our own aggregator to the explorer

---
+++ mithril-explorer/components/AggregatorSetter/AddAggregatorModal.js
@@ -0,0 +1,68 @@
+import React, {useState} from 'react';
+import {Button, Form, FormGroup, Modal} from "react-bootstrap";
+
+export default function AddAggregatorModal(props) {
+  const [value, setValue] = useState("");
+  const [isInvalid, setIsInvalid] = useState(false);
+
+  function handleClose() {
+    props.onAskClose();
+    setIsInvalid(false);
+  }
+
+  function handleSave(event) {
+    // Avoid form submit if the enter key is pressed
+    event.preventDefault();
+
+    try {
+      // Use the url constructor to check if the value is an url
+      new URL(value);
+      props.onAdd(value);
+      handleClose();
+    } catch (ex) {
+      console.warn("invalid url", ex);
+      setIsInvalid(true);
+    }
+  }
+
+  return (
+    <Modal
+      show={props.show}
+      onHide={handleClose}
+      size="lg"
+      aria-labelledby="add-aggregator-title"
+      centered>
+      <Modal.Header closeButton>
+        <Modal.Title id="add-aggregator-title">
+          New aggregator source
+        </Modal.Title>
+      </Modal.Header>
+
+      <Modal.Body>
+        <Form onSubmit={handleSave}>
+          <FormGroup>
+            <Form.Label>URL</Form.Label>
+            <Form.Control
+              type="url"
+              value={value}
+              onChange={e => setValue(e.target.value)}
+              isInvalid={isInvalid}
+              autoFocus/>
+            <Form.Control.Feedback type="invalid">
+              Invalid URL
+            </Form.Control.Feedback>
+          </FormGroup>
+        </Form>
+      </Modal.Body>
+
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          Close
+        </Button>
+        <Button variant="primary" onClick={handleSave}>
+          Save
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
--- mithril-explorer/components/AggregatorSetter/index.js
+++ mithril-explorer/components/AggregatorSetter/index.js
@@ -1,44 +1,88 @@
-import React, { useState, useEffect } from 'react';
-import { Button, InputGroup, Form, Col, OverlayTrigger, Tooltip } from "react-bootstrap";
+import React, {useEffect, useState} from 'react';
+import {Button, Col, Form, InputGroup, OverlayTrigger, Tooltip} from "react-bootstrap";
+import AddAggregatorModal from "./AddAggregatorModal";
 
 export default function AggregatorSetter(props) {
   const [availableAggregators, setAvailableAggregators] = useState([]);
-  const [selectedAggregator, setSelectedAggregator] = useState();
+  const [showAddModal, toggleAddModal] = useState(false);
+  const [canRemoveSelected, setCanRemoveSelected] = useState(false);
 
   useEffect(() => {
-    let aggregators = [];
-    aggregators.push(...props.defaultAvailableAggregators);
+    let aggregators = props.defaultAvailableAggregators;
+
     setAvailableAggregators(aggregators);
-    
-    setSelectedAggregator(aggregators.at(0))
   }, [props.defaultAvailableAggregators]);
 
-  function handleChange(event) {
-    props.onAggregatorChange(event.target.value);
-    setSelectedAggregator(event.target.value);
+  useEffect(() => {
+    setCanRemoveSelected(!props.defaultAvailableAggregators.includes(props.aggregator));
+  }, [props.aggregator]);
+
+  function handleChange(aggregator) {
+    props.onAggregatorChange(aggregator);
   }
-  
+
   function copySelected() {
-    if (window.isSecureContext && selectedAggregator) {
-      navigator.clipboard.writeText(selectedAggregator);
+    if (window.isSecureContext && props.aggregator) {
+      navigator.clipboard.writeText(props.aggregator).then(() => {});
     }
   }
-  
+
+  function showAddAggregatorSourceModal() {
+    toggleAddModal(true);
+  }
+
+  function hideAddAggregatorSourceModal() {
+    toggleAddModal(false);
+  }
+
+  function addAggregatorSource(aggregator) {
+    if (availableAggregators.includes(aggregator)) {
+      return;
+    }
+
+    setAvailableAggregators([...availableAggregators, aggregator]);
+    handleChange(aggregator);
+  }
+
+  function deleteSelectedAggregatorSource() {
+    if (!canRemoveSelected) {
+      return;
+    }
+
+    setAvailableAggregators(availableAggregators.filter(a => a !== props.aggregator));
+    handleChange(availableAggregators.at(0));
+  }
+
   return (
-    <Form.Group as={Col} className={props.className}>
-      <Form.Label>Aggregator:</Form.Label>
-      <InputGroup>
-          <Form.Select value={props.aggregator} onChange={handleChange}>
+    <>
+      <AddAggregatorModal
+        show={showAddModal}
+        onAdd={addAggregatorSource}
+        onAskClose={hideAddAggregatorSourceModal}/>
+
+      <Form.Group as={Col} className={props.className}>
+        <Form.Label>Aggregator:</Form.Label>
+        <InputGroup>
+          <Button variant="outline-success" onClick={showAddAggregatorSourceModal}>
+            <i className="bi bi-plus-circle"></i>
+          </Button>
+          {canRemoveSelected &&
+            <Button variant="outline-danger" onClick={deleteSelectedAggregatorSource}>
+              <i className="bi bi-dash-circle"></i>
+            </Button>
+          }
+          <Form.Select value={props.aggregator} onChange={e => handleChange(e.target.value)}>
             {availableAggregators.map((aggregator, index) =>
               <option key={"agg-" + index} value={aggregator}>{aggregator}</option>
             )}
           </Form.Select>
-        <OverlayTrigger overlay={<Tooltip>Copy url</Tooltip>} >
-          <Button variant="outline-secondary" onClick={copySelected}>
-            <i className="bi bi-clipboard"></i>
-          </Button>
-        </OverlayTrigger>
-      </InputGroup>
-    </Form.Group>
-  )
+          <OverlayTrigger overlay={<Tooltip>Copy url</Tooltip>}>
+            <Button variant="outline-secondary" onClick={copySelected}>
+              <i className="bi bi-clipboard"></i>
+            </Button>
+          </OverlayTrigger>
+        </InputGroup>
+      </Form.Group>
+    </>
+  );
 }
