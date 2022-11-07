2690b262b753d84e5e62cee24d7dfe29880dd7f6
DJO <790521+Alenar@users.noreply.github.com>
2022-11-07 18:39:26 +0100

    Disable NextJS Server Side Rendering for explorer:
    All contents are loaded clientSide anyway.

--- mithril-explorer/pages/_app.js
+++ mithril-explorer/pages/_app.js
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import '../styles/globals.css'
+import dynamic from "next/dynamic";
 import Script from "next/script";
 import {Provider} from "react-redux";
 import {saveToLocalStorage, storeWrapper} from "../store/store";
@@ -22,4 +23,10 @@ function MithrilExplorer({ Component, ...rest }) {
   );
 }
 
-export default MithrilExplorer;
+// Disable Server Side Rendering, this as SEO implication since the site won't be rendered by bots without
+// them running client javascript.
+// If enabled this leads to NextJs/React hydration errors since the server side rendered page won't
+// have access to the saved local storage settings.
+export default dynamic(() => Promise.resolve(MithrilExplorer), {
+  ssr: false
+});
