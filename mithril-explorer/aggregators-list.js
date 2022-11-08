41d3267f251a30664f808d0a7b79fd4ecd1b26f0
DJO <790521+Alenar@users.noreply.github.com>
2022-11-08 13:59:17 +0100

    Remove deprecated aggregator from the explorer list

--- mithril-explorer/aggregators-list.js
+++ mithril-explorer/aggregators-list.js
@@ -2,6 +2,5 @@ export default [
   "https://aggregator.release-preprod.api.mithril.network/aggregator",
   "https://aggregator.pre-release-preview.api.mithril.network/aggregator",
   "https://aggregator.testing-preview.api.mithril.network/aggregator",
-  "https://aggregator.api.mithril.network/aggregator",
   "http://localhost:8080/aggregator"
 ];
