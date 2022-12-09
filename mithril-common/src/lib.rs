c39b5b9eb085a5693601a4a1725841b6f0501c24
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2022-12-09 17:04:53 +0100

    Update OpenAPI specs
    And update the MITHRIL_API_VERSION accordingly.

--- mithril-common/src/lib.rs
+++ mithril-common/src/lib.rs
@@ -42,4 +42,4 @@ pub const SIGNER_EPOCH_RECORDING_OFFSET: i64 = 1;
 /// this is the same as the one in openapi.yml file.
 /// If you want to update this version to reflect changes in the protocol,
 /// please also update the entry in the openapi.yml
-pub const MITHRIL_API_VERSION: &str = "0.0.1";
+pub const MITHRIL_API_VERSION: &str = "0.1.0";
