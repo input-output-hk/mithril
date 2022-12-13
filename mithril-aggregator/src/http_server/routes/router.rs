43a876dd22115472e041d69aa8f7e75ce0be8d03
Grégoire HUBERT / PALO-IT <ghubert@palo-it.com>
2022-12-13 12:01:33 +0100

    add custom HTTP reply

--- mithril-aggregator/src/http_server/routes/router.rs
+++ mithril-aggregator/src/http_server/routes/router.rs
@@ -7,9 +7,16 @@ use crate::DependencyManager;
 use mithril_common::MITHRIL_API_VERSION;
 
 use reqwest::header::{HeaderMap, HeaderValue};
+use reqwest::StatusCode;
 use std::sync::Arc;
 use warp::http::Method;
-use warp::{Filter, Rejection};
+use warp::reject::Reject;
+use warp::{Filter, Rejection, Reply};
+
+#[derive(Debug)]
+pub struct VersionMismatchError;
+
+impl Reject for VersionMismatchError {}
 
 /// Routes
 pub fn routes(
@@ -33,9 +40,10 @@ pub fn routes(
                 .or(signer_routes::routes(dependency_manager.clone()))
                 .or(signatures_routes::routes(dependency_manager.clone()))
                 .or(epoch_routes::routes(dependency_manager))
-                .with(cors)
-                .with(warp::reply::with::headers(headers)),
+                .with(cors),
         )
+        .recover(handle_custom)
+        .with(warp::reply::with::headers(headers))
 }
 
 /// API Version verification
@@ -45,8 +53,16 @@ fn header_must_be() -> impl Filter<Extract = (), Error = Rejection> + Copy {
             match maybe_header {
                 None => Ok(()),
                 Some(version) if version == MITHRIL_API_VERSION => Ok(()),
-                Some(_version) => Err(warp::reject()),
+                Some(_version) => Err(warp::reject::custom(VersionMismatchError)),
             }
         })
         .untuple_one()
+}
+
+pub async fn handle_custom(reject: Rejection) -> Result<impl Reply, Rejection> {
+    if reject.find::<VersionMismatchError>().is_some() {
+        Ok(StatusCode::PRECONDITION_FAILED)
+    } else {
+        Err(reject)
+    }
 }
