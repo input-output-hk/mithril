91b506374b76bbf9d093ec783f36b919c31dcf5d
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2023-01-19 14:38:38 +0100

    Harmonize adapters name in aggregator

--- mithril-aggregator/src/message_adapters/mod.rs
+++ mithril-aggregator/src/message_adapters/mod.rs
@@ -1,11 +1,11 @@
 mod from_register_signature;
 mod from_register_signer;
-mod to_certificate_message_adapter;
+mod to_certificate_message;
 mod to_epoch_settings_message;
 mod to_snasphot_message;
 
 pub use from_register_signature::FromRegisterSingleSignatureAdapter;
 pub use from_register_signer::FromRegisterSignerAdapter;
-pub use to_certificate_message_adapter::ToCertificateMessageAdapter;
+pub use to_certificate_message::ToCertificateMessageAdapter;
 pub use to_epoch_settings_message::ToEpochSettingsMessageAdapter;
 pub use to_snasphot_message::ToSnapshotMessageAdapter;
