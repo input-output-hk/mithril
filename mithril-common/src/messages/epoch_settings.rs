6bded8b63a57e11767cc10a67d308c5607475ec3
Grégoire HUBERT / PALO-IT <ghubert@palo-it.com>
2023-01-19 11:45:56 +0100

    add epoch settings message & adapter

---
+++ mithril-common/src/messages/epoch_settings.rs
@@ -0,0 +1,72 @@
+use crate::entities::{Epoch, ProtocolParameters};
+use serde::{Deserialize, Serialize};
+
+/// EpochSettings represents the settings of an epoch
+#[derive(Clone, Debug, PartialEq, Default, Serialize, Deserialize)]
+pub struct EpochSettingsMessage {
+    /// Current Epoch
+    pub epoch: Epoch,
+
+    /// Current Protocol parameters
+    #[serde(rename = "protocol")]
+    pub protocol_parameters: ProtocolParameters,
+
+    /// Next Protocol parameters
+    #[serde(rename = "next_protocol")]
+    pub next_protocol_parameters: ProtocolParameters,
+}
+
+impl EpochSettingsMessage {
+    /// Dummy instance for test purposes.
+    pub fn dummy() -> Self {
+        Self {
+            epoch: Epoch(10),
+            protocol_parameters: ProtocolParameters {
+                k: 5,
+                m: 100,
+                phi_f: 0.65,
+            },
+            next_protocol_parameters: ProtocolParameters {
+                k: 5,
+                m: 100,
+                phi_f: 0.65,
+            },
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn golden_message() -> EpochSettingsMessage {
+        EpochSettingsMessage {
+            epoch: Epoch(10),
+            protocol_parameters: ProtocolParameters {
+                k: 5,
+                m: 100,
+                phi_f: 0.65,
+            },
+            next_protocol_parameters: ProtocolParameters {
+                k: 5,
+                m: 100,
+                phi_f: 0.65,
+            },
+        }
+    }
+
+    // Test the retro compatibility with possible future upgrades.
+    #[test]
+    fn test_v1() {
+        let json = r#"{
+"epoch": 10,
+"protocol":  { "k": 5, "m": 100, "phi_f": 0.65 },
+"next_protocol":  { "k": 5, "m": 100, "phi_f": 0.65 }
+}"#;
+        let message: EpochSettingsMessage = serde_json::from_str(json).expect(
+            "This JSON is expected to be succesfully parsed into a RegisterSignatureMessage instance.",
+        );
+
+        assert_eq!(golden_message(), message);
+    }
+}
