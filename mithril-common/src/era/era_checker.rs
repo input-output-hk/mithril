d02a19ec45620f18124750317121d453172ec09a
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2023-01-25 16:37:27 +0100

    Move Era Checker to non static

--- mithril-common/src/era/era_checker.rs
+++ mithril-common/src/era/era_checker.rs
@@ -2,37 +2,33 @@ use std::sync::RwLock;
 
 use super::SupportedEra;
 
-static CURRENT_ERA: RwLock<Option<SupportedEra>> = RwLock::new(None);
-
 /// EraChecker allows the verification of the current era
-pub struct EraChecker;
+pub struct EraChecker {
+    current_era: RwLock<SupportedEra>,
+}
 
 impl EraChecker {
-    /// Retrieve the current era
-    pub fn current_era() -> Option<SupportedEra> {
-        CURRENT_ERA.read().unwrap().to_owned()
-    }
-
-    /// Change the current era
-    pub fn change_era(new_era: SupportedEra) {
-        let mut era = CURRENT_ERA.write().unwrap();
-        *era = Some(new_era);
-    }
-
-    /// Check if an era is active
-    pub fn is_era_active(era: SupportedEra) -> bool {
-        match Self::current_era() {
-            None => {
-                panic!("Current era is NOT set, 'is_era_active' can't be called yet.");
-            }
-            Some(current_era) => current_era == era,
+    /// Era checker factory
+    pub fn new(era: SupportedEra) -> Self {
+        Self {
+            current_era: RwLock::new(era),
         }
     }
 
-    #[cfg(test)]
-    fn reset_era() {
-        let mut era = CURRENT_ERA.write().unwrap();
-        *era = None;
+    /// Retrieve the current era
+    pub fn current_era(&self) -> SupportedEra {
+        self.current_era.read().unwrap().to_owned()
+    }
+
+    /// Change the current era
+    pub fn change_era(&self, new_era: SupportedEra) {
+        let mut era = self.current_era.write().unwrap();
+        *era = new_era;
+    }
+
+    /// Check if an era is active
+    pub fn is_era_active(&self, era: SupportedEra) -> bool {
+        self.current_era() == era
     }
 }
 
@@ -42,19 +38,17 @@ mod tests {
 
     #[test]
     fn can_change_era() {
-        EraChecker::reset_era();
-
         let expected_era = SupportedEra::dummy();
-        EraChecker::change_era(expected_era);
+        let era_checker = EraChecker::new(expected_era);
+        era_checker.change_era(expected_era);
 
-        assert_eq!(Some(expected_era), EraChecker::current_era());
+        assert_eq!(expected_era, era_checker.current_era());
     }
 
     #[test]
-    #[should_panic]
     fn is_era_active_panics_when_current_era_not_set() {
-        EraChecker::reset_era();
-
-        EraChecker::is_era_active(SupportedEra::dummy());
+        let expected_era = SupportedEra::dummy();
+        let era_checker = EraChecker::new(expected_era);
+        era_checker.is_era_active(expected_era);
     }
 }
