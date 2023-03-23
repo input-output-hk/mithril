3f142f9e1def283fa870ff5cd35ddb4600421cfc
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2023-03-23 16:28:15 +0100

    Create/Migrate 'epoch_setting' table

--- mithril-aggregator/src/database/migration.rs
+++ mithril-aggregator/src/database/migration.rs
@@ -6,13 +6,17 @@ use mithril_common::database::SqlMigration;
 /// There shall be one migration per database version. There could be several
 /// statements per migration.
 pub fn get_migrations() -> Vec<SqlMigration> {
-    vec![SqlMigration::new(
-        1,
-        r#"
+    vec![
+        // Migration 1
+        // Add the `stake_pool` table and migration data from the previous
+        // `stake_store` JSON format.
+        SqlMigration::new(
+            1,
+            r#"
 create table stake_pool (
     stake_pool_id text      not null,
     epoch         integer   not null,
-    stake         integer    not null,
+    stake         integer   not null,
     created_at    text      not null default current_timestamp,
     primary key (epoch, stake_pool_id)
 );
@@ -26,5 +30,27 @@ insert into stake_pool (epoch, stake_pool_id, stake)
     order by epoch asc;
 drop table stake;
 "#,
-    )]
+        ),
+        // Migration 2
+        // Add the `epoch_setting` table and migration data from the previous
+        // `protocol_parameters` JSON format.
+        SqlMigration::new(
+            2,
+            r#"
+create table epoch_setting (
+    epoch_setting_id    integer     not null,
+    protocol_parameters json        not null,
+    primary key (epoch_setting_id)
+);
+create table if not exists protocol_parameters (key_hash text primary key, key json not null, value json not null);
+insert into epoch_setting (epoch_setting_id, protocol_parameters) 
+    select 
+        protocol_parameters.key as epoch_setting_id, 
+        protocol_parameters.value as protocol_parameters
+    from protocol_parameters
+    order by key asc;
+drop table protocol_parameters;
+"#,
+        ),
+    ]
 }
