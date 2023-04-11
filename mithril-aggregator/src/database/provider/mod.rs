9bd600fdd156f082d23c8245038a1fc769d655ee
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2023-04-11 18:54:47 +0200

    Implement SignerStore providers

--- mithril-aggregator/src/database/provider/mod.rs
+++ mithril-aggregator/src/database/provider/mod.rs
@@ -3,6 +3,7 @@ mod certificate;
 mod epoch_setting;
 mod open_message;
 mod signed_entity;
+mod signer;
 mod signer_registration;
 mod stake_pool;
 
@@ -10,5 +11,6 @@ pub use certificate::*;
 pub use epoch_setting::*;
 pub use open_message::*;
 pub use signed_entity::*;
+pub use signer::*;
 pub use signer_registration::*;
 pub use stake_pool::*;
