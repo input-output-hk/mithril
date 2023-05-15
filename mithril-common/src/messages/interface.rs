6eea40273a9bd895a57accd81a85155145768c84
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2023-05-15 17:08:14 +0200

    Apply review comments

--- mithril-common/src/messages/interface.rs
+++ mithril-common/src/messages/interface.rs
@@ -1,7 +1,10 @@
 use serde::{Deserialize, Serialize};
 
 /// Message adapter trait
-pub trait MessageAdapter<U, V: Serialize + for<'a> Deserialize<'a>> {
+pub trait MessageAdapter<U, V>
+where
+    V: Serialize + for<'a> Deserialize<'a>,
+{
     /// Adapt entity to message
     fn adapt(from: U) -> V;
 }
