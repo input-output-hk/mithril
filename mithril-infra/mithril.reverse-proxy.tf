436e2b2d22094cb72d9baea34bc0d0ce824dc3d9
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2023-06-02 17:41:48 +0200

    Separate docker network terraform deployment from reverse proxy

--- mithril-infra/mithril.reverse-proxy.tf
+++ mithril-infra/mithril.reverse-proxy.tf
@@ -1,7 +1,8 @@
 resource "null_resource" "mithril_reverse_proxy" {
   depends_on = [
     null_resource.mithril_bootstrap,
-    null_resource.mithril_mount_data_disk
+    null_resource.mithril_mount_data_disk,
+    null_resource.mithril_network
   ]
 
   triggers = {
@@ -24,7 +25,6 @@ resource "null_resource" "mithril_reverse_proxy" {
 
   provisioner "remote-exec" {
     inline = [
-      "docker network inspect mithril_network >/dev/null 2>&1 || docker network create mithril_network",
       "export CURRENT_UID=$(id -u)",
       "export DOCKER_GID=$(getent group docker | cut -d: -f3)",
       "docker-compose -f /home/curry/docker/docker-compose-reverse-proxy.yaml --profile all up -d",
