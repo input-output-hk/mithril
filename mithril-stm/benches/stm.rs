9c298a6dad6938498048c6ad972550fa085c793d
curiecrypt <gamze.kilic@iohk.io>
2023-07-04 16:46:53 +0300

    bench core verifier

--- mithril-stm/benches/stm.rs
+++ mithril-stm/benches/stm.rs
@@ -2,7 +2,10 @@ use blake2::digest::{Digest, FixedOutput};
 use blake2::{digest::consts::U32, Blake2b};
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use mithril_stm::key_reg::KeyReg;
-use mithril_stm::stm::{StmAggrSig, StmClerk, StmInitializer, StmParameters, StmSigner};
+use mithril_stm::stm::{
+    CoreVerifier, Stake, StmAggrSig, StmClerk, StmInitializer, StmParameters, StmSigner,
+    StmVerificationKey,
+};
 use rand_chacha::ChaCha20Rng;
 use rand_core::{RngCore, SeedableRng};
 use rayon::prelude::*;
@@ -144,6 +147,59 @@ fn batch_benches<H>(
     }
 }
 
+fn core_verifier_benches<H>(
+    c: &mut Criterion,
+    nr_parties: usize,
+    params: StmParameters,
+    hashing_alg: &str,
+) where
+    H: Clone + Debug + Digest + Send + Sync + FixedOutput + Default,
+{
+    let mut group = c.benchmark_group(format!("Core verifier/{hashing_alg}"));
+    let mut rng = ChaCha20Rng::from_seed([0u8; 32]);
+    let mut msg = [0u8; 16];
+    rng.fill_bytes(&mut msg);
+
+    let mut public_signers: Vec<(StmVerificationKey, Stake)> = Vec::with_capacity(nr_parties);
+    let mut initializers: Vec<StmInitializer> = Vec::with_capacity(nr_parties);
+
+    let param_string = format!(
+        "k: {}, m: {}, nr_parties: {}",
+        params.k, params.m, nr_parties
+    );
+
+    let stakes = (0..nr_parties)
+        .map(|_| 1 + (rng.next_u64() % 9999))
+        .collect::<Vec<_>>();
+
+    for stake in stakes {
+        let initializer = StmInitializer::setup(params, stake, &mut rng);
+        initializers.push(initializer.clone());
+        public_signers.push((initializer.verification_key().vk, initializer.stake));
+    }
+    let signers: Vec<StmSigner<H>> = initializers
+        .into_iter()
+        .map(|s| s.new_core_signer(&public_signers))
+        .collect();
+
+    let core_verifier = CoreVerifier::setup(&public_signers);
+
+    group.bench_function(BenchmarkId::new("Play all lotteries", &param_string), |b| {
+        b.iter(|| {
+            signers[0].core_sign(&msg, core_verifier.total_stake);
+        })
+    });
+
+    let signatures = signers
+        .par_iter()
+        .filter_map(|p| p.core_sign(&msg, core_verifier.total_stake))
+        .collect::<Vec<_>>();
+
+    group.bench_function(BenchmarkId::new("Core verification", &param_string), |b| {
+        b.iter(|| core_verifier.verify(&signatures, &params, &msg))
+    });
+}
+
 fn batch_stm_benches_blake_300(c: &mut Criterion) {
     batch_benches::<Blake2b<U32>>(
         c,
@@ -160,6 +216,19 @@ fn batch_stm_benches_blake_300(c: &mut Criterion) {
 
 fn stm_benches_blake_300(c: &mut Criterion) {
     stm_benches::<Blake2b<U32>>(
+        c,
+        300,
+        StmParameters {
+            m: 150,
+            k: 25,
+            phi_f: 0.2,
+        },
+        "Blake2b",
+    );
+}
+
+fn core_verifier_benches_blake_300(c: &mut Criterion) {
+    core_verifier_benches::<Blake2b<U32>>(
         c,
         300,
         StmParameters {
@@ -198,9 +267,24 @@ fn stm_benches_blake_2000(c: &mut Criterion) {
     );
 }
 
+fn core_verifier_benches_blake_2000(c: &mut Criterion) {
+    core_verifier_benches::<Blake2b<U32>>(
+        c,
+        2000,
+        StmParameters {
+            m: 1523,
+            k: 250,
+            phi_f: 0.2,
+        },
+        "Blake2b",
+    );
+}
+
 criterion_group!(name = benches;
                  config = Criterion::default().nresamples(1000);
                  targets =
+    core_verifier_benches_blake_300,
+    core_verifier_benches_blake_2000,
     stm_benches_blake_300,
     stm_benches_blake_2000,
     batch_stm_benches_blake_300,
