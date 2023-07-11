cd397ca91e76fd37d94ba8d582a9f084c6f09ab1
DJO <790521+Alenar@users.noreply.github.com>
2023-07-11 10:57:46 +0200

    Add documentation for the mithril-common::protocol module

--- mithril-common/src/protocol/mod.rs
+++ mithril-common/src/protocol/mod.rs
@@ -1,4 +1,8 @@
-//! Todo: module doc
+//! Protocol module
+//!
+//! This module contains types that standardize and make easier mithril protocol operations
+//! such as issuing single signatures, aggregating them as multi-signatures or computing
+//! aggregate verification keys.
 
 mod multi_signer;
 mod signer_builder;
