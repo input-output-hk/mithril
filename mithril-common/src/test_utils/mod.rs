e92cd236f9add7bb85112cb88ba009d025dd62ea
DJO <790521+Alenar@users.noreply.github.com>
2023-07-28 12:24:15 +0200

    Introduce common `fake_keys`

--- mithril-common/src/test_utils/mod.rs
+++ mithril-common/src/test_utils/mod.rs
@@ -2,12 +2,13 @@
 //!
 //! They contains:
 //! * A Open Api Spec tester
-//! * Some precomputed fake data
+//! * Some precomputed fake data and keys
 //! * A builder of [MithrilFixture] to generate signers alongside a stake distribution
 //!
 
 pub mod apispec;
 pub mod fake_data;
+pub mod fake_keys;
 mod fixture_builder;
 mod mithril_fixture;
 
