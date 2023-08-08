a704c2307df986522a8a800fbf2b6bc87af932cd
DJO <790521+Alenar@users.noreply.github.com>
2023-08-08 21:21:27 +0200

    Enable navigation using back/foward + fix infinite loop in static build

--- mithril-explorer/src/app/page.js
+++ mithril-explorer/src/app/page.js
@@ -1,35 +1,53 @@
 "use client";
 
-import React, {useEffect} from 'react';
-import {useSelector} from "react-redux";
-import {usePathname, useRouter, useSearchParams} from "next/navigation";
+import React, {useEffect, useState} from 'react';
+import {useDispatch, useSelector} from "react-redux";
+import {useRouter, useSearchParams} from "next/navigation";
 import dynamic from "next/dynamic";
 import {Col, Form, Row, Stack, Tab, Tabs} from "react-bootstrap";
 import EpochSettings from "../components/EpochSettings";
 import PendingCertificate from '../components/PendingCertificate';
 import SnapshotsList from '../components/Artifacts/SnapshotsList';
 import MithrilStakeDistributionsList from "../components/Artifacts/MithrilStakeDistributionsList";
-import {selectedAggregator as currentlySelectedAggregator} from "../store/settingsSlice";
+import {selectAggregator, selectedAggregator as currentlySelectedAggregator} from "../store/settingsSlice";
 
 // Disable SSR for the following components since they use data from the store that are not
 // available server sides (because those data can be read from the local storage).
-const AggregatorSetter = dynamic(() => import('../components/AggregatorSetter'), { ssr: false })
-const IntervalSetter = dynamic(() => import('../components/IntervalSetter'), { ssr: false })
+const AggregatorSetter = dynamic(() => import('../components/AggregatorSetter'), {ssr: false})
+const IntervalSetter = dynamic(() => import('../components/IntervalSetter'), {ssr: false})
 
 export default function Explorer() {
   const router = useRouter();
-  const pathname = usePathname();
   const searchParams = useSearchParams();
+  const dispatch = useDispatch();
+  
+  // Used to avoid infinite loop between the update of the url query and the navigation handling.
+  const [isUpdatingAggregatorInUrl, setIsUpdatingAggregatorInUrl] = useState(false);
   const selectedAggregator = useSelector(currentlySelectedAggregator);
 
+  // Update the aggregator in the url query
   useEffect(() => {
-    const params = new URLSearchParams(searchParams);
-    params.set("aggregator", selectedAggregator);
-    let new_path = pathname + "?" + params.toString();
+    const aggregatorInUrl = searchParams.get('aggregator');
 
-    router.prefetch(new_path);
-    router.push(new_path);
-  }, [router, searchParams, pathname, selectedAggregator]);
+    if (selectedAggregator !== aggregatorInUrl) {
+      const params = new URLSearchParams();
+      params.set("aggregator", selectedAggregator);
+     
+      setIsUpdatingAggregatorInUrl(true);
+      router.push("?" + params.toString(), undefined, {shallow: true});
+    }
+  }, [selectedAggregator]);
+
+  // Allow navigation to work (previous, next)
+  useEffect(() => {
+    if (isUpdatingAggregatorInUrl) {
+      setIsUpdatingAggregatorInUrl(false);
+    } else {
+      const aggregatorInUrl = searchParams.get('aggregator');
+
+      dispatch(selectAggregator(aggregatorInUrl));
+    }
+  }, [searchParams]);
 
   return (
     <Stack gap={3}>
