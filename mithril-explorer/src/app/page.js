2764318edafb824236fff5500e4fa8e2065a874a
DJO <790521+Alenar@users.noreply.github.com>
2023-08-08 18:42:33 +0200

    Fix nextjs hydration errors in explorer

--- mithril-explorer/src/app/page.js
+++ mithril-explorer/src/app/page.js
@@ -3,14 +3,18 @@
 import React, {useEffect} from 'react';
 import {useSelector} from "react-redux";
 import {usePathname, useRouter, useSearchParams} from "next/navigation";
+import dynamic from "next/dynamic";
 import {Col, Form, Row, Stack, Tab, Tabs} from "react-bootstrap";
-import AggregatorSetter from "../components/AggregatorSetter";
 import EpochSettings from "../components/EpochSettings";
-import IntervalSetter from "../components/IntervalSetter";
 import PendingCertificate from '../components/PendingCertificate';
 import SnapshotsList from '../components/Artifacts/SnapshotsList';
 import MithrilStakeDistributionsList from "../components/Artifacts/MithrilStakeDistributionsList";
 import {selectedAggregator as currentlySelectedAggregator} from "../store/settingsSlice";
+
+// Disable SSR for the following components since they use data from the store that are not
+// available server sides (because those data can be read from the local storage).
+const AggregatorSetter = dynamic(() => import('../components/AggregatorSetter'), { ssr: false })
+const IntervalSetter = dynamic(() => import('../components/IntervalSetter'), { ssr: false })
 
 export default function Explorer() {
   const router = useRouter();
@@ -22,7 +26,7 @@ export default function Explorer() {
     const params = new URLSearchParams(searchParams);
     params.set("aggregator", selectedAggregator);
     let new_path = pathname + "?" + params.toString();
-    
+
     router.prefetch(new_path);
     router.push(new_path);
   }, [router, searchParams, pathname, selectedAggregator]);
