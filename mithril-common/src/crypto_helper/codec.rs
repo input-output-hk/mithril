863dab030b3a6dd2ba4766826e9f5f580777312c
Damien LACHAUME / PALO-IT <dlachaume@palo-it.com>
2023-08-31 10:44:30 +0200

    Add `CodecError` common error

--- mithril-common/src/crypto_helper/codec.rs
+++ mithril-common/src/crypto_helper/codec.rs
@@ -3,29 +3,55 @@ use crate::entities::{HexEncodedKey, HexEncodedKeySlice};
 use hex::{FromHex, ToHex};
 use serde::de::DeserializeOwned;
 use serde::Serialize;
+use thiserror::Error;
+
+/// Error raised when the encoding or decoding fails
+#[derive(Error, Debug)]
+#[error("Codec error: {msg}")]
+pub struct CodecError {
+    msg: String,
+    #[source]
+    source: anyhow::Error,
+}
+
+impl CodecError {
+    /// [CodecError] factory.
+    pub fn new(msg: &str, source: anyhow::Error) -> Self {
+        Self {
+            msg: msg.to_string(),
+            source,
+        }
+    }
+}
 
 /// Encode key to hex helper
-pub fn key_encode_hex<T>(from: T) -> Result<HexEncodedKey, String>
+pub fn key_encode_hex<T>(from: T) -> Result<HexEncodedKey, CodecError>
 where
     T: Serialize,
 {
     Ok(serde_json::to_string(&from)
-        .map_err(|e| format!("can't convert to hex: {e}"))?
+        .map_err(|e| CodecError::new("Key encode hex: can not convert to hex", e.into()))?
         .encode_hex::<String>())
 }
 
 /// Decode key from hex helper
-pub fn key_decode_hex<T>(from: HexEncodedKeySlice) -> Result<T, String>
+pub fn key_decode_hex<T>(from: HexEncodedKeySlice) -> Result<T, CodecError>
 where
     T: DeserializeOwned,
 {
     let from_vec = Vec::from_hex(from).map_err(|e| {
-        format!("Key decode hex: can not turn hexadecimal value into bytes, error: {e}")
+        CodecError::new(
+            "Key decode hex: can not turn hexadecimal value into bytes",
+            e.into(),
+        )
     })?;
     serde_json::from_slice(from_vec.as_slice()).map_err(|e| {
-        format!(
-            "Key decode hex: can not deserialize to type '{}' from binary JSON: error {e}",
-            std::any::type_name::<T>()
+        CodecError::new(
+            &format!(
+                "Key decode hex: can not deserialize to type '{}' from binary JSON",
+                std::any::type_name::<T>()
+            ),
+            e.into(),
         )
     })
 }
