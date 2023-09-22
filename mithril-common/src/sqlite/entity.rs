7d1ae772f1cac463e8bd42559adade88d5744580
Grégoire HUBERT / PALO-IT <ghubert@palo-it.com>
2023-09-22 11:59:12 +0200

    refactor common sqlite errors

--- mithril-common/src/sqlite/entity.rs
+++ mithril-common/src/sqlite/entity.rs
@@ -13,10 +13,6 @@ pub enum HydrationError {
     /// data are missing
     #[error("some data are missing: {0}")]
     MissingData(String),
-
-    /// inconsistent data
-    #[error("data type inconsistency: {0}")]
-    InconsistentType(String),
 }
 
 /// How to hydrate an entity from a SQLite result row
