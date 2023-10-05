f8f6a51705d97c80a09e8d325e7522f47d937202
Damien LACHAUME / PALO-IT <dlachaume@palo-it.com>
2023-10-05 13:00:10 +0200

    Add CExplorer logo to pool ticker link

--- mithril-explorer/src/components/PoolTicker.js
+++ mithril-explorer/src/components/PoolTicker.js
@@ -2,6 +2,8 @@ import React, {useEffect, useState} from "react";
 import {useSelector} from "react-redux";
 import {getPool} from "../store/poolsSlice";
 import {getCExplorerUrlForPool} from "../utils";
+import Image from "next/image";
+import {OverlayTrigger, Tooltip} from "react-bootstrap";
 
 export default function PoolTicker({aggregator, partyId, ...props}) {
     const pool = useSelector((state) => getPool(state, aggregator, partyId));
@@ -16,8 +18,19 @@ export default function PoolTicker({aggregator, partyId, ...props}) {
     }, [pool.party_id, pool.network])
 
     return (url !== undefined)
-            ? <a href={url} target="_blank" className="link-dark link-underline-light">
-            <span>{pool.pool_ticker}</span>
-    </a>
+            ? <>
+            <a href={url} target="_blank" className="link-dark link-underline-light">
+                <OverlayTrigger overlay={<Tooltip>See in CExplorer</Tooltip>}>
+                    <span>
+                        <Image src="/explorer/cexplorer_logo.png"
+                               alt="CExplorer Logo"
+                               style={{verticalAlign:"text-top"}}
+                               width={20} height={20}/>
+                        <> </>
+                        {pool.pool_ticker}
+                    </span>
+                </OverlayTrigger>
+            </a>
+        </>
             : <span>{pool.pool_ticker}</span>;
 }
