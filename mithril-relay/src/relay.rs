df2cfe5762a174c25549c1f5c0a103f3fecb65c0
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2023-10-30 17:22:49 +0100

    Use a channel to publish HTTP messages

--- mithril-relay/src/relay.rs
+++ mithril-relay/src/relay.rs
@@ -1,7 +1,4 @@
-use std::{
-    net::SocketAddr,
-    sync::{Arc, Mutex},
-};
+use std::{net::SocketAddr, sync::mpsc::TryRecvError};
 
 use libp2p::Multiaddr;
 use mithril_common::{
@@ -10,34 +7,35 @@ use mithril_common::{
     StdResult,
 };
 use reqwest::StatusCode;
+use std::sync::mpsc;
 use warp::Filter;
 
 use crate::peer::{Peer, PeerEvent};
 
 pub struct Relay {
     server: TestHttpServer,
-    pub peer: Arc<Mutex<Peer>>,
+    pub peer: Peer,
+    pub message_rx: mpsc::Receiver<RegisterSignatureMessage>,
 }
 
 impl Relay {
     pub async fn start(topic_name: &str) -> StdResult<Self> {
-        let peer = Arc::new(Mutex::new(Peer::new(topic_name).start().await?));
-        let peer_reference = peer.clone();
+        let (tx, rx) = mpsc::channel::<RegisterSignatureMessage>();
+        let peer = Peer::new(topic_name).start().await?;
         let server = test_http_server(
             warp::path("register-signatures")
                 .and(warp::post())
                 .and(warp::body::json())
                 .map(move |signature_message: RegisterSignatureMessage| {
-                    let mut peer = peer.lock().unwrap();
-                    peer.publish(&signature_message).unwrap();
-
+                    tx.send(signature_message).unwrap();
                     warp::reply::with_status(warp::reply::reply(), StatusCode::CREATED)
                 }),
         );
 
         Ok(Self {
             server,
-            peer: peer_reference,
+            peer,
+            message_rx: rx,
         })
     }
 
@@ -46,12 +44,27 @@ impl Relay {
     }
 
     pub fn peer_address(&self) -> Option<Multiaddr> {
-        let peer = self.peer.lock().unwrap();
-        peer.addr.to_owned()
+        self.peer.addr.to_owned()
     }
 
     pub async fn tick_peer(&mut self) -> StdResult<Option<PeerEvent>> {
-        let mut peer = self.peer.lock().unwrap();
-        peer.tick_swarm().await
+        match self.message_rx.try_recv() {
+            Ok(signature_message) => {
+                println!(" ");
+                println!("************************************************");
+                println!("Relay publish signature: {signature_message:#?}");
+                println!("************************************************");
+                println!(" ");
+                self.peer.publish(&signature_message)?;
+            }
+            Err(TryRecvError::Empty) => {
+                //println!("No message available");
+            }
+            Err(error) => {
+                panic!("Queue disconnected: {}", error);
+            }
+        };
+
+        self.peer.tick_swarm().await
     }
 }
