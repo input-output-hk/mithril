c79fe9632444d50f41e373b7865a4334b23b43a3
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2023-10-31 14:50:06 +0100

    Enchanced cli for relay/client nodes

--- mithril-relay/src/main.rs
+++ mithril-relay/src/main.rs
@@ -1,43 +1,71 @@
+use clap::Parser;
 use libp2p::Multiaddr;
+use mithril_common::StdResult;
 use mithril_relay::{client::P2PClient, relay::Relay};
 
-#[tokio::main]
-async fn main() {
-    let args: Vec<_> = std::env::args().collect();
-    let topic_name = "mithril/signatures";
+#[derive(Parser, Debug, PartialEq, Clone)]
+pub struct Config {
+    /// Node type (relay or client)
+    #[clap(long)]
+    node_type: String,
 
-    let node_type = &args[1];
+    /// Pubsub topic name
+    #[clap(long, default_value = "mithril/signatures")]
+    topic_name: String,
+
+    /// Quorum parameter
+    #[clap(long)]
+    dial_to: Option<Multiaddr>,
+}
+
+#[tokio::main]
+async fn main() -> StdResult<()> {
+    let config = Config::parse();
+    let topic_name = config.topic_name;
+    let node_type = config.node_type;
+    let dial_to = config.dial_to;
     match node_type.as_ref() {
         "relay" => {
             println!("Start relay");
-            let mut relay = Relay::start(topic_name).await.expect("Relay start failed");
+            let mut relay = Relay::start(&topic_name).await?;
             let relay_address = relay.address();
             println!(">> Relay_address is '{relay_address:?}'");
+            if let Some(dial_to_address) = dial_to {
+                relay.peer.dial(dial_to_address.clone())?;
+            }
             loop {
-                relay.tick().await.unwrap();
+                if let Err(err) = relay.tick().await {
+                    println!("Tick error: {err:?}");
+                }
             }
         }
         "client" => {
-            let dial_to_address: Multiaddr = args[2].parse().unwrap();
             println!("Start client");
-            let mut p2p_client = P2PClient::new(topic_name)
+            let mut p2p_client = P2PClient::new(&topic_name)
                 .start()
                 .await
                 .expect("P2P client start failed");
-            p2p_client
-                .peer
-                .dial(dial_to_address.clone())
-                .expect("P2P client dial to the relay should not fail");
+            if let Some(dial_to_address) = dial_to {
+                p2p_client.peer.dial(dial_to_address.clone())?;
+            }
             loop {
-                if let Some(p2p_client_event) = p2p_client.tick().await.unwrap() {
-                    if let Ok(Some(signature_message_received)) =
-                        p2p_client.consume(p2p_client_event)
-                    {
-                        println!(" ");
-                        println!("************************************************");
-                        println!("P2P Client received signature: {signature_message_received:#?}");
-                        println!("************************************************");
-                        println!(" ");
+                match p2p_client.tick().await {
+                    Ok(Some(p2p_client_event)) => {
+                        if let Ok(Some(signature_message_received)) =
+                            p2p_client.consume(p2p_client_event)
+                        {
+                            println!(" ");
+                            println!("************************************************");
+                            println!(
+                                "P2P Client received signature: {signature_message_received:#?}"
+                            );
+                            println!("************************************************");
+                            println!(" ");
+                        }
+                    }
+                    Ok(None) => {}
+                    Err(err) => {
+                        println!("Tick error: {err:?}");
                     }
                 }
             }
