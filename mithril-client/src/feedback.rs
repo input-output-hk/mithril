0fb89b33561668814f166c4532fe1916b214ac6b
DJO <790521+Alenar@users.noreply.github.com>
2023-11-06 18:19:31 +0100

    Client `FeedbackSender` can now hold multiple receivers + Add SlogFeedbackReceiver

--- mithril-client/src/feedback.rs
+++ mithril-client/src/feedback.rs
@@ -1,4 +1,5 @@
 use async_trait::async_trait;
+use slog::{info, Logger};
 use std::sync::{Arc, RwLock};
 
 /// Event that can be reported by a [FeedbackReceiver].
@@ -14,20 +15,26 @@ pub enum MithrilEvent {
 }
 
 /// A sender of [MithrilEvent].
+///
+/// It uses Arc internally so it can be cloned at will.
 #[derive(Clone)]
 pub struct FeedbackSender {
-    receiver: Arc<dyn FeedbackReceiver>,
+    receivers: Vec<Arc<dyn FeedbackReceiver>>,
 }
 
 impl FeedbackSender {
     /// Create a new [FeedbackSender].
-    pub fn new(receiver: Arc<dyn FeedbackReceiver>) -> FeedbackSender {
-        Self { receiver }
+    pub fn new(receivers: &[Arc<dyn FeedbackReceiver>]) -> FeedbackSender {
+        Self {
+            receivers: receivers.to_vec(),
+        }
     }
 
     /// Send the given event to the known receiver.
     pub async fn send_event(&self, event: MithrilEvent) {
-        self.receiver.handle_event(event).await;
+        for receiver in &self.receivers {
+            receiver.handle_event(event.clone()).await;
+        }
     }
 }
 
@@ -36,6 +43,32 @@ impl FeedbackSender {
 pub trait FeedbackReceiver: Sync + Send {
     /// Callback called by a [FeedbackSender] when it need to send an [event][MithrilEvent].
     async fn handle_event(&self, event: MithrilEvent);
+}
+
+/// A [FeedbackReceiver] that write the event it receives in a [slog logger][Logger].
+pub struct SlogFeedbackReceiver {
+    logger: Logger,
+}
+
+impl SlogFeedbackReceiver {
+    /// Create a new [SlogFeedbackReceiver].
+    pub fn new(logger: Logger) -> SlogFeedbackReceiver {
+        Self { logger }
+    }
+}
+
+#[async_trait]
+impl FeedbackReceiver for SlogFeedbackReceiver {
+    async fn handle_event(&self, event: MithrilEvent) {
+        match event {
+            MithrilEvent::SnapshotDownloadStarted { size } => {
+                info!(self.logger, "Snapshot download started, size: {size}");
+            }
+            MithrilEvent::SnapshotDownloadComplete => {
+                info!(self.logger, "Snapshot download completed");
+            }
+        };
+    }
 }
 
 /// A [FeedbackReceiver] that stack the events that it receives in a vec.
@@ -86,7 +119,7 @@ mod tests {
     #[tokio::test]
     async fn send_event_same_thread() {
         let receiver = Arc::new(StackFeedbackReceiver::new());
-        let sender = FeedbackSender::new(receiver.clone());
+        let sender = FeedbackSender::new(&[receiver.clone()]);
 
         sender
             .send_event(SnapshotDownloadStarted { size: 10 })
@@ -105,7 +138,10 @@ mod tests {
     #[tokio::test]
     async fn send_event_multiple_thread() {
         let receiver = Arc::new(StackFeedbackReceiver::new());
-        let sender = FeedbackSender::new(receiver.clone());
+        let sender = FeedbackSender::new(&[
+            receiver.clone(),
+            Arc::new(SlogFeedbackReceiver::new(crate::test_utils::test_logger())),
+        ]);
         let sender2 = sender.clone();
         let mut join_set = JoinSet::new();
 
@@ -141,6 +177,82 @@ mod tests {
                 SnapshotDownloadStarted { size: 3 },
                 SnapshotDownloadComplete,
                 SnapshotDownloadStarted { size: 2 },
+                SnapshotDownloadComplete,
+            ]
+        );
+    }
+
+    #[tokio::test]
+    async fn send_event_in_one_thread_and_receive_in_another_thread() {
+        let receiver = Arc::new(StackFeedbackReceiver::new());
+        let receiver2 = receiver.clone();
+        let sender = FeedbackSender::new(&[receiver.clone()]);
+        let mut join_set = JoinSet::new();
+
+        join_set.spawn(async move {
+            // Step 1:
+            sender.send_event(SnapshotDownloadStarted { size: 1 }).await;
+            tokio::time::sleep(Duration::from_millis(10)).await;
+
+            // Step 2:
+            sender.send_event(SnapshotDownloadComplete).await;
+            sender.send_event(SnapshotDownloadStarted { size: 2 }).await;
+            tokio::time::sleep(Duration::from_millis(10)).await;
+
+            // Step 3:
+            sender.send_event(SnapshotDownloadComplete).await;
+            sender.send_event(SnapshotDownloadStarted { size: 3 }).await;
+            tokio::time::sleep(Duration::from_millis(10)).await;
+
+            // Final step:
+            sender.send_event(SnapshotDownloadComplete).await;
+        });
+
+        join_set.spawn(async move {
+            // Little sleep to wait for step 1 completion
+            tokio::time::sleep(Duration::from_millis(3)).await;
+            assert_eq!(
+                receiver2.stacked_events(),
+                vec![SnapshotDownloadStarted { size: 1 },]
+            );
+
+            // Wait for step 2 completion
+            tokio::time::sleep(Duration::from_millis(10)).await;
+            assert_eq!(
+                receiver2.stacked_events(),
+                vec![
+                    SnapshotDownloadStarted { size: 1 },
+                    SnapshotDownloadComplete,
+                    SnapshotDownloadStarted { size: 2 },
+                ]
+            );
+
+            // Wait for step 3 completion
+            tokio::time::sleep(Duration::from_millis(10)).await;
+            assert_eq!(
+                receiver2.stacked_events(),
+                vec![
+                    SnapshotDownloadStarted { size: 1 },
+                    SnapshotDownloadComplete,
+                    SnapshotDownloadStarted { size: 2 },
+                    SnapshotDownloadComplete,
+                    SnapshotDownloadStarted { size: 3 },
+                ]
+            );
+        });
+
+        while let Some(res) = join_set.join_next().await {
+            res.unwrap();
+        }
+
+        assert_eq!(
+            receiver.stacked_events(),
+            vec![
+                SnapshotDownloadStarted { size: 1 },
+                SnapshotDownloadComplete,
+                SnapshotDownloadStarted { size: 2 },
+                SnapshotDownloadComplete,
+                SnapshotDownloadStarted { size: 3 },
                 SnapshotDownloadComplete,
             ]
         );
