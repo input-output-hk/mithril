05c8e32e2990f8f11e6af31ffa1af83dcc348d44
Alexsander Falcucci <alex.falcucci@gmail.com>
2023-11-15 23:09:33 +0100

    remove useless TODO

--- mithril-aggregator/src/dependency_injection/builder.rs
+++ mithril-aggregator/src/dependency_injection/builder.rs
@@ -477,7 +477,6 @@ impl DependenciesBuilder {
         Ok(self.chain_observer.as_ref().cloned().unwrap())
     }
 
-    // TODO: this can be removed
     async fn build_cardano_cli_runner(&mut self) -> Result<Box<CardanoCliRunner>> {
         let cli_runner = CardanoCliRunner::new(
             self.configuration.cardano_cli_path.clone(),
