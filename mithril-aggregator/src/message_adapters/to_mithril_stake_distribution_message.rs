b8ee2cdee46826f0ebdb4ecd7634c43e0cc58bda
Damien LACHAUME / PALO-IT <dlachaume@palo-it.com>
2023-12-04 18:30:48 +0100

    Implement `dummy` for `SignedEntity`

--- mithril-aggregator/src/message_adapters/to_mithril_stake_distribution_message.rs
+++ mithril-aggregator/src/message_adapters/to_mithril_stake_distribution_message.rs
@@ -26,39 +26,22 @@ impl ToMessageAdapter<SignedEntity<MithrilStakeDistribution>, MithrilStakeDistri
 
 #[cfg(test)]
 mod tests {
-    use chrono::{DateTime, Utc};
-    use mithril_common::{
-        entities::{Epoch, SignedEntityType},
-        test_utils::fake_data,
-    };
-
     use super::*;
 
     #[test]
     fn adapt_ok() {
-        let mithril_stake_distribution = MithrilStakeDistribution {
-            epoch: Epoch(1),
-            signers_with_stake: fake_data::signers_with_stakes(2),
-            hash: "hash-123".to_string(),
-            protocol_parameters: fake_data::protocol_parameters(),
-        };
-        let signed_entity = SignedEntity {
-            signed_entity_id: "id-1234".to_string(),
-            signed_entity_type: SignedEntityType::MithrilStakeDistribution(Epoch(1)),
-            certificate_id: "cert-hash-123".to_string(),
-            artifact: mithril_stake_distribution,
-            created_at: DateTime::<Utc>::default(),
-        };
+        let signed_entity = SignedEntity::<MithrilStakeDistribution>::dummy();
         let mithril_stake_distribution_message_expected = MithrilStakeDistributionMessage {
-            epoch: Epoch(1),
+            epoch: signed_entity.artifact.epoch,
             signers_with_stake: SignerWithStakeMessagePart::from_signers(
-                fake_data::signers_with_stakes(2),
+                signed_entity.artifact.signers_with_stake.clone(),
             ),
-            hash: "hash-123".to_string(),
-            certificate_hash: "cert-hash-123".to_string(),
-            created_at: DateTime::<Utc>::default(),
-            protocol_parameters: fake_data::protocol_parameters(),
+            hash: signed_entity.artifact.hash.clone(),
+            certificate_hash: signed_entity.certificate_id.clone(),
+            created_at: signed_entity.created_at,
+            protocol_parameters: signed_entity.artifact.protocol_parameters.clone(),
         };
+
         let mithril_stake_distribution_message =
             ToMithrilStakeDistributionMessageAdapter::adapt(signed_entity);
 
--- mithril-aggregator/src/message_adapters/to_snapshot_message.rs
+++ mithril-aggregator/src/message_adapters/to_snapshot_message.rs
@@ -22,27 +22,15 @@ impl ToMessageAdapter<SignedEntity<Snapshot>, SnapshotMessage> for ToSnapshotMes
 
 #[cfg(test)]
 mod tests {
-    use chrono::{DateTime, Utc};
-    use mithril_common::{
-        entities::{Beacon, SignedEntityType},
-        test_utils::fake_data,
-    };
-
     use super::*;
 
     #[test]
     fn adapt_ok() {
-        let mut snapshot = fake_data::snapshots(1)[0].to_owned();
-        snapshot.digest = "digest123".to_string();
-        let signed_entity = SignedEntity {
-            signed_entity_id: "signed-entity-id-123".to_string(),
-            signed_entity_type: SignedEntityType::CardanoImmutableFilesFull(Beacon::default()),
-            certificate_id: "certificate-hash-123".to_string(),
-            artifact: snapshot,
-            created_at: DateTime::<Utc>::default(),
-        };
+        let signed_entity = SignedEntity::<Snapshot>::dummy();
+        let expected_digest = signed_entity.artifact.digest.clone().to_string();
+
         let snapshot_message = ToSnapshotMessageAdapter::adapt(signed_entity);
 
-        assert_eq!("digest123".to_string(), snapshot_message.digest);
+        assert_eq!(expected_digest, snapshot_message.digest);
     }
 }
