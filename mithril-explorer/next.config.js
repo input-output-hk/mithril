9e6e9d6c1d80442d06f50cbb9dbd7e628b8ca18a
Damien LACHAUME / PALO-IT <dlachaume@palo-it.com>
2023-12-22 16:31:47 +0100

    Fix build errors error: `experiments.layers` must be enabled
    error: webpack import WASM file fail
    This seems to be a known behaviour.

--- mithril-explorer/next.config.js
+++ mithril-explorer/next.config.js
@@ -8,7 +8,41 @@ const nextConfig = {
     unoptimized: true,
   },
   webpack: function (config, { isServer }) {
-    config.experiments = { asyncWebAssembly: true };
+    config.experiments = { layers: true, asyncWebAssembly: true };
+    
+    const { join } = require('path');
+    const { access, symlink } = require('fs/promises');
+    config.plugins.push(
+      new (class {
+        apply(compiler) {
+          compiler.hooks.afterEmit.tapPromise(
+            'SymlinkWebpackPlugin',
+            async (compiler) => {
+              if (isServer) {
+                const from = join(compiler.options.output.path, '../static');
+                const to = join(compiler.options.output.path, 'static');
+    
+                try {
+                  await access(from);
+                  // console.log(`${from} already exists`);
+                  return;
+                } catch (error) {
+                  if (error.code === 'ENOENT') {
+                    // No link exists
+                  } else {
+                    throw error;
+                  }
+                }
+    
+                await symlink(to, from, 'junction');
+                console.log(`created symlink ${from} -> ${to}`);
+              }
+            },
+          );
+        }
+      })(),
+    );
+
     return config;
   }
 };
