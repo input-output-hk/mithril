27d036b1edbd9c9fe44805ac991ca8fc38921777
sfauvel <sfauvel@gmail.com>
2024-01-08 13:45:21 +0100

    Output file with program name by default

--- mithril-common/src/generate_doc/mod.rs
+++ mithril-common/src/generate_doc/mod.rs
@@ -8,22 +8,30 @@ use clap::{Parser, Command};
 
 use crate::StdResult;
 
+const DEFAULT_OUTPUT_FILE_TEMPLATE: &str = "[PROGRAM NAME]-command-line.md";
+
 /// Generate documentation
 #[derive(Parser, Debug, Clone)]
 pub struct GenerateDocCommands {
     /// Generated documentation file 
-    #[clap(long, default_value = "generated_doc.md")]
+    #[clap(long, default_value = DEFAULT_OUTPUT_FILE_TEMPLATE)]
     output: String,
 }
 impl GenerateDocCommands {
     /// Generate the command line documentation.
     pub async fn execute(&self, cmd_to_document: &mut Command) -> StdResult<()> {
+        let cmd_name = cmd_to_document.get_name().to_owned();
         let doc = markdown_formatter::doc_markdown(cmd_to_document);
         
-        let mut buffer: File = File::create(&self.output)?;
+        let output = if self.output.as_str() == DEFAULT_OUTPUT_FILE_TEMPLATE {
+            format!("{}-command-line.md", cmd_name)
+        } else {
+            self.output.clone()
+        };
+        let mut buffer: File = File::create(&output)?;
         buffer.write(b"Generated doc\n\n")?;
         buffer.write(doc.as_bytes())?;
-        println!("Documentation generated in file `{}`", &self.output);
+        println!("Documentation generated in file `{}`", &output);
         Ok(())
     }
 }
