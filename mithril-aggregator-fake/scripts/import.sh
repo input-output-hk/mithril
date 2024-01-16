79b1478f395c1366d71d60f33fdf31fe0df2e470
Grégoire HUBERT / PALO-IT <ghubert@palo-it.com>
2024-01-16 11:58:14 +0100

    import complete certificate chain

--- mithril-aggregator-fake/scripts/import.sh
+++ mithril-aggregator-fake/scripts/import.sh
@@ -26,7 +26,7 @@ download_artifacts() {
     local -r url=${1:?"No URL given to download_list function."};
     local -r artifact=${2:?"No artifact type given to download_list function."};
     local -r json_field=${3:?"No JSON field given to read from artifact list."};
-    local -i nb=1
+    local -i nb=0
 
     echo -n "Downloading ${artifact} data: "
     tput sc;
@@ -38,25 +38,27 @@ download_artifacts() {
         let "nb=nb+1"
         echo -n "$nb   "
     done
-    echo
+    echo " ✅";
 }
 
 download_certificate_chain() {
-    echo -n "Downloading certificate chain: "
-    tput sc;
     local parent_hash=$(jq -r .[0].hash $DATA_DIR/certificates.json);
     local certificate_hash;
+    local -i nb=0
+
+    echo -n "Downloading certificate chain: "
+    tput sc;
 
     until [ -z "$parent_hash" ];
     do
+        tput rc;
         certificate_hash=$parent_hash;
         wget -O $DATA_DIR/certificate-${certificate_hash}.json --quiet "${BASE_URL}/certificate/${certificate_hash}";
         parent_hash=$(jq -r .previous_hash $DATA_DIR/certificate-${certificate_hash}.json);
-        echo -n "${parent_hash} ";
-        tput rc;
+        let "nb=nb+1"
+        echo -n "$nb   "
     done
-
-    echo "ok                                                                ";
+    echo " ✅";
 }
 
 # MAIN execution
@@ -71,12 +73,13 @@ export DATA_DIR URL;
 
 clean_directory;
 
-download_list $BASE_URL/certificates certificates
+download_list "$BASE_URL/certificates" "certificates"
+download_artifacts "$BASE_URL/certificate" "certificate" "hash"
 download_certificate_chain
 
-download_list $BASE_URL/artifact/snapshots snapshots
-download_artifacts $BASE_URL/artifact/snapshot snapshot digest
+download_list "$BASE_URL/artifact/snapshots" "snapshots"
+download_artifacts "$BASE_URL/artifact/snapshot" "snapshot" "digest"
 
-download_list $BASE_URL/artifact/mithril-stake-distributions  mithril-stake-distributions
-download_artifacts $BASE_URL/artifact/mithril-stake-distribution mithril-stake-distribution "hash"
+download_list "$BASE_URL/artifact/mithril-stake-distributions"  "mithril-stake-distributions"
+download_artifacts "$BASE_URL/artifact/mithril-stake-distribution" "mithril-stake-distribution" "hash"
 
