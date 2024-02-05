ffbc4d17a3886866a33adc3582dad6d80867559e
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2024-02-05 17:45:23 +0100

    chore: support 'testing-sanchonet' network in explorer

--- mithril-explorer/src/aggregators-list.js
+++ mithril-explorer/src/aggregators-list.js
@@ -3,6 +3,7 @@ export const defaultAggregators = [
   "https://aggregator.release-preprod.api.mithril.network/aggregator",
   "https://aggregator.pre-release-preview.api.mithril.network/aggregator",
   "https://aggregator.testing-preview.api.mithril.network/aggregator",
+  "https://aggregator.testing-sanchonet.api.mithril.network/aggregator",
   "http://localhost:8080/aggregator",
 ];
 
