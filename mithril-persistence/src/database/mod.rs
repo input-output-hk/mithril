d947cbcaa796525ae6b89842725b9b6ff002bc1d
DJO <790521+Alenar@users.noreply.github.com>
2024-02-08 18:20:11 +0100

    Move the last db related code to the persistence crate

--- mithril-persistence/src/database/mod.rs
+++ mithril-persistence/src/database/mod.rs
@@ -2,10 +2,12 @@
 //! This module contains providers and entities shared between all application types.
 
 mod db_version;
+mod signed_entity_hydrator;
 mod version_checker;
 
 /// Database version.
 pub type DbVersion = i64;
 
 pub use db_version::*;
+pub use signed_entity_hydrator::SignedEntityTypeHydrator;
 pub use version_checker::{DatabaseVersionChecker, SqlMigration};
