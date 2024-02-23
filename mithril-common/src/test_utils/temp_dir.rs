2ff088ecc43c67c45da226beb8c725a90a477ec0
DJO <790521+Alenar@users.noreply.github.com>
2024-02-23 15:24:12 +0100

    TempDir can generate distinct path for tests with same name but not in the same module
    This was already the case for 'non-short' paths, for short path we
    append a sha of the 'module+name' to discrimate.

--- mithril-common/src/test_utils/temp_dir.rs
+++ mithril-common/src/test_utils/temp_dir.rs
@@ -1,3 +1,4 @@
+use sha2::{Digest, Sha256};
 use std::path::{Path, PathBuf};
 
 /// A builder of temp directory for tests purpose.
@@ -6,6 +7,7 @@ pub struct TempDir {
     module_name: String,
     name: String,
     enable_short_path: bool,
+    short_path_max_len: usize,
 }
 
 const TEMP_DIR_ROOT_NAME: &str = "mithril_test";
@@ -17,6 +19,8 @@ impl TempDir {
             module_name: module.into(),
             name: name.into(),
             enable_short_path: false,
+            // 90 to have some room for the folder content (in case of restrained length like socket path)
+            short_path_max_len: 90,
         }
     }
 
@@ -30,15 +34,30 @@ impl TempDir {
 
     /// Generate the path of the temp directory (no IO operation will be executed)
     pub fn build_path(&self) -> PathBuf {
+        const SHA_LENGTH: usize = 10;
         let base_dir = std::env::temp_dir().join(TEMP_DIR_ROOT_NAME);
 
+        // Short path only:
+        // Combined max len should be lower than `self.short_path_max_len` to have some rooms for
+        // the folder content.
+        // MacOS temp folder are not in the `/tmp` folder but in a dynamic path adding 45 chars.
+        // ie: /var/folders/_k/7j0m5c_n4g94vgx9gxknp4tm0000gn/T/
         if self.enable_short_path {
-            // Combined max len should be lower than 90 to have some rooms for the folder content
-            // macOS temp folder are not in the `/tmp` folder but in a dynamic path adding 45 chars
-            // ie: /var/folders/_k/7j0m5c_n4g94vgx9gxknp4tm0000gn/T/
-            let max_len = self.name.len().min(89 - base_dir.to_string_lossy().len());
+            // In order to discriminate two tests with the same name but within different modules
+            // we append the short sha of the module+name to the path.
+            let mut hasher = Sha256::new();
+            hasher.update(&self.module_name);
+            hasher.update(&self.name);
+            let sha = hex::encode(hasher.finalize());
+            let short_sha = &sha[0..SHA_LENGTH];
 
-            base_dir.join(&self.name[0..max_len])
+            // `-2` since when joining a path this adds a `/` and we also add a `_` to join the sha
+            let max_path_len =
+                self.short_path_max_len - SHA_LENGTH - 2 - base_dir.to_string_lossy().len();
+
+            let max_len = self.name.len().min(max_path_len);
+
+            base_dir.join([&self.name[0..max_len], "_", short_sha].concat())
         } else {
             base_dir.join(&self.module_name).join(&self.name)
         }
@@ -92,13 +111,13 @@ mod tests {
 
     #[test]
     fn non_short_path_are_in_a_mithril_test_slash_module_folder_structure() {
-        let path = TempDir::new("mod", "name").build();
+        let path = TempDir::new("temp_dir", "basic").build();
 
         assert_eq!(
             Some(
                 std::env::temp_dir()
                     .join(TEMP_DIR_ROOT_NAME)
-                    .join("mod")
+                    .join("temp_dir")
                     .as_path()
             ),
             path.parent()
@@ -108,7 +127,9 @@ mod tests {
 
     #[test]
     fn short_path_are_in_a_mithril_test_folder_structure() {
-        let path = TempDir::new("mod", "name").generate_shorter_path().build();
+        let path = TempDir::new("temp_dir", "basic_short_path")
+            .generate_shorter_path()
+            .build();
 
         assert_eq!(
             Some(std::env::temp_dir().join(TEMP_DIR_ROOT_NAME).as_path()),
@@ -153,5 +174,25 @@ mod tests {
             path: `{}`",
             path.display()
         );
+    }
+
+    #[test]
+    fn same_name_but_two_different_module_generate_different_path() {
+        let path1 = TempDir::new("module_a", "test").build_path();
+        let path2 = TempDir::new("module_b", "test").build_path();
+
+        assert_ne!(path1, path2);
+    }
+
+    #[test]
+    fn same_name_but_two_different_module_generate_different_path_even_with_short_path_enabled() {
+        let path1 = TempDir::new("module_a", "test")
+            .generate_shorter_path()
+            .build_path();
+        let path2 = TempDir::new("module_b", "test")
+            .generate_shorter_path()
+            .build_path();
+
+        assert_ne!(path1, path2);
     }
 }
