8fecceee3a257fe0c3c19d9529633066b8aee18f
sfauvel <sfauvel@gmail.com>
2024-02-29 12:19:14 +0100

    Fix tests on certificate_routes

--- mithril-aggregator/src/http_server/routes/certificate_routes.rs
+++ mithril-aggregator/src/http_server/routes/certificate_routes.rs
@@ -121,15 +121,108 @@ mod handlers {
 #[cfg(test)]
 mod tests {
     use anyhow::anyhow;
-    use mithril_common::test_utils::{apispec::APISpec, fake_data};
+    use async_trait::async_trait;
+    use mithril_common::{
+        entities::CertificatePending,
+        test_utils::{apispec::APISpec, fake_data},
+    };
+    use mithril_persistence::store::adapter::AdapterError;
+    use mithril_persistence::store::adapter::StoreAdapter;
     use serde_json::Value::Null;
-    use warp::{http::Method, test::request};
+    use warp::{
+        http::{Method, StatusCode},
+        test::request,
+    };
 
     use crate::{
-        http_server::SERVER_BASE_PATH, initialize_dependencies, services::MockCertifierService,
+        http_server::SERVER_BASE_PATH, initialize_dependencies, services::MockMessageService,
+        CertificatePendingStore,
     };
 
     use super::*;
+
+    ///////////////
+
+    pub struct MockStoreAdapter {}
+
+    #[async_trait]
+    impl StoreAdapter for MockStoreAdapter {
+        type Key = String;
+        type Record = CertificatePending;
+
+        async fn store_record(
+            &mut self,
+            _key: &Self::Key,
+            _record: &Self::Record,
+        ) -> Result<(), AdapterError> {
+            Ok(())
+        }
+
+        async fn get_record(&self, _key: &Self::Key) -> Result<Option<Self::Record>, AdapterError> {
+            Err(AdapterError::GeneralError(anyhow!("Ca marche pô")))
+        }
+
+        async fn record_exists(&self, _key: &Self::Key) -> Result<bool, AdapterError> {
+            Ok(true)
+        }
+
+        async fn get_last_n_records(
+            &self,
+            _how_many: usize,
+        ) -> Result<Vec<(Self::Key, Self::Record)>, AdapterError> {
+            Ok(Vec::new())
+        }
+
+        async fn remove(&mut self, _key: &Self::Key) -> Result<Option<Self::Record>, AdapterError> {
+            Ok(None)
+        }
+
+        async fn get_iter(
+            &self,
+        ) -> Result<Box<dyn Iterator<Item = Self::Record> + '_>, AdapterError> {
+            Err(AdapterError::GeneralError(anyhow!("")))
+        }
+    }
+
+    //////////////
+    // use mockall::mock;
+
+    // mock! {
+    //     pub StoreAdapterTotoImpl<K: 'static, R: 'static> { }
+
+    //     #[async_trait]
+    //     impl<K, R> StoreAdapter for StoreAdapterTotoImpl<K, R> where K: Sync + Send , R: Sync + Send {
+    //         type Key = K;
+    //         type Record = R;
+
+    //         /// Store the given `record`.
+    //         async fn store_record(
+    //             &mut self,
+    //             key: &Self::Key,
+    //             record: &Self::Record,
+    //         ) -> Result<(), AdapterError>;
+
+    //         /// Get the record stored using the given `key`.
+    //         async fn get_record(&self, key: &Self::Key) -> Result<Option<Self::Record>, AdapterError>;
+
+    //         /// Check if a record exist for the given `key`.
+    //         async fn record_exists(&self, key: &Self::Key) -> Result<bool, AdapterError>;
+
+    //         /// Get the last `n` records in the store
+    //         async fn get_last_n_records(
+    //             &self,
+    //             how_many: usize,
+    //         ) -> Result<Vec<(Self::Key, Self::Record)>, AdapterError>;
+
+    //         /// remove values from store
+    //         ///
+    //         /// if the value exists it is returned by the adapter otherwise None is returned
+    //         async fn remove(&mut self, key: &Self::Key) -> Result<Option<Self::Record>, AdapterError>;
+
+    //         /// Get an iterator over the stored values, from the latest to the oldest.
+    //         async fn get_iter(&self) -> Result<Box<dyn Iterator<Item = Self::Record> + '_>, AdapterError>;
+    //     }
+    // }
 
     fn setup_router(
         dependency_manager: Arc<DependencyContainer>,
@@ -145,7 +238,43 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn test_certificate_pending_get_ok() {
+    async fn test_certificate_pending_with_content_get_ok_200() -> Result<(), String> {
+        let method = Method::GET.as_str();
+        let path = "/certificate-pending";
+        let dependency_manager = initialize_dependencies().await;
+        let certificate_pending = {
+            let mut signers = fake_data::signers(3);
+            signers[0].party_id = "1".to_string();
+            CertificatePending {
+                signers,
+                ..fake_data::certificate_pending()
+            }
+        };
+        dependency_manager
+            .certificate_pending_store
+            .save(certificate_pending)
+            .await
+            .unwrap();
+
+        let response = request()
+            .method(method)
+            .path(&format!("/{SERVER_BASE_PATH}{path}"))
+            .reply(&setup_router(Arc::new(dependency_manager)))
+            .await;
+
+        APISpec::verify_conformity_with_status(
+            APISpec::get_all_spec_files(),
+            method,
+            path,
+            "application/json",
+            &Null,
+            &response,
+            &StatusCode::OK,
+        )
+    }
+
+    #[tokio::test]
+    async fn test_certificate_pending_without_content_get_ok_204() -> Result<(), String> {
         let method = Method::GET.as_str();
         let path = "/certificate-pending";
         let dependency_manager = initialize_dependencies().await;
@@ -156,22 +285,26 @@ mod tests {
             .reply(&setup_router(Arc::new(dependency_manager)))
             .await;
 
-        APISpec::verify_conformity(
+        APISpec::verify_conformity_with_status(
             APISpec::get_all_spec_files(),
             method,
             path,
             "application/json",
             &Null,
             &response,
-        );
+            &StatusCode::NO_CONTENT,
+        )
     }
 
     #[tokio::test]
-    async fn test_certificate_pending_get_ok_204() {
-        let dependency_manager = initialize_dependencies().await;
-
+    async fn test_certificate_pending_get_ko_500() -> Result<(), String> {
         let method = Method::GET.as_str();
         let path = "/certificate-pending";
+        let mut dependency_manager = initialize_dependencies().await;
+
+        let adapter = MockStoreAdapter {};
+        let certificate_pending_store_store = CertificatePendingStore::new(Box::new(adapter));
+        dependency_manager.certificate_pending_store = Arc::new(certificate_pending_store_store);
 
         let response = request()
             .method(method)
@@ -179,40 +312,19 @@ mod tests {
             .reply(&setup_router(Arc::new(dependency_manager)))
             .await;
 
-        APISpec::verify_conformity(
+        APISpec::verify_conformity_with_status(
             APISpec::get_all_spec_files(),
             method,
             path,
             "application/json",
             &Null,
             &response,
-        );
+            &StatusCode::INTERNAL_SERVER_ERROR,
+        )
     }
 
     #[tokio::test]
-    async fn test_certificate_pending_get_ko_500() {
-        let method = Method::GET.as_str();
-        let path = "/certificate-pending";
-        let dependency_manager = initialize_dependencies().await;
-
-        let response = request()
-            .method(method)
-            .path(&format!("/{SERVER_BASE_PATH}{path}"))
-            .reply(&setup_router(Arc::new(dependency_manager)))
-            .await;
-
-        APISpec::verify_conformity(
-            APISpec::get_all_spec_files(),
-            method,
-            path,
-            "application/json",
-            &Null,
-            &response,
-        );
-    }
-
-    #[tokio::test]
-    async fn test_certificate_certificates_get_ok() {
+    async fn test_certificate_certificates_get_ok() -> Result<(), String> {
         let dependency_manager = initialize_dependencies().await;
         dependency_manager
             .certificate_repository
@@ -229,24 +341,26 @@ mod tests {
             .reply(&setup_router(Arc::new(dependency_manager)))
             .await;
 
-        APISpec::verify_conformity(
+        APISpec::verify_conformity_with_status(
             APISpec::get_all_spec_files(),
             method,
             path,
             "application/json",
             &Null,
             &response,
-        );
+            &StatusCode::OK,
+        )
     }
 
     #[tokio::test]
-    async fn test_certificate_certificates_get_ko() {
+    async fn test_certificate_when_error_retrieving_certificates_returns_ko_500(
+    ) -> Result<(), String> {
         let mut dependency_manager = initialize_dependencies().await;
-        let mut certifier_service = MockCertifierService::new();
-        certifier_service
-            .expect_get_latest_certificates()
+        let mut message_service = MockMessageService::new();
+        message_service
+            .expect_get_certificate_list_message()
             .returning(|_| Err(anyhow!("an error")));
-        dependency_manager.certifier_service = Arc::new(certifier_service);
+        dependency_manager.message_service = Arc::new(message_service);
 
         let method = Method::GET.as_str();
         let path = "/certificates";
@@ -257,18 +371,19 @@ mod tests {
             .reply(&setup_router(Arc::new(dependency_manager)))
             .await;
 
-        APISpec::verify_conformity(
+        APISpec::verify_conformity_with_status(
             APISpec::get_all_spec_files(),
             method,
             path,
             "application/json",
             &Null,
             &response,
-        );
+            &StatusCode::INTERNAL_SERVER_ERROR,
+        )
     }
 
     #[tokio::test]
-    async fn test_certificate_certificate_hash_get_ok() {
+    async fn test_certificate_certificate_hash_get_ok() -> Result<(), String> {
         let dependency_manager = initialize_dependencies().await;
         dependency_manager
             .certificate_repository
@@ -285,18 +400,21 @@ mod tests {
             .reply(&setup_router(Arc::new(dependency_manager)))
             .await;
 
-        APISpec::verify_conformity(
+        println!("Response: {:?}", response);
+
+        APISpec::verify_conformity_with_status(
             APISpec::get_all_spec_files(),
             method,
             path,
             "application/json",
             &Null,
             &response,
-        );
+            &StatusCode::OK,
+        )
     }
 
     #[tokio::test]
-    async fn test_certificate_certificate_hash_get_ok_404() {
+    async fn test_certificate_certificate_hash_get_ok_404() -> Result<(), String> {
         let dependency_manager = initialize_dependencies().await;
 
         let method = Method::GET.as_str();
@@ -308,41 +426,48 @@ mod tests {
             .reply(&setup_router(Arc::new(dependency_manager)))
             .await;
 
-        APISpec::verify_conformity(
+        println!("Response: {:?}", response);
+        APISpec::verify_conformity_with_status(
             APISpec::get_all_spec_files(),
             method,
             path,
             "application/json",
             &Null,
             &response,
-        );
+            &StatusCode::NOT_FOUND,
+        )
     }
 
     #[tokio::test]
-    async fn test_certificate_certificate_hash_get_ko() {
+    async fn test_certificate_when_error_on_retrieving_certificate_hash_returns_ko_500(
+    ) -> Result<(), String> {
         let mut dependency_manager = initialize_dependencies().await;
-        let mut certifier_service = MockCertifierService::new();
-        certifier_service
-            .expect_get_certificate_by_hash()
+        let mut message_service = MockMessageService::new();
+        message_service
+            .expect_get_certificate_message()
             .returning(|_| Err(anyhow!("an error")));
-        dependency_manager.certifier_service = Arc::new(certifier_service);
+        dependency_manager.message_service = Arc::new(message_service);
 
         let method = Method::GET.as_str();
         let path = "/certificate/{certificate_hash}";
 
         let response = request()
             .method(method)
-            .path(&format!("/{SERVER_BASE_PATH}{path}"))
+            .path(&format!(
+                "/{SERVER_BASE_PATH}{}",
+                path.replace("{certificate_hash}", "whatever")
+            ))
             .reply(&setup_router(Arc::new(dependency_manager)))
             .await;
 
-        APISpec::verify_conformity(
+        APISpec::verify_conformity_with_status(
             APISpec::get_all_spec_files(),
             method,
             path,
             "application/json",
             &Null,
             &response,
-        );
+            &StatusCode::INTERNAL_SERVER_ERROR,
+        )
     }
 }
