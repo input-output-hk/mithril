49461701311904d731bfd323447b986d64b4b344
DJO <790521+Alenar@users.noreply.github.com>
2024-03-05 18:26:34 +0100

    Basic running Txs certification in explorer

--- mithril-explorer/src/components/CertifyCardanoTransactionsModal/index.js
+++ mithril-explorer/src/components/CertifyCardanoTransactionsModal/index.js
@@ -1,21 +1,49 @@
-import React, { useEffect, useState } from "react";
+import { MithrilClient } from "@mithril-dev/mithril-client-wasm";
+import React, { useEffect, useRef, useState } from "react";
 import { Container, Modal } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import { fetchGenesisVerificationKey } from "../../utils";
+import CertificateVerifier from "../VerifyCertificate/verifier";
 
 export default function CertifyCardanoTransactionsModal({ transactionHashes, ...props }) {
+  const currentAggregator = useSelector((state) => state.settings.selectedAggregator);
+  const certificateVerifier = useRef(null);
+  const [transactionsProofs, setTransactionsProofs] = useState({});
+  const [showWarning, setShowWarning] = useState(false);
+
   useEffect(() => {
-    if (!transactionHashes) {
+    if (transactionHashes?.length < 1) {
       return;
     }
-  }, [transactionHashes]);
 
-  function close() {
-    props.onHashesChange([]);
+    if (transactionHashes?.length > 0) {
+      getTransactionsProofs(currentAggregator, transactionHashes).catch((err) => {
+        console.error("Cardano Transactions Certification Error:", err);
+      });
+    }
+  }, [currentAggregator, transactionHashes]);
+
+  async function getTransactionsProofs(aggregator, transactionHashes) {
+    const genesisVerificationKey = await fetchGenesisVerificationKey(aggregator);
+    const client = new MithrilClient(aggregator, genesisVerificationKey);
+    const proofs = await client.unstable.get_cardano_transaction_proofs(transactionHashes);
+
+    setTransactionsProofs(proofs);
+  }
+
+  function closeIfNotRunning() {
+    // Only allow closing if not loading
+    if (certificateVerifier?.current?.loading) {
+      setShowWarning(true);
+    } else {
+      props.onHashesChange([]);
+    }
   }
 
   return (
     <Modal
       show={transactionHashes !== undefined && transactionHashes.length > 0}
-      onHide={close}
+      onHide={closeIfNotRunning}
       size="xl"
       aria-labelledby="contained-modal-title-vcenter"
       centered>
@@ -26,6 +54,19 @@ export default function CertifyCardanoTransactionsModal({ transactionHashes, ...
       </Modal.Header>
 
       <Modal.Body>
+        {Object.entries(transactionsProofs).length > 0 && (
+          <>
+            <div>Transactions Certified: {transactionsProofs.transactions_hashes}</div>
+            <div>Transactions not certified: {transactionsProofs.non_certified_transactions}</div>
+            <div>Certificate hash: {transactionsProofs.certificate_hash}</div>
+
+            <CertificateVerifier
+              stateRef={certificateVerifier}
+              certificateHash={transactionsProofs.certificate_hash}
+              showLoadingWarning={false}
+            />
+          </>
+        )}
         <Container></Container>
       </Modal.Body>
       <Modal.Footer></Modal.Footer>
