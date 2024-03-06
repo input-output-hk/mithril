72e8d4c628ec76b0a81a6e65d647052b293c101e
Damien LACHAUME / PALO-IT <dlachaume@palo-it.com>
2024-03-06 16:05:15 +0100

    Add a `CommandType` to redirect the commands logs to the right output

--- mithril-aggregator/src/main.rs
+++ mithril-aggregator/src/main.rs
@@ -1,19 +1,25 @@
 #![doc = include_str!("../README.md")]
 
 use clap::Parser;
-use mithril_aggregator::MainOpts;
+use mithril_aggregator::{CommandType, MainOpts};
 use mithril_common::StdResult;
-use slog::{Drain, Logger};
+use slog::{Drain, Fuse, Level, Logger};
+use slog_async::Async;
 use std::sync::Arc;
+
+fn build_io_logger<W: std::io::Write + Send + 'static>(log_level: Level, io: W) -> Fuse<Async> {
+    let drain = slog_bunyan::new(io).set_pretty(false).build().fuse();
+    let drain = slog::LevelFilter::new(drain, log_level).fuse();
+
+    slog_async::Async::new(drain).build().fuse()
+}
 
 /// Build a logger from args.
 pub fn build_logger(args: &MainOpts) -> Logger {
-    let drain = slog_bunyan::new(std::io::stdout())
-        .set_pretty(false)
-        .build()
-        .fuse();
-    let drain = slog::LevelFilter::new(drain, args.log_level()).fuse();
-    let drain = slog_async::Async::new(drain).build().fuse();
+    let drain = match args.command.command_type() {
+        CommandType::Server => build_io_logger(args.log_level(), std::io::stdout()),
+        CommandType::CommandLine => build_io_logger(args.log_level(), std::io::stderr()),
+    };
 
     Logger::root(Arc::new(drain), slog::o!())
 }
