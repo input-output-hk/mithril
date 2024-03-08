4fea4d495e4c85eabe2c8e92be92daeaa151fa7b
DJO <790521+Alenar@users.noreply.github.com>
2024-03-08 12:28:13 +0100

    Make `CopyableHash` component more customizable

--- mithril-explorer/src/components/CopyableHash.js
+++ mithril-explorer/src/components/CopyableHash.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { CopyButton } from "./CopyButton";
 
-export default function CopyableHash({ hash }) {
+export default function CopyableHash({ hash, className, ...props }) {
   return (
-    <span className="text-break">
+    <span className={`text-break ${className}`} {...props}>
       {hash} <CopyButton textToCopy={hash} />
     </span>
   );
