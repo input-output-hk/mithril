49a108406bd59cb4db06646c4da44c49f04f2208
jpraynaud <jpraynaud@users.noreply.github.com>
2024-03-27 16:58:38 +0000

    deploy: 46c7b79bfdc03d10957592937aada3cf62fbf8b7

--- rust-doc/trait.impl/mithril_persistence/store/stake_store/trait.StakeStorer.js
+++ rust-doc/trait.impl/mithril_persistence/store/stake_store/trait.StakeStorer.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"mithril_aggregator":[["impl StakeStorer for <a class=\"struct\" href=\"mithril_aggregator/database/provider/struct.StakePoolStore.html\" title=\"struct mithril_aggregator::database::provider::StakePoolStore\">StakePoolStore</a>"]],
+"mithril_aggregator":[["impl <a class=\"trait\" href=\"mithril_persistence/store/stake_store/trait.StakeStorer.html\" title=\"trait mithril_persistence::store::stake_store::StakeStorer\">StakeStorer</a> for <a class=\"struct\" href=\"mithril_aggregator/database/provider/struct.StakePoolStore.html\" title=\"struct mithril_aggregator::database::provider::StakePoolStore\">StakePoolStore</a>"]],
 "mithril_persistence":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
