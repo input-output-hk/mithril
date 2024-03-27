321939f084081705bc864997e685735cd8e1fab3
Alenar <Alenar@users.noreply.github.com>
2024-03-27 15:03:47 +0000

    deploy: aabd9fc908eb86652bd8b8e2d4eae6060c61b2c7

--- rust-doc/trait.impl/mithril_persistence/store/store_pruner/trait.StorePruner.js
+++ rust-doc/trait.impl/mithril_persistence/store/store_pruner/trait.StorePruner.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
 "mithril_persistence":[],
-"mithril_signer":[["impl <a class=\"trait\" href=\"mithril_persistence/store/store_pruner/trait.StorePruner.html\" title=\"trait mithril_persistence::store::store_pruner::StorePruner\">StorePruner</a> for <a class=\"struct\" href=\"mithril_signer/struct.ProtocolInitializerStore.html\" title=\"struct mithril_signer::ProtocolInitializerStore\">ProtocolInitializerStore</a>"]]
+"mithril_signer":[["impl StorePruner for <a class=\"struct\" href=\"mithril_signer/struct.ProtocolInitializerStore.html\" title=\"struct mithril_signer::ProtocolInitializerStore\">ProtocolInitializerStore</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
