4141cbd6b9f7fd46938963b718b9e82eba8a598b
Alexsander Falcucci <alex.falcucci@gmail.com>
2024-05-20 22:23:42 +0200

    refactor: refactor chain block roll functions and test cases
    - remove `println!` statements for debugging
    - refactor `get_fake_chain_point` into separate methods for forwards and backwards
    - modify the `setup_server` function to take an `action` parameter instead of `intersections`
    - create tests for `get_next_chain_block_roll_backwards` and `get_next_chain_block_roll_forward` with appropriate setups
    - assert the expected behavior in the test cases for getting the next chain block

--- mithril-common/src/chain_reader/pallas_chain_reader.rs
+++ mithril-common/src/chain_reader/pallas_chain_reader.rs
@@ -70,7 +70,6 @@ impl ChainBlockReader for PallasChainReader {
                 }))
             }
             NextResponse::RollBackward(rollback_point, _) => {
-                println!("Rolling back to {:?}", rollback_point);
                 Ok(Some(ChainBlockNextAction::RollBackward {
                     rollback_point: rollback_point.into(),
                 }))
@@ -85,21 +84,15 @@ impl ChainBlockReader for PallasChainReader {
     ) -> StdResult<Option<ChainBlockNextAction>> {
         let client = self.get_client().await?;
         let chainsync = client.chainsync();
-        println!("1.find_intersect start");
 
-        let intersection = chainsync
+        chainsync
             .find_intersect(vec![point.to_owned().into()])
             .await?;
-        println!("2.find_intersect done");
-        println!("intersection: {:?}", intersection);
 
         let next = match chainsync.has_agency() {
             true => chainsync.request_next().await?,
             false => chainsync.recv_while_must_reply().await?,
         };
-
-        println!("3.request_next done");
-        println!("next: {:?}", next);
 
         self.process_next_chain_block(next).await
     }
@@ -122,12 +115,35 @@ mod tests {
 
     use crate::test_utils::TempDir;
 
-    fn get_fake_chain_point() -> ChainPoint {
+    /// Returns a fake chain point for testing purposes.
+    fn get_fake_chain_point_backwards() -> ChainPoint {
         ChainPoint::from(Point::Specific(
             1654413,
             hex::decode("7de1f036df5a133ce68a82877d14354d0ba6de7625ab918e75f3e2ecb29771c2")
                 .unwrap(),
         ))
+    }
+
+    /// Returns a fake chain point for testing purposes.
+    fn get_fake_chain_point_forwards() -> ChainPoint {
+        Tip(
+            Point::Specific(
+                1654413,
+                hex::decode("7de1f036df5a133ce68a82877d14354d0ba6de7625ab918e75f3e2ecb29771c2")
+                    .unwrap(),
+            ),
+            1337,
+        )
+        .into()
+    }
+
+    /// Returns a fake intersection point for testing purposes.
+    fn get_fake_intersection_point() -> Point {
+        Point::Specific(
+            1654413,
+            hex::decode("7de1f036df5a133ce68a82877d14354d0ba6de7625ab918e75f3e2ecb29771c2")
+                .unwrap(),
+        )
     }
 
     /// Creates a new work directory in the system's temporary folder.
@@ -140,24 +156,20 @@ mod tests {
     /// Use the `intersections` parameter to define exactly how many
     /// local state queries should be intersepted by the `mock_server`
     /// and avoid any panic errors.
-    async fn setup_server(socket_path: PathBuf, intersections: u32) -> tokio::task::JoinHandle<()> {
+    async fn setup_server(socket_path: PathBuf, action: String) -> tokio::task::JoinHandle<()> {
         tokio::spawn({
             async move {
                 if socket_path.exists() {
                     fs::remove_file(&socket_path).expect("Previous socket removal failed");
                 }
 
-                let known_point = Point::Specific(
-                    1654413,
-                    hex::decode("7de1f036df5a133ce68a82877d14354d0ba6de7625ab918e75f3e2ecb29771c2")
-                        .unwrap(),
-                );
-
+                let known_point = get_fake_intersection_point();
                 let unix_listener = UnixListener::bind(socket_path.as_path()).unwrap();
                 let mut server = NodeServer::accept(&unix_listener, 10).await.unwrap();
 
                 let chansync_server = server.chainsync();
 
+                // chansync_server receives request next from client, sends rollbackwards
                 chansync_server.recv_while_idle().await.unwrap();
 
                 chansync_server
@@ -168,41 +180,33 @@ mod tests {
                 // chansync_server receives request next from client, sends rollbackwards
                 chansync_server.recv_while_idle().await.unwrap();
 
-                chansync_server
-                    .send_roll_backward(known_point.clone(), Tip(known_point.clone(), 1337))
-                    .await
-                    .unwrap();
-
-                // server receives request next from client, sends rollforwards
-                chansync_server.recv_while_idle().await.unwrap();
-
-                // mock
-                let block = BlockContent(hex::decode("c0ffeec0ffeec0ffee").unwrap());
-
-                chansync_server
-                    .send_roll_forward(block, Tip(known_point.clone(), 1337))
-                    .await
-                    .unwrap();
-
-                // server receives request next from client, sends await reply
-                // then rollforwards
-                chansync_server.recv_while_idle().await.unwrap();
-
-                chansync_server.send_await_reply().await.unwrap();
-                server.abort().await;
+                match action.as_str() {
+                    "roll_backwards" => {
+                        chansync_server
+                            .send_roll_backward(known_point.clone(), Tip(known_point.clone(), 1337))
+                            .await
+                            .unwrap();
+                    }
+                    "roll_forwards" => {
+                        // mock
+                        let block = BlockContent(hex::decode("c0ffeec0ffeec0ffee").unwrap());
+                        chansync_server
+                            .send_roll_forward(block, Tip(known_point.clone(), 1337))
+                            .await
+                            .unwrap();
+                    }
+                    _ => panic!("Unexpected action"),
+                }
             }
         })
     }
 
     #[tokio::test]
-    async fn get_next_chain_block() {
-        let socket_path = create_temp_dir("get_next_chain_block").join("node.socket");
-        let known_point = Point::Specific(
-            1654413,
-            hex::decode("7de1f036df5a133ce68a82877d14354d0ba6de7625ab918e75f3e2ecb29771c2")
-                .unwrap(),
-        );
-        let server = setup_server(socket_path.clone(), 1).await;
+    async fn get_next_chain_block_roll_backwards() {
+        let socket_path =
+            create_temp_dir("get_next_chain_block_roll_backwards").join("node.socket");
+        let known_point = get_fake_intersection_point();
+        let server = setup_server(socket_path.clone(), "roll_backwards".to_string()).await;
         let client = tokio::spawn(async move {
             let mut chain_reader =
                 PallasChainReader::new(socket_path.as_path(), CardanoNetwork::TestNet(10));
@@ -217,7 +221,44 @@ mod tests {
         let chain_block = client_res.expect("Client failed to get next chain block");
         match chain_block {
             ChainBlockNextAction::RollBackward { rollback_point } => {
-                assert_eq!(rollback_point, get_fake_chain_point());
+                assert_eq!(rollback_point, get_fake_chain_point_backwards());
+            }
+            _ => panic!("Unexpected chain block action"),
+        }
+    }
+
+    #[tokio::test]
+    async fn get_next_chain_block_roll_forward() {
+        let socket_path = create_temp_dir("get_next_chain_block_roll_forward").join("node.socket");
+        let known_point = Point::Specific(
+            1654413,
+            hex::decode("7de1f036df5a133ce68a82877d14354d0ba6de7625ab918e75f3e2ecb29771c2")
+                .unwrap(),
+        );
+        let server = setup_server(socket_path.clone(), "roll_forwards".to_string()).await;
+        let client = tokio::spawn(async move {
+            let mut chain_reader =
+                PallasChainReader::new(socket_path.as_path(), CardanoNetwork::TestNet(10));
+            chain_reader
+                .get_next_chain_block(&ChainPoint::from(known_point))
+                .await
+                .unwrap()
+                .unwrap()
+        });
+
+        let (_, client_res) = tokio::join!(server, client);
+        let chain_block = client_res.expect("Client failed to get next chain block");
+        match chain_block {
+            ChainBlockNextAction::RollForward {
+                next_point,
+                raw_block,
+            } => {
+                println!("raw_block: {:?}", raw_block.0);
+                assert_eq!(next_point, get_fake_chain_point_forwards().into());
+                // assert_eq!(
+                //     raw_block.0,
+                //     BlockContent(hex::decode("c0ffeec0ffeec0ffee").unwrap())
+                // );
             }
             _ => panic!("Unexpected chain block action"),
         }
