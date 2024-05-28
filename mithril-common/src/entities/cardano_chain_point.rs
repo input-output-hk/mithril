fbdc1772d0c9a63ec98ffd8846b2b00176aa50a7
DJO <790521+Alenar@users.noreply.github.com>
2024-05-28 16:45:56 +0200

    Add `new`, `dummy` and `Ord`/`PartialOrd` to `ChainPoint`

--- mithril-common/src/entities/cardano_chain_point.rs
+++ mithril-common/src/entities/cardano_chain_point.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use std::cmp::Ordering;
 cfg_fs! {
     use pallas_network::miniprotocols::{chainsync::Tip, Point};
 }
@@ -26,9 +27,47 @@ pub struct ChainPoint {
 }
 
 impl ChainPoint {
+    /// [ChainPoint] factory
+    pub fn new<T: Into<BlockHash>>(
+        slot_number: SlotNumber,
+        block_number: BlockNumber,
+        block_hash: T,
+    ) -> ChainPoint {
+        ChainPoint {
+            slot_number,
+            block_number,
+            block_hash: block_hash.into(),
+        }
+    }
+
     /// Check if origin chain point
     pub fn is_origin(&self) -> bool {
         self.slot_number == 0 && self.block_number == 0 && self.block_hash.is_empty()
+    }
+
+    cfg_test_tools! {
+        /// Create a dummy ChainPoint
+        pub fn dummy() -> Self {
+            Self {
+                slot_number: 100,
+                block_number: 50,
+                block_hash: "block_hash-50".to_string(),
+            }
+        }
+    }
+}
+
+impl PartialOrd for ChainPoint {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for ChainPoint {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.block_number
+            .cmp(&other.block_number)
+            .then(self.slot_number.cmp(&other.slot_number))
     }
 }
 
@@ -79,5 +118,60 @@ cfg_fs! {
             let point: Point = chain_point.into();
             Tip(point, block_number)
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::cmp::Ordering;
+
+    use super::*;
+
+    #[test]
+    fn chain_point_ord_cmp_block_number_take_precedence_over_other_fields() {
+        let chain_point1 = ChainPoint {
+            slot_number: 15,
+            block_number: 10,
+            block_hash: "hash2".to_string(),
+        };
+        let chain_point2 = ChainPoint {
+            slot_number: 5,
+            block_number: 20,
+            block_hash: "hash1".to_string(),
+        };
+
+        assert_eq!(Ordering::Less, chain_point1.cmp(&chain_point2));
+    }
+
+    #[test]
+    fn chain_point_ord_cmp_if_block_number_equals_then_compare_slot_numbers() {
+        let chain_point1 = ChainPoint {
+            slot_number: 15,
+            block_number: 0,
+            block_hash: "hash2".to_string(),
+        };
+        let chain_point2 = ChainPoint {
+            slot_number: 5,
+            block_number: 0,
+            block_hash: "hash1".to_string(),
+        };
+
+        assert_eq!(Ordering::Greater, chain_point1.cmp(&chain_point2));
+    }
+
+    #[test]
+    fn chain_point_ord_cmp_block_hash_doesnt_matter() {
+        let chain_point1 = ChainPoint {
+            slot_number: 5,
+            block_number: 10,
+            block_hash: "hash1".to_string(),
+        };
+        let chain_point2 = ChainPoint {
+            slot_number: 5,
+            block_number: 10,
+            block_hash: "hash2".to_string(),
+        };
+
+        assert_eq!(Ordering::Equal, chain_point1.cmp(&chain_point2));
     }
 }
