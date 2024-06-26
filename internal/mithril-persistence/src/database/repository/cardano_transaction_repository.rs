1caa4e8304ed062dfc495b038ab899a39649e70f
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2024-06-26 16:27:12 +0200

    chore: apply review comments

--- internal/mithril-persistence/src/database/repository/cardano_transaction_repository.rs
+++ internal/mithril-persistence/src/database/repository/cardano_transaction_repository.rs
@@ -247,8 +247,7 @@ impl CardanoTransactionRepository {
         slot_number: SlotNumber,
     ) -> StdResult<BlockNumber> {
         let query = GetCardanoTransactionQuery::by_slot_number(slot_number);
-        let connection = self.connection_pool.connection()?;
-        let record = connection.fetch_first(query)?;
+        let record = self.connection_pool.connection()?.fetch_first(query)?;
 
         record
             .map(|r| r.block_number)
@@ -859,45 +858,22 @@ mod tests {
             SqliteConnectionPool::build_from_connection(connection),
         ));
 
-        let transaction_block_number = 100;
-        let transaction_slot_number = 500;
+        let transactions = vec![
+            CardanoTransactionRecord::new("tx-1", 100, 500, "block-1", 99),
+            CardanoTransactionRecord::new("tx-2", 100, 500, "block-1", 99),
+            CardanoTransactionRecord::new("tx-3", 101, 501, "block-1", 99),
+        ];
         repository
-            .create_transaction(
-                "tx-1",
-                transaction_block_number,
-                transaction_slot_number,
-                "block-1",
-                99,
-            )
-            .await
-            .unwrap();
-        repository
-            .create_transaction(
-                "tx-2",
-                transaction_block_number,
-                transaction_slot_number,
-                "block-1",
-                99,
-            )
-            .await
-            .unwrap();
-        repository
-            .create_transaction(
-                "tx-3",
-                transaction_block_number + 1,
-                transaction_slot_number + 1,
-                "block-1",
-                99,
-            )
+            .create_transactions(transactions.clone())
             .await
             .unwrap();
 
         let transaction_block_number_retrieved = repository
-            .get_block_number_by_slot_number(transaction_slot_number)
+            .get_block_number_by_slot_number(500)
             .await
             .unwrap();
 
-        assert_eq!(transaction_block_number_retrieved, transaction_block_number);
+        assert_eq!(transaction_block_number_retrieved, 100);
     }
 
     #[tokio::test]
