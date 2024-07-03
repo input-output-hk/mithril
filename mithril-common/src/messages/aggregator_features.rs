97dd1ad49f9858c3337ed1e2358a890d254418fd
Damien Lachaume <135982616+dlachaume@users.noreply.github.com>
2024-07-03 16:50:27 +0200

    Add retro compatibility test for `AggregatorFeaturesMessage`
    Co-authored-by: Sébastien Fauvel <sfauvel@users.noreply.github.com>

--- mithril-common/src/messages/aggregator_features.rs
+++ mithril-common/src/messages/aggregator_features.rs
@@ -50,3 +50,42 @@ pub struct CardanoTransactionsProverCapabilities {
     /// Maximum number of hashes allowed for a single request
     pub max_hashes_allowed_by_request: usize,
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn golden_message() -> AggregatorFeaturesMessage {
+        AggregatorFeaturesMessage {
+            open_api_version: "0.0.1".to_string(),
+            documentation_url: "https://example.com".to_string(),
+            capabilities: AggregatorCapabilities {
+                signed_entity_types: BTreeSet::from([
+                    SignedEntityTypeDiscriminants::CardanoTransactions,
+                ]),
+                cardano_transactions_prover: Some(CardanoTransactionsProverCapabilities {
+                    max_hashes_allowed_by_request: 100,
+                }),
+            },
+        }
+    }
+
+    // Test the retro compatibility with possible future upgrades.
+    #[test]
+    fn test_v1() {
+        let json = r#"{
+            "open_api_version": "0.0.1",
+            "documentation_url": "https://example.com",
+            "capabilities": {
+                "signed_entity_types": ["CardanoTransactions"],
+                "cardano_transactions_prover": {
+                    "max_hashes_allowed_by_request": 100
+                }
+            }
+        }"#;
+
+        let message: AggregatorFeaturesMessage = serde_json::from_str(json).unwrap();
+
+        assert_eq!(golden_message(), message);
+    }
+}
