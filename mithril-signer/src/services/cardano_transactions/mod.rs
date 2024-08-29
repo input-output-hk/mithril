a96b9507be9c38c6a262707de462592f30e3a69d
DJO <790521+Alenar@users.noreply.github.com>
2024-08-29 15:22:00 +0200

    Futher reorganize cardano transactions services inside a single mod

---
+++ mithril-signer/src/services/cardano_transactions/mod.rs
@@ -0,0 +1,5 @@
+mod importer;
+mod preloader_checker;
+
+pub use importer::*;
+pub use preloader_checker::*;
--- mithril-signer/src/services/mod.rs
+++ mithril-signer/src/services/mod.rs
@@ -3,22 +3,20 @@
 //! This module regroups services. Services are adapters in charge of the different  bounded contexts of the application:
 //!
 //! * Aggregator Client: communicate with the Aggregator
-//! * Cardano Transaction Importer: Import transactions from the Cardano chain
+//! * Cardano Transactions: handle Cardano transactions (import, preload, etc.)
 //! * Single Signer: create single signatures
 //! * Upkeep: perform maintenance tasks
 //!
 //! Each service is defined by a public API (a trait) that is used in the controllers (runtimes).
 
 mod aggregator_client;
-mod cardano_transactions_importer;
-mod cardano_transactions_preloader_checker;
+mod cardano_transactions;
 mod single_signer;
 mod upkeep_service;
 
 #[cfg(test)]
 pub use aggregator_client::dumb::DumbAggregatorClient;
 pub use aggregator_client::*;
-pub use cardano_transactions_importer::*;
-pub use cardano_transactions_preloader_checker::*;
+pub use cardano_transactions::*;
 pub use single_signer::*;
 pub use upkeep_service::*;
