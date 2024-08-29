8c0797366331c3c3af1a2470a4605b3e9275b2ba
DJO <790521+Alenar@users.noreply.github.com>
2024-08-29 12:13:54 +0200

    Move tx importer decorators to the new module

--- mithril-signer/src/services/cardano_transactions_importer/mod.rs
+++ mithril-signer/src/services/cardano_transactions_importer/mod.rs
@@ -1,3 +1,9 @@
+mod importer_by_chunk;
+mod importer_with_pruner;
+mod importer_with_vacuum;
 mod service;
 
+pub use importer_by_chunk::*;
+pub use importer_with_pruner::*;
+pub use importer_with_vacuum::*;
 pub use service::*;
--- mithril-signer/src/services/mod.rs
+++ mithril-signer/src/services/mod.rs
@@ -13,9 +13,6 @@ mod aggregator_client;
 mod cardano_transactions_importer;
 mod cardano_transactions_preloader_checker;
 mod single_signer;
-mod transactions_importer_by_chunk;
-mod transactions_importer_with_pruner;
-mod transactions_importer_with_vacuum;
 mod upkeep_service;
 
 #[cfg(test)]
@@ -24,7 +21,4 @@ pub use aggregator_client::*;
 pub use cardano_transactions_importer::*;
 pub use cardano_transactions_preloader_checker::*;
 pub use single_signer::*;
-pub use transactions_importer_by_chunk::*;
-pub use transactions_importer_with_pruner::*;
-pub use transactions_importer_with_vacuum::*;
 pub use upkeep_service::*;
