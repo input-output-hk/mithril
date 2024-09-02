e02968d83b61eb0ee9c8d0fab27756dbda960abb
DJO <790521+Alenar@users.noreply.github.com>
2024-09-02 11:20:17 +0200

    Scaffold integration test for signing with buffered signatures

---
+++ mithril-aggregator/tests/create_certificate_with_buffered_signatures.rs
@@ -0,0 +1,110 @@
+mod test_extensions;
+
+use mithril_aggregator::Configuration;
+use mithril_common::{
+    entities::{
+        BlockNumber, CardanoDbBeacon, CardanoTransactionsSigningConfig, ChainPoint, Epoch,
+        ProtocolParameters, SignedEntityType, SignedEntityTypeDiscriminants, SlotNumber,
+        StakeDistributionParty, TimePoint,
+    },
+    test_utils::MithrilFixtureBuilder,
+};
+use test_extensions::{utilities::get_test_dir, ExpectedCertificate, RuntimeTester};
+
+#[tokio::test]
+async fn create_certificate_with_buffered_signatures() {
+    let protocol_parameters = ProtocolParameters {
+        k: 5,
+        m: 150,
+        phi_f: 0.95,
+    };
+    let configuration = Configuration {
+        protocol_parameters: protocol_parameters.clone(),
+        signed_entity_types: Some(SignedEntityTypeDiscriminants::CardanoTransactions.to_string()),
+        data_stores_directory: get_test_dir("create_certificate_with_buffered_signatures"),
+        cardano_transactions_signing_config: CardanoTransactionsSigningConfig {
+            security_parameter: BlockNumber(0),
+            step: BlockNumber(30),
+        },
+        ..Configuration::new_sample()
+    };
+    let mut tester = RuntimeTester::build(
+        TimePoint {
+            epoch: Epoch(1),
+            immutable_file_number: 1,
+            chain_point: ChainPoint {
+                slot_number: SlotNumber(10),
+                block_number: BlockNumber(100),
+                block_hash: "block_hash-100".to_string(),
+            },
+        },
+        configuration,
+    )
+    .await;
+
+    comment!("create signers & declare stake distribution");
+    let fixture = MithrilFixtureBuilder::default()
+        .with_signers(10)
+        .with_protocol_parameters(protocol_parameters.clone())
+        .build();
+
+    tester.init_state_from_fixture(&fixture).await.unwrap();
+
+    comment!("Bootstrap the genesis certificate");
+    tester.register_genesis_certificate(&fixture).await.unwrap();
+
+    assert_last_certificate_eq!(
+        tester,
+        ExpectedCertificate::new_genesis(
+            CardanoDbBeacon::new("devnet".to_string(), 1, 1),
+            fixture.compute_and_encode_avk()
+        )
+    );
+
+    comment!("Increase immutable number");
+    tester.increase_immutable_number().await.unwrap();
+
+    comment!("start the runtime state machine");
+    cycle!(tester, "ready");
+
+    comment!("signers send their single signature before the state machine is signing");
+    tester
+        .send_single_signatures(
+            SignedEntityTypeDiscriminants::MithrilStakeDistribution,
+            &fixture.signers_fixture(),
+        )
+        .await
+        .unwrap();
+    cycle!(tester, "signing");
+
+    comment!("register signers");
+    // Note: removing this registration makes the epoch_service unable to build its
+    // next_protocol_multi_signer because the list of next_signers is empty.
+    // Todo: Is this a bug ?
+    tester
+        .register_signers(&fixture.signers_fixture())
+        .await
+        .unwrap();
+
+    comment!("Using buffered signatures, the state machine should issue a certificate for the MithrilStakeDistribution");
+    cycle!(tester, "ready");
+    assert_last_certificate_eq!(
+        tester,
+        ExpectedCertificate::new(
+            CardanoDbBeacon::new("devnet".to_string(), 1, 2),
+            StakeDistributionParty::from_signers(fixture.signers_with_stake()).as_slice(),
+            fixture.compute_and_encode_avk(),
+            SignedEntityType::MithrilStakeDistribution(Epoch(1)),
+            ExpectedCertificate::genesis_identifier(&CardanoDbBeacon::new(
+                "devnet".to_string(),
+                1,
+                1
+            )),
+        )
+    );
+
+    tester.increase_epoch().await.unwrap();
+    cycle!(tester, "idle");
+
+    cycle!(tester, "ready");
+}
