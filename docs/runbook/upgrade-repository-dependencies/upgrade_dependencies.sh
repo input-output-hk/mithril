da333886e75c7d4143b8d3791122f428763477f0
DJO <790521+Alenar@users.noreply.github.com>
2024-09-09 15:12:41 +0200

    Enable nix flake deps update step in upgrade dependencies runbook

--- docs/runbook/upgrade-repository-dependencies/upgrade_dependencies.sh
+++ docs/runbook/upgrade-repository-dependencies/upgrade_dependencies.sh
@@ -82,11 +82,10 @@ mkdir -p "$TMP_SCRIPT_DIR"
 echo "git config --global --add safe.directory '*'
 nix --extra-experimental-features 'nix-command flakes' flake update" > "$TMP_SCRIPT_DIR/$FLAKE_UPDATE_SCRIPT"
 
-# The nix update is deactivated while waiting to be compatible with the latest version
-# # Upgrade Nix Flake dependencies
-# docker run -v "$(pwd)":/mithril -v "$TMP_SCRIPT_DIR":/scripts/mithril -w /mithril nixos/nix /bin/sh -c ". /scripts/mithril/$FLAKE_UPDATE_SCRIPT"
-# rm "$TMP_SCRIPT_DIR/$FLAKE_UPDATE_SCRIPT"
-# 
-# git commit -am "chore: update nix flake dependencies
-# 
-# By running 'nix flake update' command."
+ # Upgrade Nix Flake dependencies
+ docker run -v "$(pwd)":/mithril -v "$TMP_SCRIPT_DIR":/scripts/mithril -w /mithril nixos/nix /bin/sh -c ". /scripts/mithril/$FLAKE_UPDATE_SCRIPT"
+ rm "$TMP_SCRIPT_DIR/$FLAKE_UPDATE_SCRIPT"
+
+ git commit -am "chore: update nix flake dependencies
+
+ By running 'nix flake update' command."
