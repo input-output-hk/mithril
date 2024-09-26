90ee068114b5e01e2ea38473c3278cc4d645668d
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2024-09-26 15:52:30 +0200

    fix: compute next protocol parameters from ProtocolInitializer in SignerSignableSeedBuilder

--- mithril-signer/src/services/signable_builder/signable_seed_builder.rs
+++ mithril-signer/src/services/signable_builder/signable_seed_builder.rs
@@ -9,7 +9,9 @@ use std::sync::Arc;
 use tokio::sync::RwLock;
 
 use mithril_common::{
-    entities::ProtocolMessagePartValue, signable_builder::SignableSeedBuilder, StdResult,
+    entities::{ProtocolMessagePartValue, ProtocolParameters},
+    signable_builder::SignableSeedBuilder,
+    StdResult,
 };
 
 use crate::{
@@ -54,7 +56,6 @@ impl SignableSeedBuilder for SignerSignableSeedBuilder {
             .ok_or_else(|| {
                 anyhow!("can not get protocol_initializer at epoch {next_signer_retrieval_epoch}")
             })?;
-
         let next_signers_with_stake = epoch_service.next_signers_with_stake().await?;
         let next_aggregate_verification_key = self
             .single_signer
@@ -71,9 +72,19 @@ impl SignableSeedBuilder for SignerSignableSeedBuilder {
         &self,
     ) -> StdResult<ProtocolMessagePartValue> {
         let epoch_service = self.epoch_service.read().await;
-        let next_protocol_parameters = epoch_service.next_protocol_parameters()?.compute_hash();
+        let epoch = (*epoch_service).epoch_of_current_data()?;
+        let next_signer_retrieval_epoch = epoch.offset_to_next_signer_retrieval_epoch();
+        let next_protocol_initializer = self
+            .protocol_initializer_store
+            .get_protocol_initializer(next_signer_retrieval_epoch)
+            .await?
+            .ok_or_else(|| {
+                anyhow!("can not get protocol_initializer at epoch {next_signer_retrieval_epoch}")
+            })?;
+        let next_protocol_parameters: ProtocolParameters =
+            next_protocol_initializer.get_protocol_parameters().into();
 
-        Ok(next_protocol_parameters)
+        Ok(next_protocol_parameters.compute_hash())
     }
 
     async fn compute_current_epoch_protocol_message_part_value(
@@ -127,12 +138,13 @@ mod tests {
     #[tokio::test]
     async fn test_compute_next_aggregate_verification_key_protocol_message_value() {
         let epoch = Epoch(5);
-        let fixture = MithrilFixtureBuilder::default().with_signers(5).build();
         let next_fixture = MithrilFixtureBuilder::default().with_signers(4).build();
+        let protocol_initializer = next_fixture.signers_fixture()[0]
+            .protocol_initializer
+            .clone();
         let expected_next_aggregate_verification_key = next_fixture.compute_and_encode_avk();
         let expected_next_aggregate_verification_key_clone =
             expected_next_aggregate_verification_key.clone();
-        let protocol_initializer = fixture.signers_fixture()[0].protocol_initializer.clone();
         let mut mock_container = MockDependencyInjector::new();
         mock_container.mock_epoch_service =
             MockEpochServiceImpl::new_with_config(|mock_epoch_service| {
@@ -170,20 +182,27 @@ mod tests {
 
     #[tokio::test]
     async fn test_compute_next_protocol_parameters_protocol_message_value() {
-        const NEXT_PROTOCOL_PARAMETERS: ProtocolParameters = ProtocolParameters {
-            k: 10,
-            m: 10,
-            phi_f: 1.0,
-        };
-        let expected_next_protocol_parameters = NEXT_PROTOCOL_PARAMETERS.compute_hash();
+        let epoch = Epoch(5);
+        let next_fixture = MithrilFixtureBuilder::default().with_signers(4).build();
+        let protocol_initializer = next_fixture.signers_fixture()[0]
+            .protocol_initializer
+            .clone();
+        let expected_next_protocol_parameters =
+            Into::<ProtocolParameters>::into(protocol_initializer.get_protocol_parameters())
+                .compute_hash();
         let mut mock_container = MockDependencyInjector::new();
         mock_container.mock_epoch_service =
             MockEpochServiceImpl::new_with_config(|mock_epoch_service| {
                 mock_epoch_service
-                    .expect_next_protocol_parameters()
-                    .return_once(|| Ok(&NEXT_PROTOCOL_PARAMETERS))
+                    .expect_epoch_of_current_data()
+                    .return_once(move || Ok(epoch))
                     .once();
             });
+        mock_container
+            .mock_protocol_initializer_store
+            .expect_get_protocol_initializer()
+            .return_once(move |_| Ok(Some(protocol_initializer)))
+            .once();
         let signable_seed_builder = mock_container.build_signable_builder_service();
 
         let next_protocol_parameters = signable_seed_builder
