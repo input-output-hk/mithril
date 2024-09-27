962c3018dce737e2ad29a094c8c2b7c7d5e4d61b
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2024-09-27 17:15:19 +0200

    fix: call to craneLib
    Co-authored-by: Daniel Firth <locallycompact@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
         ...
       }: let
         inherit (inputs.nixpkgs) lib;
-        craneLib = inputs.crane.lib.${system};
+        craneLib = inputs.crane.mkLib pkgs;
 
         clean = root:
           lib.cleanSourceWith {
