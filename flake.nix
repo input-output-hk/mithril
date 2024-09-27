76f48d8dbcabf4d8d68db5cc394d37a4a459fe0f
Sebastian Nagel <sebastian.nagel@ncoding.at>
2024-09-27 18:15:39 +0200

    Drop invalid override of crane nixpkgs

--- flake.nix
+++ flake.nix
@@ -5,7 +5,6 @@
     treefmt-nix.url = "github:numtide/treefmt-nix";
     treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
     crane.url = "github:ipetkov/crane";
-    crane.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs:
