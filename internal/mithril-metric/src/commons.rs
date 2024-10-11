1f9da7be293fb12aced1606007ef448d8f257eba
sfauvel <sfauvel@palo-it.com>
2024-10-11 18:20:02 +0200

    Add a macro to generate the metric service

--- internal/mithril-metric/src/commons.rs
+++ internal/mithril-metric/src/commons.rs
@@ -9,6 +9,116 @@ pub type MetricName = str;
 /// Type alias for a counter value.
 pub type CounterValue = u32;
 
+/// Create a MetricService.
+///
+/// To build the service you need to provide the structure name and a list of metrics.
+/// Each metrics is defined by an attribute name, a type, a metric name and a help message.
+/// When the metric name is not provided the attribute name will be used.
+/// We can specify a prefix that will be added to all metric names.
+///
+/// The attribute name will be used to create a getter method for the metric.
+///
+/// Crate that use this macro should have `paste` as dependency.
+///
+/// build_metrics_service!(
+///     MetricsService,
+///     counter_example: MetricCounter(
+///         "custom_counter_example_name",
+///         "Example of a counter metric"
+///     ),
+///     gauge_example: MetricGauge(
+///         "custom_gauge_example_name",
+///         "Example of a gauge metric"
+///     )
+/// );
+///
+/// build_metrics_service!(
+///     MetricsService,
+///     counter_example: MetricCounter("Example of a counter metric"),
+///     gauge_example: MetricGauge("Example of a gauge metric")
+/// );
+///
+/// build_metrics_service!(
+///     MetricsService,
+///     "metric_prefix",
+///     counter_example: MetricCounter("Example of a counter metric"),
+///     gauge_example: MetricGauge("Example of a gauge metric")
+/// );
+///
+/// let service = MetricsServiceExampleBuildWithMacro::new(TestLogger::stdout()).unwrap();
+/// service.get_counter_example().record();
+/// service.get_gauge_example().record(Epoch(12));
+#[macro_export]
+macro_rules! build_metrics_service {
+    ($service:ident, $($metric_attribute:ident:$metric_type:ident($help:literal)),*) => {
+        build_metrics_service!($service, "", $($metric_attribute:$metric_type("", $help)),*);
+    };
+    ($service:ident, $prefix:literal, $($metric_attribute:ident:$metric_type:ident($help:literal)),*) => {
+        build_metrics_service!($service, $prefix, $($metric_attribute:$metric_type("", $help)),*);
+    };
+    ($service:ident, $($metric_attribute:ident:$metric_type:ident($name:literal, $help:literal)),*) => {
+        build_metrics_service!($service, "", $($metric_attribute:$metric_type($name, $help)),*);
+    };
+    ($service:ident, $prefix:literal, $($metric_attribute:ident:$metric_type:ident($name:literal, $help:literal)),*) => {
+        paste::item! {
+            /// Metrics service which is responsible for recording and exposing metrics.
+            pub struct $service {
+                registry: Registry,
+                $(
+                    $metric_attribute: $metric_type,
+                )*
+            }
+
+            impl $service {
+                pub fn new(logger: Logger) -> StdResult<Self> {
+
+                    let registry = Registry::new();
+
+                    let prefix = if $prefix.is_empty() {
+                        "".to_string()
+                    } else {
+                        format!("{}_", $prefix)
+                    };
+
+                    $(
+                        let metric_name = if $name.is_empty() {
+                            stringify!($metric_attribute)
+                        } else {
+                            $name
+                        };
+                        let $metric_attribute = $metric_type::new(
+                            logger.clone(),
+                            &format!("{}{}", prefix, metric_name),
+                            $help,
+                        )?;
+                        registry.register($metric_attribute.collector())?;
+                    )*
+
+                    Ok(Self {
+                        registry,
+                        $(
+                            $metric_attribute,
+                        )*
+                    })
+                }
+                $(
+                    /// Get the `$metric_attribute` counter.
+                    pub fn [<get_ $metric_attribute>](&self) -> &$metric_type {
+                        &self.$metric_attribute
+                    }
+                )*
+            }
+
+            impl MetricsServiceTrait for $service {
+                fn export_metrics(&self) -> StdResult<String> {
+                    metrics_tools::export_metrics(&self.registry)
+                }
+            }
+
+        }
+    };
+}
+
 /// Mithril metric
 pub trait MithrilMetric {
     /// Metric name
@@ -111,11 +221,11 @@ pub mod metrics_tools {
     use prometheus::TextEncoder;
 
     pub fn export_metrics(registry: &prometheus::Registry) -> StdResult<String> {
-        // let mut buffer = vec![];
         let encoder = TextEncoder::new();
         let metric_families = registry.gather();
+        // TODO check encode_utf8 do the same as encode and remove the commented code
+        // let mut buffer = vec![];
         // encoder.encode(&metric_families, &mut buffer)?;
-
         // Ok(String::from_utf8(buffer)?)
 
         let mut buffer = String::new();
@@ -149,8 +259,24 @@ pub mod test_tools {
 
 #[cfg(test)]
 mod tests {
+    use std::collections::BTreeMap;
+
+    use crate::MetricsServiceTrait;
+
     use super::*;
+    use prometheus::Registry;
+    use prometheus_parse::Value;
     use test_tools::TestLogger;
+
+    fn parse_metrics(raw_metrics: &str) -> StdResult<BTreeMap<String, Value>> {
+        Ok(
+            prometheus_parse::Scrape::parse(raw_metrics.lines().map(|s| Ok(s.to_owned())))?
+                .samples
+                .into_iter()
+                .map(|s| (s.metric, s.value))
+                .collect::<BTreeMap<_, _>>(),
+        )
+    }
 
     #[test]
     fn test_metric_counter_can_be_incremented() {
@@ -177,20 +303,8 @@ mod tests {
     mod tools {
         use super::*;
         use prometheus::Registry;
-        use prometheus_parse::Value;
-        use std::{collections::BTreeMap, sync::Arc};
 
         use mithril_common::entities::Epoch;
-
-        fn parse_metrics(raw_metrics: &str) -> StdResult<BTreeMap<String, Value>> {
-            Ok(
-                prometheus_parse::Scrape::parse(raw_metrics.lines().map(|s| Ok(s.to_owned())))?
-                    .samples
-                    .into_iter()
-                    .map(|s| (s.metric, s.value))
-                    .collect::<BTreeMap<_, _>>(),
-            )
-        }
 
         #[test]
         fn test_export_metrics() {
@@ -204,8 +318,8 @@ mod tests {
             gauge_metric.record(Epoch(12));
 
             let registry = Registry::new();
-            registry.register(counter_metric.collector());
-            registry.register(gauge_metric.collector());
+            registry.register(counter_metric.collector()).unwrap();
+            registry.register(gauge_metric.collector()).unwrap();
 
             let exported_metrics = metrics_tools::export_metrics(&registry).unwrap();
 
@@ -218,5 +332,162 @@ mod tests {
 
             assert_eq!(parsed_metrics_expected, parsed_metrics);
         }
+    }
+
+    pub struct MetricsServiceExample {
+        registry: Registry,
+        counter_example: MetricCounter,
+        gauge_example: MetricGauge,
+    }
+
+    impl MetricsServiceExample {
+        pub fn new(logger: Logger) -> StdResult<Self> {
+            let registry = Registry::new();
+
+            let counter_example = MetricCounter::new(
+                logger.clone(),
+                "counter_example",
+                "Example of a counter metric",
+            )?;
+            registry.register(counter_example.collector())?;
+
+            let gauge_example =
+                MetricGauge::new(logger.clone(), "gauge_example", "Example of a gauge metric")?;
+            registry.register(gauge_example.collector())?;
+
+            Ok(Self {
+                registry,
+                counter_example,
+                gauge_example,
+            })
+        }
+
+        /// Get the `counter_example` counter.
+        pub fn get_counter_example(&self) -> &MetricCounter {
+            &self.counter_example
+        }
+
+        /// Get the `gauge_example` counter.
+        pub fn get_gauge_example(&self) -> &MetricGauge {
+            &self.gauge_example
+        }
+    }
+
+    impl MetricsServiceTrait for MetricsServiceExample {
+        fn export_metrics(&self) -> StdResult<String> {
+            metrics_tools::export_metrics(&self.registry)
+        }
+    }
+
+    #[test]
+    fn test_service_creation() {
+        let service = MetricsServiceExample::new(TestLogger::stdout()).unwrap();
+        service.get_counter_example().record();
+        service.get_counter_example().record();
+        service.get_gauge_example().record(Epoch(12));
+
+        assert_eq!(2, service.get_counter_example().get());
+        assert_eq!(Epoch(12), service.get_gauge_example().get());
+    }
+
+    build_metrics_service!(
+        MetricsServiceExampleBuildWithMacro,
+        counter_example: MetricCounter(
+            "custom_counter_example_name",
+            "Example of a counter metric"
+        ),
+        gauge_example: MetricGauge(
+            "custom_gauge_example_name",
+            "Example of a gauge metric"
+        )
+    );
+
+    #[test]
+    fn test_service_creation_using_build_metrics_service_macro() {
+        let service = MetricsServiceExampleBuildWithMacro::new(TestLogger::stdout()).unwrap();
+        service.get_counter_example().record();
+        service.get_counter_example().record();
+        service.get_gauge_example().record(Epoch(12));
+
+        assert_eq!(2, service.get_counter_example().get());
+        assert_eq!(Epoch(12), service.get_gauge_example().get());
+    }
+
+    #[test]
+    fn test_build_metrics_service_named_metrics_with_attribute_name() {
+        let service = MetricsServiceExampleBuildWithMacro::new(TestLogger::stdout()).unwrap();
+        assert_eq!(
+            "custom_counter_example_name",
+            service.get_counter_example().name()
+        );
+        assert_eq!(
+            "custom_gauge_example_name",
+            service.get_gauge_example().name()
+        );
+    }
+
+    build_metrics_service!(
+        MetricsServiceExampleBuildWithMacroWithoutMetricName,
+        counter_example: MetricCounter("Example of a counter metric"),
+        gauge_example: MetricGauge("Example of a gauge metric")
+    );
+
+    #[test]
+    fn test_build_metrics_service_macro_without_metric_name() {
+        let service = MetricsServiceExampleBuildWithMacro::new(TestLogger::stdout()).unwrap();
+        service.get_counter_example().record();
+        service.get_counter_example().record();
+        service.get_gauge_example().record(Epoch(12));
+
+        assert_eq!(2, service.get_counter_example().get());
+        assert_eq!(Epoch(12), service.get_gauge_example().get());
+    }
+
+    #[test]
+    fn test_build_metrics_service_named_metrics_without_attribute_name() {
+        let service =
+            MetricsServiceExampleBuildWithMacroWithoutMetricName::new(TestLogger::stdout())
+                .unwrap();
+        assert_eq!("counter_example", service.get_counter_example().name());
+        assert_eq!("gauge_example", service.get_gauge_example().name());
+    }
+
+    build_metrics_service!(
+        MetricsServiceExampleBuildWithMacroUsingPrefix,
+        "metric_prefix",
+        counter_example: MetricCounter("Example of a counter metric"),
+        gauge_example: MetricGauge("Example of a gauge metric")
+    );
+
+    #[test]
+    fn test_build_metrics_service_named_metrics_with_attribute_name_and_prefix() {
+        let service =
+            MetricsServiceExampleBuildWithMacroUsingPrefix::new(TestLogger::stdout()).unwrap();
+        assert_eq!(
+            "metric_prefix_counter_example",
+            service.get_counter_example().name()
+        );
+        assert_eq!(
+            "metric_prefix_gauge_example",
+            service.get_gauge_example().name()
+        );
+    }
+
+    #[test]
+    fn test_build_metrics_service_implement_export() {
+        let service =
+            MetricsServiceExampleBuildWithMacroUsingPrefix::new(TestLogger::stdout()).unwrap();
+        service.get_gauge_example().record(Epoch(12));
+
+        let export = service.export_metrics().unwrap();
+        let metrics = parse_metrics(&export).unwrap();
+
+        assert_eq!(
+            Value::Gauge(12.0),
+            metrics
+                .get(&service.get_gauge_example().name())
+                .unwrap()
+                .clone()
+        );
     }
 }
--- mithril-signer/src/metrics/service.rs
+++ mithril-signer/src/metrics/service.rs
@@ -1,258 +1,137 @@
-use mithril_metric::MetricsServiceTrait;
+use mithril_metric::{build_metrics_service, metrics_tools, MetricsServiceTrait};
 use prometheus::Registry;
 use slog::Logger;
 
-use mithril_common::logging::LoggerExtensions;
 use mithril_common::{entities::Epoch, StdResult};
 
-use mithril_metric::commons::metrics_tools;
 use mithril_metric::commons::{CounterValue, MetricCounter, MetricGauge, MithrilMetric};
 
-/// Metrics service which is responsible for recording and exposing metrics.
-pub struct MetricsService {
-    registry: Registry,
-    signer_registration_success_since_startup_counter: MetricCounter,
-    signer_registration_total_since_startup_counter: MetricCounter,
-    signer_registration_success_last_epoch_gauge: MetricGauge,
-    signature_registration_success_since_startup_counter: MetricCounter,
-    signature_registration_total_since_startup_counter: MetricCounter,
-    signature_registration_success_last_epoch_gauge: MetricGauge,
-    runtime_cycle_success_since_startup_counter: MetricCounter,
-    runtime_cycle_total_since_startup_counter: MetricCounter,
-}
+/// Build the metrics service with the required metrics.
+build_metrics_service!(
+    MetricsService,
+    "mithril_signer",
+    signer_registration_success_since_startup_counter: MetricCounter(
+        "Number of successful signer registrations since startup on a Mithril signer node"
+    ),
+    signer_registration_total_since_startup_counter: MetricCounter(
+        "Number of signer registrations since startup on a Mithril signer node"
+    ),
+    signer_registration_success_last_epoch_gauge: MetricGauge(
+        "Latest epoch at which signer successfully registered on a Mithril signer node"
+    ),
+    signature_registration_success_since_startup_counter: MetricCounter(
+        "Number of successful signature registrations since startup on a Mithril signer node"
+    ),
+    signature_registration_total_since_startup_counter: MetricCounter(
+        "Number of signature registrations since startup on a Mithril signer node"
+    ),
+    signature_registration_success_last_epoch_gauge: MetricGauge(
+        "Latest epoch at which signature successfully registered on a Mithril signer node"
+    ),
+    // Runtime cycle metrics
+    runtime_cycle_success_since_startup_counter: MetricCounter(
+        "Number of successful runtime cycles since startup on a Mithril signer node"
+    ),
+    runtime_cycle_total_since_startup_counter: MetricCounter(
+        "Number of runtime cycles since startup on a Mithril signer node"
+    )
 
-impl MetricsServiceTrait for MetricsService {
-    /// Export the metrics as a string with the Open Metrics standard format.
-    /// These metrics can be exposed on an HTTP server.
-    fn export_metrics(&self) -> StdResult<String> {
-        metrics_tools::export_metrics(&self.registry)
-    }
-}
+);
 
+// TODO these functions are kept while changes are made for all uses
 impl MetricsService {
-    /// Create a new `MetricsService` instance.
-    pub fn new(logger: Logger) -> StdResult<Self> {
-        let logger = logger.new_with_component_name::<Self>();
-
-        let registry = Registry::new();
-
-        fn register<T: MithrilMetric>(registry: &Registry, metric: T) -> StdResult<T> {
-            registry.register(metric.collector())?;
-            Ok(metric)
-        }
-
-        let signer_registration_success_since_startup_counter = register(
-            &registry,
-            MetricCounter::new(
-                logger.clone(),
-                "mithril_signer_signer_registration_success_since_startup",
-                "Number of successful signer registrations since startup on a Mithril signer node",
-            )?,
-        )?;
-
-        let signer_registration_total_since_startup_counter = register(
-            &registry,
-            MetricCounter::new(
-                logger.clone(),
-                "mithril_signer_signer_registration_total_since_startup",
-                "Number of signer registrations since startup on a Mithril signer node",
-            )?,
-        )?;
-
-        let signer_registration_success_last_epoch_gauge = register(
-            &registry,
-            MetricGauge::new(
-                logger.clone(),
-                "mithril_signer_signer_registration_success_last_epoch",
-                "Latest epoch at which signer successfully registered on a Mithril signer node",
-            )?,
-        )?;
-        // Signature registration metrics
-
-        let signature_registration_success_since_startup_counter = register(
-            &registry,
-            MetricCounter::new(
-                logger.clone(),
-                "mithril_signer_signature_registration_success_since_startup",
-                "Number of successful signature registrations since startup on a Mithril signer node",
-            )?,
-        )?;
-
-        let signature_registration_total_since_startup_counter = register(
-            &registry,
-            MetricCounter::new(
-                logger.clone(),
-                "mithril_signer_signature_registration_total_since_startup",
-                "Number of signature registrations since startup on a Mithril signer node",
-            )?,
-        )?;
-
-        let signature_registration_success_last_epoch_gauge = register(
-            &registry,
-            MetricGauge::new(
-                logger.clone(),
-                "mithril_signer_signature_registration_success_last_epoch",
-                "Latest epoch at which signature successfully registered on a Mithril signer node",
-            )?,
-        )?;
-
-        // Runtime cycle metrics
-        let runtime_cycle_success_since_startup_counter = register(
-            &registry,
-            MetricCounter::new(
-                logger.clone(),
-                "mithril_signer_runtime_cycle_success_since_startup",
-                "Number of successful runtime cycles since startup on a Mithril signer node",
-            )?,
-        )?;
-        let runtime_cycle_total_since_startup_counter = register(
-            &registry,
-            MetricCounter::new(
-                logger.clone(),
-                "mithril_signer_runtime_cycle_total_since_startup",
-                "Number of runtime cycles since startup on a Mithril signer node",
-            )?,
-        )?;
-
-        Ok(Self {
-            registry,
-            signer_registration_success_since_startup_counter,
-            signer_registration_total_since_startup_counter,
-            signer_registration_success_last_epoch_gauge,
-            signature_registration_success_since_startup_counter,
-            signature_registration_total_since_startup_counter,
-            signature_registration_success_last_epoch_gauge,
-            runtime_cycle_success_since_startup_counter,
-            runtime_cycle_total_since_startup_counter,
-        })
-    }
-
     /// Increment the `signer_registration_success_since_startup` counter.
     pub fn signer_registration_success_since_startup_counter_increment(&self) {
-        self.signer_registration_success_since_startup_counter
+        self.get_signer_registration_success_since_startup_counter()
             .record();
     }
 
     /// Get the `signer_registration_success_since_startup` counter.
     pub fn signer_registration_success_since_startup_counter_get(&self) -> CounterValue {
-        self.signer_registration_success_since_startup_counter.get()
+        self.get_signer_registration_success_since_startup_counter()
+            .get()
     }
 
     /// Increment the `signer_registration_total_since_startup` counter.
     pub fn signer_registration_total_since_startup_counter_increment(&self) {
-        self.signer_registration_total_since_startup_counter
+        self.get_signer_registration_total_since_startup_counter()
             .record();
     }
 
     /// Get the `signer_registration_total_since_startup` counter.
     pub fn signer_registration_total_since_startup_counter_get(&self) -> CounterValue {
-        self.signer_registration_total_since_startup_counter.get()
+        self.get_signer_registration_total_since_startup_counter()
+            .get()
     }
 
     /// Set the `signer_registration_success_last_epoch` gauge value.
     pub fn signer_registration_success_last_epoch_gauge_set(&self, value: Epoch) {
-        self.signer_registration_success_last_epoch_gauge
+        self.get_signer_registration_success_last_epoch_gauge()
             .record(value);
     }
 
     /// Get the `signer_registration_success_last_epoch` gauge value.
     pub fn signer_registration_success_last_epoch_gauge_get(&self) -> Epoch {
-        self.signer_registration_success_last_epoch_gauge.get()
+        self.get_signer_registration_success_last_epoch_gauge()
+            .get()
     }
 
     /// Increment the `signature_registration_success_since_startup` counter.
     pub fn signature_registration_success_since_startup_counter_increment(&self) {
-        self.signature_registration_success_since_startup_counter
+        self.get_signature_registration_success_since_startup_counter()
             .record();
     }
 
     /// Get the `signature_registration_success_since_startup` counter.
     pub fn signature_registration_success_since_startup_counter_get(&self) -> CounterValue {
-        self.signature_registration_success_since_startup_counter
+        self.get_signature_registration_success_since_startup_counter()
             .get()
     }
 
     /// Increment the `signature_registration_total_since_startup` counter.
     pub fn signature_registration_total_since_startup_counter_increment(&self) {
-        self.signature_registration_total_since_startup_counter
+        self.get_signature_registration_total_since_startup_counter()
             .record();
     }
 
     /// Get the `signature_registration_total_since_startup` counter.
     pub fn signature_registration_total_since_startup_counter_get(&self) -> CounterValue {
-        self.signature_registration_total_since_startup_counter
+        self.get_signature_registration_total_since_startup_counter()
             .get()
     }
 
     /// Set the `signature_registration_success_last_epoch` gauge value.
     pub fn signature_registration_success_last_epoch_gauge_set(&self, value: Epoch) {
-        self.signature_registration_success_last_epoch_gauge
+        self.get_signature_registration_success_last_epoch_gauge()
             .record(value);
     }
 
     /// Get the `signature_registration_success_last_epoch` gauge value.
     pub fn signature_registration_success_last_epoch_gauge_get(&self) -> Epoch {
-        self.signature_registration_success_last_epoch_gauge.get()
+        self.get_signature_registration_success_last_epoch_gauge()
+            .get()
     }
 
     /// Increment the `runtime_cycle_total_since_startup` counter.
     pub fn runtime_cycle_total_since_startup_counter_increment(&self) {
-        self.runtime_cycle_total_since_startup_counter.record();
+        self.get_runtime_cycle_total_since_startup_counter()
+            .record();
     }
 
     /// Get the `runtime_cycle_total_since_startup` counter.
     pub fn runtime_cycle_total_since_startup_counter_get(&self) -> CounterValue {
-        self.runtime_cycle_total_since_startup_counter.get()
+        self.get_runtime_cycle_total_since_startup_counter().get()
     }
 
     /// Increment the `runtime_cycle_success_since_startup` counter.
     pub fn runtime_cycle_success_since_startup_counter_increment(&self) {
-        self.runtime_cycle_success_since_startup_counter.record();
+        self.get_runtime_cycle_success_since_startup_counter()
+            .record();
     }
 
     /// Get the `runtime_cycle_success_since_startup` counter.
     pub fn runtime_cycle_success_since_startup_counter_get(&self) -> CounterValue {
-        self.runtime_cycle_success_since_startup_counter.get()
-    }
-
-    /// Get the `signer_registration_success_since_startup_counter` counter.
-    pub fn get_signer_registration_success_since_startup_counter(&self) -> &MetricCounter {
-        &self.signer_registration_success_since_startup_counter
-    }
-
-    /// Get the `signer_registration_total_since_startup_counter` counter.
-    pub fn get_signer_registration_total_since_startup_counter(&self) -> &MetricCounter {
-        &self.signer_registration_total_since_startup_counter
-    }
-
-    /// Get the `signer_registration_success_last_epoch_gauge` counter.
-    pub fn get_signer_registration_success_last_epoch_gauge(&self) -> &MetricGauge {
-        &self.signer_registration_success_last_epoch_gauge
-    }
-
-    /// Get the `signature_registration_success_since_startup_counter` counter.
-    pub fn get_signature_registration_success_since_startup_counter(&self) -> &MetricCounter {
-        &self.signature_registration_success_since_startup_counter
-    }
-
-    /// Get the `signature_registration_total_since_startup_counter` counter.
-    pub fn get_signature_registration_total_since_startup_counter(&self) -> &MetricCounter {
-        &self.signature_registration_total_since_startup_counter
-    }
-
-    /// Get the `signature_registration_success_last_epoch_gauge` counter.
-    pub fn get_signature_registration_success_last_epoch_gauge(&self) -> &MetricGauge {
-        &self.signature_registration_success_last_epoch_gauge
-    }
-
-    /// Get the `runtime_cycle_success_since_startup_counter` counter.
-    pub fn get_runtime_cycle_success_since_startup_counter(&self) -> &MetricCounter {
-        &self.runtime_cycle_success_since_startup_counter
-    }
-
-    /// Get the `runtime_cycle_total_since_startup_counter` counter.
-    pub fn get_runtime_cycle_total_since_startup_counter(&self) -> &MetricCounter {
-        &self.runtime_cycle_total_since_startup_counter
+        self.get_runtime_cycle_success_since_startup_counter().get()
     }
 }
 
