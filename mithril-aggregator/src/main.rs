d1a1ee59124cc420d7e13647747afe4bed7624e3
DJO <790521+Alenar@users.noreply.github.com>
2024-10-11 19:06:58 +0200

    style(aggregator): set application name in log to `mithril-aggregator`
    Instead of the default `slog-rs`.

--- mithril-aggregator/src/main.rs
+++ mithril-aggregator/src/main.rs
@@ -1,17 +1,21 @@
 #![doc = include_str!("../README.md")]
 
 use clap::Parser;
-use mithril_aggregator::{CommandType, MainOpts};
-use mithril_common::StdResult;
 use slog::{Drain, Fuse, Level, Logger};
 use slog_async::Async;
 use std::sync::Arc;
 
+use mithril_aggregator::{CommandType, MainOpts};
+use mithril_common::StdResult;
+
 fn build_io_logger<W: std::io::Write + Send + 'static>(log_level: Level, io: W) -> Fuse<Async> {
-    let drain = slog_bunyan::new(io).set_pretty(false).build().fuse();
+    let drain = slog_bunyan::with_name("mithril-aggregator", io)
+        .set_pretty(false)
+        .build()
+        .fuse();
     let drain = slog::LevelFilter::new(drain, log_level).fuse();
 
-    slog_async::Async::new(drain).build().fuse()
+    Async::new(drain).build().fuse()
 }
 
 /// Build a logger from args.
