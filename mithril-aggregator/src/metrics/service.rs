d3937f02bc334e1d113bd50bd1c60b83c36a8e89
sfauvel <sfauvel@palo-it.com>
2024-10-15 16:45:43 +0200

    Add MetricsService to the dependency_builder and start MetricsServer

--- mithril-aggregator/src/metrics/service.rs
+++ mithril-aggregator/src/metrics/service.rs
@@ -1,47 +1,10 @@
 use mithril_metric::{build_metrics_service, MetricsServiceExporter};
-use prometheus::Registry;
-use slog::Logger;
 
-use mithril_common::{entities::Epoch, StdResult};
+use mithril_metric::metric::{MetricCollector, MetricCounter};
 
-use mithril_metric::metric::{CounterValue, MetricCollector, MetricCounter, MetricGauge};
-
+// TODO Change help text to reflect the actual metric
 build_metrics_service!(
     MetricsService,
-    // signer_registration_success_since_startup_counter:MetricCounter(
-    //     "mithril_signer_signer_registration_success_since_startup",
-    //     "Number of successful signer registrations since startup on a Mithril signer node"
-    // ),
-    // signer_registration_total_since_startup_counter:MetricCounter(
-    //     "mithril_signer_signer_registration_total_since_startup",
-    //     "Number of signer registrations since startup on a Mithril signer node"
-    // ),
-    // signer_registration_success_last_epoch_gauge:MetricGauge(
-    //     "mithril_signer_signer_registration_success_last_epoch",
-    //     "Latest epoch at which signer successfully registered on a Mithril signer node"
-    // ),
-    // signature_registration_success_since_startup_counter:MetricCounter(
-    //     "mithril_signer_signature_registration_success_since_startup",
-    //     "Number of successful signature registrations since startup on a Mithril signer node"
-    // ),
-    // signature_registration_total_since_startup_counter:MetricCounter(
-    //     "mithril_signer_signature_registration_total_since_startup",
-    //     "Number of signature registrations since startup on a Mithril signer node"
-    // ),
-    // signature_registration_success_last_epoch_gauge:MetricGauge(
-    //     "mithril_signer_signature_registration_success_last_epoch",
-    //     "Latest epoch at which signature successfully registered on a Mithril signer node"
-    // ),
-    // // Runtime cycle metrics
-    // runtime_cycle_success_since_startup_counter:MetricCounter(
-    //     "mithril_signer_runtime_cycle_success_since_startup",
-    //     "Number of successful runtime cycles since startup on a Mithril signer node"
-    // ),
-    // runtime_cycle_total_since_startup_counter:MetricCounter(
-    //     "mithril_signer_runtime_cycle_total_since_startup",
-    //     "Number of runtime cycles since startup on a Mithril signer node"
-    // )
-
     certificate_detail_total_served_since_startup:MetricCounter(
         "mithril_aggregator_certificate_detail_total_served_since_startup_counter",
         "certificate_detail_total_served_since_startup"
@@ -104,8 +67,3 @@ build_metrics_service!(
     )
 
 );
-
-#[cfg(test)]
-mod tests {
-    // TODO Do we need tests?
-}
