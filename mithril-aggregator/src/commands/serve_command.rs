a05781c833c29e5b0459ed744263a2639c263ec0
Damien Lachaume <135982616+dlachaume@users.noreply.github.com>
2024-10-22 10:12:40 +0200

    fix: allow use of clap parameters that where missing

--- mithril-aggregator/src/commands/serve_command.rs
+++ mithril-aggregator/src/commands/serve_command.rs
@@ -87,6 +87,31 @@ impl Source for ServeCommand {
                 ),
             );
         }
+        result.insert(
+            "disable_digests_cache".to_string(),
+            Value::new(
+                Some(&namespace),
+                ValueKind::from(self.disable_digests_cache),
+            ),
+        );
+        result.insert(
+            "reset_digests_cache".to_string(),
+            Value::new(Some(&namespace), ValueKind::from(self.reset_digests_cache)),
+        );
+        result.insert(
+            "allow_unparsable_block".to_string(),
+            Value::new(
+                Some(&namespace),
+                ValueKind::from(self.allow_unparsable_block),
+            ),
+        );
+        result.insert(
+            "enable_metrics_server".to_string(),
+            Value::new(
+                Some(&namespace),
+                ValueKind::from(self.enable_metrics_server),
+            ),
+        );
         if let Some(metrics_server_ip) = self.metrics_server_ip.clone() {
             result.insert(
                 "metrics_server_ip".to_string(),
