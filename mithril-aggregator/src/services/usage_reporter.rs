fd1ff67ce799e9c1fc1cba7fd4293d56c6094320
sfauvel <sfauvel@palo-it.com>
2024-10-28 15:06:06 +0100

    Format message content to json including duration and metric date

--- mithril-aggregator/src/services/usage_reporter.rs
+++ mithril-aggregator/src/services/usage_reporter.rs
@@ -4,9 +4,18 @@ use crate::{
     event_store::{EventMessage, TransmitterService},
     MetricsService,
 };
+use chrono::{DateTime, Utc};
 use mithril_common::logging::LoggerExtensions;
+use serde::{Deserialize, Serialize};
 use slog::{info, Logger};
 
+/// Message sent to the event store to report a metric value.
+#[derive(Serialize, Deserialize)]
+struct MetricMessage {
+    counter: i64,
+    duration: Duration,
+    date: DateTime<Utc>,
+}
 /// Reporter of metrics about the usage of the application.
 pub struct UsageReporter {
     transmitter_service: Arc<TransmitterService<EventMessage>>,
@@ -45,28 +54,36 @@ impl UsageReporter {
             .collect()
     }
 
-    fn send_metrics(&mut self) -> () {
+    fn send_metrics(&mut self, duration: &Duration) {
         let metrics = self.metric_service.export_metrics_map();
         let delta = UsageReporter::metrics_delta(&self.last_metrics, &metrics);
+        let date = Utc::now();
+
         self.last_metrics = metrics;
 
         for (name, value) in delta {
-            let _result = self.transmitter_service.send_event_message::<i64>(
-                "Metrics",
-                &name,
-                &value,
-                vec![],
-            );
+            let _result = self
+                .transmitter_service
+                .send_event_message::<MetricMessage>(
+                    "Metrics",
+                    &name,
+                    &MetricMessage {
+                        counter: value,
+                        duration: *duration,
+                        date,
+                    },
+                    vec![],
+                );
         }
     }
 
-    /// Start a loop that call [run][Self::send_metrics] at the given time interval.
+    /// Start a loop that send event about metrics at the given time interval.
     pub async fn run_forever(&mut self, run_interval: Duration) {
         let mut interval = tokio::time::interval(run_interval);
 
         loop {
             interval.tick().await;
-            self.send_metrics();
+            self.send_metrics(&run_interval);
 
             info!(
                 self.logger,
@@ -80,10 +97,27 @@ impl UsageReporter {
 #[cfg(test)]
 mod tests {
     use mithril_metric::MetricCollector;
+    use tokio::sync::mpsc::UnboundedReceiver;
 
     use super::UsageReporter;
     use super::*;
     use crate::test_tools::TestLogger;
+
+    fn build_usage_reporter() -> (
+        UsageReporter,
+        Arc<MetricsService>,
+        UnboundedReceiver<EventMessage>,
+    ) {
+        let (tx, rx) = tokio::sync::mpsc::unbounded_channel::<EventMessage>();
+        let metric_service = Arc::new(MetricsService::new(TestLogger::stdout()).unwrap());
+        let transmitter_service = Arc::new(TransmitterService::new(tx, TestLogger::stdout()));
+        let usage_reporter = UsageReporter::new(
+            transmitter_service.clone(),
+            metric_service.clone(),
+            TestLogger::stdout(),
+        );
+        (usage_reporter, metric_service, rx)
+    }
 
     fn received_messages(
         rx: &mut tokio::sync::mpsc::UnboundedReceiver<EventMessage>,
@@ -95,65 +129,61 @@ mod tests {
         received_messages
     }
 
+    fn extract_metric_value(message: &EventMessage) -> (String, i64) {
+        let metric_delta: MetricMessage = serde_json::from_value(message.content.clone()).unwrap();
+        (message.action.clone(), metric_delta.counter)
+    }
+
     #[tokio::test]
     async fn when_no_metrics_no_message_sent() {
-        let (tx, mut rx) = tokio::sync::mpsc::unbounded_channel::<EventMessage>();
-        let metric_service = Arc::new(MetricsService::new(TestLogger::stdout()).unwrap());
-        let transmitter_service = Arc::new(TransmitterService::new(tx, TestLogger::stdout()));
-        let mut usage_reporter = UsageReporter::new(
-            transmitter_service.clone(),
-            metric_service.clone(),
-            TestLogger::stdout(),
-        );
+        let (mut usage_reporter, _metric_service, mut rx) = build_usage_reporter();
 
-        usage_reporter.send_metrics();
+        usage_reporter.send_metrics(&Duration::from_secs(10));
 
         let received_messages = received_messages(&mut rx);
         assert_eq!(0, received_messages.len());
     }
 
     #[tokio::test]
+    async fn verify_event_content_on_a_metric() {
+        let (mut usage_reporter, metric_service, mut rx) = build_usage_reporter();
+
+        let metric = metric_service.get_certificate_total_produced_since_startup();
+        metric.increment_by(3);
+        usage_reporter.send_metrics(&Duration::from_secs(10));
+
+        let received_messages = received_messages(&mut rx);
+        let message = &received_messages[0];
+        assert_eq!(message.source, "Metrics");
+        assert_eq!(message.action, metric.name());
+        let message_content: MetricMessage =
+            serde_json::from_value(message.content.clone()).unwrap();
+        assert_eq!(3, message_content.counter);
+        assert_eq!(Duration::from_secs(10), message_content.duration);
+    }
+
+    #[tokio::test]
     async fn send_one_message_for_each_non_zero_value_metrics() {
-        let (tx, mut rx) = tokio::sync::mpsc::unbounded_channel::<EventMessage>();
-        let metric_service = Arc::new(MetricsService::new(TestLogger::stdout()).unwrap());
-        let transmitter_service = Arc::new(TransmitterService::new(tx, TestLogger::stdout()));
-        let mut usage_reporter = UsageReporter::new(
-            transmitter_service.clone(),
-            metric_service.clone(),
-            TestLogger::stdout(),
-        );
+        let (mut usage_reporter, metric_service, mut rx) = build_usage_reporter();
 
         let metric_1 = metric_service.get_certificate_total_produced_since_startup();
         let metric_2 = metric_service.get_certificate_detail_total_served_since_startup();
         metric_1.increment();
         metric_2.increment();
 
-        usage_reporter.send_metrics();
+        usage_reporter.send_metrics(&Duration::from_secs(10));
 
         let received_messages = received_messages(&mut rx);
+        let metric_values: Vec<(String, i64)> =
+            received_messages.iter().map(extract_metric_value).collect();
 
-        assert!(received_messages.contains(&EventMessage::new(
-            "Metrics",
-            &metric_1.name(),
-            serde_json::json!(1)
-        )));
-        assert!(received_messages.contains(&EventMessage::new(
-            "Metrics",
-            &metric_2.name(),
-            serde_json::json!(1)
-        )));
+        assert!(metric_values.contains(&(metric_1.name(), 1)));
+        assert!(metric_values.contains(&(metric_2.name(), 1)));
     }
 
     #[tokio::test]
     async fn resend_only_delta_since_last_send() {
-        let (tx, mut rx) = tokio::sync::mpsc::unbounded_channel::<EventMessage>();
-        let metric_service = Arc::new(MetricsService::new(TestLogger::stdout()).unwrap());
-        let transmitter_service = Arc::new(TransmitterService::new(tx, TestLogger::stdout()));
-        let mut usage_reporter = UsageReporter::new(
-            transmitter_service.clone(),
-            metric_service.clone(),
-            TestLogger::stdout(),
-        );
+        let (mut usage_reporter, metric_service, mut rx) = build_usage_reporter();
 
         let metric_1 = metric_service.get_certificate_total_produced_since_startup();
         let metric_2 = metric_service.get_certificate_detail_total_served_since_startup();
@@ -161,7 +191,7 @@ mod tests {
         {
             metric_1.increment_by(12);
             metric_2.increment_by(5);
-            usage_reporter.send_metrics();
+            usage_reporter.send_metrics(&Duration::from_secs(10));
 
             let received_messages = received_messages(&mut rx);
             assert_eq!(2, received_messages.len());
@@ -169,26 +199,26 @@ mod tests {
         {
             metric_2.increment_by(20);
             metric_2.increment_by(33);
-            usage_reporter.send_metrics();
+            usage_reporter.send_metrics(&Duration::from_secs(10));
 
             let received_messages = received_messages(&mut rx);
             assert_eq!(1, received_messages.len());
 
             assert_eq!(
-                EventMessage::new("Metrics", &metric_2.name(), serde_json::json!(53)),
-                received_messages[0]
+                (metric_2.name(), 53),
+                extract_metric_value(&received_messages[0])
             );
         }
         {
             metric_2.increment_by(15);
-            usage_reporter.send_metrics();
+            usage_reporter.send_metrics(&Duration::from_secs(10));
 
             let received_messages = received_messages(&mut rx);
             assert_eq!(1, received_messages.len());
 
             assert_eq!(
-                EventMessage::new("Metrics", &metric_2.name(), serde_json::json!(15)),
-                received_messages[0]
+                (metric_2.name(), 15),
+                extract_metric_value(&received_messages[0])
             );
         }
     }
@@ -275,5 +305,3 @@ mod tests {
         }
     }
 }
-
-// TODO What are the source, action, json content ?
