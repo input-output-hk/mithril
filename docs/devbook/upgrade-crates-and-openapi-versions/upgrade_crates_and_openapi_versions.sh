cf13db29c02249411bd9845f6e6dd08f4f21bcf8
sfauvel <sfauvel@palo-it.com>
2024-11-05 17:11:21 +0100

    Add `--update` option to not commit unfollow files

--- docs/devbook/upgrade-crates-and-openapi-versions/upgrade_crates_and_openapi_versions.sh
+++ docs/devbook/upgrade-crates-and-openapi-versions/upgrade_crates_and_openapi_versions.sh
@@ -106,7 +106,7 @@ else
 
   if [ true = $COMMIT ]
   then
-    git add $OPEN_API_FILE Cargo.lock ./*/Cargo.toml ./internal/*/Cargo.toml ./mithril-test-lab/*/Cargo.toml
+    git add --update $OPEN_API_FILE Cargo.lock ./*/Cargo.toml ./internal/*/Cargo.toml ./mithril-test-lab/*/Cargo.toml
     git commit -m "$COMMIT_MESSAGE"
   fi
 fi
