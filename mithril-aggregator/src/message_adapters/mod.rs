0b62312715c1209ed7591061fb8c5a87b66b631a
DJO <790521+Alenar@users.noreply.github.com>
2024-11-05 19:15:00 +0100

    refactor(aggregator): remove unused adapters

--- mithril-aggregator/src/message_adapters/mod.rs
+++ mithril-aggregator/src/message_adapters/mod.rs
@@ -1,33 +1,9 @@
 mod from_register_signature;
 mod from_register_signer;
-mod to_cardano_stake_distribution_list_message;
-mod to_cardano_stake_distribution_message;
-mod to_cardano_transaction_list_message;
-mod to_cardano_transaction_message;
 mod to_cardano_transactions_proof_message;
 mod to_certificate_pending_message;
-mod to_mithril_stake_distribution_list_message;
-mod to_mithril_stake_distribution_message;
-mod to_snapshot_list_message;
-mod to_snapshot_message;
 
 pub use from_register_signature::FromRegisterSingleSignatureAdapter;
 pub use from_register_signer::FromRegisterSignerAdapter;
-#[cfg(test)]
-pub use to_cardano_stake_distribution_list_message::ToCardanoStakeDistributionListMessageAdapter;
-#[cfg(test)]
-pub use to_cardano_stake_distribution_message::ToCardanoStakeDistributionMessageAdapter;
-#[cfg(test)]
-pub use to_cardano_transaction_list_message::ToCardanoTransactionListMessageAdapter;
-#[cfg(test)]
-pub use to_cardano_transaction_message::ToCardanoTransactionMessageAdapter;
 pub use to_cardano_transactions_proof_message::ToCardanoTransactionsProofsMessageAdapter;
 pub use to_certificate_pending_message::ToCertificatePendingMessageAdapter;
-#[cfg(test)]
-pub use to_mithril_stake_distribution_list_message::ToMithrilStakeDistributionListMessageAdapter;
-#[cfg(test)]
-pub use to_mithril_stake_distribution_message::ToMithrilStakeDistributionMessageAdapter;
-#[cfg(test)]
-pub use to_snapshot_list_message::ToSnapshotListMessageAdapter;
-#[cfg(test)]
-pub use to_snapshot_message::ToSnapshotMessageAdapter;
