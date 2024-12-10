594f0e166a9c250febf000c908f40aba19a7b507
Damien Lachaume <135982616+dlachaume@users.noreply.github.com>
2024-12-10 10:58:57 +0100

    refactor: rename uploader implementation modules for consistency

--- mithril-aggregator/src/file_uploaders/mod.rs
+++ mithril-aggregator/src/file_uploaders/mod.rs
@@ -1,13 +1,13 @@
-mod dumb_snapshot_uploader;
+mod dumb_file_uploader;
 mod file_uploader;
-mod local_snapshot_uploader;
-mod remote_snapshot_uploader;
+mod local_file_uploader;
+mod remote_file_uploader;
 
-pub use dumb_snapshot_uploader::*;
+pub use dumb_file_uploader::*;
 pub use file_uploader::FileLocation;
 pub use file_uploader::FileUploader;
-pub use local_snapshot_uploader::LocalSnapshotUploader;
-pub use remote_snapshot_uploader::RemoteSnapshotUploader;
+pub use local_file_uploader::LocalSnapshotUploader;
+pub use remote_file_uploader::RemoteSnapshotUploader;
 
 #[cfg(test)]
 pub use file_uploader::MockFileUploader;
