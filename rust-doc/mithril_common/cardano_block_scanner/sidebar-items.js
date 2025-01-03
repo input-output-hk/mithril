b6c5bc9295fdbdad1e05824ad1b0be5ce53f9437
jpraynaud <jpraynaud@users.noreply.github.com>
2025-01-03 15:35:14 +0000

    deploy: 27cee3f5fdf27348544977c281e2f135b47dadd9

---
+++ rust-doc/mithril_common/cardano_block_scanner/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["ChainScannedBlocks"],"struct":["CardanoBlockScanner","ChainReaderBlockStreamer","DumbBlockScanner","DumbBlockStreamer","RawCardanoPoint","ScannedBlock"],"trait":["BlockScanner","BlockStreamer","BlockStreamerTestExtensions"]};
