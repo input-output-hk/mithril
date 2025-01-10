7d02e2469f7f45e916527ce03edb5502a1e1dab3
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2025-01-10 11:58:27 +0100

    chore: update default devnet and e2e test Cardano node version to '10.1.4'

--- mithril-test-lab/mithril-devnet/mkfiles/mkfiles-init.sh
+++ mithril-test-lab/mithril-devnet/mkfiles/mkfiles-init.sh
@@ -9,7 +9,7 @@ esac
 
 # Cardano node version
 if [ -z "${CARDANO_NODE_VERSION}" ]; then 
-  CARDANO_NODE_VERSION="10.1.3"
+  CARDANO_NODE_VERSION="10.1.4"
 fi
 if [ -z "${CARDANO_NODE_VERSION_RELEASE}" ]; then 
   CARDANO_NODE_VERSION_RELEASE=$(echo "${CARDANO_NODE_VERSION}" | cut -d'-' -f1)
