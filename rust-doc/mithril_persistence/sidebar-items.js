dfc30026865302e04993d9a16bada432b09ac115
sfauvel <sfauvel@users.noreply.github.com>
2025-01-28 13:21:37 +0000

    deploy: 4ee78f34a0c8f0d8eea0b03aaf2d5b2a1afdeb8c

---
+++ rust-doc/mithril_persistence/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"mod":["database","sqlite","store"]};
---
+++ rust-doc/trait.impl/mithril_common/signable_builder/cardano_transactions/trait.BlockRangeRootRetriever.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["mithril_persistence",[["impl&lt;S: MKTreeStorer&gt; BlockRangeRootRetriever&lt;S&gt; for <a class=\"struct\" href=\"mithril_persistence/database/repository/struct.CardanoTransactionRepository.html\" title=\"struct mithril_persistence::database::repository::CardanoTransactionRepository\">CardanoTransactionRepository</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[326]}
