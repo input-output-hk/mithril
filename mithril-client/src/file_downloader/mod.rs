e04db1d8b5f7b76a33e8f5ec4f306fdd08bad32d
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2025-01-31 15:44:45 +0100

    feat: implement a file downloader resolver trait

--- mithril-client/src/file_downloader/mod.rs
+++ mithril-client/src/file_downloader/mod.rs
@@ -3,7 +3,11 @@
 //! This module provides the necessary abstractions to download files from different sources.
 
 mod interface;
+mod resolver;
 
 #[cfg(test)]
 pub use interface::MockFileDownloader;
 pub use interface::{FileDownloader, FileDownloaderUri};
+#[cfg(test)]
+pub use resolver::MockFileDownloaderResolver;
+pub use resolver::{FileDownloaderResolver, ImmutablesFileDownloaderResolver};
