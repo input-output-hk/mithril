3409f06a42a766d751be6d89ddc318410afdb992
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2025-02-04 16:39:16 +0100

    refactor: extend 'ImmutableDigester' trait
    To compute digests for a range of immutable file numbers.

--- mithril-common/src/digesters/immutable_digester.rs
+++ mithril-common/src/digesters/immutable_digester.rs
@@ -1,15 +1,22 @@
-use crate::{
-    crypto_helper::{MKTree, MKTreeStoreInMemory},
-    digesters::ImmutableFileListingError,
-    entities::{CardanoDbBeacon, ImmutableFileNumber},
-    StdError,
-};
 use async_trait::async_trait;
+use sha2::Sha256;
+use slog::{info, Logger};
 use std::{
+    collections::BTreeMap,
     io,
+    ops::RangeInclusive,
     path::{Path, PathBuf},
 };
 use thiserror::Error;
+
+use crate::{
+    crypto_helper::{MKTree, MKTreeStoreInMemory},
+    digesters::ImmutableFileListingError,
+    entities::{CardanoDbBeacon, HexEncodedDigest, ImmutableFileName, ImmutableFileNumber},
+    StdError,
+};
+
+use super::ImmutableFile;
 
 /// A digester than can compute the digest used for mithril signatures
 ///
@@ -68,6 +75,13 @@ pub trait ImmutableDigester: Sync + Send {
         beacon: &CardanoDbBeacon,
     ) -> Result<String, ImmutableDigesterError>;
 
+    /// Compute the digests for a range of immutable files
+    async fn compute_digests_for_range(
+        &self,
+        dirpath: &Path,
+        range: &RangeInclusive<ImmutableFileNumber>,
+    ) -> Result<ComputedImmutablesDigests, ImmutableDigesterError>;
+
     /// Compute the digests merkle tree
     async fn compute_merkle_tree(
         &self,
@@ -102,4 +116,99 @@ pub enum ImmutableDigesterError {
     /// Error raised when the Merkle tree computation failed.
     #[error("Merkle tree computation failed")]
     MerkleTreeComputationError(StdError),
+}
+
+/// Computed immutables digests
+pub struct ComputedImmutablesDigests {
+    pub entries: BTreeMap<ImmutableFile, HexEncodedDigest>,
+    pub(super) new_cached_entries: Vec<ImmutableFileName>,
+}
+
+impl ComputedImmutablesDigests {
+    pub(super) fn compute_immutables_digests(
+        entries: BTreeMap<ImmutableFile, Option<HexEncodedDigest>>,
+        logger: Logger,
+    ) -> Result<ComputedImmutablesDigests, io::Error> {
+        let mut new_cached_entries = Vec::new();
+        let mut progress = Progress {
+            index: 0,
+            total: entries.len(),
+        };
+
+        let mut digests = BTreeMap::new();
+
+        for (ix, (entry, cache)) in entries.into_iter().enumerate() {
+            let hash = match cache {
+                None => {
+                    new_cached_entries.push(entry.filename.clone());
+                    hex::encode(entry.compute_raw_hash::<Sha256>()?)
+                }
+                Some(digest) => digest,
+            };
+            digests.insert(entry, hash);
+
+            if progress.report(ix) {
+                info!(logger, "Hashing: {progress}");
+            }
+        }
+
+        Ok(ComputedImmutablesDigests {
+            entries: digests,
+            new_cached_entries,
+        })
+    }
+}
+
+pub(super) struct Progress {
+    pub(super) index: usize,
+    pub(super) total: usize,
+}
+
+impl Progress {
+    pub(super) fn report(&mut self, ix: usize) -> bool {
+        self.index = ix;
+        (20 * ix) % self.total == 0
+    }
+
+    pub(super) fn percent(&self) -> f64 {
+        (self.index as f64 * 100.0 / self.total as f64).ceil()
+    }
+}
+
+impl std::fmt::Display for Progress {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+        write!(f, "{}/{} ({}%)", self.index, self.total, self.percent())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn reports_progress_every_5_percent() {
+        let mut progress = Progress {
+            index: 0,
+            total: 7000,
+        };
+
+        assert!(!progress.report(1));
+        assert!(!progress.report(4));
+        assert!(progress.report(350));
+        assert!(!progress.report(351));
+    }
+
+    #[test]
+    fn reports_progress_when_total_lower_than_20() {
+        let mut progress = Progress {
+            index: 0,
+            total: 16,
+        };
+
+        assert!(progress.report(4));
+        assert!(progress.report(12));
+        assert!(!progress.report(3));
+        assert!(!progress.report(15));
+    }
 }
