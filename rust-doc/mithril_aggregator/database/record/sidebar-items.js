03ad6ef8cbe5ed9be722efc4444ecc02744afa86
jpraynaud <jpraynaud@users.noreply.github.com>
2025-02-06 16:17:08 +0000

    deploy: 7635d4e0ac2b447a2a2a78322b786f63293a2136

---
+++ rust-doc/mithril_aggregator/database/record/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["BufferedSingleSignatureRecord","CertificatePendingRecord","CertificateRecord","EpochSettingsRecord","ImmutableFileDigestRecord","OpenMessageRecord","OpenMessageWithSingleSignaturesRecord","SignedEntityRecord","SignerRecord","SignerRegistrationRecord","SingleSignatureRecord","StakePool"]};
