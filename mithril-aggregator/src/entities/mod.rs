6fc3288a368e9acf87b9d6a0e086289f36285c9d
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2025-03-03 15:30:37 +0100

    feat: add master aggregator epoch settings in aggregator

--- mithril-aggregator/src/entities/mod.rs
+++ mithril-aggregator/src/entities/mod.rs
@@ -3,11 +3,13 @@
 //! This module provide domain entities for the services & state machine.
 //!
 mod aggregator_epoch_settings;
+mod master_aggregator_epoch_settings;
 mod open_message;
 mod signer_registration_message;
 mod signer_ticker_message;
 
 pub use aggregator_epoch_settings::AggregatorEpochSettings;
+pub use master_aggregator_epoch_settings::MasterAggregatorEpochSettings;
 pub use open_message::OpenMessage;
 pub use signer_registration_message::{
     SignerRegistrationsListItemMessage, SignerRegistrationsMessage,
