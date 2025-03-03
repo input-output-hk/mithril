8d39610a27a2d5374cf42fb7348084ba3236fc86
sfauvel <sfauvel@palo-it.com>
2025-03-03 16:50:14 +0100

    feat: deprecate `get_snapshot` and `list_snapshots` and replace them by `list_cardano_database`and `get_cardano_database`

--- mithril-client-wasm/src/client_wasm.rs
+++ mithril-client-wasm/src/client_wasm.rs
@@ -136,9 +136,16 @@ impl MithrilClient {
         )))
     }
 
+    #[deprecated]
     /// Call the client to get a snapshot from a digest
     #[wasm_bindgen]
     pub async fn get_snapshot(&self, digest: &str) -> WasmResult {
+        self.get_cardano_database(digest).await
+    }
+
+    /// Call the client to get a snapshot from a digest
+    #[wasm_bindgen]
+    pub async fn get_cardano_database(&self, digest: &str) -> WasmResult {
         let result = self
             .client
             .snapshot()
@@ -152,9 +159,16 @@ impl MithrilClient {
         Ok(serde_wasm_bindgen::to_value(&result)?)
     }
 
+    #[deprecated]
     /// Call the client to get the list of available snapshots
     #[wasm_bindgen]
     pub async fn list_snapshots(&self) -> WasmResult {
+        self.list_cardano_database().await
+    }
+
+    /// Call the client to get the list of available snapshots
+    #[wasm_bindgen]
+    pub async fn list_cardano_database(&self) -> WasmResult {
         let result = self
             .client
             .snapshot()
