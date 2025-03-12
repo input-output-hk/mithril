c8be935aaaf2f884688633a3ecfe3d5fb9250ef8
DJO <790521+Alenar@users.noreply.github.com>
2025-03-12 17:32:46 +0100

    chore(aggregator): remove `SnapshotterCompressionAlgorithm`
    Not used anymore as the FileArchiver hold directly the algorithms
    parameters.

--- mithril-aggregator/src/services/snapshotter/interface.rs
+++ mithril-aggregator/src/services/snapshotter/interface.rs
@@ -3,7 +3,6 @@ use std::path::PathBuf;
 use mithril_common::StdResult;
 
 use crate::tools::file_archiver::FileArchive;
-use crate::ZstandardCompressionParameters;
 
 #[cfg_attr(test, mockall::automock)]
 /// Define the ability to create snapshots.
@@ -17,19 +16,4 @@ pub trait Snapshotter: Sync + Send {
         archive_name_without_extension: &str,
         files: Vec<PathBuf>,
     ) -> StdResult<FileArchive>;
-}
-
-/// Compression algorithm and parameters of the [crate::services::CompressedArchiveSnapshotter].
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum SnapshotterCompressionAlgorithm {
-    /// Gzip compression format
-    Gzip,
-    /// Zstandard compression format
-    Zstandard(ZstandardCompressionParameters),
-}
-
-impl From<ZstandardCompressionParameters> for SnapshotterCompressionAlgorithm {
-    fn from(params: ZstandardCompressionParameters) -> Self {
-        Self::Zstandard(params)
-    }
 }
