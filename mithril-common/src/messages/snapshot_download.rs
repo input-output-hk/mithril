f2625cf727ad7b345f044490678710b2009f6352
DJO <790521+Alenar@users.noreply.github.com>
2025-03-17 18:02:42 +0100

    feat: add `ancillary_locations` and `ancillary_size` to all snapshot related messages
    Co-authored-by: Damien Lachaume <dlachaume@users.noreply.github.com>

--- mithril-common/src/messages/snapshot_download.rs
+++ mithril-common/src/messages/snapshot_download.rs
@@ -14,11 +14,19 @@ pub struct SnapshotDownloadMessage {
     /// Mithril beacon on the Cardano chain
     pub beacon: CardanoDbBeacon,
 
-    /// Size of the snapshot file in Bytes
+    /// Size of the immutables snapshot file in Bytes
     pub size: u64,
 
-    /// Locations where the binary content of the snapshot can be retrieved
+    /// Size of the ancillary files in Bytes
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub ancillary_size: Option<u64>,
+
+    /// Locations where the snapshot of the immutable files can be retrieved
     pub locations: Vec<String>,
+
+    /// Locations where the snapshot of the ancillary files can be retrieved
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub ancillary_locations: Option<Vec<String>>,
 
     /// Compression algorithm of the snapshot archive
     pub compression_algorithm: CompressionAlgorithm,
@@ -38,7 +46,9 @@ impl SnapshotDownloadMessage {
                 immutable_file_number: 1728,
             },
             size: 807803196,
+            ancillary_size: Some(123456789),
             locations: vec!["https://host/certificate.tar.gz".to_string()],
+            ancillary_locations: Some(vec!["https://host/ancillary.tar.gz".to_string()]),
             compression_algorithm: CompressionAlgorithm::Gzip,
             cardano_node_version: "0.0.1".to_string(),
         }
@@ -49,6 +59,17 @@ impl SnapshotDownloadMessage {
 mod tests {
     use super::*;
 
+    #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+    pub struct SnapshotDownloadMessageUntilV0_1_47 {
+        pub digest: String,
+        pub network: String,
+        pub beacon: CardanoDbBeacon,
+        pub size: u64,
+        pub locations: Vec<String>,
+        pub compression_algorithm: CompressionAlgorithm,
+        pub cardano_node_version: String,
+    }
+
     const CURRENT_JSON: &str = r#"{
         "digest": "0b9f5ad7f33cc523775c82249294eb8a1541d54f08eb3107cafc5638403ec7c6",
         "network": "preview",
@@ -57,15 +78,19 @@ mod tests {
             "immutable_file_number": 1728
         },
         "size": 807803196,
+        "ancillary_size": 123456789,
         "locations": [
             "https://host/certificate.tar.gz"
+        ],
+        "ancillary_locations": [
+            "https://host/ancillary.tar.gz"
         ],
         "compression_algorithm": "gzip",
         "cardano_node_version": "0.0.1"
     }"#;
 
-    fn golden_message_current() -> SnapshotDownloadMessage {
-        SnapshotDownloadMessage {
+    fn golden_message_until_open_api_0_1_47() -> SnapshotDownloadMessageUntilV0_1_47 {
+        SnapshotDownloadMessageUntilV0_1_47 {
             digest: "0b9f5ad7f33cc523775c82249294eb8a1541d54f08eb3107cafc5638403ec7c6".to_string(),
             network: "preview".to_string(),
             beacon: CardanoDbBeacon {
@@ -77,6 +102,31 @@ mod tests {
             compression_algorithm: CompressionAlgorithm::Gzip,
             cardano_node_version: "0.0.1".to_string(),
         }
+    }
+
+    fn golden_message_current() -> SnapshotDownloadMessage {
+        SnapshotDownloadMessage {
+            digest: "0b9f5ad7f33cc523775c82249294eb8a1541d54f08eb3107cafc5638403ec7c6".to_string(),
+            network: "preview".to_string(),
+            beacon: CardanoDbBeacon {
+                epoch: Epoch(86),
+                immutable_file_number: 1728,
+            },
+            size: 807803196,
+            ancillary_size: Some(123456789),
+            locations: vec!["https://host/certificate.tar.gz".to_string()],
+            ancillary_locations: Some(vec!["https://host/ancillary.tar.gz".to_string()]),
+            compression_algorithm: CompressionAlgorithm::Gzip,
+            cardano_node_version: "0.0.1".to_string(),
+        }
+    }
+
+    #[test]
+    fn test_current_json_deserialized_into_message_supported_until_open_api_0_1_47() {
+        let json = CURRENT_JSON;
+        let message: SnapshotDownloadMessageUntilV0_1_47 = serde_json::from_str(json).unwrap();
+
+        assert_eq!(golden_message_until_open_api_0_1_47(), message);
     }
 
     #[test]
