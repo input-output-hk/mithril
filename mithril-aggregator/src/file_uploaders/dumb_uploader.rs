6870438ce7b6434e2c0314b309d1c549e1cfb880
DJO <790521+Alenar@users.noreply.github.com>
2025-03-18 12:13:45 +0100

    test(aggregator): make `DumbUploader` store multiple uploads instead of only the last
    Co-authored-by: Damien Lachaume <dlachaume@users.noreply.github.com>

--- mithril-aggregator/src/file_uploaders/dumb_uploader.rs
+++ mithril-aggregator/src/file_uploaders/dumb_uploader.rs
@@ -7,10 +7,10 @@ use crate::file_uploaders::{FileUploadRetryPolicy, FileUploader};
 
 /// Dummy uploader for test purposes.
 ///
-/// It actually does NOT upload any file but remembers the last file it
+/// It actually does NOT upload any file but keep the history of all the files it
 /// was asked to upload. This is intended to by used by integration tests.
 pub struct DumbUploader {
-    last_uploaded: RwLock<Option<FileUri>>,
+    uploads_history: RwLock<Vec<FileUri>>,
     retry_policy: FileUploadRetryPolicy,
 }
 
@@ -18,19 +18,30 @@ impl DumbUploader {
     /// Create a new instance with a custom retry policy.
     pub fn new(retry_policy: FileUploadRetryPolicy) -> Self {
         Self {
-            last_uploaded: RwLock::new(None),
+            uploads_history: RwLock::new(Vec::new()),
             retry_policy,
         }
     }
 
     /// Return the last upload that was triggered.
     pub fn get_last_upload(&self) -> StdResult<Option<FileUri>> {
-        let value = self
-            .last_uploaded
-            .read()
-            .map_err(|e| anyhow!("Error while saving filepath location: {e}"))?;
+        let last_upload = self.get_last_n_uploads(1)?;
+        Ok(last_upload.first().cloned())
+    }
 
-        Ok(value.as_ref().map(Clone::clone))
+    /// Return the last `n` uploads that were triggered in anti-chronological order.
+    pub fn get_last_n_uploads(&self, n: usize) -> StdResult<Vec<FileUri>> {
+        let value = self
+            .uploads_history
+            .read()
+            .map_err(|e| anyhow!(e.to_string()).context("Error while reading filepath location"))?;
+
+        Ok(value
+            .iter()
+            .rev()
+            .take(n)
+            .cloned()
+            .collect::<Vec<FileUri>>())
     }
 }
 
@@ -44,13 +55,13 @@ impl Default for DumbUploader {
 impl FileUploader for DumbUploader {
     /// Upload a file
     async fn upload_without_retry(&self, filepath: &Path) -> StdResult<FileUri> {
-        let mut value = self
-            .last_uploaded
+        let mut uploads_history = self
+            .uploads_history
             .write()
             .map_err(|e| anyhow!("Error while saving filepath location: {e}"))?;
 
         let location = FileUri(filepath.to_string_lossy().to_string());
-        *value = Some(location.clone());
+        uploads_history.push(location.clone());
 
         Ok(location)
     }
@@ -83,6 +94,36 @@ mod tests {
             uploader
                 .get_last_upload()
                 .expect("getting dumb uploader last value after a fake download should not fail")
+        );
+    }
+
+    #[tokio::test]
+    async fn get_history_of_multiple_uploads() {
+        let uploader = DumbUploader::default();
+        assert_eq!(
+            Vec::<FileUri>::new(),
+            uploader.get_last_n_uploads(usize::MAX).unwrap()
+        );
+
+        uploader.upload(Path::new("/tmp/whatever")).await.unwrap();
+        uploader.upload(Path::new("/tmp/whatever2")).await.unwrap();
+        uploader.upload(Path::new("/tmp/whatever3")).await.unwrap();
+
+        assert_eq!(
+            vec![
+                FileUri("/tmp/whatever3".to_string()),
+                FileUri("/tmp/whatever2".to_string()),
+                FileUri("/tmp/whatever".to_string()),
+            ],
+            uploader.get_last_n_uploads(usize::MAX).unwrap()
+        );
+
+        assert_eq!(
+            vec![
+                FileUri("/tmp/whatever3".to_string()),
+                FileUri("/tmp/whatever2".to_string()),
+            ],
+            uploader.get_last_n_uploads(2).unwrap()
         );
     }
 
