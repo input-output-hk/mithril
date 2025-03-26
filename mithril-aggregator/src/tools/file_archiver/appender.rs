4a1b6bb90687544dcc5147cd3e98a9bdd2860289
DJO <790521+Alenar@users.noreply.github.com>
2025-03-26 18:17:35 +0100

    feat(aggregator): add `AppenderData` and `ChainAppender`
    - `AppenderData` allow to add raw bytes from memory directly into a tar
      archive, with a utility ctor to be built from serializable object into
      json
    - `ChainAppender` allow to chain two appender together, allowing to pass
      them at once to a file archiver
    - Add a `chain` method on the `TarAppender` trait with a blanket
      implementation to allow easy chaining without needing to import of the
      `ChainAppender`.
    
    Co-authored-by: Damien Lachaume <dlachaume@users.noreply.github.com>

--- mithril-aggregator/src/tools/file_archiver/appender.rs
+++ mithril-aggregator/src/tools/file_archiver/appender.rs
@@ -1,4 +1,5 @@
 use anyhow::{anyhow, Context};
+use serde::Serialize;
 use std::fs::File;
 use std::io::Write;
 use std::path::PathBuf;
@@ -7,11 +8,20 @@ use mithril_common::StdResult;
 
 use crate::tools::file_size;
 
+const READ_WRITE_PERMISSION: u32 = 0o666;
+
 /// Define multiple ways to append content to a tar archive.
 pub trait TarAppender: Send {
     fn append<T: Write>(&self, tar: &mut tar::Builder<T>) -> StdResult<()>;
 
     fn compute_uncompressed_data_size(&self) -> StdResult<u64>;
+
+    fn chain<A2: TarAppender>(self, appender_right: A2) -> ChainAppender<Self, A2>
+    where
+        Self: Sized,
+    {
+        ChainAppender::new(self, appender_right)
+    }
 }
 
 pub struct AppenderDirAll {
@@ -154,6 +164,96 @@ impl TarAppender for AppenderEntries {
             .map(|entry| self.base_directory.join(entry))
             .collect();
         file_size::compute_size(full_entries_path)
+    }
+}
+
+/// Append data to the archive.
+pub struct AppenderData {
+    /// Location of the file in the archive where the data will be appended.
+    location_in_archive: PathBuf,
+    /// Byte array of the data to append.
+    bytes: Vec<u8>,
+}
+
+impl AppenderData {
+    /// Create a new instance of `AppenderData` from an object that will be serialized to JSON.
+    pub fn from_json<T: Serialize + Send>(
+        location_in_archive: PathBuf,
+        object: &T,
+    ) -> StdResult<Self> {
+        let json_bytes = serde_json::to_vec(object).with_context(|| {
+            format!(
+                "Can not serialize JSON to file in archive: {:?}",
+                location_in_archive.display()
+            )
+        })?;
+
+        Ok(Self::from_raw_bytes(location_in_archive, json_bytes))
+    }
+
+    /// Create a new instance of `AppenderData` from a byte array.
+    pub fn from_raw_bytes(location_in_archive: PathBuf, bytes: Vec<u8>) -> Self {
+        Self {
+            location_in_archive,
+            bytes,
+        }
+    }
+}
+
+impl TarAppender for AppenderData {
+    fn append<T: Write>(&self, tar: &mut tar::Builder<T>) -> StdResult<()> {
+        let mut header = tar::Header::new_gnu();
+        header.set_size(self.bytes.len() as u64);
+        header.set_mode(READ_WRITE_PERMISSION);
+        header.set_mtime(chrono::Utc::now().timestamp() as u64);
+        header.set_cksum();
+
+        tar.append_data(
+            &mut header,
+            &self.location_in_archive,
+            self.bytes.as_slice(),
+        )
+        .with_context(|| {
+            format!(
+                "Can not add file: '{}' to the archive",
+                self.location_in_archive.display()
+            )
+        })?;
+
+        Ok(())
+    }
+
+    fn compute_uncompressed_data_size(&self) -> StdResult<u64> {
+        Ok(self.bytes.len() as u64)
+    }
+}
+
+/// Chain multiple `TarAppender` instances together.
+pub struct ChainAppender<L, R> {
+    appender_left: L,
+    appender_right: R,
+}
+
+impl<L: TarAppender, R: TarAppender> ChainAppender<L, R> {
+    pub fn new(appender_left: L, appender_right: R) -> Self {
+        Self {
+            appender_left,
+            appender_right,
+        }
+    }
+}
+
+impl<L: TarAppender, R: TarAppender> TarAppender for ChainAppender<L, R> {
+    fn append<T: Write>(&self, tar: &mut tar::Builder<T>) -> StdResult<()> {
+        self.appender_left.append(tar)?;
+        self.appender_right.append(tar)
+    }
+
+    fn compute_uncompressed_data_size(&self) -> StdResult<u64> {
+        // Size is aggregated even if the data is overwritten by the right appender because we
+        // can't know if there is an overlap or not
+        Ok(self.appender_left.compute_uncompressed_data_size()?
+            + self.appender_right.compute_uncompressed_data_size()?)
     }
 }
 
@@ -388,6 +488,204 @@ mod tests {
             let expected_total_size =
                 (immutable_trio_file_size * 2) + (3 * ledger_file_size) + volatile_file_size;
             assert_eq!(expected_total_size, entries_size);
+        }
+    }
+
+    mod appender_data {
+        use flate2::read::GzDecoder;
+        use serde::Deserialize;
+
+        use super::*;
+
+        #[derive(Debug, PartialEq, Serialize, Deserialize)]
+        struct TestStruct {
+            field1: String,
+            field2: i32,
+        }
+
+        #[test]
+        fn append_serializable_json() {
+            let test_dir = temp_dir_create!();
+            let object = TestStruct {
+                field1: "test".to_string(),
+                field2: 42,
+            };
+            let location_in_archive = PathBuf::from("folder").join("test.json");
+
+            let data_appender =
+                AppenderData::from_json(location_in_archive.clone(), &object).unwrap();
+            let file_archiver = FileArchiver::new_for_test(test_dir.join("verification"));
+            let archive = file_archiver
+                .archive(
+                    ArchiveParameters {
+                        archive_name_without_extension: "archive".to_string(),
+                        target_directory: test_dir.clone(),
+                        compression_algorithm: CompressionAlgorithm::Gzip,
+                    },
+                    data_appender,
+                )
+                .unwrap();
+
+            let unpack_path = archive.unpack_gzip(&test_dir);
+            let unpacked_file_path = unpack_path.join(&location_in_archive);
+
+            assert!(unpacked_file_path.exists());
+
+            let deserialized_object: TestStruct =
+                serde_json::from_reader(File::open(unpacked_file_path).unwrap()).unwrap();
+            assert_eq!(object, deserialized_object);
+        }
+
+        #[test]
+        fn appended_entry_have_read_write_permissions_and_time_metadata() {
+            let test_dir = temp_dir_create!();
+            let object = TestStruct {
+                field1: "test".to_string(),
+                field2: 42,
+            };
+            let location_in_archive = PathBuf::from("folder").join("test.json");
+            let start_time_stamp = chrono::Utc::now().timestamp() as u64;
+
+            let data_appender =
+                AppenderData::from_json(location_in_archive.clone(), &object).unwrap();
+            let file_archiver = FileArchiver::new_for_test(test_dir.join("verification"));
+            let archive = file_archiver
+                .archive(
+                    ArchiveParameters {
+                        archive_name_without_extension: "archive".to_string(),
+                        target_directory: test_dir.clone(),
+                        compression_algorithm: CompressionAlgorithm::Gzip,
+                    },
+                    data_appender,
+                )
+                .unwrap();
+
+            let archive_file = File::open(archive.get_file_path()).unwrap();
+            let mut archive = tar::Archive::new(GzDecoder::new(archive_file));
+            let mut archive_entries = archive.entries().unwrap();
+            let appended_entry = archive_entries.next().unwrap().unwrap();
+
+            assert_eq!(
+                READ_WRITE_PERMISSION,
+                appended_entry.header().mode().unwrap()
+            );
+            let mtime = appended_entry.header().mtime().unwrap();
+            assert!(
+                mtime >= start_time_stamp,
+                "entry mtime should be greater than or equal to the timestamp before the archive\
+                creation:\n {mtime} < {start_time_stamp}"
+            );
+        }
+
+        #[test]
+        fn compute_uncompressed_size() {
+            let object = TestStruct {
+                field1: "test".to_string(),
+                field2: 42,
+            };
+
+            let data_appender =
+                AppenderData::from_json(PathBuf::from("whatever.json"), &object).unwrap();
+
+            let expected_size = serde_json::to_vec(&object).unwrap().len() as u64;
+            let entry_size = data_appender.compute_uncompressed_data_size().unwrap();
+            assert_eq!(expected_size, entry_size);
+        }
+    }
+
+    mod chain_appender {
+        use super::*;
+
+        #[test]
+        fn chain_non_overlapping_appenders() {
+            let test_dir = temp_dir_create!();
+            let file_to_archive = create_file(&test_dir, "test_file.txt");
+            let json_location_in_archive = PathBuf::from("folder").join("test.json");
+
+            let file_archiver = FileArchiver::new_for_test(test_dir.join("verification"));
+            let archive = file_archiver
+                .archive(
+                    ArchiveParameters {
+                        archive_name_without_extension: "archive".to_string(),
+                        target_directory: test_dir.clone(),
+                        compression_algorithm: CompressionAlgorithm::Gzip,
+                    },
+                    ChainAppender::new(
+                        AppenderFile::append_at_archive_root(test_dir.join(&file_to_archive))
+                            .unwrap(),
+                        AppenderData::from_json(json_location_in_archive.clone(), &"test").unwrap(),
+                    ),
+                )
+                .unwrap();
+
+            let unpack_path = archive.unpack_gzip(&test_dir);
+
+            assert!(unpack_path.join(file_to_archive).exists());
+            assert!(unpack_path.join(json_location_in_archive).exists());
+        }
+
+        #[test]
+        fn chain_overlapping_appenders_data_from_right_appender_overwrite_left_appender_data() {
+            let test_dir = temp_dir_create!();
+            let json_location_in_archive = PathBuf::from("test.json");
+
+            let file_archiver = FileArchiver::new_for_test(test_dir.join("verification"));
+            let archive = file_archiver
+                .archive(
+                    ArchiveParameters {
+                        archive_name_without_extension: "archive".to_string(),
+                        target_directory: test_dir.clone(),
+                        compression_algorithm: CompressionAlgorithm::Gzip,
+                    },
+                    ChainAppender::new(
+                        AppenderData::from_json(
+                            json_location_in_archive.clone(),
+                            &"will be overwritten",
+                        )
+                        .unwrap(),
+                        AppenderData::from_json(json_location_in_archive.clone(), &"test").unwrap(),
+                    ),
+                )
+                .unwrap();
+
+            let unpack_path = archive.unpack_gzip(&test_dir);
+            let unpacked_json_path = unpack_path.join(&json_location_in_archive);
+
+            let deserialized_object: String =
+                serde_json::from_reader(File::open(&unpacked_json_path).unwrap()).unwrap();
+            assert_eq!("test", deserialized_object);
+        }
+
+        #[test]
+        fn compute_non_overlapping_uncompressed_size() {
+            let left_appender =
+                AppenderData::from_json(PathBuf::from("whatever1.json"), &"foo").unwrap();
+            let right_appender =
+                AppenderData::from_json(PathBuf::from("whatever2.json"), &"bar").unwrap();
+
+            let expected_size = left_appender.compute_uncompressed_data_size().unwrap()
+                + right_appender.compute_uncompressed_data_size().unwrap();
+
+            let chain_appender = left_appender.chain(right_appender);
+            let size = chain_appender.compute_uncompressed_data_size().unwrap();
+            assert_eq!(expected_size, size);
+        }
+
+        #[test]
+        fn compute_uncompressed_size_cant_discriminate_overlaps_and_return_aggregated_appenders_sizes(
+        ) {
+            let overlapping_path = PathBuf::from("whatever.json");
+            let left_appender =
+                AppenderData::from_json(overlapping_path.clone(), &"overwritten data").unwrap();
+            let right_appender =
+                AppenderData::from_json(overlapping_path.clone(), &"final data").unwrap();
+
+            let expected_size = left_appender.compute_uncompressed_data_size().unwrap()
+                + right_appender.compute_uncompressed_data_size().unwrap();
+
+            let chain_appender = left_appender.chain(right_appender);
+            let size = chain_appender.compute_uncompressed_data_size().unwrap();
+            assert_eq!(expected_size, size);
         }
     }
 }
