876fb4c51aba99b76e1917986385fe0f4904cb5a
DJO <790521+Alenar@users.noreply.github.com>
2025-04-15 18:57:55 +0200

    feat(aggregator): add `AncillarySignerWithGcpHsm`
    - use `gcloud_kms` crate as backend
    - define a `GcpCryptoKeyVersionResourceName` struct that's not stricly
      necessary, as we could pass a string directly, but allow format check
      at configuration time instead of at the last moment when the request
      is sent.

--- mithril-aggregator/src/services/snapshotter/ancillary_signer/mod.rs
+++ mithril-aggregator/src/services/snapshotter/ancillary_signer/mod.rs
@@ -1,6 +1,10 @@
 //! Tools for signing generating signed manifests of ancillary data.
+mod gcp_kms_resource_name;
 mod interface;
+mod with_gcp_kms;
 mod with_secret_key;
 
+pub use gcp_kms_resource_name::*;
 pub use interface::*;
+pub use with_gcp_kms::*;
 pub use with_secret_key::*;
