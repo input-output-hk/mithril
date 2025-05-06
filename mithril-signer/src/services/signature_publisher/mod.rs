2d6ff81bf277ce77938a9d59b5116a428b4b462c
Damien Lachaume <135982616+dlachaume@users.noreply.github.com>
2025-05-06 15:09:05 +0200

    feat(signer): add `SignaturePublisherNoop` for delay logic in preparation for DMQ/HTTP coexistence

--- mithril-signer/src/services/signature_publisher/mod.rs
+++ mithril-signer/src/services/signature_publisher/mod.rs
@@ -1,6 +1,7 @@
 mod http;
 mod interface;
 mod signature_publisher_delayer;
+mod signature_publisher_noop;
 mod signature_publisher_retrier;
 
 pub use interface::*;
---
+++ mithril-signer/src/services/signature_publisher/signature_publisher_noop.rs
@@ -0,0 +1,22 @@
+use mithril_common::{
+    entities::{ProtocolMessage, SignedEntityType, SingleSignature},
+    StdResult,
+};
+
+use super::SignaturePublisher;
+
+/// A no-op implementation of the [SignaturePublisher] trait.
+/// This implementation performs no action when a signature is published.
+pub struct SignaturePublisherNoop;
+
+#[async_trait::async_trait]
+impl SignaturePublisher for SignaturePublisherNoop {
+    async fn publish(
+        &self,
+        _signed_entity_type: &SignedEntityType,
+        _signature: &SingleSignature,
+        _protocol_message: &ProtocolMessage,
+    ) -> StdResult<()> {
+        Ok(())
+    }
+}
