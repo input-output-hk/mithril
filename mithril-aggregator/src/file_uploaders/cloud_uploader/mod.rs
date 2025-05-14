7227b91bc423664bc2e3342a15b313a59d0cbca4
DJO <790521+Alenar@users.noreply.github.com>
2025-05-14 15:25:26 +0200

    fix(aggregator): incorrect object path when updating file acl

--- mithril-aggregator/src/file_uploaders/cloud_uploader/mod.rs
+++ mithril-aggregator/src/file_uploaders/cloud_uploader/mod.rs
@@ -5,3 +5,16 @@ mod interface;
 pub use api::*;
 pub use gcloud_backend::*;
 pub use interface::*;
+
+use percent_encoding::{utf8_percent_encode, AsciiSet, NON_ALPHANUMERIC};
+
+const ENCODE_SET: &AsciiSet = &NON_ALPHANUMERIC
+    .remove(b'*')
+    .remove(b'-')
+    .remove(b'.')
+    .remove(b'_');
+
+/// Encode a string for use in a GCP URL, satisfying: https://cloud.google.com/storage/docs/request-endpoints#encoding
+pub fn gcp_percent_encode(input: &str) -> String {
+    utf8_percent_encode(input, ENCODE_SET).to_string()
+}
