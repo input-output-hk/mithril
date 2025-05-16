1c819dd42f0f8de86bcb56a5fb4c49c98e940a54
dlachaume <dlachaume@users.noreply.github.com>
2025-05-16 14:23:09 +0000

    deploy: 895149d382d5e753629fd3c3f9488e0a57d1aaea

---
+++ rust-doc/trait.impl/mithril_common/digesters/cache/provider/trait.ImmutableFileDigestCacheProvider.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["mithril_aggregator",[["impl <a class=\"trait\" href=\"mithril_common/digesters/cache/provider/trait.ImmutableFileDigestCacheProvider.html\" title=\"trait mithril_common::digesters::cache::provider::ImmutableFileDigestCacheProvider\">ImmutableFileDigestCacheProvider</a> for <a class=\"struct\" href=\"mithril_aggregator/database/repository/struct.ImmutableFileDigestRepository.html\" title=\"struct mithril_aggregator::database::repository::ImmutableFileDigestRepository\">ImmutableFileDigestRepository</a>"]]],["mithril_common",[]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[512,22]}
---
+++ rust-doc/trait.impl/slog/trait.Drain.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["mithril_common",[["impl Drain for <a class=\"struct\" href=\"mithril_common/test_utils/struct.MemoryDrainForTest.html\" title=\"struct mithril_common::test_utils::MemoryDrainForTest\">MemoryDrainForTest</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[212]}
