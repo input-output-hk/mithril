dbb87862fa10dd44db852f678d01f80d59e5eaf3
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2025-05-23 11:08:01 +0200

    chore(infra): bump Cardano node default version to '10.4.1'

--- mithril-infra/variables.tf
+++ mithril-infra/variables.tf
@@ -153,7 +153,7 @@ locals {
 variable "cardano_image_id" {
   type        = string
   description = "The Cardano image tag of service to deploy"
-  default     = "10.3.1"
+  default     = "10.4.1"
 }
 
 variable "cardano_image_registry" {
