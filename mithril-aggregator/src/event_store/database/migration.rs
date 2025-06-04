c60f684321c738f08bd6db8013f5a71ce64c3b9c
Clément Turmel <cturmel@palo-it.com>
2025-06-04 11:38:33 +0200

    feat(aggregator): add a view to retrieve client_type metric

--- mithril-aggregator/src/event_store/database/migration.rs
+++ mithril-aggregator/src/event_store/database/migration.rs
@@ -94,5 +94,17 @@ create view if not exists metrics_per_day_and_origin as select metric_date as da
 group by action, origin, date;
           "#,
         ),
+        SqlMigration::new(
+            5,
+            r#"
+create view if not exists metrics_per_day_and_client_type as select metric_date as date, action as counter_name, client_type, sum(counter) value from 
+  (
+      select action, json_extract(content, '$.content.client_type') client_type, json_extract(content, '$.content.value') counter, date(json_extract(content, '$.content.date')) metric_date 
+      from event 
+      where source='Metrics'
+  ) 
+group by action, client_type, date;
+          "#,
+        ),
     ]
 }
