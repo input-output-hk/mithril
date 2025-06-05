f9ec8a429deccec28a0c8c8dbf5da998617b90ac
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2025-06-05 15:21:26 +0200

    feat(common): support bytes hex encoding for individual signatures

--- mithril-common/src/crypto_helper/types/wrappers.rs
+++ mithril-common/src/crypto_helper/types/wrappers.rs
@@ -28,7 +28,10 @@ pub type ProtocolAggregateVerificationKey = ProtocolKey<StmAggrVerificationKey<D
 pub type ProtocolMkProof = ProtocolKey<MKMapProof<BlockRange>>;
 
 impl_codec_and_type_conversions_for_protocol_key!(
-    json_hex_codec => StmVerificationKeyPoP, Sum6KesSig, StmSig, StmAggrSig<D>, OpCert,
-        ed25519_dalek::VerifyingKey, ed25519_dalek::SigningKey, StmAggrVerificationKey<D>,
-        MKProof,ed25519_dalek::Signature
+    json_hex_codec => StmAggrSig<D>, ed25519_dalek::VerifyingKey, ed25519_dalek::SigningKey, StmAggrVerificationKey<D>,
+        MKProof, StmVerificationKeyPoP, Sum6KesSig, OpCert
+);
+
+impl_codec_and_type_conversions_for_protocol_key!(
+    bytes_hex_codec => StmSig, ed25519_dalek::Signature
 );
