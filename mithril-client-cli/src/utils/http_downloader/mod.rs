25a7bb7667d80eb537983ac015ca4e8005bad10b
Damien Lachaume <135982616+dlachaume@users.noreply.github.com>
2025-06-05 14:37:59 +0200

    refactor(client-cli): move `HttpDownloader` trait in a dedicated `interface.rs` sub module

--- mithril-client-cli/src/utils/http_downloader/mod.rs
+++ mithril-client-cli/src/utils/http_downloader/mod.rs
@@ -1,23 +1,5 @@
+mod interface;
 mod reqwest_http_downloader;
 
+pub use interface::*;
 pub use reqwest_http_downloader::*;
-
-use async_trait::async_trait;
-use mithril_client::MithrilResult;
-use reqwest::Url;
-use std::path::{Path, PathBuf};
-
-/// Trait for downloading a file over HTTP from a URL,
-/// saving it to a target directory with the given filename.
-///
-/// Returns the path to the downloaded file.
-#[cfg_attr(test, mockall::automock)]
-#[async_trait]
-pub trait HttpDownloader {
-    async fn download_file(
-        &self,
-        url: Url,
-        download_dir: &Path,
-        filename: &str,
-    ) -> MithrilResult<PathBuf>;
-}
