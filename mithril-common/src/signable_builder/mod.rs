0302d370d8feb88a87adb2598518cf57747f5665
DJO <790521+Alenar@users.noreply.github.com>
2025-06-17 16:00:23 +0200

    refactor(common): remove last code behind feature `fs` and depracte the feature

--- mithril-common/src/signable_builder/mod.rs
+++ mithril-common/src/signable_builder/mod.rs
@@ -1,19 +1,15 @@
 //! The module used for building signables
 
 mod cardano_stake_distribution;
+mod cardano_transactions;
 mod interface;
 mod mithril_stake_distribution;
 mod signable_builder_service;
 mod signed_entity;
 
 pub use cardano_stake_distribution::*;
+pub use cardano_transactions::*;
 pub use interface::*;
 pub use mithril_stake_distribution::*;
 pub use signable_builder_service::*;
 pub use signed_entity::*;
-
-cfg_fs! {
-    mod cardano_transactions;
-
-    pub use cardano_transactions::*;
-}
