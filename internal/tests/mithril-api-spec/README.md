d617ca54ac5ddf52c3a76f557db56ff8723bdfcc
DJO <790521+Alenar@users.noreply.github.com>
2025-06-17 18:09:44 +0200

    chore: scaffold `internal/tests/mithril-api-spec` crate

---
+++ internal/tests/mithril-api-spec/README.md
@@ -0,0 +1,3 @@
+# Mithril-api-spec
+
+This crate provides a toolset to verify conformity of http routes against an Open Api specification.
---
+++ internal/tests/mithril-api-spec/src/lib.rs
@@ -0,0 +1,2 @@
+#![warn(missing_docs)]
+//! This crate provides a toolset to verify conformity of http routes against an Open Api specification.
