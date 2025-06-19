108134eea4c051acd615963ffadcdc9f4039d75e
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2025-06-19 18:22:14 +0200

    feat(signer): add Pallas based 'SignaturePublisher' implementation

--- mithril-signer/src/services/signature_publisher/mod.rs
+++ mithril-signer/src/services/signature_publisher/mod.rs
@@ -2,9 +2,11 @@ mod delayer;
 mod http;
 mod interface;
 mod noop;
+mod pallas;
 mod retrier;
 
 pub use delayer::*;
 pub use interface::*;
 pub use noop::*;
+pub use pallas::*;
 pub use retrier::*;
