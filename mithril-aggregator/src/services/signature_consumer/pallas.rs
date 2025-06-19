478bf39f4b89d17d032752caebcb489962451f89
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2025-06-19 18:22:37 +0200

    feat(aggregator): add Pallas based 'SignatureConsumer' implementation

---
+++ mithril-aggregator/src/services/signature_consumer/pallas.rs
@@ -0,0 +1,39 @@
+use anyhow::Context;
+use async_trait::async_trait;
+
+use mithril_common::{
+    entities::{SignedEntityType, SingleSignature},
+    messages::RegisterSignatureMessageDmq,
+    StdResult,
+};
+
+use mithril_dmq_node::DmqConsumerPallas;
+
+use super::SignatureConsumer;
+
+#[async_trait]
+impl SignatureConsumer for DmqConsumerPallas<RegisterSignatureMessageDmq> {
+    async fn get_signatures(&self) -> StdResult<Vec<(SingleSignature, SignedEntityType)>> {
+        self.consume_messages()
+            .await
+            .map(|messages| {
+                messages
+                    .into_iter()
+                    .map(|(message, party_id)| {
+                        let signature = message.signature;
+                        let won_indexes = signature.indexes.clone();
+                        let single_signature =
+                            SingleSignature::new(party_id, signature, won_indexes);
+                        let signed_entity_type = message.signed_entity_type;
+
+                        (single_signature, signed_entity_type)
+                    })
+                    .collect()
+            })
+            .with_context(|| "Failed to get signatures from DMQ")
+    }
+
+    fn get_origin_tag(&self) -> String {
+        "DMQ".to_string()
+    }
+}
