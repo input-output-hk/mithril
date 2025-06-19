5651cafde21c583abbfb9527d484a380f9de89e5
Filip Stoyanov <filip.stoyanov@iohk.io>
2025-06-19 16:24:04 +0100

    filip(feat): docs site redesign

---
+++ docs/website/src/hooks/useIsLandingPage.ts
@@ -0,0 +1,8 @@
+import { useContext } from "react";
+import { PageContext, PageType } from "../context/PageContext";
+
+export function useIsLandingPage() {
+  const context = useContext(PageContext);
+
+  return context && context.page === PageType.Landing;
+}
---
+++ docs/website/src/pages/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Layout from "@theme/Layout";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import { PageContext, PageType } from "../context/PageContext";
+import HomepageHero from "../components/HomepageHero";
+import Features from "../components/Features";
+import WhyMithril from "../components/WhyMithril";
+import AnimatedText from "../components/AnimatedText";
+import UseCases from "../components/UseCases";
+
+export default function Home() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <PageContext.Provider value={{ page: PageType.Landing }}>
+      <div className="z-index:1000">
+        <Layout>
+          <HomepageHero />
+          <main>
+            <WhyMithril />
+            <AnimatedText />
+            <Features />
+            <UseCases />
+          </main>
+        </Layout>
+      </div>
+    </PageContext.Provider>
+  );
+}
