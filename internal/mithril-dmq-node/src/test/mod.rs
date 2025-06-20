ac05c2b644b2b3197fc55ad2b182307d00297b4f
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2025-06-20 11:54:09 +0200

    test(dmq): add 'DmqConsumerFake' test double

--- internal/mithril-dmq-node/src/test/mod.rs
+++ internal/mithril-dmq-node/src/test/mod.rs
@@ -1,5 +1,8 @@
 //! Test utilities.
 //!
 //! ⚠ Do not use in production code ⚠
+//!
+//! This module provides in particular test doubles for the traits defined in this crate.
 
+pub mod double;
 pub(crate) mod payload;
