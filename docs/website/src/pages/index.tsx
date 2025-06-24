0c2a660e8f00150cf748fc028aba88c20b1507ce
Filip Stoyanov <filip.stoyanov@iohk.io>
2025-06-24 11:16:46 +0100

    filip(fix): add client side wrapper to homepage

--- docs/website/src/pages/index.tsx
+++ docs/website/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import BrowserOnly from "@docusaurus/BrowserOnly";
 import Layout from "@theme/Layout";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import { PageContext, PageType } from "../context/PageContext";
@@ -8,21 +9,24 @@ import WhyMithril from "../components/WhyMithril";
 import AnimatedText from "../components/AnimatedText";
 import UseCases from "../components/UseCases";
 
-export default function Home() {
-  const { siteConfig } = useDocusaurusContext();
+export default function HomePage() {
   return (
-    <PageContext.Provider value={{ page: PageType.Landing }}>
-      <div className="z-index:1000">
-        <Layout>
-          <HomepageHero />
-          <main>
-            <WhyMithril />
-            <AnimatedText />
-            <Features />
-            <UseCases />
-          </main>
-        </Layout>
-      </div>
-    </PageContext.Provider>
+    <BrowserOnly fallback={<div />}>
+      {() => (
+        <PageContext.Provider value={{ page: PageType.Landing }}>
+          <div style={{ zIndex: 1000 }}>
+            <Layout>
+              <HomepageHero />
+              <main>
+                <WhyMithril />
+                <AnimatedText />
+                <Features />
+                <UseCases />
+              </main>
+            </Layout>
+          </div>
+        </PageContext.Provider>
+      )}
+    </BrowserOnly>
   );
 }
