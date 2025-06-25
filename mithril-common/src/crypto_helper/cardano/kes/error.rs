0330a3a69c5df0ef3acdbe963c04232c4556f749
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2025-06-25 17:45:00 +0200

    refactor(common): split 'KesError' in 'KesSignError' and 'KESVerifyError'

--- mithril-common/src/crypto_helper/cardano/kes/error.rs
+++ mithril-common/src/crypto_helper/cardano/kes/error.rs
@@ -2,17 +2,9 @@ use thiserror::Error;
 
 use crate::crypto_helper::KESPeriod;
 
-/// New KES error
+/// KES verification error
 #[derive(Error, Debug)]
-pub enum KesError {
-    /// Error raised when a KES update error occurs
-    #[error("KES key cannot be updated for period {0}")]
-    UpdateKey(KESPeriod),
-
-    /// Period of key file does not match with period provided by user
-    #[error("Period of key file, {0}, does not match with period provided by user, {1}")]
-    PeriodMismatch(KESPeriod, KESPeriod),
-
+pub enum KesVerifyError {
     /// Error raised when an operational certificate is invalid
     #[error("invalid operational certificate")]
     OpCertInvalid,
@@ -20,4 +12,16 @@ pub enum KesError {
     /// Error raised when a KES Signature verification fails
     #[error("KES signature verification error: CurrentKesPeriod={0}, StartKesPeriod={1}")]
     SignatureInvalid(KESPeriod, KESPeriod),
+}
+
+/// KES signature error
+#[derive(Error, Debug)]
+pub enum KesSignError {
+    /// Error raised when a KES update error occurs
+    #[error("KES key cannot be updated for period {0}")]
+    UpdateKey(KESPeriod),
+
+    /// Period of key file does not match with period provided by user
+    #[error("Period of key file, {0}, does not match with period provided by user, {1}")]
+    PeriodMismatch(KESPeriod, KESPeriod),
 }
