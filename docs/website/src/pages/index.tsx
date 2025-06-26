b12535c8d5bdbab90b85e3bb667e5be148b99752
Filip Stoyanov <filip.stoyanov@iohk.io>
2025-06-26 11:37:58 +0100

    filip(fix): rewrite scroll-based animation with pure css/js to exclude framer

--- docs/website/src/pages/index.tsx
+++ docs/website/src/pages/index.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import Layout from "@theme/Layout";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import { PageContext, PageType } from "../context/PageContext";
 import HomepageHero from "../components/HomepageHero";
 import Features from "../components/Features";
-import WhyMithril from "../components/why-mithril/WhyMithril";
 import AnimatedText from "../components/AnimatedText";
 import UseCases from "../components/UseCases";
+import WhyMithril from "../components/WhyMithril";
 
 export default function HomePage() {
   return (
