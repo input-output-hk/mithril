0fd9ca9b2fc451f528401bf235ea10e146e08b17
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2025-06-30 18:41:44 +0200

    fix(dmq): remove unneeded 'Debug' in 'DmqPublisher' trait

--- internal/mithril-dmq-node/src/publisher/interface.rs
+++ internal/mithril-dmq-node/src/publisher/interface.rs
@@ -1,11 +1,9 @@
-use std::fmt::Debug;
-
 use mithril_common::{crypto_helper::TryToBytes, StdResult};
 
 /// Trait for publishing messages from a DMQ node.
 #[cfg_attr(test, mockall::automock)]
 #[async_trait::async_trait]
-pub trait DmqPublisher<M: TryToBytes + Debug + Send + Sync>: Send + Sync {
+pub trait DmqPublisher<M: TryToBytes + Send + Sync>: Send + Sync {
     /// Publishes a message to the DMQ node.
     async fn publish_message(&self, message: M) -> StdResult<()>;
 }
