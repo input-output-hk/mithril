4cd18ac24b78ebfdb66fa56056af1ac4272bbe3d
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2025-07-15 15:02:34 +0200

    fix(dmq): activate Pallas server sides for Unix only

--- internal/mithril-dmq/src/consumer/server/mod.rs
+++ internal/mithril-dmq/src/consumer/server/mod.rs
@@ -1,6 +1,10 @@
 mod interface;
+#[cfg(unix)]
 mod pallas;
+#[cfg(unix)]
 mod queue;
 
 pub use interface::*;
+
+#[cfg(unix)]
 pub use pallas::*;
--- internal/mithril-dmq/src/publisher/server/mod.rs
+++ internal/mithril-dmq/src/publisher/server/mod.rs
@@ -1,5 +1,7 @@
 mod interface;
+#[cfg(unix)]
 mod pallas;
 
 pub use interface::*;
+#[cfg(unix)]
 pub use pallas::*;
