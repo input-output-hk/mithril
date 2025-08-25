63268995567abf07322f3d69dee390a912ed4ca5
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2025-08-25 16:25:26 +0200

    refactor(cardano-node-chain): update 'FakeChainObserver' implementation of 'ChainObserver'

--- internal/cardano-node/mithril-cardano-node-chain/src/test/double/chain_observer.rs
+++ internal/cardano-node/mithril-cardano-node-chain/src/test/double/chain_observer.rs
@@ -1,7 +1,7 @@
 use async_trait::async_trait;
 use tokio::sync::RwLock;
 
-use mithril_common::crypto_helper::{KesPeriod, OpCert};
+use mithril_common::crypto_helper::KesPeriod;
 use mithril_common::entities::{
     BlockNumber, ChainPoint, Epoch, SignerWithStake, SlotNumber, StakeDistribution, TimePoint,
 };
@@ -202,10 +202,7 @@ impl ChainObserver for FakeChainObserver {
         ))
     }
 
-    async fn get_current_kes_period(
-        &self,
-        _opcert: &OpCert,
-    ) -> Result<Option<KesPeriod>, ChainObserverError> {
+    async fn get_current_kes_period(&self) -> Result<Option<KesPeriod>, ChainObserverError> {
         Ok(Some(0))
     }
 }
