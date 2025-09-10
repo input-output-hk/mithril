2e6b781adb9ad302e112e927ccd30466020fa9a9
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2025-09-10 16:45:10 +0200

    feat(client): add a temporary folder provider in utils

--- mithril-client/src/utils/mod.rs
+++ mithril-client/src/utils/mod.rs
@@ -2,19 +2,21 @@
 //! This module contains tools needed mostly for the snapshot download and unpack.
 
 cfg_fs! {
-    pub const ANCILLARIES_NOT_SIGNED_BY_MITHRIL:&str = "Ancillary verification does not use the Mithril certification: as a mitigation, IOG owned keys are used to sign these files.";
+    pub const ANCILLARIES_NOT_SIGNED_BY_MITHRIL: &str = "Ancillary verification does not use the Mithril certification: as a mitigation, IOG owned keys are used to sign these files.";
 
     mod ancillary_verifier;
+    mod bootstrap_files;
     mod fs;
     mod stream_reader;
-    mod bootstrap_files;
+    mod temp_dir_provider;
     mod unexpected_downloaded_file_verifier;
     mod vec_deque_extensions;
 
-    pub use fs::*;
-    pub use vec_deque_extensions::VecDequeExtensions;
     pub use ancillary_verifier::AncillaryVerifier;
-    pub(crate) use unexpected_downloaded_file_verifier::*;
-    pub use stream_reader::*;
     pub use bootstrap_files::*;
+    pub use fs::*;
+    pub use stream_reader::*;
+    pub(crate) use temp_dir_provider::{TempDirectoryProvider, TimestampTempDirectoryProvider};
+    pub(crate) use unexpected_downloaded_file_verifier::*;
+    pub use vec_deque_extensions::VecDequeExtensions;
 }
