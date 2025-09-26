aee359053ca177d75a0cb3c8fd6fbc188d6704ba
Clément Turmel <turmel@teragone-solutions.com>
2025-09-26 17:59:50 +0200

    feature(protocol-config): build MithrilNetworkConfiguration result with available signe entity type and config

--- internal/mithril-protocol-config/src/http_client/http_impl.rs
+++ internal/mithril-protocol-config/src/http_client/http_impl.rs
@@ -1,19 +1,15 @@
 use anyhow::anyhow;
-use std::{
-    collections::{BTreeSet, HashMap},
-    sync::Arc,
-    time::Duration,
-};
+use std::{collections::HashMap, sync::Arc, time::Duration};
 
 use crate::{
     HTTP_REQUEST_TIMEOUT_DURATION,
     http_client::aggregator_client::{AggregatorClient, AggregatorHTTPClient},
     interface::MithrilNetworkConfigurationProvider,
-    model::MithrilNetworkConfiguration,
+    model::{MithrilNetworkConfiguration, SignedEntityTypeConfiguration},
 };
 use async_trait::async_trait;
-use mithril_common::StdResult;
 use mithril_common::api_version::APIVersionProvider;
+use mithril_common::{StdResult, entities::SignedEntityTypeDiscriminants};
 
 struct HttpMithrilNetworkConfigurationProvider {
     aggregator_client: AggregatorHTTPClient,
@@ -49,8 +45,18 @@ impl MithrilNetworkConfigurationProvider for HttpMithrilNetworkConfigurationProv
             return Err(anyhow!("Failed to retrieve epoch settings"));
         };
 
-        let available_signed_entity_types = BTreeSet::new(); // To be implemented to be retrieve from /aggreagator-features from aggregator_client.rs
-        let signed_entity_types_config = HashMap::new(); // To be implemented
+        let aggregator_features = self.aggregator_client.retrieve_aggregator_features().await?;
+        let available_signed_entity_types = aggregator_features.capabilities.signed_entity_types;
+
+        let mut signed_entity_types_config = HashMap::new();
+        signed_entity_types_config.insert(
+            SignedEntityTypeDiscriminants::CardanoTransactions,
+            SignedEntityTypeConfiguration::CardanoTransactions(
+                epoch_settings.cardano_transactions_signing_config.ok_or_else(|| {
+                    anyhow!("Cardano transactions signing config is missing in epoch settings")
+                })?,
+            ),
+        );
 
         Ok(MithrilNetworkConfiguration {
             epoch: epoch_settings.epoch,
