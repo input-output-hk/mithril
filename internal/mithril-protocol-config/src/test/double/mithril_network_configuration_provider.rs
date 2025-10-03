cab8da0ae54447dfd71f0ac2fd598a7e9984fd21
Clément Turmel <turmel@teragone-solutions.com>
2025-10-03 18:06:14 +0200

    feature(protocol-config): improve tests of FakeMithrilNetworkConfigurationProvider

--- internal/mithril-protocol-config/src/test/double/mithril_network_configuration_provider.rs
+++ internal/mithril-protocol-config/src/test/double/mithril_network_configuration_provider.rs
@@ -80,3 +80,89 @@ impl MithrilNetworkConfigurationProvider for FakeMithrilNetworkConfigurationProv
         })
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::{
+        collections::{BTreeSet, HashMap},
+        sync::Arc,
+    };
+
+    use mithril_common::{
+        entities::{
+            BlockNumber, CardanoTransactionsSigningConfig, ChainPoint, Epoch, ProtocolParameters,
+            SignedEntityTypeDiscriminants, TimePoint,
+        },
+        test::double::Dummy,
+    };
+    use mithril_ticker::MithrilTickerService;
+
+    use crate::{
+        interface::MithrilNetworkConfigurationProvider, model::SignedEntityTypeConfiguration,
+        test::double::mithril_network_configuration_provider::FakeMithrilNetworkConfigurationProvider,
+    };
+    use mithril_cardano_node_chain::test::double::FakeChainObserver;
+    use mithril_cardano_node_internal_database::test::double::DumbImmutableFileObserver;
+
+    async fn ticker_service() -> Arc<MithrilTickerService> {
+        let immutable_observer = Arc::new(DumbImmutableFileObserver::new());
+        immutable_observer.shall_return(Some(1)).await;
+        let chain_observer = Arc::new(FakeChainObserver::new(Some(TimePoint {
+            epoch: Epoch(1),
+            immutable_file_number: 1,
+            chain_point: ChainPoint::dummy(),
+        })));
+
+        Arc::new(MithrilTickerService::new(
+            chain_observer.clone(),
+            immutable_observer.clone(),
+        ))
+    }
+
+    #[tokio::test]
+    async fn test_get() {
+        let signer_registration_protocol_parameters = ProtocolParameters {
+            k: 2,
+            m: 3,
+            phi_f: 0.5,
+        };
+        let available_signed_entity_types = BTreeSet::from([
+            SignedEntityTypeDiscriminants::MithrilStakeDistribution,
+            SignedEntityTypeDiscriminants::CardanoTransactions,
+        ]);
+        let signed_entity_types_config = HashMap::from([(
+            SignedEntityTypeDiscriminants::CardanoTransactions,
+            SignedEntityTypeConfiguration::CardanoTransactions(CardanoTransactionsSigningConfig {
+                security_parameter: BlockNumber(12),
+                step: BlockNumber(10),
+            }),
+        )]);
+
+        let mithril_network_configuration_provider = FakeMithrilNetworkConfigurationProvider::new(
+            signer_registration_protocol_parameters.clone(),
+            available_signed_entity_types.clone(),
+            signed_entity_types_config.clone(),
+            ticker_service().await,
+        );
+
+        let actual_config = mithril_network_configuration_provider.get().await.unwrap();
+
+        assert_eq!(actual_config.epoch, Epoch(1));
+        assert_eq!(
+            actual_config.signer_registration_protocol_parameters,
+            ProtocolParameters {
+                k: 2,
+                m: 3,
+                phi_f: 0.5
+            }
+        );
+        assert_eq!(
+            actual_config.available_signed_entity_types,
+            available_signed_entity_types
+        );
+        assert_eq!(
+            actual_config.signed_entity_types_config,
+            signed_entity_types_config
+        );
+    }
+}
