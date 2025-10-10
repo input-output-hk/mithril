caf5d720b22ad370523199517628e7d06c3cd43e
Clément Turmel <turmel@teragone-solutions.com>
2025-10-10 09:12:56 +0200

    refactor(protocol-config): move aggregator_client at the root of the crate

--- internal/mithril-protocol-config/src/http_client/mod.rs
+++ internal/mithril-protocol-config/src/http_client/mod.rs
@@ -1,4 +1,3 @@
 //! This module provides HTTP client implementations for interacting with the Mithril Network.
 
-mod aggregator_client;
 pub mod http_impl;
--- internal/mithril-protocol-config/src/lib.rs
+++ internal/mithril-protocol-config/src/lib.rs
@@ -1,6 +1,7 @@
 #![warn(missing_docs)]
 //! This crate provides functionality for interacting with Mithril Network
 
+mod aggregator_client;
 pub mod http_client;
 pub mod interface;
 pub mod model;
