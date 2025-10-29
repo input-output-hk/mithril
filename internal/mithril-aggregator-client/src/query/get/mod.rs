b6483bd6f34aee6d86694f97ac4d199e13c0fc27
DJO <790521+Alenar@users.noreply.github.com>
2025-10-29 16:33:24 +0100

    feat(aggregator-client): add `GetProtocolConfigurationQuery`

--- internal/mithril-aggregator-client/src/query/get/mod.rs
+++ internal/mithril-aggregator-client/src/query/get/mod.rs
@@ -1,7 +1,9 @@
 mod get_certificate;
 mod get_certificates_list;
 mod get_epoch_settings;
+mod get_protocol_configuration;
 
 pub use get_certificate::*;
 pub use get_certificates_list::*;
 pub use get_epoch_settings::*;
+pub use get_protocol_configuration::*;
