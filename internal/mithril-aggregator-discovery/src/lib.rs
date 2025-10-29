415b381d3ae568789a43dc6aa228dce0cfe56e11
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2025-10-29 18:20:15 +0100

    feat(aggregator-discovery): introduce 'AggregatorDiscoverer' trait

--- internal/mithril-aggregator-discovery/src/lib.rs
+++ internal/mithril-aggregator-discovery/src/lib.rs
@@ -1,4 +1,8 @@
 #![warn(missing_docs)]
 //! This crate provides mechanisms to discover aggregators in a Mithril network.
+
+mod interface;
 mod model;
+
+pub use interface::AggregatorDiscoverer;
 pub use model::{AggregatorEndpoint, MithrilNetwork};
