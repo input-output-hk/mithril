d48a6f0fc1806f40e18902d7c496b005ec007dcd
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2025-10-29 18:20:15 +0100

    feat(aggregator-discovery): introduce 'AggregatorDiscoverer' trait

---
+++ internal/mithril-aggregator-discovery/src/interface.rs
@@ -0,0 +1,18 @@
+//! Interface definition for Mithril Protocol Configuration provider.
+
+use mithril_common::StdResult;
+
+use crate::model::{AggregatorEndpoint, MithrilNetwork};
+
+/// An aggregator discoverer.
+#[cfg_attr(test, mockall::automock)]
+#[async_trait::async_trait]
+pub trait AggregatorDiscoverer: Sync + Send {
+    /// Get an iterator over a list of available aggregators in a Mithril network.
+    ///
+    /// Note: there is no guarantee that the returned aggregators is sorted, complete or up-to-date.
+    async fn get_available_aggregators(
+        &self,
+        network: MithrilNetwork,
+    ) -> StdResult<Vec<AggregatorEndpoint>>;
+}
