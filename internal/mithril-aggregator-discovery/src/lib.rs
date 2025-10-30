8d45b0a8d971678ca94f83acdf37e2e565342dbb
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2025-10-30 14:05:21 +0100

    feat(aggregator-discovery): add 'HttpConfigAggregatorDiscoverer'
    Implementation for 'AggregatorDiscoverer' trait.

--- internal/mithril-aggregator-discovery/src/lib.rs
+++ internal/mithril-aggregator-discovery/src/lib.rs
@@ -1,9 +1,12 @@
 #![warn(missing_docs)]
 //! This crate provides mechanisms to discover aggregators in a Mithril network.
 
+mod http_config;
 mod interface;
 mod model;
 pub mod test;
 
+pub use capabilities_discoverer::CapableAggregatorDiscoverer;
+pub use http_config_discoverer::HttpConfigAggregatorDiscoverer;
 pub use interface::AggregatorDiscoverer;
 pub use model::{AggregatorEndpoint, MithrilNetwork};
