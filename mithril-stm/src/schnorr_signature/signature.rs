e8af7696b4c61530da38fd505e5bc6eda3f9db50
Dam <damien.robissout@iohk.io>
2025-11-06 18:12:23 +0100

    Added verification function and tests to signature.

--- mithril-stm/src/schnorr_signature/signature.rs
+++ mithril-stm/src/schnorr_signature/signature.rs
@@ -1,4 +1,17 @@
+use midnight_circuits::hash::poseidon::PoseidonChip;
+use midnight_circuits::instructions::HashToCurveCPU;
+use midnight_circuits::instructions::hash::HashCPU;
 use midnight_curves::{Fq as JubjubBase, Fr as JubjubScalar, JubjubSubgroup};
+
+use group::Group;
+
+use crate::{
+    error::MultiSignatureError,
+    schnorr_signature::{
+        JubjubHashToCurve, get_coordinates, hash_msg_to_jubjubbase,
+        verification_key::SchnorrVerificationKey,
+    },
+};
 
 /// Structure of the Schnorr signature to use with the SNARK
 /// This signature includes a value `sigma` which depends only on
@@ -8,4 +21,52 @@ pub(crate) struct SchnorrSignature {
     pub(crate) sigma: JubjubSubgroup,
     pub(crate) s: JubjubScalar,
     pub(crate) c: JubjubBase,
+}
+
+impl SchnorrSignature {
+    pub(crate) fn verify(
+        &self,
+        msg: &[u8],
+        vk: &SchnorrVerificationKey,
+    ) -> Result<(), MultiSignatureError> {
+        let g = JubjubSubgroup::generator();
+
+        // First hashing the message to a scalar then hashing it to a curve point
+        let hash = JubjubHashToCurve::hash_to_curve(&[hash_msg_to_jubjubbase(msg)]);
+
+        // Computing R1 = H(msg) * s + sigma * c
+        let c_bytes = self.c.to_bytes_le();
+        let c_scalar = JubjubScalar::from_raw([
+            u64::from_le_bytes(c_bytes[0..8].try_into().unwrap()),
+            u64::from_le_bytes(c_bytes[8..16].try_into().unwrap()),
+            u64::from_le_bytes(c_bytes[16..24].try_into().unwrap()),
+            u64::from_le_bytes(c_bytes[24..32].try_into().unwrap()),
+        ]);
+        let h_s = hash * self.s;
+        let sigma_c = self.sigma * c_scalar;
+        let r1_tilde = h_s + sigma_c;
+
+        // Computing R2 = g * s + vk * c
+        let g_s = g * self.s;
+        let vk_c = vk.0 * c_scalar;
+        let r2_tilde = g_s + vk_c;
+
+        let (gx, gy) = get_coordinates(g);
+        let (hashx, hashy) = get_coordinates(hash);
+        let (vkx, vky) = get_coordinates(vk.0);
+        let (sigmax, sigmay) = get_coordinates(self.sigma);
+        let (r1x, r1y) = get_coordinates(r1_tilde);
+        let (r2x, r2y) = get_coordinates(r2_tilde);
+
+        let c_tilde = PoseidonChip::<JubjubBase>::hash(&[
+            gx, gy, hashx, hashy, vkx, vky, sigmax, sigmay, r1x, r1y, r2x, r2y,
+        ]);
+
+        if c_tilde != self.c {
+            // TODO: Wrong error for now, need to change that once the errors are added
+            return Err(MultiSignatureError::BatchInvalid);
+        }
+
+        Ok(())
+    }
 }
