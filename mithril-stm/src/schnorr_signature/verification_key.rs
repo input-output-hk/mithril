f190460bcd0606ea90819ee7357dac6e737ab7b4
Dam <damien.robissout@iohk.io>
2025-11-06 15:29:38 +0100

    Added conversion from sk to vk for use in signature.

--- mithril-stm/src/schnorr_signature/verification_key.rs
+++ mithril-stm/src/schnorr_signature/verification_key.rs
@@ -1,3 +1,30 @@
+use group::Group;
 pub use midnight_curves::JubjubSubgroup;
 
+use crate::schnorr_signature::signing_key::SchnorrSigningKey;
+
 pub struct SchnorrVerificationKey(JubjubSubgroup);
+
+impl From<&SchnorrSigningKey> for SchnorrVerificationKey {
+    /// Convert a Shnorr secret key into a verification key
+    /// This is done by computing `vk = g * sk` where g is the generator
+    /// of the subgroup and sk is the schnorr secret key
+    fn from(sk: &SchnorrSigningKey) -> Self {
+        let g = JubjubSubgroup::generator();
+        SchnorrVerificationKey(g * sk.0)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rand_chacha::ChaCha20Rng;
+    use rand_core::SeedableRng;
+
+    #[test]
+    fn test_generate_signing_key() {
+        let mut rng = ChaCha20Rng::from_seed([0u8; 32]);
+        let sk = SchnorrSigningKey::generate(&mut rng);
+        let _vk = SchnorrVerificationKey::from(&sk);
+    }
+}
