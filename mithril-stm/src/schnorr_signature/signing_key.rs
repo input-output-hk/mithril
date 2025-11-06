5ef9fc0b47c3238fcc9ef09e8f8ccde19d34afa4
Dam <damien.robissout@iohk.io>
2025-11-06 15:16:01 +0100

    Added generate function for Schnorr signature.

--- mithril-stm/src/schnorr_signature/signing_key.rs
+++ mithril-stm/src/schnorr_signature/signing_key.rs
@@ -1,3 +1,24 @@
-pub use midnight_curves::Fr as JubjubScalar;
+use ff::Field;
+use midnight_curves::Fr as JubjubScalar;
+use rand_core::{CryptoRng, RngCore};
 
-pub struct SchnorrSigningKey(JubjubScalar);
+struct SchnorrSigningKey(JubjubScalar);
+
+impl SchnorrSigningKey {
+    fn generate(rng: &mut (impl RngCore + CryptoRng)) -> Self {
+        SchnorrSigningKey(JubjubScalar::random(rng))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rand_chacha::ChaCha20Rng;
+    use rand_core::SeedableRng;
+
+    #[test]
+    fn test_generate_signing_key() {
+        let mut rng = ChaCha20Rng::from_seed([0u8; 32]);
+        let _sk = SchnorrSigningKey::generate(&mut rng);
+    }
+}
