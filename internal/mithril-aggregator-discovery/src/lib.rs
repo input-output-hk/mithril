8af317fce781fbc013b130256fff25837f5fb04d
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2025-11-10 16:16:51 +0100

    feat(aggregator-discovery): add 'ShuffleAggregatorDiscoverer' decorator

--- internal/mithril-aggregator-discovery/src/lib.rs
+++ internal/mithril-aggregator-discovery/src/lib.rs
@@ -4,9 +4,13 @@
 mod http_config_discoverer;
 mod interface;
 mod model;
+#[cfg(feature = "rand")]
+mod rand_discoverer;
 pub mod test;
 
 pub use capabilities_discoverer::CapableAggregatorDiscoverer;
 pub use http_config_discoverer::HttpConfigAggregatorDiscoverer;
 pub use interface::AggregatorDiscoverer;
 pub use model::{AggregatorEndpoint, MithrilNetwork};
+#[cfg(feature = "rand")]
+pub use rand_discoverer::ShuffleAggregatorDiscoverer;
