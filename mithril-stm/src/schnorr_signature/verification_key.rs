ac6ca3d65fcf96470de309311270500fe3650ae1
Dam <damien.robissout@iohk.io>
2025-11-13 18:08:38 +0100

    Finished changing variables name.

--- mithril-stm/src/schnorr_signature/verification_key.rs
+++ mithril-stm/src/schnorr_signature/verification_key.rs
@@ -10,18 +10,10 @@ pub(crate) use crate::schnorr_signature::signing_key::SchnorrSigningKey;
 pub struct SchnorrVerificationKey(pub(crate) JubjubSubgroup);
 
 impl SchnorrVerificationKey {
-    /// TODO: Make sure this is correct as the previous implementation is
-    /// using coordinates decomposition
     pub(crate) fn to_bytes(self) -> [u8; 32] {
-        // let (x, y) = get_coordinates(self.0);
-        // let mut bytes = [0u8; 64];
-        // bytes[0..32].copy_from_slice(&x.to_bytes_le());
-        // bytes[32..64].copy_from_slice(&y.to_bytes_le());
-        // bytes
         self.0.to_bytes()
     }
 
-    /// Do we really need to separate the coordinates?
     pub(crate) fn from_bytes(bytes: &[u8]) -> Result<Self> {
         let bytes = bytes
             .get(0..32)
@@ -37,25 +29,12 @@ impl SchnorrVerificationKey {
 
 impl From<&SchnorrSigningKey> for SchnorrVerificationKey {
     /// Convert a Shnorr secret key into a verification key
+    ///
     /// This is done by computing `vk = g * sk` where g is the generator
     /// of the subgroup and sk is the schnorr secret key
     fn from(sk: &SchnorrSigningKey) -> Self {
         let g = JubjubSubgroup::generator();
 
         SchnorrVerificationKey(g * sk.0)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use rand_chacha::ChaCha20Rng;
-    use rand_core::SeedableRng;
-
-    #[test]
-    fn test_generate_signing_key() {
-        let mut rng = ChaCha20Rng::from_seed([0u8; 32]);
-        let sk = SchnorrSigningKey::generate(&mut rng);
-        let _vk = SchnorrVerificationKey::from(&sk);
     }
 }
