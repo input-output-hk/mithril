8b191104b7f4327471d8559faa0975877ccdf9e7
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2025-11-18 17:25:54 +0100

    refactor(aggregator-discoverer): introduce 'RequiredAggregatorCapabilities' type
    Which allows to combine signed entity types and aggregate signature type in a flexible way.

--- internal/mithril-aggregator-discovery/src/lib.rs
+++ internal/mithril-aggregator-discovery/src/lib.rs
@@ -8,7 +8,7 @@ mod model;
 mod rand_discoverer;
 pub mod test;
 
-pub use capabilities_discoverer::CapableAggregatorDiscoverer;
+pub use capabilities_discoverer::{CapableAggregatorDiscoverer, RequiredAggregatorCapabilities};
 pub use http_config_discoverer::HttpConfigAggregatorDiscoverer;
 pub use interface::AggregatorDiscoverer;
 pub use model::{AggregatorEndpoint, MithrilNetwork};
