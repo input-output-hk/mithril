1282d3e8617f03183d4607a6650621f493161b0a
DJO <790521+Alenar@users.noreply.github.com>
2025-11-20 12:58:56 +0100

    feat(e2e): parse mithril command version
    So the version can be used later to enable/disable/adapt tests scenarios

--- mithril-test-lab/mithril-end-to-end/src/utils/mod.rs
+++ mithril-test-lab/mithril-end-to-end/src/utils/mod.rs
@@ -3,11 +3,39 @@ mod mithril_command;
 mod spec_utils;
 mod file_utils;
 mod formatting;
+mod version_req;
 
 pub use formatting::*;
 pub use mithril_command::MithrilCommand;
 pub use spec_utils::AttemptResult;
+pub use version_req::NodeVersion;
 
 pub fn is_running_in_github_actions() -> bool {
     std::env::var("GITHUB_ACTIONS").is_ok()
+}
+
+fn get_process_path(
+    bin_name: &str,
+    bin_dir: &std::path::Path,
+) -> mithril_common::StdResult<std::path::PathBuf> {
+    use anyhow::Context;
+
+    let process_path = bin_dir
+        .canonicalize()
+        .with_context(|| {
+            format!(
+                "expected '{bin_dir}/{bin_name}' to be an existing executable",
+                bin_dir = bin_dir.display(),
+            )
+        })?
+        .join(bin_name);
+
+    if !process_path.exists() {
+        anyhow::bail!(
+            "cannot find '{bin_name}' executable in expected location '{bin_dir}'",
+            bin_dir = bin_dir.display(),
+        );
+    }
+
+    Ok(process_path)
 }
