fdba933ebe0c14f159f6f93617c2d0be1ac21a92
DJO <790521+Alenar@users.noreply.github.com>
2025-11-28 17:43:39 +0100

    doc(aggregator-client): better queries doc & fix a method doc

--- internal/mithril-aggregator-client/src/query/mod.rs
+++ internal/mithril-aggregator-client/src/query/mod.rs
@@ -1,7 +1,14 @@
 //! Provides queries to retrieve or send data to a Mithril aggregator
 //!
 //! Available queries
-//! - Certificate: Get by hash, get latest genesis certificate
+//! - Get:
+//!   - Aggregator features: [Get current aggregator features][GetAggregatorFeaturesQuery]
+//!   - Certificate: [List][GetCertificatesListQuery], [Get by hash, get latest genesis certificate][GetCertificateQuery]
+//!   - Epoch settings: [Get current epoch settings][GetEpochSettingsQuery]
+//!   - Protocol Configuration: [Get for a given epoch][GetProtocolConfigurationQuery]
+//! - Post:
+//!   - Signature: [Register a signature][PostRegisterSignatureQuery]
+//!   - Registration: [Send a signer registration][PostRegisterSignerQuery]
 //!
 mod api;
 mod get;
