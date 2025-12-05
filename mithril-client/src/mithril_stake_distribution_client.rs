b12e11a90712524f4d8807608b75a8a8ede8cab6
DJO <790521+Alenar@users.noreply.github.com>
2025-12-05 15:35:51 +0100

    refactor(client-lib): switch mithril stake distribution client backend to the shared aggregator client

--- mithril-client/src/mithril_stake_distribution_client.rs
+++ mithril-client/src/mithril_stake_distribution_client.rs
@@ -40,14 +40,11 @@
 
 use std::sync::Arc;
 
-use crate::aggregator_client::{AggregatorClient, AggregatorClientError, AggregatorRequest};
-use anyhow::Context;
-
 use crate::{MithrilResult, MithrilStakeDistribution, MithrilStakeDistributionListItem};
 
 /// HTTP client for MithrilStakeDistribution API from the Aggregator
 pub struct MithrilStakeDistributionClient {
-    aggregator_client: Arc<dyn AggregatorClient>,
+    aggregator_requester: Arc<dyn MithrilStakeDistributionAggregatorRequest>,
 }
 
 /// Define the requests against an Aggregator related to Mithril stake distribution.
@@ -64,86 +61,52 @@ pub trait MithrilStakeDistributionAggregatorRequest: Send + Sync {
 
 impl MithrilStakeDistributionClient {
     /// Constructs a new `MithrilStakeDistributionClient`.
-    pub fn new(aggregator_client: Arc<dyn AggregatorClient>) -> Self {
-        Self { aggregator_client }
+    pub fn new(aggregator_requester: Arc<dyn MithrilStakeDistributionAggregatorRequest>) -> Self {
+        Self {
+            aggregator_requester,
+        }
     }
 
     /// Fetch a list of signed MithrilStakeDistribution
     pub async fn list(&self) -> MithrilResult<Vec<MithrilStakeDistributionListItem>> {
-        let response = self
-            .aggregator_client
-            .get_content(AggregatorRequest::ListMithrilStakeDistributions)
-            .await
-            .with_context(|| "MithrilStakeDistribution Client can not get the artifact list")?;
-        let items = serde_json::from_str::<Vec<MithrilStakeDistributionListItem>>(&response)
-            .with_context(|| "MithrilStakeDistribution Client can not deserialize artifact list")?;
-
-        Ok(items)
+        self.aggregator_requester.list_latest().await
     }
 
-    /// Get the given stake distribution data. If it cannot be found, a None is returned.
+    /// Get the given stake distribution data. If it cannot be found, None is returned.
     pub async fn get(&self, hash: &str) -> MithrilResult<Option<MithrilStakeDistribution>> {
-        match self
-            .aggregator_client
-            .get_content(AggregatorRequest::GetMithrilStakeDistribution {
-                hash: hash.to_string(),
-            })
-            .await
-        {
-            Ok(content) => {
-                let stake_distribution_entity: MithrilStakeDistribution = serde_json::from_str(
-                    &content,
-                )
-                .with_context(|| "MithrilStakeDistribution Client can not deserialize artifact")?;
-
-                Ok(Some(stake_distribution_entity))
-            }
-            Err(AggregatorClientError::RemoteServerLogical(_)) => Ok(None),
-            Err(e) => Err(e.into()),
-        }
+        self.aggregator_requester.get_by_hash(hash).await
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use chrono::{DateTime, Utc};
+    use mockall::predicate::eq;
+
     use mithril_common::test::double::fake_data;
+    use mithril_common::test::mock_extensions::MockBuilder;
 
     use crate::MithrilSigner;
-    use crate::aggregator_client::MockAggregatorClient;
-    use crate::common::Epoch;
+    use crate::common::test::Dummy;
 
     use super::*;
 
-    fn fake_messages() -> Vec<MithrilStakeDistributionListItem> {
-        vec![
-            MithrilStakeDistributionListItem {
-                epoch: Epoch(1),
-                hash: "hash-123".to_string(),
-                certificate_hash: "cert-hash-123".to_string(),
-                created_at: DateTime::parse_from_rfc3339("2023-01-19T13:43:05.618857482Z")
-                    .unwrap()
-                    .with_timezone(&Utc),
-            },
-            MithrilStakeDistributionListItem {
-                epoch: Epoch(2),
-                hash: "hash-456".to_string(),
-                certificate_hash: "cert-hash-456".to_string(),
-                created_at: DateTime::parse_from_rfc3339("2023-01-19T13:43:05.618857482Z")
-                    .unwrap()
-                    .with_timezone(&Utc),
-            },
-        ]
-    }
-
     #[tokio::test]
     async fn get_mithril_stake_distribution_list() {
-        let message = fake_messages();
-        let mut http_client = MockAggregatorClient::new();
-        http_client
-            .expect_get_content()
-            .return_once(move |_| Ok(serde_json::to_string(&message).unwrap()));
-        let client = MithrilStakeDistributionClient::new(Arc::new(http_client));
+        let requester =
+            MockBuilder::<MockMithrilStakeDistributionAggregatorRequest>::configure(|mock| {
+                let messages = vec![
+                    MithrilStakeDistributionListItem {
+                        hash: "hash-123".to_string(),
+                        ..Dummy::dummy()
+                    },
+                    MithrilStakeDistributionListItem {
+                        hash: "hash-456".to_string(),
+                        ..Dummy::dummy()
+                    },
+                ];
+                mock.expect_list_latest().return_once(move || Ok(messages));
+            });
+        let client = MithrilStakeDistributionClient::new(requester);
         let items = client.list().await.unwrap();
 
         assert_eq!(2, items.len());
@@ -153,26 +116,28 @@ mod tests {
 
     #[tokio::test]
     async fn get_mithril_stake_distribution() {
-        let mut http_client = MockAggregatorClient::new();
-        let message = MithrilStakeDistribution {
-            certificate_hash: "certificate-hash-123".to_string(),
-            epoch: Epoch(1),
-            signers_with_stake: MithrilSigner::from_signers(fake_data::signers_with_stakes(2)),
-            hash: "hash".to_string(),
-            created_at: DateTime::<Utc>::default(),
-            protocol_parameters: fake_data::protocol_parameters(),
-        };
-        http_client
-            .expect_get_content()
-            .return_once(move |_| Ok(serde_json::to_string(&message).unwrap()));
-        let client = MithrilStakeDistributionClient::new(Arc::new(http_client));
+        let requester =
+            MockBuilder::<MockMithrilStakeDistributionAggregatorRequest>::configure(|mock| {
+                let message = MithrilStakeDistribution {
+                    hash: "hash".to_string(),
+                    signers_with_stake: MithrilSigner::from_signers(
+                        fake_data::signers_with_stakes(2),
+                    ),
+                    ..Dummy::dummy()
+                };
+                mock.expect_get_by_hash()
+                    .with(eq(message.hash.clone()))
+                    .return_once(move |_| Ok(Some(message)));
+            });
+        let client = MithrilStakeDistributionClient::new(requester);
+
         let stake_distribution_entity = client
             .get("hash")
             .await
             .unwrap()
-            .expect("This test returns a stake distribution");
+            .expect("should return a mithril stake distribution");
 
-        assert_eq!("hash".to_string(), stake_distribution_entity.hash);
+        assert_eq!("hash", &stake_distribution_entity.hash);
         assert_eq!(2, stake_distribution_entity.signers_with_stake.len(),);
     }
 }
