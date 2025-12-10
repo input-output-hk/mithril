f291606883b468ad4b9a5eadcc27ac222946ea24
DJO <790521+Alenar@users.noreply.github.com>
2025-12-10 10:32:00 +0100

    ci(hydra): remove unneeded packages
    Only public binaries and libraries should be built, with the addition of
    eventual tools that may be usefull to third parties (only the e2e at the
    moment).

--- flake.nix
+++ flake.nix
@@ -110,8 +110,6 @@
           };
           mithril-aggregator = buildPackage ./mithril-aggregator/Cargo.toml mithril.cargoArtifacts { cargoTestCommand = "cargo test --no-default-features"; };
           mithril-signer = buildPackage ./mithril-signer/Cargo.toml mithril.cargoArtifacts { cargoTestCommand = "cargo test --no-default-features"; };
-          mithril-persistence = buildPackage ./internal/mithril-persistence/Cargo.toml mithril.cargoArtifacts {};
-          mithrildemo = buildPackage ./demo/protocol-demo/Cargo.toml mithril.cargoArtifacts {};
           mithril-end-to-end = buildPackage ./mithril-test-lab/mithril-end-to-end/Cargo.toml null {};
         };
 
