116262fe457e08d068abc1be68b136723b01c05a
Alenar <Alenar@users.noreply.github.com>
2025-12-12 09:33:20 +0000

    deploy: 16c4ecf4752db61ee1b2a3f729cce78c513a5a35

---
+++ rust-doc/mithril_client/certificate_client/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["CertificateClient","MemoryCertificateVerifierCache","MithrilCertificateVerifier"],"trait":["CertificateAggregatorRequest","CertificateVerifier","CertificateVerifierCache"]};
