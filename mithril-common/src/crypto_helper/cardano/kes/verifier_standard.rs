1b3f7789ba2a3e9e15bf2f2961d2e9cfc3db9ab4
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2025-12-19 15:40:01 +0100

    fix(common): KES period range verified was too narrow

--- mithril-common/src/crypto_helper/cardano/kes/verifier_standard.rs
+++ mithril-common/src/crypto_helper/cardano/kes/verifier_standard.rs
@@ -25,10 +25,11 @@ impl KesVerifier for KesVerifierStandard {
             .validate()
             .map_err(|_| KesVerifyError::OpCertInvalid)?;
 
-        // Check if the signature verifies for the provided KES evolutions value +/- 1
+        // Check if the signature verifies for the provided KES evolutions value +/- 1 KES period.
+        // This is needed to account for clock skew between signer and verifier.
         let kes_evolutions_try_min = std::cmp::max(0, kes_evolutions.saturating_sub(1));
         let kes_evolutions_try_max = std::cmp::min(64, kes_evolutions.saturating_add(1));
-        for kes_evolutions_try in kes_evolutions_try_min..kes_evolutions_try_max {
+        for kes_evolutions_try in kes_evolutions_try_min..=kes_evolutions_try_max {
             if signature
                 .verify(
                     kes_evolutions_try,
@@ -107,6 +108,48 @@ mod tests {
 
         KesVerifierStandard
             .verify(b"Different message", &signature, &op_cert, kes_evolutions)
+            .expect_err("Signature verification should fail");
+    }
+
+    #[test]
+    fn verify_valid_signature_invalid_kes_evolutions_fails() {
+        let start_kes_period = 10 as KesPeriod;
+        let kes_evolutions = 5;
+        let signing_kes_period = start_kes_period + kes_evolutions;
+        let KesCryptographicMaterialForTest {
+            party_id: _,
+            operational_certificate_file,
+            kes_secret_key_file,
+        } = create_kes_cryptographic_material(
+            1 as KesPartyIndexForTest,
+            start_kes_period,
+            current_function!(),
+        );
+        let message = b"Test message for KES signing";
+        let kes_signer = KesSignerStandard::new(kes_secret_key_file, operational_certificate_file);
+
+        let (signature, op_cert) = kes_signer
+            .sign(message, signing_kes_period)
+            .expect("Signing should not fail");
+
+        KesVerifierStandard
+            .verify(message, &signature, &op_cert, kes_evolutions - 2)
+            .expect_err("Signature verification should fail");
+
+        KesVerifierStandard
+            .verify(message, &signature, &op_cert, kes_evolutions - 1)
+            .expect("Signature verification should not fail");
+
+        KesVerifierStandard
+            .verify(message, &signature, &op_cert, kes_evolutions)
+            .expect("Signature verification should not fail");
+
+        KesVerifierStandard
+            .verify(message, &signature, &op_cert, kes_evolutions + 1)
+            .expect("Signature verification should not fail");
+
+        KesVerifierStandard
+            .verify(message, &signature, &op_cert, kes_evolutions + 2)
             .expect_err("Signature verification should fail");
     }
 }
