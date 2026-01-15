69d3df25855d3b0623c29b37c101603050e59588
DJO <790521+Alenar@users.noreply.github.com>
2026-01-15 17:12:02 +0100

    refactor: introduce `chain_importer` module in mithril-cardano-node-chain
    Its code is a copy of the identical `CardanoTransactionsImporter` of the
    signer and aggregator services.
    Differences are:
    - Renamed to `CardanoChainDataImporter` as its scope will soon be
      broaden, this name should be futurproof
    - Renamed its `TransactionStore` trait to `ChainDataStore` for the same
      motives
    - Introduce `InMemoryBlockRangeRoot` in the crates test doubles, to
      replace the use of the concrete database that was used in the copied
      code (which is not available here)

--- internal/cardano-node/mithril-cardano-node-chain/src/lib.rs
+++ internal/cardano-node/mithril-cardano-node-chain/src/lib.rs
@@ -2,6 +2,7 @@
 //! This crate provides functionality for interacting with the Cardano blockchain through integration with Cardano
 //! nodes, offering chain observation, block scanning, and transaction handling capabilities.
 
+pub mod chain_importer;
 pub mod chain_observer;
 pub mod chain_reader;
 pub mod chain_scanner;
