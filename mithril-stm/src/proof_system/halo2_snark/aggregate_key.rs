39174c1b4db0b1f0bacec059dda66e8c24d1291c
Dam <damien.robissout@iohk.io>
2026-01-23 16:53:26 +0100

    feat(stm): added struct AggregateVerificationKeyForSnark and its basic implementation

---
+++ mithril-stm/src/proof_system/halo2_snark/aggregate_key.rs
@@ -0,0 +1,30 @@
+use serde::{Deserialize, Serialize};
+
+use crate::{
+    MembershipDigest, Stake,
+    membership_commitment::{MerkleTreeCommitment, MerkleTreeConcatenationLeaf},
+};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct AggregateVerificationKeyForSnark<D: MembershipDigest> {
+    // TODO: Change Leaf once PR1 is merged
+    merkle_tree_commitment: MerkleTreeCommitment<D::SnarkHash, MerkleTreeConcatenationLeaf>,
+    // TODO: Change to EligibilityValue once PR1 is merged
+    target_value: Stake,
+}
+
+impl<D: MembershipDigest> AggregateVerificationKeyForSnark<D> {
+    /// Get the Merkle tree batch commitment.
+    // TODO: Change Leaf once PR1 is merged
+    pub(crate) fn get_merkle_tree_commitment(
+        &self,
+    ) -> MerkleTreeCommitment<D::SnarkHash, MerkleTreeConcatenationLeaf> {
+        self.merkle_tree_commitment.clone()
+    }
+
+    /// Get the total stake.
+    // TODO: Change to EligibilityValue once PR1 is merged
+    pub fn get_target_value(&self) -> Stake {
+        self.target_value
+    }
+}
