c340c9c4b381e63917d974aed700d47e78b34a72
Clément Turmel <turmel@teragone-solutions.com>
2026-01-27 12:06:37 +0100

    feature(aggregator): add a new ProverService, that only compute cache for now

---
+++ mithril-aggregator/src/services/prover.rs
@@ -0,0 +1,72 @@
+use async_trait::async_trait;
+use rayon::prelude::*;
+use slog::{Logger, debug, info};
+use std::sync::Arc;
+
+use mithril_common::{
+    StdResult,
+    crypto_helper::{MKMap, MKMapNode, MKTreeStorer},
+    entities::{BlockNumber, BlockRange},
+    signable_builder::BlockRangeRootRetriever,
+};
+use mithril_resource_pool::ResourcePool;
+
+/// Prover service is the cryptographic engine in charge of producing cryptographic proofs for transactions and blocks
+/// /!\ The trait is not final /!\
+#[cfg_attr(test, mockall::automock)]
+#[async_trait]
+pub trait ProverService: Sync + Send {
+    /// Compute the cache
+    async fn compute_cache(&self, up_to: BlockNumber) -> StdResult<()>;
+}
+
+/// Mithril prover
+pub struct MithrilProverService<S: MKTreeStorer> {
+    // transaction_retriever: Arc<dyn TransactionsRetriever>,
+    block_range_root_retriever: Arc<dyn BlockRangeRootRetriever<S>>,
+    mk_map_pool: ResourcePool<MKMap<BlockRange, MKMapNode<BlockRange, S>, S>>,
+    logger: Logger,
+}
+
+#[async_trait]
+impl<S: MKTreeStorer> ProverService for MithrilProverService<S> {
+    async fn compute_cache(&self, up_to: BlockNumber) -> StdResult<()> {
+        let pool_size = self.mk_map_pool.size();
+        info!(
+            self.logger, "Starts computing the Merkle map pool resource of size {pool_size}";
+            "up_to_block_number" => *up_to,
+        );
+        let mk_map_cache = self
+            .block_range_root_retriever
+            .compute_merkle_map_from_block_range_roots(up_to)
+            .await?;
+        let mk_maps_new = (1..=pool_size)
+            .into_par_iter()
+            .map(|i| {
+                debug!(
+                    self.logger,
+                    "Computing the Merkle map pool resource {i}/{pool_size}"
+                );
+                mk_map_cache.clone()
+            })
+            .collect::<Vec<MKMap<_, _, _>>>();
+        debug!(self.logger, "Draining the Merkle map pool");
+        let discriminant_new = self.mk_map_pool.discriminant()? + 1;
+        self.mk_map_pool.set_discriminant(discriminant_new)?;
+        self.mk_map_pool.clear();
+        debug!(
+            self.logger,
+            "Giving back new resources to the Merkle map pool"
+        );
+        mk_maps_new
+            .into_iter()
+            .map(|mk_map| self.mk_map_pool.give_back_resource(mk_map, discriminant_new))
+            .collect::<StdResult<Vec<_>>>()?;
+        info!(
+            self.logger,
+            "Completed computing the Merkle map pool resource of size {pool_size}"
+        );
+
+        Ok(())
+    }
+}
