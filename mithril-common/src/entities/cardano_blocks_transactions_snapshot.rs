b9f17cf5fc3e76b1c93bdeac993b69dbce3b289f
Clément Turmel <turmel@teragone-solutions.com>
2026-01-28 17:02:23 +0100

    feature(aggregator): offset_security_parameter is part of CardanoBlocksTransactionsSnapshot and used for inside hash computation

--- mithril-common/src/entities/cardano_blocks_transactions_snapshot.rs
+++ mithril-common/src/entities/cardano_blocks_transactions_snapshot.rs
@@ -6,47 +6,44 @@ use super::BlockNumber;
 /// Snapshot of a set of Cardano transactions
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub struct CardanoBlocksTransactionsSnapshot {
-    /// Hash of the Cardano transactions set
+    /// Hash of the Cardano blocks transactions set
     pub hash: String,
 
-    /// Merkle root of the Cardano transactions set
+    /// Merkle root of the Cardano blocks transactions set
     pub merkle_root: String,
 
-    /// Beacon of the Cardano transactions set
+    /// Beacon of the Cardano blocks transactions set
     pub block_number_signed: BlockNumber,
 
-    /// Block number of the tip at snapshot time
+    /// Aproximate block number of the tip at snapshot time
     pub block_number_tip: BlockNumber,
 }
 
 impl CardanoBlocksTransactionsSnapshot {
-    /// Creates a new [CardanoTransactionsSnapshot]
-    pub fn new(merkle_root: String, block_number_signed: BlockNumber) -> Self {
+    /// Creates a new [CardanoBlocksTransactionsSnapshot]
+    pub fn new(
+        merkle_root: String,
+        block_number_signed: BlockNumber,
+        offset_security_parameter: BlockNumber,
+    ) -> Self {
         let mut snapshot = Self {
             merkle_root,
             block_number_signed,
-            block_number_tip: BlockNumber(0),
+            block_number_tip: block_number_signed + offset_security_parameter,
             hash: "".to_string(),
         };
-        snapshot.block_number_tip = snapshot.compute_block_number_tip();
         snapshot.hash = snapshot.compute_hash();
-
         snapshot
     }
 
-    /// Cardano transactions snapshot hash computation
+    /// Cardano blocks transactions snapshot hash computation
     fn compute_hash(&self) -> String {
         let mut hasher = Sha256::new();
-        hasher.update(self.merkle_root.clone().as_bytes());
+        hasher.update(self.merkle_root.as_bytes());
         hasher.update(self.block_number_signed.to_be_bytes());
-        //TODO also hasher update the block number tip ?
+        hasher.update(self.block_number_tip.to_be_bytes());
 
         hex::encode(hasher.finalize())
-    }
-
-    fn compute_block_number_tip(&self) -> BlockNumber {
-        //TODO sends offset_security_parameter & delta (or timepoint) throught the construction of CardanoBlocksTransactionsSnapshot ?
-        self.block_number_signed + offset_security_parameter + delta // delta = current_tiempoint - block_number_signed ?
     }
 }
 
@@ -55,25 +52,48 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_cardano_transactions_snapshot_compute_hash() {
-        let hash_expected = "fac28cf7aa07922258d7d3ad8b16d859d8a3b812822b90050cddbc2e6671aab5";
+    fn test_cardano_blocks_transactions_snapshot_compute_hash() {
+        let mk_root_123_50_15_hash =
+            "d99fe4ea053d416de6726deef785766e44970351e63172521f4783f23c8cb66c";
 
         assert_eq!(
-            hash_expected,
-            CardanoBlocksTransactionsSnapshot::new("mk-root-123".to_string(), BlockNumber(50))
-                .compute_hash()
+            mk_root_123_50_15_hash,
+            CardanoBlocksTransactionsSnapshot::new(
+                "mk-root-123".to_string(),
+                BlockNumber(50),
+                BlockNumber(15)
+            )
+            .compute_hash()
         );
 
         assert_ne!(
-            hash_expected,
-            CardanoBlocksTransactionsSnapshot::new("mk-root-456".to_string(), BlockNumber(50))
-                .compute_hash()
+            mk_root_123_50_15_hash,
+            CardanoBlocksTransactionsSnapshot::new(
+                "mk-root-456".to_string(),
+                BlockNumber(50),
+                BlockNumber(15)
+            )
+            .compute_hash()
         );
 
         assert_ne!(
-            hash_expected,
-            CardanoBlocksTransactionsSnapshot::new("mk-root-123".to_string(), BlockNumber(47))
-                .compute_hash()
+            mk_root_123_50_15_hash,
+            CardanoBlocksTransactionsSnapshot::new(
+                "mk-root-123".to_string(),
+                BlockNumber(47),
+                BlockNumber(15)
+            )
+            .compute_hash()
+        );
+
+        assert_ne!(
+            mk_root_123_50_15_hash,
+            CardanoBlocksTransactionsSnapshot::new(
+                "mk-root-123".to_string(),
+                BlockNumber(50),
+                BlockNumber(42)
+            )
+            .compute_hash()
         );
     }
 }
