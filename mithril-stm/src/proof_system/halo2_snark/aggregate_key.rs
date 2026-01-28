336ea4d55493c6f4c07162288a9a5751ee84e3d0
Dam <damien.robissout@iohk.io>
2026-01-28 19:03:54 +0100

    feat(stm): updated to/from bytes for MerkleTreeSnarkLeaf to match new implementation

--- mithril-stm/src/proof_system/halo2_snark/aggregate_key.rs
+++ mithril-stm/src/proof_system/halo2_snark/aggregate_key.rs
@@ -2,24 +2,22 @@ use serde::{Deserialize, Serialize};
 
 use crate::{
     MembershipDigest, Stake,
-    membership_commitment::{MerkleTreeCommitment, MerkleTreeConcatenationLeaf},
+    membership_commitment::{MerkleTreeCommitment, MerkleTreeSnarkLeaf},
 };
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct AggregateVerificationKeyForSnark<D: MembershipDigest> {
-    // TODO: Change Leaf once PR1 is merged
-    merkle_tree_commitment: MerkleTreeCommitment<D::SnarkHash, MerkleTreeConcatenationLeaf>,
+    merkle_tree_commitment: MerkleTreeCommitment<D::SnarkHash, MerkleTreeSnarkLeaf>,
     // TODO: Change to EligibilityValue once PR1 is merged
     target_value: Stake,
 }
 // TODO: remove this allow dead_code directive when function is called or future_snark is activated
 #[allow(dead_code)]
 impl<D: MembershipDigest> AggregateVerificationKeyForSnark<D> {
-    /// Get the Merkle tree batch commitment.
-    // TODO: Change Leaf once PR1 is merged
+    /// Get the Merkle tree commitment.
     pub(crate) fn get_merkle_tree_commitment(
         &self,
-    ) -> MerkleTreeCommitment<D::SnarkHash, MerkleTreeConcatenationLeaf> {
+    ) -> MerkleTreeCommitment<D::SnarkHash, MerkleTreeSnarkLeaf> {
         self.merkle_tree_commitment.clone()
     }
 
