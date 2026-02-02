c21c785a97d953c3abf6d43e447a13a28a452294
curiecrypt <gamze.kilic@iohk.io>
2026-02-02 23:00:52 +0300

    feat(stm): snark key registration SingleSignatureWithRegisteredParty uses ClosedRegistrationEntry

--- mithril-stm/src/protocol/key_registration/concatenation_registration_entry.rs
+++ mithril-stm/src/protocol/key_registration/concatenation_registration_entry.rs
@@ -1,16 +1,9 @@
 use crate::membership_commitment::MerkleTreeConcatenationLeaf;
 
-use super::{ClosedRegistrationEntry, RegistrationEntry};
+use super::ClosedRegistrationEntry;
 
 /// The type used for committing signer registrations for the Concatenation proof system.
 pub type RegistrationEntryForConcatenation = MerkleTreeConcatenationLeaf;
-
-/// Converts a registration entry into the related registration entry for concatenation proof.
-impl From<RegistrationEntry> for RegistrationEntryForConcatenation {
-    fn from(entry: RegistrationEntry) -> Self {
-        MerkleTreeConcatenationLeaf(entry.get_bls_verification_key(), entry.get_stake())
-    }
-}
 
 impl From<ClosedRegistrationEntry> for RegistrationEntryForConcatenation {
     fn from(entry: ClosedRegistrationEntry) -> Self {
