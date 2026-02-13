0f0ed97daf2ea1641cad0d718975fece22e961ad
DJO <790521+Alenar@users.noreply.github.com>
2026-02-13 19:30:10 +0100

    feat(common): add `MkSetProof`
    A generic type to hold proof of inclusion for "v2" transactions and
    blocks.

--- mithril-common/src/test/entities_extensions.rs
+++ mithril-common/src/test/entities_extensions.rs
@@ -3,11 +3,10 @@
 use std::collections::HashMap;
 
 use crate::StdResult;
-use crate::crypto_helper::{
-    MKMap, MKMapNode, MKTree, MKTreeNode, MKTreeStoreInMemory, MKTreeStorer,
-};
+use crate::crypto_helper::{MKMap, MKMapNode, MKTree, MKTreeNode, MKTreeStorer};
 use crate::entities::{
-    BlockNumber, BlockRange, CardanoTransactionsSetProof, ProtocolParameters, SingleSignature,
+    BlockNumber, BlockRange, CardanoBlock, CardanoTransaction, CardanoTransactionsSetProof,
+    IntoMKTreeNode, MkSetProof, ProtocolParameters, SingleSignature,
     SingleSignatureAuthenticationStatus, TransactionHash,
 };
 use crate::test::builder::{MithrilFixtureBuilder, StakeDistributionGenerationMethod};
@@ -35,33 +34,50 @@ impl CardanoTransactionsSetProofTestExtension for CardanoTransactionsSetProof {
     ) -> StdResult<CardanoTransactionsSetProof> {
         let transactions_hashes: Vec<TransactionHash> =
             leaves.iter().map(|(_, t)| t.into()).collect();
-        let mut transactions_by_block_ranges: HashMap<BlockRange, Vec<TransactionHash>> =
-            HashMap::new();
-        for (block_number, transaction_hash) in leaves {
-            let block_range = BlockRange::from_block_number(*block_number);
-            transactions_by_block_ranges
-                .entry(block_range)
-                .or_default()
-                .push(transaction_hash.to_owned());
-        }
-        let mk_map = MKMap::<_, _, MKTreeStoreInMemory>::new(
-            transactions_by_block_ranges
-                .into_iter()
-                .try_fold(
-                    vec![],
-                    |mut acc,
-                     (block_range, transactions)|
-                     -> StdResult<Vec<(_, MKMapNode<_, S>)>> {
-                        acc.push((block_range, MKTree::<S>::new(&transactions)?.into()));
-                        Ok(acc)
-                    },
-                )?
-                .as_slice(),
+        let transactions_by_block_ranges = private::group_by_block_range(leaves.iter().cloned());
+        let mk_map = private::fold_nodes_per_block_range_into_mkmap::<_, _, S>(
+            transactions_by_block_ranges,
         )?;
-        let mk_leaves: Vec<MKTreeNode> =
-            transactions_hashes.iter().map(|h| h.to_owned().into()).collect();
-        let mk_proof = mk_map.compute_proof(&mk_leaves)?;
+        let mk_proof = mk_map.compute_proof(&transactions_hashes)?;
         Ok(Self::new(transactions_hashes, mk_proof))
+    }
+}
+
+/// Extension trait adding test utilities to [MkSetProof]
+pub trait MkSetProofTestExtension<L: IntoMKTreeNode + Clone> {
+    /// `TEST ONLY` - Helper to create a proof from a list of leaves
+    fn from_leaves<S: MKTreeStorer>(leaves: &[L]) -> StdResult<MkSetProof<L>>;
+}
+
+impl MkSetProofTestExtension<CardanoBlock> for MkSetProof<CardanoBlock> {
+    fn from_leaves<S: MKTreeStorer>(
+        leaves: &[CardanoBlock],
+    ) -> StdResult<MkSetProof<CardanoBlock>> {
+        let node_per_block_range = private::group_by_block_range(
+            leaves.iter().map(|l| (l.block_number, l.clone().into_mk_tree_node())),
+        );
+        let all_nodes = node_per_block_range.values().flatten().cloned().collect::<Vec<_>>();
+        let mk_map =
+            private::fold_nodes_per_block_range_into_mkmap::<_, _, S>(node_per_block_range)?;
+        let proof = mk_map.compute_proof(&all_nodes)?;
+
+        Ok(MkSetProof::new(leaves.to_vec(), proof))
+    }
+}
+
+impl MkSetProofTestExtension<CardanoTransaction> for MkSetProof<CardanoBlock> {
+    fn from_leaves<S: MKTreeStorer>(
+        leaves: &[CardanoTransaction],
+    ) -> StdResult<MkSetProof<CardanoTransaction>> {
+        let node_per_block_range = private::group_by_block_range(
+            leaves.iter().map(|l| (l.block_number, l.clone().into_mk_tree_node())),
+        );
+        let all_nodes = node_per_block_range.values().flatten().cloned().collect::<Vec<_>>();
+        let mk_map =
+            private::fold_nodes_per_block_range_into_mkmap::<_, _, S>(node_per_block_range)?;
+        let proof = mk_map.compute_proof(&all_nodes)?;
+
+        Ok(MkSetProof::new(leaves.to_vec(), proof))
     }
 }
 
@@ -96,5 +112,47 @@ impl SingleSignatureTestExtension for SingleSignature {
             won_indexes: vec![10, 15],
             authentication_status: SingleSignatureAuthenticationStatus::Unauthenticated,
         }
+    }
+}
+
+mod private {
+    use super::*;
+
+    pub(super) fn group_by_block_range<N, I>(iter: I) -> HashMap<BlockRange, Vec<N>>
+    where
+        I: Iterator<Item = (BlockNumber, N)>,
+    {
+        let mut result: HashMap<BlockRange, Vec<N>> = HashMap::new();
+
+        for (block_number, item) in iter.into_iter() {
+            let block_range = BlockRange::from_block_number(block_number);
+            result.entry(block_range).or_default().push(item);
+        }
+
+        result
+    }
+
+    pub(super) fn fold_nodes_per_block_range_into_mkmap<N, I, S>(
+        nodes_per_block_range: I,
+    ) -> StdResult<MKMap<BlockRange, MKMapNode<BlockRange, S>, S>>
+    where
+        N: Into<MKTreeNode> + Clone,
+        I: IntoIterator<Item = (BlockRange, Vec<N>)>,
+        S: MKTreeStorer,
+    {
+        let mk_map = MKMap::<_, _, S>::new(
+            nodes_per_block_range
+                .into_iter()
+                .try_fold(
+                    vec![],
+                    |mut acc, (block_range, nodes)| -> StdResult<Vec<(_, MKMapNode<_, S>)>> {
+                        acc.push((block_range, MKTree::<S>::new(&nodes)?.into()));
+                        Ok(acc)
+                    },
+                )?
+                .as_slice(),
+        )?;
+
+        Ok(mk_map)
     }
 }
