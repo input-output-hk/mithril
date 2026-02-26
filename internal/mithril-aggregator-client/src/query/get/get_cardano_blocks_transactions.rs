67868f403f716e433a2e58817a54f374985648aa
Clément Turmel <turmel@teragone-solutions.com>
2026-02-26 15:55:24 +0100

    feature(client): implement route retrieval blocks and transactions artifact

---
+++ internal/mithril-aggregator-client/src/query/get/get_cardano_blocks_transactions.rs
@@ -0,0 +1,111 @@
+use async_trait::async_trait;
+use reqwest::StatusCode;
+
+use mithril_common::messages::CardanoBlocksTransactionsSnapshotMessage;
+
+use crate::AggregatorHttpClientResult;
+use crate::query::{AggregatorQuery, QueryContext, QueryMethod, ResponseExt};
+
+/// Query to get the details of a Cardano blocks and transactions snapshot detail
+pub struct GetCardanoBlocksTransactionsQuery {
+    hash: String,
+}
+
+impl GetCardanoBlocksTransactionsQuery {
+    /// Instantiate a query to get a Cardano blocks and transactions snapshot detail by hash
+    pub fn by_hash<H: Into<String>>(hash: H) -> Self {
+        Self { hash: hash.into() }
+    }
+}
+
+#[cfg_attr(target_family = "wasm", async_trait(?Send))]
+#[cfg_attr(not(target_family = "wasm"), async_trait)]
+impl AggregatorQuery for GetCardanoBlocksTransactionsQuery {
+    type Response = Option<CardanoBlocksTransactionsSnapshotMessage>;
+    type Body = ();
+
+    fn method() -> QueryMethod {
+        QueryMethod::Get
+    }
+
+    fn route(&self) -> String {
+        format!("artifact/cardano-blocks-transactions/{}", self.hash)
+    }
+
+    async fn handle_response(
+        &self,
+        context: QueryContext,
+    ) -> AggregatorHttpClientResult<Self::Response> {
+        match context.response.status() {
+            StatusCode::OK => context.response.parse_json_option().await,
+            StatusCode::NOT_FOUND => Ok(None),
+            _ => Err(context.unhandled_status_code().await),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use serde_json::json;
+
+    use mithril_common::test::double::Dummy;
+
+    use crate::AggregatorHttpClientError;
+    use crate::test::{assert_error_matches, setup_server_and_client};
+
+    use super::*;
+
+    #[tokio::test]
+    async fn test_cardano_blocks_transactions_details_ok_200() {
+        let (server, client) = setup_server_and_client();
+        let expected_message = CardanoBlocksTransactionsSnapshotMessage::dummy();
+        let _server_mock = server.mock(|when, then| {
+            when.path(format!(
+                "/artifact/cardano-blocks-transactions/{}",
+                expected_message.hash
+            ));
+            then.status(200).body(json!(expected_message).to_string());
+        });
+
+        let fetched_message = client
+            .send(GetCardanoBlocksTransactionsQuery::by_hash(
+                &expected_message.hash,
+            ))
+            .await
+            .unwrap();
+
+        assert_eq!(Some(expected_message), fetched_message);
+    }
+
+    #[tokio::test]
+    async fn test_cardano_blocks_transactions_details_ok_404() {
+        let (server, client) = setup_server_and_client();
+        let _server_mock = server.mock(|when, then| {
+            when.any_request();
+            then.status(404);
+        });
+
+        let fetched_message = client
+            .send(GetCardanoBlocksTransactionsQuery::by_hash("whatever"))
+            .await
+            .unwrap();
+
+        assert_eq!(None, fetched_message);
+    }
+
+    #[tokio::test]
+    async fn test_cardano_blocks_transactions_details_ko_500() {
+        let (server, client) = setup_server_and_client();
+        let _server_mock = server.mock(|when, then| {
+            when.any_request();
+            then.status(500).body("an error occurred");
+        });
+
+        let error = client
+            .send(GetCardanoBlocksTransactionsQuery::by_hash("whatever"))
+            .await
+            .unwrap_err();
+
+        assert_error_matches!(error, AggregatorHttpClientError::RemoteServerTechnical(_));
+    }
+}
