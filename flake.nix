0ce631269cb1c05b5a205247ff81125afedb2bdb
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2026-02-27 17:58:22 +0100

    feat(nix): build static versions of the aggregator, signer and client

--- flake.nix
+++ flake.nix
@@ -50,33 +50,60 @@
         else
           null;
 
+        sourceFilter = orig_path: type: let
+          path = builtins.toString orig_path;
+          base = builtins.baseNameOf path;
+          parentDir = builtins.baseNameOf (builtins.dirOf path);
+          matchesSuffix = lib.any (suffix: lib.hasSuffix suffix base) [".rs" ".toml" ".md"];
+          isCargoFile = base == "Cargo.lock";
+          isCargoConfig = parentDir == ".cargo" && base == "config";
+          isOpenApiYaml = base == "openapi.yaml";
+          isFakeAggregatorDefaultData = lib.hasInfix "/mithril-aggregator-fake/default_data/" path;
+          isTestDataAsset = lib.hasInfix "/test_data/" path;
+        in
+          type == "directory" || matchesSuffix || isCargoFile || isCargoConfig || isOpenApiYaml
+                || isFakeAggregatorDefaultData || isTestDataAsset;
+
         clean = root:
           lib.cleanSourceWith {
             src = lib.cleanSource (craneLib.path root);
-            filter = orig_path: type: let
-              path = builtins.toString orig_path;
-              base = builtins.baseNameOf path;
-              parentDir = builtins.baseNameOf (builtins.dirOf path);
-              matchesSuffix = lib.any (suffix: lib.hasSuffix suffix base) [".rs" ".toml" ".md"];
-              isCargoFile = base == "Cargo.lock";
-              isCargoConfig = parentDir == ".cargo" && base == "config";
-              isOpenApiYaml = base == "openapi.yaml";
-              isFakeAggregatorDefaultData = lib.hasInfix "/mithril-aggregator-fake/default_data/" path;
-              isTestDataAsset = lib.hasInfix "/test_data/" path;
-            in
-              type == "directory" || matchesSuffix || isCargoFile || isCargoConfig || isOpenApiYaml
-                    || isFakeAggregatorDefaultData || isTestDataAsset;
+            filter = sourceFilter;
+          };
+
+        cleanMusl = root:
+          lib.cleanSourceWith {
+            src = lib.cleanSource (craneLibMusl.path root);
+            filter = sourceFilter;
           };
 
         buildInputs =
           [
-            pkgs.openssl
             pkgs.pkg-config
             pkgs.gnum4
+            pkgs.openssl
           ]
           ++ lib.optionals (pkgs.stdenv.isDarwin) [
             pkgs.libiconv
           ];
+
+        opensslMusl =
+          if pkgsMusl != null then
+            pkgsMusl.openssl.override { static = true; }
+          else null;
+
+        nativeBuildInputsMusl =
+          if pkgsMusl != null then [
+            pkgsMusl.stdenv.cc
+            pkgsMusl.pkg-config
+            pkgsMusl.gnum4
+          ]
+          else [];
+
+        buildInputsMusl =
+          if pkgsMusl != null then [
+            opensslMusl
+          ]
+          else [];
 
         commonsArgs = {
           pname = "mithril";
@@ -86,31 +113,46 @@
           CARGO_TERM_VERBOSE = "true";
         };
 
-        buildDeps = cargoToml: cargoArtifacts:
-          (craneLib.buildDepsOnly (commonsArgs
+        commonsArgsMusl =
+          if pkgsMusl != null then {
+            pname = "mithril";
+            version = "0.0.1";
+            src = cleanMusl ./.;
+            nativeBuildInputs = nativeBuildInputsMusl;
+            buildInputs = buildInputsMusl;
+            CARGO_TERM_VERBOSE = "true";
+            OPENSSL_STATIC = "1";
+            OPENSSL_LIB_DIR = "${opensslMusl.out}/lib";
+            OPENSSL_INCLUDE_DIR = "${opensslMusl.dev}/include";
+          }
+          else {};
+
+        preInstallScript = let
+          localLibs = "dummy|mithril|stm|multi_sig|size_benches|digester";
+        in ''
+          shopt -s extglob
+          TARGET_DIR="''${CARGO_TARGET_DIR:-target}"
+          if [ -n "$CARGO_BUILD_TARGET" ]; then
+            PROFILE_DIR="$TARGET_DIR/$CARGO_BUILD_TARGET/release"
+          else
+            PROFILE_DIR="$TARGET_DIR/release"
+          fi
+
+          rm "$PROFILE_DIR/deps/"*@(${localLibs})*
+          rm -r "$PROFILE_DIR/"*@(${localLibs})*
+          rm -r "$PROFILE_DIR/build/"*@(${localLibs})*
+          rm -r "$PROFILE_DIR/.fingerprint/"*@(${localLibs})*
+        '';
+
+        buildDeps = craneLibFor: commonArgs: cargoToml: cargoArtifacts:
+          (craneLibFor.buildDepsOnly (commonArgs
             // lib.optionalAttrs (cargoToml != null) rec {
-              inherit (craneLib.crateNameFromCargoToml {inherit cargoToml;}) pname version;
+              inherit (craneLibFor.crateNameFromCargoToml {inherit cargoToml;}) pname version;
               cargoExtraArgs = "-p ${pname}";
             }))
           .overrideAttrs (_: {
             inherit cargoArtifacts;
-            preInstall = let
-              # Need to remove dummy builds of local builds so that they can be built again
-              localLibs = "dummy|mithril|stm|multi_sig|size_benches|digester";
-            in ''
-              shopt -s extglob
-              TARGET_DIR="''${CARGO_TARGET_DIR:-target}"
-              if [ -n "$CARGO_BUILD_TARGET" ]; then
-                PROFILE_DIR="$TARGET_DIR/$CARGO_BUILD_TARGET/release"
-              else
-                PROFILE_DIR="$TARGET_DIR/release"
-              fi
-
-              rm "$PROFILE_DIR/deps/"*@(${localLibs})*
-              rm -r "$PROFILE_DIR/"*@(${localLibs})*
-              rm -r "$PROFILE_DIR/build/"*@(${localLibs})*
-              rm -r "$PROFILE_DIR/.fingerprint/"*@(${localLibs})*
-            '';
+            preInstall = preInstallScript;
           });
 
         buildPackage = cargoToml: baseCargoArtifacts: args:
@@ -120,7 +162,8 @@
               cargoExtraArgs = "-p ${pname}";
             }
             // {
-              cargoArtifacts = buildDeps cargoToml baseCargoArtifacts;
+              cargoArtifacts = buildDeps craneLib commonsArgs cargoToml baseCargoArtifacts;
+              doCheck = false;
             }
             // {
               cargoTestCommand = "RUST_BACKTRACE=1 cargo test --profile release";
@@ -128,24 +171,28 @@
             // args);
 
         buildPackageMusl = cargoToml: baseCargoArtifacts: args:
-          craneLibMusl.buildPackage (commonsArgs
+          craneLibMusl.buildPackage (commonsArgsMusl
             // lib.optionalAttrs (cargoToml != null) rec {
-              inherit (craneLib.crateNameFromCargoToml {inherit cargoToml;}) pname version;
+              inherit (craneLibMusl.crateNameFromCargoToml {inherit cargoToml;}) pname version;
               cargoExtraArgs = "-p ${pname}";
             }
             // {
-              cargoArtifacts = buildDeps cargoToml baseCargoArtifacts;
+              cargoArtifacts = buildDeps craneLibMusl commonsArgsMusl cargoToml baseCargoArtifacts;
               RUSTFLAGS = "-C target-feature=+crt-static";
-              OPENSSL_STATIC = "1";
-              # doCheck = false;
+              doCheck = false;
             }
             // args);
 
+        # Standard (glibc) builds
         mithril-stm = buildPackage ./mithril-stm/Cargo.toml null {};
         mithril-common = buildPackage ./mithril-common/Cargo.toml mithril-stm.cargoArtifacts { cargoExtraArgs = "-p mithril-common"; };
         mithril = buildPackage null mithril-common.cargoArtifacts {
           doCheck = false;
         };
+
+        # Static (musl) base dependencies
+        mithril-stm-musl = if pkgsMusl != null then buildPackageMusl ./mithril-stm/Cargo.toml null {} else null;
+        mithril-common-musl = if pkgsMusl != null then buildPackageMusl ./mithril-common/Cargo.toml mithril-stm-musl.cargoArtifacts { cargoExtraArgs = "-p mithril-common"; } else null;
       in {
         packages = {
           default = mithril;
@@ -154,24 +201,25 @@
           mithril-client-cli = buildPackage ./mithril-client-cli/Cargo.toml mithril.cargoArtifacts {
             pname = "mithril-client";
           };
-          mithril-aggregator = buildPackage ./mithril-aggregator/Cargo.toml mithril.cargoArtifacts {cargoTestExtraArgs = "--no-default-features"; };
-          mithril-signer = buildPackage ./mithril-signer/Cargo.toml mithril.cargoArtifacts {cargoTestExtraArgs = "--no-default-features"; };
+          mithril-aggregator = buildPackage ./mithril-aggregator/Cargo.toml mithril.cargoArtifacts { cargoExtraArgs = "-p mithril-aggregator --features rustls-tls,jemallocator"; cargoTestExtraArgs = "--no-default-features"; };
+          mithril-signer = buildPackage ./mithril-signer/Cargo.toml mithril.cargoArtifacts { cargoExtraArgs = "-p mithril-signer --features rustls-tls,jemallocator"; cargoTestExtraArgs = "--no-default-features"; };
           mithril-end-to-end = buildPackage ./mithril-test-lab/mithril-end-to-end/Cargo.toml null {};
-        
+
         } // lib.optionalAttrs (pkgsMusl != null) {
 
           mithril-aggregator-static =
-            buildPackageMusl ./mithril-aggregator/Cargo.toml mithril.cargoArtifacts {
-              cargoExtraArgs = "-p mithril-aggregator --features bundle_tls";
-              cargoTestExtraArgs = "--no-default-features";
+            buildPackageMusl ./mithril-aggregator/Cargo.toml mithril-common-musl.cargoArtifacts {
+              cargoExtraArgs = "-p mithril-aggregator --features rustls-tls,jemallocator";
             };
           mithril-signer-static =
-            buildPackageMusl ./mithril-signer/Cargo.toml mithril.cargoArtifacts {
-              cargoExtraArgs = "-p mithril-signer --features bundle_tls";
-              cargoTestExtraArgs = "--no-default-features";
+            buildPackageMusl ./mithril-signer/Cargo.toml mithril-common-musl.cargoArtifacts {
+              cargoExtraArgs = "-p mithril-signer --features rustls-tls,jemallocator";
             };
           mithril-client-cli-static =
-            buildPackageMusl ./mithril-client-cli/Cargo.toml mithril.cargoArtifacts {};
+            buildPackageMusl ./mithril-client-cli/Cargo.toml mithril-common-musl.cargoArtifacts {
+              pname = "mithril-client";
+              cargoExtraArgs = "-p mithril-client-cli --features rustls-tls";
+            };
         };
 
         devShells.default = pkgs.mkShell {
