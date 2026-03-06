570b40821f3ff236930b18a824638c6686cc514f
DJO <790521+Alenar@users.noreply.github.com>
2026-03-06 11:16:49 +0100

    test(common): add `build_blocks` and `build_blocks_for_block_ranges` to `CardanoTransactionsBuilder`

--- mithril-common/src/test/builder/cardano_transactions_builder.rs
+++ mithril-common/src/test/builder/cardano_transactions_builder.rs
@@ -63,6 +63,51 @@ use crate::entities::{
 ///         txs
 ///     );
 /// ```
+///
+/// # Example 'build_blocks'
+///
+/// ```
+///     use mithril_common::entities::{BlockNumber, CardanoBlockWithTransactions, SlotNumber};
+///     use mithril_common::test::builder::CardanoTransactionsBuilder;
+///
+///     let blocks = CardanoTransactionsBuilder::new()
+///         .max_transactions_per_block(3)
+///         .blocks_per_block_range(2)
+///         .build_blocks(3);
+///
+///     assert_eq!(3, blocks.len());
+///     assert_eq!(
+///         vec![
+///             CardanoBlockWithTransactions::new("block-hash-0", BlockNumber(0), SlotNumber(100), vec!["tx-hash-0-100","tx-hash-0-101","tx-hash-0-102"]),
+///             CardanoBlockWithTransactions::new("block-hash-1", BlockNumber(1), SlotNumber(101), vec!["tx-hash-1-103","tx-hash-1-104","tx-hash-1-105"]),
+///             CardanoBlockWithTransactions::new("block-hash-15", BlockNumber(15), SlotNumber(115), vec!["tx-hash-15-106","tx-hash-15-107","tx-hash-15-108"]),
+///         ],
+///         blocks
+///     );
+/// ```
+///
+/// # Example 'build_blocks_for_block_ranges'
+///
+/// ```
+///     use mithril_common::entities::{BlockNumber, CardanoBlockWithTransactions, SlotNumber};
+///     use mithril_common::test::builder::CardanoTransactionsBuilder;
+///
+///     let blocks = CardanoTransactionsBuilder::new()
+///         .max_transactions_per_block(3)
+///         .blocks_per_block_range(2)
+///         .build_blocks_for_block_ranges(2);
+///
+///     assert_eq!(2 * 2, blocks.len());
+///     assert_eq!(
+///         vec![
+///             CardanoBlockWithTransactions::new("block-hash-0", BlockNumber(0), SlotNumber(100), vec!["tx-hash-0-100","tx-hash-0-101","tx-hash-0-102"]),
+///             CardanoBlockWithTransactions::new("block-hash-1", BlockNumber(1), SlotNumber(101), vec!["tx-hash-1-103","tx-hash-1-104","tx-hash-1-105"]),
+///             CardanoBlockWithTransactions::new("block-hash-15", BlockNumber(15), SlotNumber(115), vec!["tx-hash-15-106","tx-hash-15-107","tx-hash-15-108"]),
+///             CardanoBlockWithTransactions::new("block-hash-16", BlockNumber(16), SlotNumber(116), vec!["tx-hash-16-109","tx-hash-16-110","tx-hash-16-111"]),
+///         ],
+///         blocks
+///     );
+/// ```
 pub struct CardanoTransactionsBuilder {
     max_transactions_per_block: usize,
     max_blocks_per_block_range: usize,
@@ -139,6 +184,30 @@ impl CardanoTransactionsBuilder {
             .collect()
     }
 
+    /// Build the number of blocks requested.
+    pub fn build_blocks(self, block_count: usize) -> Vec<CardanoBlockWithTransactions> {
+        let transactions_count = block_count * self.max_transactions_per_block;
+        self.build_blocks_with_transactions(
+            block_count,
+            transactions_count,
+            FillUntil::BlockCountReached,
+        )
+    }
+
+    /// Build a list of blocks to get the number of block range requested.
+    pub fn build_blocks_for_block_ranges(
+        self,
+        block_ranges_count: usize,
+    ) -> Vec<CardanoBlockWithTransactions> {
+        let block_count = block_ranges_count * self.max_blocks_per_block_range;
+        let transactions_count = block_count * self.max_transactions_per_block;
+        self.build_blocks_with_transactions(
+            block_count,
+            transactions_count,
+            FillUntil::BlockCountReached,
+        )
+    }
+
     fn build_blocks_with_transactions(
         self,
         block_count: usize,
@@ -193,6 +262,19 @@ mod test {
         R: Eq + std::hash::Hash,
     {
         list.iter().cloned().map(extract_value).collect::<HashSet<R>>().len()
+    }
+
+    fn count_distinct_nested_values<T, R, U>(list: &[T], extract_values: &dyn Fn(T) -> U) -> usize
+    where
+        T: Clone,
+        R: Eq + std::hash::Hash,
+        U: IntoIterator<Item = R>,
+    {
+        list.iter()
+            .cloned()
+            .flat_map(extract_values)
+            .collect::<HashSet<R>>()
+            .len()
     }
 
     fn group_by<'a, T, R>(list: &'a [T], extract_value: &dyn Fn(T) -> R) -> HashMap<R, Vec<&'a T>>
@@ -361,6 +443,171 @@ mod test {
             );
             assert_eq!(3 * 2, count_distinct_values(&txs, &|t| t.block_number));
             assert_eq!(3 * 2, count_distinct_values(&txs, &|t| t.block_hash));
+        }
+    }
+
+    mod build_blocks {
+        use super::*;
+
+        #[test]
+        fn return_given_number_of_blocks_with_distinct_values() {
+            let blocks = CardanoTransactionsBuilder::new()
+                .max_transactions_per_block(2)
+                .build_blocks(3);
+
+            assert_eq!(blocks.len(), 3);
+
+            assert_eq!(3, count_distinct_values(&blocks, &|b| b.block_hash));
+            assert_eq!(3, count_distinct_values(&blocks, &|b| b.block_number));
+            assert_eq!(3, count_distinct_values(&blocks, &|b| b.slot_number));
+            assert_eq!(3, count_distinct_values(&blocks, &|b| b.block_hash));
+            assert_eq!(
+                6,
+                count_distinct_nested_values(&blocks, &|b| b.transactions_hashes)
+            );
+        }
+
+        #[test]
+        fn return_empty_blocks_when_ask_0_transactions_per_block() {
+            let blocks = CardanoTransactionsBuilder::new()
+                .max_transactions_per_block(0)
+                .build_blocks(3);
+
+            assert_eq!(blocks.len(), 3);
+            assert!(blocks.iter().map(|b| b.transactions_hashes.len()).all(|n| n == 0));
+        }
+
+        #[test]
+        fn build_one_block_with_multiples_transactions() {
+            let blocks = CardanoTransactionsBuilder::new()
+                .max_transactions_per_block(5)
+                .build_blocks(1);
+
+            assert_eq!(
+                blocks,
+                vec![CardanoBlockWithTransactions::new(
+                    "block-hash-0",
+                    BlockNumber(0),
+                    SlotNumber(100),
+                    vec![
+                        "tx-hash-0-100",
+                        "tx-hash-0-101",
+                        "tx-hash-0-102",
+                        "tx-hash-0-103",
+                        "tx-hash-0-104"
+                    ],
+                ),]
+            );
+        }
+
+        #[test]
+        fn build_a_block_on_three_different_block_ranges() {
+            let blocks = CardanoTransactionsBuilder::new()
+                .max_transactions_per_block(1)
+                .blocks_per_block_range(1)
+                .build_blocks(3);
+
+            assert_eq!(
+                blocks,
+                vec![
+                    CardanoBlockWithTransactions::new(
+                        "block-hash-0",
+                        BlockNumber(0),
+                        SlotNumber(100),
+                        vec!["tx-hash-0-100"]
+                    ),
+                    CardanoBlockWithTransactions::new(
+                        "block-hash-15",
+                        BlockNumber(15),
+                        SlotNumber(115),
+                        vec!["tx-hash-15-101"]
+                    ),
+                    CardanoBlockWithTransactions::new(
+                        "block-hash-30",
+                        BlockNumber(30),
+                        SlotNumber(130),
+                        vec!["tx-hash-30-102"]
+                    ),
+                ]
+            );
+        }
+
+        #[test]
+        fn return_no_more_transactions_in_a_same_block_than_number_per_block_requested() {
+            let blocks = CardanoTransactionsBuilder::new()
+                .max_transactions_per_block(3)
+                .build_blocks(4);
+
+            assert_eq!(blocks.len(), 4);
+
+            assert_eq!(
+                12,
+                count_distinct_nested_values(&blocks, &|b| b.transactions_hashes)
+            );
+            assert_eq!(4, count_distinct_values(&blocks, &|t| t.block_number));
+            assert_eq!(4, count_distinct_values(&blocks, &|t| t.block_hash));
+        }
+    }
+
+    mod build_blocks_for_block_ranges {
+        use super::*;
+
+        #[test]
+        fn generate_blocks_for_one_block_range_return_one_block_by_default() {
+            let blocks = CardanoTransactionsBuilder::new().build_blocks_for_block_ranges(1);
+            assert_eq!(blocks.len(), 1);
+        }
+
+        #[test]
+        fn build_blocks_for_block_ranges_return_the_number_of_block_ranges_requested() {
+            let blocks = CardanoTransactionsBuilder::new().build_blocks_for_block_ranges(3);
+
+            assert_eq!(blocks.len(), 3);
+            assert_eq!(
+                3,
+                count_distinct_values(&blocks, &|b| BlockRange::start(b.block_number))
+            );
+        }
+
+        #[test]
+        fn build_blocks_for_block_ranges_return_blocks_with_many_transactions_when_requested() {
+            let blocks = CardanoTransactionsBuilder::new()
+                .max_transactions_per_block(5)
+                .build_blocks_for_block_ranges(3);
+
+            assert_eq!(blocks.len(), 3);
+            assert_eq!(
+                3 * 5,
+                count_distinct_nested_values(&blocks, &|b| b.transactions_hashes)
+            );
+
+            assert_eq!(
+                3,
+                count_distinct_values(&blocks, &|b| BlockRange::start(b.block_number))
+            );
+            assert_eq!(3, count_distinct_values(&blocks, &|b| b.block_number));
+            assert_eq!(3, count_distinct_values(&blocks, &|b| b.block_hash));
+        }
+
+        #[test]
+        fn build_blocks_for_block_ranges_with_many_blocks_per_block_ranges() {
+            let blocks = CardanoTransactionsBuilder::new()
+                .max_transactions_per_block(5)
+                .blocks_per_block_range(2)
+                .build_blocks_for_block_ranges(3);
+
+            assert_eq!(blocks.len(), 3 * 2);
+            assert_eq!(
+                3 * 2 * 5,
+                count_distinct_nested_values(&blocks, &|b| b.transactions_hashes)
+            );
+
+            assert_eq!(
+                3,
+                count_distinct_values(&blocks, &|t| BlockRange::start(t.block_number))
+            );
+            assert_eq!(3 * 2, count_distinct_values(&blocks, &|b| b.block_number));
+            assert_eq!(3 * 2, count_distinct_values(&blocks, &|b| b.block_hash));
         }
     }
 }
