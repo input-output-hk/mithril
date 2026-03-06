d77330a74e62596c2b3bf6c05e9c3be266ae96cc
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2026-03-06 17:24:19 +0100

    feat(stm): add SNARK aggregate verification key to protocol layer

--- mithril-stm/src/proof_system/halo2_snark/mod.rs
+++ mithril-stm/src/proof_system/halo2_snark/mod.rs
@@ -1,10 +1,12 @@
 mod aggregate_key;
+mod clerk;
 mod eligibility;
 mod message;
 mod signer;
 mod single_signature;
 
-pub(crate) use aggregate_key::AggregateVerificationKeyForSnark;
+pub use aggregate_key::AggregateVerificationKeyForSnark;
+pub(crate) use clerk::SnarkClerk;
 pub(crate) use eligibility::compute_winning_lottery_indices;
 pub(crate) use message::build_snark_message;
 pub(crate) use signer::SnarkProofSigner;
