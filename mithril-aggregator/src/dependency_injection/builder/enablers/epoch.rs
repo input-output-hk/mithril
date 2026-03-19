cc252ddf588676f84d2c96e3daf5c870f5744196
DJO <790521+Alenar@users.noreply.github.com>
2026-03-19 16:34:36 +0100

    feat(aggregator): automatic disabling inconsistent signed entities

--- mithril-aggregator/src/dependency_injection/builder/enablers/epoch.rs
+++ mithril-aggregator/src/dependency_injection/builder/enablers/epoch.rs
@@ -58,6 +58,7 @@ impl DependenciesBuilder {
                     self.configuration
                         .compute_allowed_signed_entity_types_discriminants()?,
                     self.get_epoch_settings_store().await?,
+                    self.root_logger(),
                 ))
             };
 
