9768b41fff059959beb30c55d15ba489e4709c82
Clément Turmel <turmel@teragone-solutions.com>
2026-03-24 17:50:41 +0100

    feature(client-cli): handle argument --backend v2 on cardano transaction certify

---
+++ mithril-client-cli/src/commands/cardano_transaction/certify/mod.rs
@@ -0,0 +1,100 @@
+mod v1;
+mod v2;
+
+use v1::execute as certify_v1;
+use v2::execute as certify_v2;
+
+use clap::Parser;
+use mithril_client::common::SignedEntityTypeDiscriminants;
+use std::collections::HashMap;
+
+use mithril_client::MithrilResult;
+
+use crate::commands::build_client;
+use crate::commands::cardano_transaction::CardanoTransactionCommandsBackend;
+use crate::utils::{ProgressOutputType, ProgressPrinter};
+use crate::{
+    CommandContext,
+    configuration::{ConfigError, ConfigSource},
+};
+
+/// Clap command to certify that given Cardano transaction hashes are included in a Mithril-certified Cardano transaction set.
+#[derive(Parser, Debug, Clone)]
+pub struct CardanoTransactionsCertifyCommand {
+    ///Backend to use, either: `v1` (default) or `v2`
+    #[arg(short, long, value_enum, default_value_t)]
+    backend: CardanoTransactionCommandsBackend,
+
+    /// Genesis verification key to check the certificate chain.
+    #[clap(long, env = "GENESIS_VERIFICATION_KEY")]
+    genesis_verification_key: Option<String>,
+
+    /// Hashes of the transactions to certify.
+    #[clap(value_delimiter = ',', required = true)]
+    transactions_hashes: Vec<String>,
+}
+
+impl CardanoTransactionsCertifyCommand {
+    /// Cardano transaction certify command
+    pub async fn execute(&self, mut context: CommandContext) -> MithrilResult<()> {
+        context.config_parameters_mut().add_source(self)?;
+        let logger = context.logger();
+
+        let progress_output_type = if context.is_json_output_enabled() {
+            ProgressOutputType::JsonReporter
+        } else {
+            ProgressOutputType::Tty
+        };
+        let progress_printer = ProgressPrinter::new(progress_output_type, 4);
+
+        match self.backend {
+            CardanoTransactionCommandsBackend::V1 => {
+                let client =
+                    build_client(&context, SignedEntityTypeDiscriminants::CardanoTransactions)?;
+                certify_v1(
+                    &self.transactions_hashes,
+                    client,
+                    progress_printer,
+                    context.is_json_output_enabled(),
+                    logger.clone(),
+                )
+                .await?;
+            }
+            CardanoTransactionCommandsBackend::V2 => {
+                context.require_unstable(
+                    "cardano-transaction certify <tx1>,<tx2>,...,<txn> --backend v2",
+                    None,
+                )?;
+                let client = build_client(
+                    &context,
+                    SignedEntityTypeDiscriminants::CardanoBlocksTransactions,
+                )?;
+                certify_v2(
+                    &self.transactions_hashes,
+                    client,
+                    progress_printer,
+                    context.is_json_output_enabled(),
+                    logger.clone(),
+                )
+                .await?;
+            }
+        }
+
+        Ok(())
+    }
+}
+
+impl ConfigSource for CardanoTransactionsCertifyCommand {
+    fn collect(&self) -> Result<HashMap<String, String>, ConfigError> {
+        let mut map = HashMap::new();
+
+        if let Some(genesis_verification_key) = self.genesis_verification_key.clone() {
+            map.insert(
+                "genesis_verification_key".to_string(),
+                genesis_verification_key,
+            );
+        }
+
+        Ok(map)
+    }
+}
---
+++ mithril-client-cli/src/commands/cardano_transaction/certify/v1.rs
@@ -0,0 +1,135 @@
+use anyhow::{Context, anyhow};
+use cli_table::{Cell, Table, print_stdout};
+use mithril_client::{
+    CardanoTransactionsProofs, MessageBuilder, MithrilCertificate, MithrilResult,
+    VerifiedCardanoTransactions, VerifyCardanoTransactionsProofsError, common::TransactionHash,
+};
+use slog::debug;
+
+use crate::utils::ProgressPrinter;
+
+pub async fn execute(
+    transactions_hashes: &Vec<String>,
+    client: mithril_client::Client,
+    progress_printer: ProgressPrinter,
+    is_json_output_enabled: bool,
+    logger: slog::Logger,
+) -> MithrilResult<()> {
+    progress_printer.report_step(1, "Fetching a proof for the given transactions…")?;
+    let cardano_transaction_proof = client
+        .cardano_transaction()
+        .get_proofs(transactions_hashes)
+        .await
+        .with_context(|| {
+            format!(
+                "Can not get proof from aggregator, transactions hashes: '{:?}'",
+                transactions_hashes
+            )
+        })?;
+    debug!(logger, "Got Proof from aggregator"; "proof" => ?cardano_transaction_proof);
+
+    let verified_transactions =
+        verify_proof_validity(2, &progress_printer, &cardano_transaction_proof)?;
+
+    progress_printer.report_step(
+        3,
+        "Fetching the associated certificate and verifying the certificate chain…",
+    )?;
+    let certificate = client
+        .certificate()
+        .verify_chain(&cardano_transaction_proof.certificate_hash)
+        .await
+        .with_context(|| {
+            format!(
+                "Can not verify the certificate chain from certificate_hash: '{}'",
+                verified_transactions.certificate_hash()
+            )
+        })?;
+
+    verify_proof_match_certificate(4, &progress_printer, &certificate, &verified_transactions)?;
+
+    log_certify_information(
+        &verified_transactions,
+        &cardano_transaction_proof.non_certified_transactions,
+        is_json_output_enabled,
+    )
+}
+
+fn verify_proof_validity(
+    step_number: u16,
+    progress_printer: &ProgressPrinter,
+    cardano_transaction_proof: &CardanoTransactionsProofs,
+) -> MithrilResult<VerifiedCardanoTransactions> {
+    progress_printer.report_step(step_number, "Verifying the proof…")?;
+    match cardano_transaction_proof.verify() {
+        Ok(verified_transactions) => Ok(verified_transactions),
+        Err(VerifyCardanoTransactionsProofsError::NoCertifiedTransaction) => Err(anyhow!(
+            "Mithril could not certify any of the given transactions.
+
+Mithril may not have signed those transactions yet, please try again later."
+        )),
+        err => err.with_context(|| "Proof verification failed"),
+    }
+}
+
+fn verify_proof_match_certificate(
+    step_number: u16,
+    progress_printer: &ProgressPrinter,
+    certificate: &MithrilCertificate,
+    verified_transactions: &VerifiedCardanoTransactions,
+) -> MithrilResult<()> {
+    progress_printer.report_step(
+        step_number,
+        "Verify that the proof is signed in the associated certificate",
+    )?;
+    let message = MessageBuilder::new()
+        .compute_cardano_transactions_proofs_message(certificate, verified_transactions);
+    if !certificate.match_message(&message) {
+        return Err(anyhow!(
+            "Proof and certificate don't match (certificate hash = '{}').",
+            certificate.hash
+        ));
+    }
+
+    Ok(())
+}
+
+fn log_certify_information(
+    verified_transactions: &VerifiedCardanoTransactions,
+    non_certified_transactions: &[TransactionHash],
+    json_output: bool,
+) -> MithrilResult<()> {
+    if json_output {
+        println!(
+            r#"{{"certified_transactions": {}, "non_certified_transactions": {}}}"#,
+            serde_json::to_string(verified_transactions.certified_transactions())?,
+            serde_json::to_string(non_certified_transactions)?,
+        );
+    } else {
+        println!(
+            r###"Cardano transactions proof has been successfully signed in the associated Mithril certificate."###,
+        );
+
+        if !non_certified_transactions.is_empty() {
+            println!(
+                r###"
+No proof could be computed for some Cardano transactions. Mithril may not have signed those transactions yet, please try again later."###,
+            );
+        }
+
+        let result_table =
+            verified_transactions
+                .certified_transactions()
+                .iter()
+                .map(|tx| vec![tx.cell(), "✅".cell().justify(cli_table::format::Justify::Center)])
+                .chain(non_certified_transactions.iter().map(|tx| {
+                    vec![tx.cell(), "❌".cell().justify(cli_table::format::Justify::Center)]
+                }))
+                .table()
+                .title(vec!["Transaction Hash", "Certified"]);
+
+        print_stdout(result_table)?
+    }
+
+    Ok(())
+}
