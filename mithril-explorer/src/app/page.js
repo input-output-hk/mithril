6aa509272c749d51f9314bbd32a30f289c9fbf3a
DJO <790521+Alenar@users.noreply.github.com>
2026-04-01 17:42:49 +0200

    refactor(explorer): change default tab to `Certificates`
    Previous default was `Cardano Db`, but we will remove it this year and
    the `Certificates` tab is the least likely to be removed.

--- mithril-explorer/src/app/page.js
+++ mithril-explorer/src/app/page.js
@@ -33,7 +33,8 @@ import { updatePoolsForAggregator } from "@/store/poolsSlice";
 Chart.register(ArcElement, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 setChartJsDefaults(Chart);
 
-const defaultTab = signedEntityType.CardanoImmutableFilesFull;
+const certificateTab = "certificates";
+const defaultTab = certificateTab;
 
 export default function Explorer() {
   const searchParams = useSearchParams();
@@ -135,7 +136,7 @@ export default function Explorer() {
           eventKey={signedEntityType.MithrilStakeDistribution}>
           <MithrilStakeDistributionsList />
         </Tab>
-        <Tab title="Certificates" eventKey="certificates">
+        <Tab title="Certificates" eventKey={certificateTab}>
           <CertificatesList />
         </Tab>
       </Tabs>
