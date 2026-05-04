d48efab2be54c0837b17bfaed75aaf8175cb84c4
Dam <damien.robissout@iohk.io>
2026-05-04 15:47:29 +0200

    fix(stm): Fixed and improved verification tests

--- mithril-stm/src/proof_system/halo2_snark/helpers.rs
+++ mithril-stm/src/proof_system/halo2_snark/helpers.rs
@@ -48,8 +48,15 @@ fn check_and_verify_stored_srs(srs_path: &str, srs_hash: &str, srs_url: &str) ->
     let srs_file_path = Path::new(srs_path);
 
     if !srs_file_exists(srs_file_path) {
-        download_srs_file(srs_file_path, srs_url)?;
-        let result_srs_check = verify_srs_file_hash(srs_file_path, srs_hash)?;
+        std::fs::create_dir_all(srs_file_path.parent().ok_or(anyhow!(
+            "Parent directory for the given file does not exists!"
+        ))?)
+        .with_context(|| "Subdirectory creation should have succeeded!")?;
+        download_srs_file(srs_file_path, srs_url)
+            .with_context(|| "Download of the SRS file should have succeeded!")?;
+        let result_srs_check = verify_srs_file_hash(srs_file_path, srs_hash).with_context(
+            || "Verification of the hash of the downloaded file should have passed!",
+        )?;
         match result_srs_check {
             true => {}
             false => {
@@ -66,6 +73,8 @@ fn check_and_verify_stored_srs(srs_path: &str, srs_hash: &str, srs_url: &str) ->
 
 #[cfg(test)]
 mod tests {
+    use tempfile::NamedTempFile;
+
     use super::*;
 
     const SRS_HASH_K3: &str = "4be827a6472193df80d8f08b4b25a85baef436fdd1965d89b6af89f4ec4e99e2";
@@ -73,42 +82,65 @@ mod tests {
     const SRS_URL_K3: &str = "https://srs.midnight.network/midnight-srs-2p3";
 
     #[test]
-    fn test_quick_srs_hash() {
-        let _ = check_and_verify_stored_srs(SRS_PATH_K3, SRS_HASH_K3, SRS_URL_K3);
-    }
-
-    #[test]
-    fn test_dl_srs_file() {
-        let _ = download_srs_file(Path::new(SRS_PATH_K3), SRS_URL_K3).unwrap();
-    }
-
-    #[test]
     fn missing_srs_file_triggers_download() {
-        let srs_path = Path::new("/tmp/trusted_setup/midnight-srs-2p3");
+        let dummy_path = "/tmp/trusted_setup/srs_for_test";
+        let srs_path = Path::new(dummy_path);
 
         if srs_path.exists() {
             std::fs::remove_file(srs_path).unwrap();
         }
-        check_and_verify_stored_srs(SRS_PATH_K3, SRS_HASH_K3, SRS_URL_K3).unwrap();
+        check_and_verify_stored_srs(dummy_path, SRS_HASH_K3, SRS_URL_K3).unwrap();
 
-        assert!(srs_path.exists())
+        assert!(srs_path.exists());
+        std::fs::remove_file(srs_path).unwrap();
     }
 
     #[test]
     fn valid_file_hash_succeeds() {
-        assert!(check_and_verify_stored_srs(SRS_PATH_K3, SRS_HASH_K3, SRS_URL_K3).is_ok());
+        let srs_path = Path::new(SRS_PATH_K3);
+
+        let result = verify_srs_file_hash(srs_path, SRS_HASH_K3).unwrap();
+
+        assert_eq!(result, true);
     }
 
     #[test]
     fn invalid_file_hash_fails() {
-        let mut invalid_srs_hash = String::from(SRS_HASH_K3);
-        invalid_srs_hash.pop();
-        // '5' is never the last digit of the midnight's hashes
-        invalid_srs_hash.push('5');
+        let real_srs_path = Path::new(SRS_PATH_K3);
+        // Creates a dummy file to store the tampered SRS
+        let dummy_srs_path = NamedTempFile::new_in("/tmp").unwrap();
+        // Creates and downloads the srs if it does not exists
+        check_and_verify_stored_srs(SRS_PATH_K3, SRS_HASH_K3, SRS_URL_K3).unwrap();
+        // Opens the srs file and loads its bytes
+        let mut file = File::open(real_srs_path).unwrap();
+        let mut srs_buffer = vec![];
+        file.read_to_end(&mut srs_buffer).unwrap();
+        // Tampers the first byte
+        srs_buffer[0] += 1;
+        // Saves the tampered SRS in the temporary file
+        std::fs::write(&dummy_srs_path, &srs_buffer).unwrap();
+
+        let result = verify_srs_file_hash(dummy_srs_path.path(), SRS_HASH_K3).unwrap();
+
+        assert_eq!(result, false);
+    }
+
+    #[test]
+    fn any_file_on_disk_succeeds() {
+        let dummy_srs_path = NamedTempFile::new_in("/tmp").unwrap();
+
+        if dummy_srs_path.path().exists() {
+            let bytes = vec![0, 1, 2, 3, 4];
+            std::fs::write(&dummy_srs_path, &bytes).unwrap();
+        }
 
         assert!(
-            check_and_verify_stored_srs(SRS_PATH_K3, invalid_srs_hash.as_str(), SRS_URL_K3)
-                .is_err()
+            check_and_verify_stored_srs(
+                dummy_srs_path.path().to_str().unwrap(),
+                SRS_HASH_K3,
+                SRS_URL_K3
+            )
+            .is_ok()
         );
     }
 }
