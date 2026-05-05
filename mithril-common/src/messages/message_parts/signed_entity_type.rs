c3c24c33bf1f1c742bd84c9ee8b6822f1f683d84
DJO <790521+Alenar@users.noreply.github.com>
2026-05-05 18:45:31 +0200

    feat(common): add infallible conversions between signed entities and messages

--- mithril-common/src/messages/message_parts/signed_entity_type.rs
+++ mithril-common/src/messages/message_parts/signed_entity_type.rs
@@ -1,7 +1,10 @@
 use serde::{Deserialize, Serialize};
 use strum::{AsRefStr, Display, EnumDiscriminants, EnumIter};
 
-use crate::entities::{BlockNumber, BlockNumberOffset, CardanoDbBeacon, Epoch};
+use crate::entities::{
+    BlockNumber, BlockNumberOffset, CardanoDbBeacon, Epoch, SignedEntityType,
+    SignedEntityTypeDiscriminants,
+};
 
 /// The signed entity type that represents a type of data signed by the Mithril protocol.
 ///
@@ -38,33 +41,223 @@ pub enum SignedEntityTypeMessage {
     Unknown,
 }
 
-// Manual implementation instead of using strum::EnumString because it does not allow a "catch all"
-// variant if that variant has no associated value (which is the case for derived discriminants).
-impl From<&str> for SignedEntityTypeDiscriminantsMessage {
-    fn from(value: &str) -> Self {
-        match value {
-            "MithrilStakeDistribution" => {
-                SignedEntityTypeDiscriminantsMessage::MithrilStakeDistribution
+mod infallible_conversions {
+    use super::*;
+
+    // Manual implementation instead of using strum::EnumString because it does not allow a "catch all"
+    // variant if that variant has no associated value (which is the case for derived discriminants).
+    impl From<&str> for SignedEntityTypeDiscriminantsMessage {
+        fn from(value: &str) -> Self {
+            match value {
+                "MithrilStakeDistribution" => {
+                    SignedEntityTypeDiscriminantsMessage::MithrilStakeDistribution
+                }
+                "CardanoStakeDistribution" => {
+                    SignedEntityTypeDiscriminantsMessage::CardanoStakeDistribution
+                }
+                "CardanoImmutableFilesFull" => {
+                    SignedEntityTypeDiscriminantsMessage::CardanoImmutableFilesFull
+                }
+                "CardanoDatabase" => SignedEntityTypeDiscriminantsMessage::CardanoDatabase,
+                "CardanoTransactions" => SignedEntityTypeDiscriminantsMessage::CardanoTransactions,
+                "CardanoBlocksTransactions" => {
+                    SignedEntityTypeDiscriminantsMessage::CardanoBlocksTransactions
+                }
+                _ => SignedEntityTypeDiscriminantsMessage::Unknown,
             }
-            "CardanoStakeDistribution" => {
-                SignedEntityTypeDiscriminantsMessage::CardanoStakeDistribution
+        }
+    }
+
+    impl From<SignedEntityType> for SignedEntityTypeMessage {
+        fn from(value: SignedEntityType) -> Self {
+            match value {
+                SignedEntityType::MithrilStakeDistribution(epoch) => {
+                    SignedEntityTypeMessage::MithrilStakeDistribution(epoch)
+                }
+                SignedEntityType::CardanoStakeDistribution(epoch) => {
+                    SignedEntityTypeMessage::CardanoStakeDistribution(epoch)
+                }
+                SignedEntityType::CardanoImmutableFilesFull(beacon) => {
+                    SignedEntityTypeMessage::CardanoImmutableFilesFull(beacon)
+                }
+                SignedEntityType::CardanoDatabase(beacon) => {
+                    SignedEntityTypeMessage::CardanoDatabase(beacon)
+                }
+                SignedEntityType::CardanoTransactions(epoch, block) => {
+                    SignedEntityTypeMessage::CardanoTransactions(epoch, block)
+                }
+                SignedEntityType::CardanoBlocksTransactions(epoch, block, offset) => {
+                    SignedEntityTypeMessage::CardanoBlocksTransactions(epoch, block, offset)
+                }
             }
-            "CardanoImmutableFilesFull" => {
-                SignedEntityTypeDiscriminantsMessage::CardanoImmutableFilesFull
+        }
+    }
+
+    impl From<SignedEntityType> for SignedEntityTypeDiscriminantsMessage {
+        fn from(value: SignedEntityType) -> Self {
+            match value {
+                SignedEntityType::MithrilStakeDistribution(..) => {
+                    SignedEntityTypeDiscriminantsMessage::MithrilStakeDistribution
+                }
+                SignedEntityType::CardanoStakeDistribution(..) => {
+                    SignedEntityTypeDiscriminantsMessage::CardanoStakeDistribution
+                }
+                SignedEntityType::CardanoImmutableFilesFull(..) => {
+                    SignedEntityTypeDiscriminantsMessage::CardanoImmutableFilesFull
+                }
+                SignedEntityType::CardanoDatabase(..) => {
+                    SignedEntityTypeDiscriminantsMessage::CardanoDatabase
+                }
+                SignedEntityType::CardanoTransactions(..) => {
+                    SignedEntityTypeDiscriminantsMessage::CardanoTransactions
+                }
+                SignedEntityType::CardanoBlocksTransactions(..) => {
+                    SignedEntityTypeDiscriminantsMessage::CardanoBlocksTransactions
+                }
             }
-            "CardanoDatabase" => SignedEntityTypeDiscriminantsMessage::CardanoDatabase,
-            "CardanoTransactions" => SignedEntityTypeDiscriminantsMessage::CardanoTransactions,
-            "CardanoBlocksTransactions" => {
-                SignedEntityTypeDiscriminantsMessage::CardanoBlocksTransactions
+        }
+    }
+
+    impl From<SignedEntityTypeDiscriminants> for SignedEntityTypeDiscriminantsMessage {
+        fn from(value: SignedEntityTypeDiscriminants) -> Self {
+            match value {
+                SignedEntityTypeDiscriminants::MithrilStakeDistribution => {
+                    SignedEntityTypeDiscriminantsMessage::MithrilStakeDistribution
+                }
+                SignedEntityTypeDiscriminants::CardanoStakeDistribution => {
+                    SignedEntityTypeDiscriminantsMessage::CardanoStakeDistribution
+                }
+                SignedEntityTypeDiscriminants::CardanoImmutableFilesFull => {
+                    SignedEntityTypeDiscriminantsMessage::CardanoImmutableFilesFull
+                }
+                SignedEntityTypeDiscriminants::CardanoDatabase => {
+                    SignedEntityTypeDiscriminantsMessage::CardanoDatabase
+                }
+                SignedEntityTypeDiscriminants::CardanoTransactions => {
+                    SignedEntityTypeDiscriminantsMessage::CardanoTransactions
+                }
+                SignedEntityTypeDiscriminants::CardanoBlocksTransactions => {
+                    SignedEntityTypeDiscriminantsMessage::CardanoBlocksTransactions
+                }
             }
-            _ => SignedEntityTypeDiscriminantsMessage::Unknown,
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::assert_equivalent;
+    use crate::test::entities_extensions::SignedEntityTypeDiscriminantsTestExtension;
+
     use super::*;
+
+    fn infallible_conversion_cases() -> Vec<(
+        SignedEntityType,
+        SignedEntityTypeDiscriminants,
+        SignedEntityTypeMessage,
+        SignedEntityTypeDiscriminantsMessage,
+    )> {
+        vec![
+            (
+                SignedEntityType::MithrilStakeDistribution(Epoch(6)),
+                SignedEntityTypeDiscriminants::MithrilStakeDistribution,
+                SignedEntityTypeMessage::MithrilStakeDistribution(Epoch(6)),
+                SignedEntityTypeDiscriminantsMessage::MithrilStakeDistribution,
+            ),
+            (
+                SignedEntityType::CardanoStakeDistribution(Epoch(7)),
+                SignedEntityTypeDiscriminants::CardanoStakeDistribution,
+                SignedEntityTypeMessage::CardanoStakeDistribution(Epoch(7)),
+                SignedEntityTypeDiscriminantsMessage::CardanoStakeDistribution,
+            ),
+            (
+                SignedEntityType::CardanoImmutableFilesFull(CardanoDbBeacon::new(8, 109)),
+                SignedEntityTypeDiscriminants::CardanoImmutableFilesFull,
+                SignedEntityTypeMessage::CardanoImmutableFilesFull(CardanoDbBeacon::new(8, 109)),
+                SignedEntityTypeDiscriminantsMessage::CardanoImmutableFilesFull,
+            ),
+            (
+                SignedEntityType::CardanoDatabase(CardanoDbBeacon::new(9, 110)),
+                SignedEntityTypeDiscriminants::CardanoDatabase,
+                SignedEntityTypeMessage::CardanoDatabase(CardanoDbBeacon::new(9, 110)),
+                SignedEntityTypeDiscriminantsMessage::CardanoDatabase,
+            ),
+            (
+                SignedEntityType::CardanoTransactions(Epoch(10), BlockNumber(11)),
+                SignedEntityTypeDiscriminants::CardanoTransactions,
+                SignedEntityTypeMessage::CardanoTransactions(Epoch(10), BlockNumber(11)),
+                SignedEntityTypeDiscriminantsMessage::CardanoTransactions,
+            ),
+            (
+                SignedEntityType::CardanoBlocksTransactions(
+                    Epoch(12),
+                    BlockNumber(13),
+                    BlockNumberOffset(14),
+                ),
+                SignedEntityTypeDiscriminants::CardanoBlocksTransactions,
+                SignedEntityTypeMessage::CardanoBlocksTransactions(
+                    Epoch(12),
+                    BlockNumber(13),
+                    BlockNumberOffset(14),
+                ),
+                SignedEntityTypeDiscriminantsMessage::CardanoBlocksTransactions,
+            ),
+        ]
+    }
+
+    #[test]
+    fn infallible_conversion_cases_are_exhaustive() {
+        let discriminants: Vec<_> = infallible_conversion_cases()
+            .into_iter()
+            .map(|(_, discriminant, _, _)| discriminant)
+            .collect();
+
+        assert_equivalent!(
+            SignedEntityTypeDiscriminants::all_with_unstable_vec(),
+            discriminants
+        );
+    }
+
+    mod infallible_conversions {
+        use super::*;
+
+        #[test]
+        fn from_signed_entity_to_message() {
+            for (signed_entity, _, message, _) in infallible_conversion_cases() {
+                assert_eq!(SignedEntityTypeMessage::from(signed_entity), message);
+            }
+        }
+
+        #[test]
+        fn from_signed_entity_to_discriminant_message() {
+            for (signed_entity, _, _, discriminant_message) in infallible_conversion_cases() {
+                assert_eq!(
+                    SignedEntityTypeDiscriminantsMessage::from(signed_entity),
+                    discriminant_message,
+                )
+            }
+        }
+
+        #[test]
+        fn from_signed_entity_discriminant_to_discriminant_message() {
+            for (_, discriminant, _, discriminant_message) in infallible_conversion_cases() {
+                assert_eq!(
+                    SignedEntityTypeDiscriminantsMessage::from(discriminant),
+                    discriminant_message,
+                )
+            }
+        }
+
+        #[test]
+        fn from_signed_entity_type_message_to_discriminant_message() {
+            for (_, _, message_entity, discriminant_message) in infallible_conversion_cases() {
+                assert_eq!(
+                    SignedEntityTypeDiscriminantsMessage::from(message_entity),
+                    discriminant_message,
+                )
+            }
+        }
+    }
 
     mod deserialization {
         use super::*;
