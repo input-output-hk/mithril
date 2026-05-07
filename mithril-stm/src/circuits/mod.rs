4547af1e38e198129adb9a5844c7366c3b7c7eca
Dam <damien.robissout@iohk.io>
2026-05-07 11:25:35 +0200

    refactor(stm): applied comments to rename and clean functions and comments

--- mithril-stm/src/circuits/mod.rs
+++ mithril-stm/src/circuits/mod.rs
@@ -18,14 +18,3 @@ pub(crate) use halo2::witness::{
 
 /// Constant holding the current path of the cached values related to the circuits
 const MITHRIL_CIRCUIT_CACHE_FOLDER: &str = "mithril-circuit";
-
-/// Errors which can be outputted by the trusted setup verification.
-#[cfg(feature = "future_snark")]
-#[derive(Debug, Clone, thiserror::Error, PartialEq, Eq)]
-pub enum TrustedSetupError {
-    /// The hash verification of the SRS bytes failed
-    #[error(
-        "The hash of the SRS file does not match the hard-coded value! Expected: {0}, Current hash: {1}"
-    )]
-    VerifyHashFail(String, String),
-}
