dd58bfdec1cb02656ed3d71cb8586933dce6aa49
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2026-06-19 15:18:47 +0200

    wip(common): activate bytes codec for SNARK proofs

--- mithril-common/src/crypto_helper/types/wrappers.rs
+++ mithril-common/src/crypto_helper/types/wrappers.rs
@@ -60,20 +60,16 @@ pub type ProtocolAncillaryProverData = ProtocolKey<AncillaryProverData>;
 pub type ProtocolAncillaryVerifierData = ProtocolKey<AncillaryVerifierData>;
 
 impl_codec_and_type_conversions_for_protocol_key!(
-    json_hex_codec => AggregateSignature<ProtocolMembershipDigest>, ed25519_dalek::VerifyingKey, ed25519_dalek::SigningKey, AggregateVerificationKeyForConcatenation<ProtocolMembershipDigest>,
+    json_hex_codec =>  ed25519_dalek::VerifyingKey, ed25519_dalek::SigningKey, AggregateVerificationKeyForConcatenation<ProtocolMembershipDigest>,
         MKProof, VerificationKeyProofOfPossessionForConcatenation, Sum6KesSig, OpCert, SingleSignature
 );
 
 impl_codec_and_type_conversions_for_protocol_key!(
-    bytes_hex_codec => ed25519_dalek::Signature, AncillaryProverData, AncillaryVerifierData
+    bytes_hex_codec => ed25519_dalek::Signature, AncillaryProverData, AncillaryVerifierData,
+        AggregateSignature<ProtocolMembershipDigest>
 );
 
 #[cfg(feature = "future_snark")]
 impl_codec_and_type_conversions_for_protocol_key!(
-    json_hex_codec => VerificationKeyForSnark
-);
-
-#[cfg(feature = "future_snark")]
-impl_codec_and_type_conversions_for_protocol_key!(
-    bytes_hex_codec => AggregateVerificationKeyForSnark<ProtocolMembershipDigest>
+    bytes_hex_codec => AggregateVerificationKeyForSnark<ProtocolMembershipDigest>, VerificationKeyForSnark
 );
