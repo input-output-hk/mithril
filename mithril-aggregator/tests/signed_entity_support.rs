fb390a70c7311075dee4fa189f664823cb2672ba
DJO <790521+Alenar@users.noreply.github.com>
2026-06-23 17:33:32 +0200

    test(aggregator): simplify follower unknown/discontinued entity support test
    Use initialization helpers to be able to bootstrap genesis on leader and
    sign on follower on the first epoch, removing the need to handle
    multiples epochs (already tested in `create_certificate_follower`).

--- mithril-aggregator/tests/signed_entity_support.rs
+++ mithril-aggregator/tests/signed_entity_support.rs
@@ -32,7 +32,9 @@ async fn follower_can_cycle_to_ready_to_signing_with_unknown_and_discontinued_si
     let leader_configuration = ServeCommandConfiguration {
         protocol_parameters: Some(protocol_parameters.clone()),
         data_stores_directory: test_dir.join("leader"),
-        signed_entity_types: None,
+        signed_entity_types: Some(
+            SignedEntityTypeDiscriminants::CardanoStakeDistribution.to_string(),
+        ),
         ..ServeCommandConfiguration::new_sample(temp_dir!())
     };
     let mut leader_tester =
@@ -59,39 +61,7 @@ async fn follower_can_cycle_to_ready_to_signing_with_unknown_and_discontinued_si
     let signers = fixture.signers_fixture();
 
     leader_tester.init_state_from_fixture(&fixture).await.unwrap();
-    follower_tester
-        .update_stake_distribution(fixture.stake_distribution())
-        .await
-        .unwrap();
-
-    comment!(
-        "Epoch 1:
-    - startup both the leader and follower
-    - the leader aggregator can't transition to 'Ready' yet because genesis certificate is not registered
-    - the follower can't transition to 'Ready' yet"
-    );
-
-    comment!("Leader: start the runtime state machine");
-    cycle_err!(leader_tester, "idle");
-
-    comment!("Leader: register signers");
-    leader_tester
-        .register_signers(&fixture.signers_fixture())
-        .await
-        .unwrap();
-
-    comment!("Follower: start the runtime state machine");
-    cycle_err!(follower_tester, "idle");
-
-    comment!(
-        "Epoch 2:
-    - bootstrap the genesis certificate on the leader aggregator
-    - the leader aggregator can transition to 'Signing'
-    - the follower startup but can't go to 'Ready' yet"
-    );
-
-    comment!("Leader: change the epoch");
-    leader_tester.increase_epoch().await.unwrap();
+    follower_tester.init_state_from_fixture(&fixture).await.unwrap();
 
     comment!("Leader: bootstrap the genesis certificate");
     leader_tester.register_genesis_certificate(&fixture).await.unwrap();
@@ -100,37 +70,14 @@ async fn follower_can_cycle_to_ready_to_signing_with_unknown_and_discontinued_si
     assert_last_certificate_eq!(
         leader_tester,
         ExpectedCertificate::new_genesis(
-            Epoch(2),
+            Epoch(1),
             fixture.compute_and_encode_concatenation_aggregate_verification_key()
         )
     );
 
-    comment!("Leader: register signers");
-    leader_tester
-        .register_signers(&fixture.signers_fixture())
-        .await
-        .unwrap();
-
-    comment!("Leader: can go to signing");
-    cycle!(leader_tester, "signing");
-
-    comment!("Follower: change the epoch after leader created a certificate");
-    follower_tester.increase_epoch().await.unwrap();
-    cycle_err!(follower_tester, "idle");
-
     comment!(
-        "Epoch 3:
-    - the follower aggregator sign a MithrilStakeDistribution"
+        "Follower: change the epoch after leader created a certificate -- synchronise chain then sign"
     );
-
-    comment!("Leader: change the epoch");
-    leader_tester.increase_epoch().await.unwrap();
-    cycle!(leader_tester, "idle");
-    cycle!(leader_tester, "ready");
-
-    comment!("Follower: change the epoch after leader created a certificate");
-    follower_tester.increase_epoch().await.unwrap();
-
     cycle_err!(follower_tester, "idle");
     cycle!(follower_tester, "ready");
     cycle!(follower_tester, "signing");
@@ -138,7 +85,7 @@ async fn follower_can_cycle_to_ready_to_signing_with_unknown_and_discontinued_si
     comment!("Follower: signers send their single signature");
     follower_tester
         .send_single_signatures(
-            SignedEntityTypeDiscriminants::MithrilStakeDistribution,
+            SignedEntityTypeDiscriminants::CardanoStakeDistribution,
             &signers,
         )
         .await
@@ -146,11 +93,11 @@ async fn follower_can_cycle_to_ready_to_signing_with_unknown_and_discontinued_si
     cycle!(follower_tester, "ready");
 
     let expected_certificate = ExpectedCertificate::new(
-        Epoch(3),
+        Epoch(1),
         StakeDistributionParty::from_signers(fixture.signers_with_stake()).as_slice(),
         fixture.compute_and_encode_concatenation_aggregate_verification_key(),
-        SignedEntityType::MithrilStakeDistribution(Epoch(3)),
-        ExpectedCertificate::genesis_identifier(Epoch(2)),
+        SignedEntityType::CardanoStakeDistribution(Epoch(0)),
+        ExpectedCertificate::genesis_identifier(Epoch(1)),
     );
     assert_last_certificate_eq!(follower_tester, expected_certificate);
 }
